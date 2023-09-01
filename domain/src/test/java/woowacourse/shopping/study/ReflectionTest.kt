e88ba64015cbe80701f4b8ec561cf9f4514c8f9d
pingu244 <pingu244@naver.com>
2023-09-01 20:19:45 +0900

    [test] Reflection study test 수업 내용 작성

--- domain/src/test/java/woowacourse/shopping/study/ReflectionTest.kt
+++ domain/src/test/java/woowacourse/shopping/study/ReflectionTest.kt
@@ -1,6 +1,7 @@
 package woowacourse.shopping.study
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import kotlin.reflect.KMutableProperty
 import kotlin.reflect.full.declaredFunctions
@@ -16,6 +17,8 @@ class Person(var firstName: String, val lastName: String, private var age: Int) 
     fun greeting() {}
     private fun fullName() {}
     private fun Int.isAdult() {}
+
+    fun getAge(): Int = age
 
     companion object {
         fun noname(age: Int): Person = Person("", "", age)
@@ -99,5 +102,41 @@ class ReflectionTest {
     @Test
     fun `클래스 내에서 선언된 정적 함수`() {
         assertThat(Person::class.staticFunctions.size).isEqualTo(0)
+    }
+
+    // 수업 코드
+    @Test
+    fun `변경 가능한 공개 프로퍼티 값 변경_firstName`() {
+        val person = Person("pingu", "An", 20)
+        Person::firstName.set(person, "heeae") // Person의 firstName에 접근해서 set()호출
+        Assertions.assertEquals("heeae", person.firstName)
+    }
+
+    @Test
+    fun `변경 불가능한 공개 프로퍼티 값 변경_lastName`() {
+        val person = Person("pingu", "An", 20)
+        // lastName은 value여서 set이 없어요
+        // 읽기전용의 field에 직접 접근해야해서 자바로 해야해
+        val field = Person::class.java.getDeclaredField("lastName") // java의 필드에 접근
+        // 읽기전용이니까 field는 private으로 선언되어 있어. 그러니 이거를 열어줘야해
+        field.isAccessible = true
+        field.set(person, "Kim")
+        Assertions.assertEquals("Kim", person.lastName)
+    }
+
+    @Test
+    fun `변경 가능한 비공개 프로퍼티 값 변경_age`() {
+        // age
+        val person = Person("pingu", "An", 20)
+        // 모든 프로퍼티를 들고 와
+        // 그리고 mutable(변경 가능)한 프로퍼티들을 가져와
+        val property1 = person::class.declaredMemberProperties
+            .filterIsInstance<KMutableProperty<*>>()
+        // 위에랑 밑이랑 또이또이
+        val property2 = person::class.declaredMemberProperties
+            .first { it.name == "age" } as KMutableProperty<*>
+
+        // property1.setter.call(person, "30") <- 이것이 되게해보아라(숙제)
+        Assertions.assertEquals(30, person.getAge())
     }
 }
