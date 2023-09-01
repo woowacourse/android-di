87601f55712b76026331c3245b168c35fe6eb593
Sujin Kim <sujinkim919@gmail.com>
2023-09-01 20:25:20 +0900

    fix: view model이 생성되지 않는 문제 해결
    - viewmodel factory 이용

--- app/src/main/java/woowacourse/shopping/ui/MainViewModel.kt
+++ app/src/main/java/woowacourse/shopping/ui/MainViewModel.kt
@@ -3,6 +3,9 @@ package woowacourse.shopping.ui
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import woowacourse.shopping.data.CartRepository
 import woowacourse.shopping.data.ProductRepository
 import woowacourse.shopping.model.Product
@@ -18,7 +21,6 @@ class MainViewModel(
     private val _onProductAdded: MutableLiveData<Boolean> = MutableLiveData(false)
     val onProductAdded: LiveData<Boolean> get() = _onProductAdded
 
-
     fun addCartProduct(product: Product) {
         cartRepository.addCartProduct(product)
         _onProductAdded.value = true
@@ -26,5 +28,16 @@ class MainViewModel(
 
     fun getAllProducts() {
         _products.value = productRepository.getAllProducts()
+    }
+
+    companion object {
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                MainViewModel(
+                    productRepository = ProductRepository(),
+                    cartRepository = CartRepository(),
+                )
+            }
+        }
     }
 }
--- app/src/main/java/woowacourse/shopping/ui/cart/CartViewModel.kt
+++ app/src/main/java/woowacourse/shopping/ui/cart/CartViewModel.kt
@@ -3,6 +3,9 @@ package woowacourse.shopping.ui.cart
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import woowacourse.shopping.data.CartRepository
 import woowacourse.shopping.model.Product
 
@@ -10,8 +13,7 @@ class CartViewModel(
     private val cartRepository: CartRepository,
 ) : ViewModel() {
 
-    private val _cartProducts: MutableLiveData<List<Product>> =
-        MutableLiveData(emptyList())
+    private val _cartProducts: MutableLiveData<List<Product>> = MutableLiveData(emptyList())
     val cartProducts: LiveData<List<Product>> get() = _cartProducts
 
     private val _onCartProductDeleted: MutableLiveData<Boolean> = MutableLiveData(false)
@@ -24,5 +26,15 @@ class CartViewModel(
     fun deleteCartProduct(id: Int) {
         cartRepository.deleteCartProduct(id)
         _onCartProductDeleted.value = true
+    }
+
+    companion object {
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                CartViewModel(
+                    cartRepository = CartRepository(),
+                )
+            }
+        }
     }
 }
