29fb66fe733c7c1f17e82580ee550a4993edd424
re4rk <37167652+re4rk@users.noreply.github.com>
2023-09-02 20:14:44 +0900

    fix : viewModel 생성시 MainThread에서 실행하지 않아서 생기는 버그 수정

--- app/src/main/java/woowacourse/shopping/ui/util/ViewModelExt.kt
+++ app/src/main/java/woowacourse/shopping/ui/util/ViewModelExt.kt
@@ -1,12 +1,14 @@
 package woowacourse.shopping.ui.util
 
 import androidx.activity.ComponentActivity
+import androidx.annotation.MainThread
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelLazy
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import woowacourse.shopping.di.ApiModule.createInstance
 
+@MainThread
 inline fun <reified VM : ViewModel> ComponentActivity.viewModels(): Lazy<VM> {
     return ViewModelLazy(
         viewModelClass = VM::class,
