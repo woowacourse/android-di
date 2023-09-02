48f3302397c3775ae394fe66ace309e779a19197
chws0508 <chws0508@naver.com>
2023-09-02 16:53:38 +0900

    feat : CartActivityTest 테스트 구현

---
+++ app/src/test/java/woowacourse/shopping/ui/cart/CartActivityTest.kt
@@ -0,0 +1,42 @@
+package woowacourse.shopping.ui.cart
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.ViewModelProvider
+import com.google.common.truth.Truth.assertThat
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class CartActivityTest {
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Test
+    fun `Activity 실행 테스트`() {
+        // given
+        val activity = Robolectric
+            .buildActivity(CartActivity::class.java)
+            .create()
+            .get()
+
+        // then
+        assertThat(activity).isNotNull()
+    }
+
+    @Test
+    fun `ViewModel 주입 테스트`() {
+        // given
+        val activity = Robolectric
+            .buildActivity(CartActivity::class.java)
+            .create()
+            .get()
+        val viewModel = ViewModelProvider(activity)[CartViewModel::class.java]
+
+        // then
+        assertThat(viewModel).isNotNull()
+    }
+}
