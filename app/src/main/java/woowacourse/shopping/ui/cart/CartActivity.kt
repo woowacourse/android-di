c92c96f7138fde7c8b4754fea91154a65352938b
re4rk <37167652+re4rk@users.noreply.github.com>
2023-09-02 18:20:43 +0900

    feat: 장바구니 화면 di로 뷰모델 받아오도록 변캍

--- app/src/main/java/woowacourse/shopping/ui/cart/CartActivity.kt
+++ app/src/main/java/woowacourse/shopping/ui/cart/CartActivity.kt
@@ -3,17 +3,15 @@ package woowacourse.shopping.ui.cart
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
 import woowacourse.shopping.R
 import woowacourse.shopping.databinding.ActivityCartBinding
+import woowacourse.shopping.ui.util.viewModels
 
 class CartActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityCartBinding.inflate(layoutInflater) }
 
-    private val viewModel by lazy {
-        ViewModelProvider(this)[CartViewModel::class.java]
-    }
+    private val viewModel: CartViewModel by viewModels()
 
     private lateinit var dateFormatter: DateFormatter
 
