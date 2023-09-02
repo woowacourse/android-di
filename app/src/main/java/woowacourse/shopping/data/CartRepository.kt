fb88fd4f92857cd61d6159f061702142d485720d
mendel <rhthrhrl0@naver.com>
2023-09-02 17:07:22 +0900

    feat: Repository 인터페이스 추가 및 적용

--- app/src/main/java/woowacourse/shopping/data/CartRepository.kt
+++ app/src/main/java/woowacourse/shopping/data/CartRepository.kt
@@ -2,19 +2,8 @@ package woowacourse.shopping.data
 
 import woowacourse.shopping.model.Product
 
-// TODO: Step2 - CartProductDao를 참조하도록 변경
-class CartRepository {
-
-    private val cartProducts: MutableList<Product> = mutableListOf()
-    fun addCartProduct(product: Product) {
-        cartProducts.add(product)
-    }
-
-    fun getAllCartProducts(): List<Product> {
-        return cartProducts.toList()
-    }
-
-    fun deleteCartProduct(id: Int) {
-        cartProducts.removeAt(id)
-    }
+interface CartRepository {
+    fun addCartProduct(product: Product)
+    fun getAllCartProducts(): List<Product>
+    fun deleteCartProduct(id: Int)
 }
