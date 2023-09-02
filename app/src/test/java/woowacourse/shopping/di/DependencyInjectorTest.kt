7179bda207090c648e8e6ec34df01d7c79d34541
DYGames <dygames@naver.com>
2023-09-03 02:19:54 +0900

    test: 의존 주입간에 발생할 수 있는 상황에 대한 테스트 코드 작성

--- app/src/test/java/woowacourse/shopping/di/DependencyInjectorTest.kt
+++ app/src/test/java/woowacourse/shopping/di/DependencyInjectorTest.kt
@@ -1,23 +1,75 @@
 package woowacourse.shopping.di
 
+import org.junit.Assert.assertNotNull
 import org.junit.Test
-import woowacourse.shopping.data.CartRepository
-import woowacourse.shopping.data.DefaultCartRepository
 import woowacourse.shopping.di.DependencyInjector.inject
-import woowacourse.shopping.ui.MainViewModel
 
 class DependencyInjectorTest {
+    internal interface TestRepository
+    internal interface TestRepository2
+
+    internal class TestName
+    internal class TestID
+    internal class TestProduct(
+        private val testName: TestName,
+        private val testID: TestID
+    )
+
+    internal class DefaultTestRepository : TestRepository
+    internal class DefaultTestRepository2 : TestRepository2
+
+    internal class TestViewModel(
+        private val testRepository: TestRepository,
+        private val testRepository2: TestRepository2,
+        private val testProduct: TestProduct
+    )
 
     @Test(expected = IllegalStateException::class)
-    fun `Singleton에 CartRepository만 존재하면 ViewModel 생성에 실패한다`() {
+    fun `Singleton에 TestRepository만 존재하면 TestViewModel 생성에 실패한다`() {
         // given
         DependencyInjector.dependencies = object : Dependencies {
-            val cartRepository: CartRepository by lazy { DefaultCartRepository() }
+            val testRepository: TestRepository by lazy { DefaultTestRepository() }
         }
 
         // when
-        inject<MainViewModel>()
+        inject<TestViewModel>()
 
         // then
+    }
+
+    @Test
+    fun `Singleton에 의존이 모두 존재하면 TestViewModel 생성에 성공한다`() {
+        // given
+        DependencyInjector.dependencies = object : Dependencies {
+            val testRepository: TestRepository by lazy { DefaultTestRepository() }
+            val testRepository2: TestRepository2 by lazy { DefaultTestRepository2() }
+            val testProduct: TestProduct by lazy {
+                TestProduct(
+                    TestName(),
+                    TestID()
+                )
+            }
+        }
+
+        // when
+        val testViewModel = inject<TestViewModel>()
+
+        // then
+        assertNotNull(testViewModel)
+    }
+
+    @Test
+    fun `Singleton에 의존이 존재하지 않더라도 의존 관계의 모든 클래스에 생성자가 존재하면 TestViewModel 생성에 성공한다`() {
+        // given
+        DependencyInjector.dependencies = object : Dependencies {
+            val testRepository: TestRepository by lazy { DefaultTestRepository() }
+            val testRepository2: TestRepository2 by lazy { DefaultTestRepository2() }
+        }
+
+        // when
+        val testViewModel = inject<TestViewModel>()
+
+        // then
+        assertNotNull(testViewModel)
     }
 }
