6dde64aa477e03fd5b64ee1c87a62eb38eb92dee
re4rk <37167652+re4rk@users.noreply.github.com>
2023-09-02 18:19:52 +0900

    feat: 앱 전역 수동 Di 객체 추가

---
+++ app/src/main/java/woowacourse/shopping/di/ApiModule.kt
@@ -0,0 +1,14 @@
+package woowacourse.shopping.di
+
+import woowacourse.shopping.data.CartSampleRepository
+import woowacourse.shopping.data.ProductSampleRepository
+import woowacourse.shopping.repository.CartRepository
+import woowacourse.shopping.repository.ProductRepository
+
+object ApiModule : DiContainer() {
+    val productRepository: ProductRepository =
+        this.createInstance(ProductSampleRepository::class.java)
+
+    val cartRepository: CartRepository =
+        this.createInstance(CartSampleRepository::class.java)
+}
