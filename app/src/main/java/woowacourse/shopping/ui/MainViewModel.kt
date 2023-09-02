161900e946b36c94c71c8b164e3de97e124dea3b
briandr97 <briandr@naver.com>
2023-09-02 17:55:13 +0900

    feat: 뷰모델이 레포지터리 구현체가 아닌 추상체를 참조하도록 변경

--- app/src/main/java/woowacourse/shopping/ui/MainViewModel.kt
+++ app/src/main/java/woowacourse/shopping/ui/MainViewModel.kt
@@ -3,9 +3,9 @@ package woowacourse.shopping.ui
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import woowacourse.shopping.data.CartRepository
-import woowacourse.shopping.data.ProductRepository
 import woowacourse.shopping.model.Product
+import woowacourse.shopping.repository.CartRepository
+import woowacourse.shopping.repository.ProductRepository
 
 class MainViewModel(
     private val productRepository: ProductRepository,
@@ -24,6 +24,6 @@ class MainViewModel(
     }
 
     fun getAllProducts() {
-        _products.value = productRepository.getAllProducts()
+        productRepository.getAllProducts()
     }
 }
