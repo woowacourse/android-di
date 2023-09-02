79932301c45972c0729c7ea269205f80674f36d5
re4rk <37167652+re4rk@users.noreply.github.com>
2023-09-02 21:35:14 +0900

    test: 메인 화면 테스트 케이스 추가

--- app/src/test/java/woowacourse/shopping/ui/MainActivityTest.kt
+++ app/src/test/java/woowacourse/shopping/ui/MainActivityTest.kt
@@ -1,5 +1,6 @@
 package woowacourse.shopping.ui
 
+import androidx.appcompat.widget.Toolbar
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.ViewModelProvider
 import com.google.common.truth.Truth.assertThat
@@ -8,6 +9,10 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.Shadows
+import org.robolectric.shadows.ShadowActivity
+import woowacourse.shopping.R
+import woowacourse.shopping.ui.cart.CartActivity
 
 @RunWith(RobolectricTestRunner::class)
 class MainActivityTest {
@@ -38,5 +43,28 @@ class MainActivityTest {
 
         // then
         assertThat(viewModel).isNotNull()
+    }
+
+    @Test
+    fun `장바구니 버튼을 누르면 장바구니 화면으로 이동한다`() {
+        // given
+        val activity = Robolectric
+            .buildActivity(MainActivity::class.java)
+            .create()
+            .get()
+
+        val toolbar = activity.findViewById<Toolbar>(R.id.toolbar).menu
+
+        activity.onCreateOptionsMenu(toolbar)
+
+        val cartMenuActionView = toolbar.findItem(R.id.cart).actionView!!
+
+        // when
+        cartMenuActionView.performClick()
+
+        // and
+        val shadowActivity: ShadowActivity = Shadows.shadowOf(activity)
+        val startedIntent = shadowActivity.nextStartedActivity
+        assertThat(startedIntent.component?.className).isEqualTo(CartActivity::class.java.name)
     }
 }
