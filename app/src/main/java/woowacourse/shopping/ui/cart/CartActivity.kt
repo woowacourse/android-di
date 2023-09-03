08224cc07a1fd9fddbb35353e35edaeead1174dc
briandr97 <briandr@naver.com>
2023-09-03 21:43:06 +0900

    feat: CartActivity가 BaseActivity를 상속 받고 뷰모델 생성 방식 수정

--- app/src/main/java/woowacourse/shopping/ui/cart/CartActivity.kt
+++ app/src/main/java/woowacourse/shopping/ui/cart/CartActivity.kt
@@ -2,17 +2,17 @@ package woowacourse.shopping.ui.cart
 
 import android.os.Bundle
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import woowacourse.shopping.R
 import woowacourse.shopping.databinding.ActivityCartBinding
+import woowacourse.shopping.ui.base.BaseActivity
 
-class CartActivity : AppCompatActivity() {
+class CartActivity : BaseActivity() {
 
     private val binding by lazy { ActivityCartBinding.inflate(layoutInflater) }
 
     private val viewModel by lazy {
-        ViewModelProvider(this)[CartViewModel::class.java]
+        ViewModelProvider(this, Factory)[CartViewModel::class.java]
     }
 
     private lateinit var dateFormatter: DateFormatter
