a5afc403c830cef9431c75e303d0570798a18757
briandr97 <briandr@naver.com>
2023-09-03 21:02:11 +0900

    feat: Application 파일에서 DependencyFactory 인스턴스 생성

---
+++ app/src/main/java/woowacourse/shopping/DiApplication.kt
@@ -0,0 +1,16 @@
+package woowacourse.shopping
+
+import android.app.Application
+import woowacourse.shopping.di.DependencyFactory
+import woowacourse.shopping.di.RepositoryContainer
+
+class DiApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        repositoryFactory = DependencyFactory(RepositoryContainer())
+    }
+
+    companion object {
+        lateinit var repositoryFactory: DependencyFactory
+    }
+}
