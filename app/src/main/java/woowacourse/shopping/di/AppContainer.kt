4f4b0d542d37bb3221f75b1df9ec23ed46a319d4
ippnsj <ippnsj@gmail.com>
2023-09-03 22:11:14 +0900

    feat: ViewModel에 Repository 자동 주입

--- app/src/main/java/woowacourse/shopping/di/AppContainer.kt
+++ app/src/main/java/woowacourse/shopping/di/AppContainer.kt
@@ -2,8 +2,26 @@ package woowacourse.shopping.di
 
 import woowacourse.shopping.data.CartRepository
 import woowacourse.shopping.data.ProductRepository
+import kotlin.reflect.full.declaredMemberProperties
+import kotlin.reflect.full.primaryConstructor
+import kotlin.reflect.jvm.isAccessible
 
 class AppContainer {
-    val productRepository = ProductRepository()
-    val cartRepository = CartRepository()
+    private val productRepository = ProductRepository()
+    private val cartRepository = CartRepository()
+
+    fun <T : Any> inject(clazz: Class<T>): T {
+        val constructor = clazz.kotlin.primaryConstructor
+            ?: throw NullPointerException("주입할 클래스의 주생성자가 존재하지 않습니다.")
+        val args = constructor.parameters.associateWith { parameter ->
+            this::class.declaredMemberProperties
+                .find { it.returnType == parameter.type }
+                ?.let {
+                    it.isAccessible = true
+                    it.getter.call(this)
+                } ?: throw NoSuchElementException("생성자 주입을 위한 프로퍼티의 인스턴스가 존재하지 않습니다.")
+        }
+
+        return constructor.callBy(args)
+    }
 }
--- app/src/main/java/woowacourse/shopping/di/ViewModelFactory.kt
+++ app/src/main/java/woowacourse/shopping/di/ViewModelFactory.kt
@@ -5,20 +5,11 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
 import androidx.lifecycle.viewmodel.CreationExtras
 import woowacourse.shopping.ShoppingApplication
-import woowacourse.shopping.ui.MainViewModel
-import woowacourse.shopping.ui.cart.CartViewModel
 
-@Suppress("UNCHECKED_CAST")
 val viewModelFactory = object : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
         val application = checkNotNull(extras[APPLICATION_KEY]) as ShoppingApplication
         val appContainer = application.appContainer
-        return with(modelClass) {
-            when {
-                isAssignableFrom(MainViewModel::class.java) -> MainViewModel(appContainer.productRepository, appContainer.cartRepository)
-                isAssignableFrom(CartViewModel::class.java) -> CartViewModel(appContainer.cartRepository)
-                else -> throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
-            }
-        } as T
+        return appContainer.inject(modelClass)
     }
 }
