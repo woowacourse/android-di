8fa65de947224a7b28d7b7af6d951a6d25a39190
hyemdooly <hyemdooly@gmail.com>
2023-09-03 16:22:15 +0900

    feat: 커스텀 ViewModelFactory 적용

--- app/src/main/java/woowacourse/shopping/ui/MainActivity.kt
+++ app/src/main/java/woowacourse/shopping/ui/MainActivity.kt
@@ -4,19 +4,18 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
 import woowacourse.shopping.R
 import woowacourse.shopping.databinding.ActivityMainBinding
 import woowacourse.shopping.ui.cart.CartActivity
+import woowacourse.shopping.ui.common.ViewModelFactory
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
-    private val viewModel by lazy {
-        ViewModelProvider(this)[MainViewModel::class.java]
-    }
+    private val viewModel by viewModels<MainViewModel>(factoryProducer = { ViewModelFactory() })
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +25,6 @@ class MainActivity : AppCompatActivity() {
         setupToolbar()
         setupView()
     }
-
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.cart_menu, menu)
@@ -58,7 +56,7 @@ class MainActivity : AppCompatActivity() {
         viewModel.products.observe(this) {
             val adapter = ProductAdapter(
                 items = it,
-                onClickProduct = viewModel::addCartProduct
+                onClickProduct = viewModel::addCartProduct,
             )
             binding.rvProducts.adapter = adapter
         }
