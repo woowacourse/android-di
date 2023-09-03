56ec2e6283ccebff75e61b92759e9bb1de951bde
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-09-03 18:30:14 +0900

    refactor: repository 타입 수정

--- app/src/main/java/woowacourse/shopping/di/container/RepositoryContainer.kt
+++ app/src/main/java/woowacourse/shopping/di/container/RepositoryContainer.kt
@@ -2,12 +2,14 @@ package woowacourse.shopping.di.container
 
 import woowacourse.shopping.data.LocalCartRepository
 import woowacourse.shopping.data.LocalProductRepository
+import woowacourse.shopping.model.repository.CartRepository
+import woowacourse.shopping.model.repository.ProductRepository
 
 class RepositoryContainer {
-    val postRepository: LocalProductRepository by lazy {
+    val postRepository: ProductRepository by lazy {
         LocalProductRepository()
     }
-    val cartRepository: LocalCartRepository by lazy {
+    val cartRepository: CartRepository by lazy {
         LocalCartRepository()
     }
 }
