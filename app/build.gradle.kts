e1076a248f7326f2f845f56ba3b907c1527fe697
krrong <khk9664@naver.com>
2023-09-03 21:06:38 +0900

    chore: 리플렉션 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,6 +73,7 @@ dependencies {
     // Hilt
     implementation("com.google.dagger:hilt-android:2.44")
     kapt("com.google.dagger:hilt-android-compiler:2.44")
+    implementation(kotlin("reflect"))
 }
 
 // Allow references to generated code
