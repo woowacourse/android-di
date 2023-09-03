44101752b7353029827c4e46168acd01c13a4cff
kwakjoohyeong <rhkrwngud445@gmail.com>
2023-09-04 02:59:18 +0900

    feat: kotlin reflect 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,4 +72,7 @@ dependencies {
     // Hilt
     implementation("com.google.dagger:hilt-android:2.44")
     kapt("com.google.dagger:hilt-android-compiler:2.44")
+
+    // kotlin reflect
+    implementation("org.jetbrains.kotlin", "kotlin-reflect", "1.8.21")
 }
