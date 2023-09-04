661b57b9be0973a9b7a38f8257070be0096fd8e3
ki960213 <ki960213@gmail.com>
2023-09-04 17:06:33 +0900

    feat(ClassesTopologicalSorter): 클래스들의 의존성에 따라 클래스들을 위상 정렬하는 기능 구현

--- sheath/build.gradle.kts
+++ sheath/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("com.google.truth:truth:1.1.3")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     // Robolectric
---
+++ sheath/src/test/java/com/ki960213/sheath/util/ClassesTopologicalSorterTest.kt
@@ -0,0 +1,102 @@
+package com.ki960213.sheath.util
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+internal class ClassesTopologicalSorterTest {
+
+    @Test
+    fun `클래스들을 받으면 위상 정렬된 클래스들을 반환한다`() {
+        val classes =
+            listOf(Test1::class.java, Test2::class.java, Test3::class.java, Test4::class.java)
+
+        val result = ClassesTopologicalSorter.sort(classes)
+
+        val expected = listOf(
+            Test4::class.java,
+            Test2::class.java,
+            Test3::class.java,
+            Test1::class.java,
+        )
+        assertThat(result).isEqualTo(expected)
+    }
+
+    @Test
+    fun `클래스들 중 종속 항목인 클래스가 클래스 목록에 없다면 에러가 발생한다`() {
+        val classes = listOf(
+            Test1::class.java,
+            Test2::class.java,
+            Test3::class.java,
+            Test4::class.java,
+            Test5::class.java,
+        )
+
+        try {
+            ClassesTopologicalSorter.sort(classes)
+        } catch (e: IllegalStateException) {
+            assertThat(e).hasMessageThat().isEqualTo("${Test6::class.java.name} 클래스가 존재하지 않습니다.")
+        }
+    }
+
+    @Test
+    fun `클래스들 중 종속 항목인 클래스가 클래스 목록에 2개 이상 있다면 에러가 발생한다`() {
+        val classes = listOf(
+            Test8::class.java,
+            Test9::class.java,
+            Test10::class.java,
+        )
+
+        try {
+            ClassesTopologicalSorter.sort(classes)
+        } catch (e: IllegalStateException) {
+            assertThat(e).hasMessageThat()
+                .isEqualTo("${Test7::class.java.name} 클래스에 주입될 클래스가 모호합니다.")
+        }
+    }
+
+    @Test
+    fun `클래스들 간에 의존 사이클이 존재하면 에러가 발생한다`() {
+        val classes = listOf(
+            Test11::class.java,
+            Test12::class.java,
+            Test13::class.java,
+        )
+
+        try {
+            ClassesTopologicalSorter.sort(classes)
+        } catch (e: IllegalStateException) {
+            assertThat(e).hasMessageThat()
+                .isEqualTo("클래스들 간에 의존 사이클이 존재합니다.")
+        }
+    }
+
+    // 위상 정렬 기능 해피 케이스 테스트 용
+    class Test1(val test2: Test2, val test3: Test3)
+
+    class Test2(val test4: Test4)
+
+    class Test3(val test4: Test4)
+
+    class Test4
+
+    // 종속 항목 부재 테스트 용
+    class Test5(val test6: Test6)
+
+    class Test6
+
+    // 모호한 의존성 테스트 용
+    open class Test7
+
+    class Test8 : Test7()
+
+    class Test9 : Test7()
+
+    class Test10(val test7: Test7)
+
+    // 의존 사아클 테스트 용
+    class Test11(val test12: Test12)
+
+    class Test12(val test13: Test13)
+
+    class Test13(val test11: Test11)
+}
