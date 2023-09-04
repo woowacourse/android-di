6085079868fc3516bd7a36f3e7725acaea2e49a7
mendel <rhthrhrl0@naver.com>
2023-09-05 04:58:20 +0900

    feat: ShoppingApplication에 생성된 인젝터들을 관리하는 map객체와 관련 메소드 추가

--- app/src/main/java/woowacourse/shopping/di/activity/DiEntryPointActivity.kt
+++ app/src/main/java/woowacourse/shopping/di/activity/DiEntryPointActivity.kt
@@ -5,7 +5,8 @@ import androidx.annotation.MainThread
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelLazy
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import woowacourse.shopping.di.AutoInjector
 import woowacourse.shopping.di.Injector
 import woowacourse.shopping.di.module.ActivityModule
@@ -14,23 +15,26 @@ import kotlin.reflect.full.primaryConstructor
 
 abstract class DiEntryPointActivity<T : ActivityModule>(activityModuleClassType: Class<T>) :
     AppCompatActivity() {
-    private var _autoInjector: Injector? = null
-    protected val autoInjector: Injector
-        get() = _autoInjector!!
+
+    val autoInjector: Injector
+        get() = ShoppingApplication.getInjectorForInstance(this.hashCode())
 
     init {
-        // 애플리케이션 모듈에 대한 참조를 갖고 있는 액티비티 모듈을 생성해서, 이걸 인젝터 객체 생성자에 넘겨서 객체 생성.
-        val activityModule =
-            activityModuleClassType.kotlin.primaryConstructor?.call(ShoppingApplication.applicationModule)
-                ?: throw NullPointerException("주생성자 없음")
-        _autoInjector = AutoInjector(activityModule)
+        // 애플리케이션 모듈에 대한 참조를 갖고 있는 액티비티 모듈을 생성해서, 이걸 인젝터 객체 생성자에 넘겨서 객체 생성하고 애플리케이션이 가진 맵에 저장시킴. 이 액티비티가 살아있는 한 같은 인젝터를 얻게 된다.
+        if (!ShoppingApplication.hasInjectorForInstance(this.hashCode())) {
+            val activityModule =
+                activityModuleClassType.kotlin.primaryConstructor?.call(ShoppingApplication.applicationModule)
+                    ?: throw NullPointerException("주생성자 없음")
+            ShoppingApplication.addInjectorForInstance(
+                this.hashCode(),
+                AutoInjector(activityModule),
+            )
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        if (isFinishing) {
-            _autoInjector = null
-        }
+        if (isFinishing) ShoppingApplication.removeInjectorForInstance(this.hashCode())
     }
 
     @MainThread
@@ -38,13 +42,13 @@ abstract class DiEntryPointActivity<T : ActivityModule>(activityModuleClassType:
         return ViewModelLazy(
             VM::class,
             { viewModelStore },
-            ::viewModelFactory,
+            {
+                viewModelFactory {
+                    initializer {
+                        autoInjector.inject(VM::class.java)
+                    }
+                }
+            },
         )
-    }
-
-    fun viewModelFactory() = object : ViewModelProvider.Factory {
-        override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            return autoInjector.inject(modelClass)
-        }
     }
 }
--- app/src/main/java/woowacourse/shopping/global/ShoppingApplication.kt
+++ app/src/main/java/woowacourse/shopping/global/ShoppingApplication.kt
@@ -1,8 +1,10 @@
 package woowacourse.shopping.global
 
 import android.app.Application
+import woowacourse.shopping.di.Injector
 import woowacourse.shopping.di.module.ApplicationModule
 import woowacourse.shopping.di.module.DefaultApplicationModule
+import javax.annotation.CheckReturnValue
 
 class ShoppingApplication : Application() {
     override fun onCreate() {
@@ -13,5 +15,25 @@ class ShoppingApplication : Application() {
     companion object {
         lateinit var applicationModule: ApplicationModule
             private set
+
+        private val injectorMap: MutableMap<Int, Injector?> = mutableMapOf()
+
+        @CheckReturnValue
+        fun hasInjectorForInstance(instanceHashCode: Int): Boolean {
+            return injectorMap[instanceHashCode] != null
+        }
+
+        fun getInjectorForInstance(instanceHashCode: Int): Injector {
+            requireNotNull(injectorMap[instanceHashCode]) { "인젝터가 존재하지 않습니다. 먼저 초기화해주세요." }
+            return injectorMap[instanceHashCode]!!
+        }
+
+        fun addInjectorForInstance(instanceHashCode: Int, injector: Injector) {
+            injectorMap[instanceHashCode] = injector
+        }
+
+        fun removeInjectorForInstance(instanceHashCode: Int) {
+            injectorMap[instanceHashCode] = null
+        }
     }
 }
