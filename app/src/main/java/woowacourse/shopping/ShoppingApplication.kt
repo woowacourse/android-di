cf6d8566f696ce4209984105c04fc8ad03be9b51
hyunji1203 <hj27720@gmail.com>
2023-09-04 17:36:16 +0900

    feat: Application에 Injector 프로퍼티 생성

--- app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
+++ app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
@@ -1,5 +1,8 @@
 package woowacourse.shopping
 
 import android.app.Application
+import woowacourse.shopping.di.inject.AutoDependencyInjector
 
-class ShoppingApplication : Application()
+class ShoppingApplication : Application() {
+    val autoDependencyInjector = AutoDependencyInjector()
+}
