e86a499c477b889c6b508d29b2d881c326278944
vrexpert <vrexpert2302@gmail.com>
2023-09-05 00:15:00 +0900

    test: ProductFixture 생성

---
+++ app/src/test/java/woowacourse/shopping/fixture/ProductFixture.kt
@@ -0,0 +1,9 @@
+package woowacourse.shopping.fixture
+
+import woowacourse.shopping.model.Product
+
+object ProductFixture {
+    fun getProduct(id: Int = 1) = Product("Product$id", id * 1000, "product$id.com")
+
+    fun getProducts(ids: List<Int>) = ids.map { getProduct(it) }
+}
