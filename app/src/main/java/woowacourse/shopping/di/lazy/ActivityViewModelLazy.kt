d4fce6697d449b45640e463de7f82bca9d2e6978
buna <tmdgh1592@naver.com>
2023-09-04 19:57:03 +0900

    refactor: @MainThread 애노테이션 제거

--- app/src/main/java/woowacourse/shopping/di/lazy/ActivityViewModelLazy.kt
+++ app/src/main/java/woowacourse/shopping/di/lazy/ActivityViewModelLazy.kt
@@ -1,13 +1,11 @@
 package woowacourse.shopping.di.lazy
 
 import androidx.activity.ComponentActivity
-import androidx.annotation.MainThread
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelLazy
 import woowacourse.shopping.di.injector.ClassInjector
 import woowacourse.shopping.di.util.viewModelFactory
 
-@MainThread
 inline fun <reified VM : ViewModel> ComponentActivity.viewModel(): Lazy<VM> {
     return ViewModelLazy(
         VM::class,
@@ -16,7 +14,6 @@ inline fun <reified VM : ViewModel> ComponentActivity.viewModel(): Lazy<VM> {
     )
 }
 
-@MainThread
 inline fun <reified VM : ViewModel> createViewModel(): VM {
     return ClassInjector.inject()
 }
