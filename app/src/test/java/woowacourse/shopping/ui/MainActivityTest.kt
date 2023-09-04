7aad017705711a7ba1c5efa532d44549c0203c5b
ki960213 <ki960213@gmail.com>
2023-09-04 22:00:02 +0900

    fix(MainActivityTest): 기존 테스트가 통과하도록 수정

--- app/src/test/java/woowacourse/shopping/ui/MainActivityTest.kt
+++ app/src/test/java/woowacourse/shopping/ui/MainActivityTest.kt
@@ -1,19 +1,34 @@
 package woowacourse.shopping.ui
 
+import android.app.Application
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.ViewModelProvider
 import com.google.common.truth.Truth.assertThat
+import com.ki960213.sheath.SheathApplication
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+import woowacourse.shopping.data.CartRepositoryImpl
+import woowacourse.shopping.data.ProductRepositoryImpl
 
 @RunWith(RobolectricTestRunner::class)
+@Config(application = MainActivityTest.TestShoppingApplication::class)
 class MainActivityTest {
 
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    class TestShoppingApplication : SheathApplication() {
+        override fun onCreate() {
+            Application().onCreate()
+            val containerField = SheathApplication::class.java.getDeclaredField("_container")
+            containerField.isAccessible = true
+            containerField.set(this, listOf(ProductRepositoryImpl(), CartRepositoryImpl()))
+        }
+    }
 
     @Test
     fun `Activity 실행 테스트`() {
