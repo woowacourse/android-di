27489ffbcf8a9c0a9c7f7294232b68c51c1bd989
vrexpert <vrexpert2302@gmail.com>
2023-09-04 14:26:43 +0900

    refactor: viewModelFactory 분리 및 자동화

---
+++ app/src/main/java/woowacourse/shopping/di/ViewModelFactory.kt
@@ -0,0 +1,34 @@
+package woowacourse.shopping.di
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import java.lang.reflect.Constructor
+import java.lang.reflect.Field
+
+class ViewModelFactory : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        val constructor = modelClass.declaredConstructors.first()
+        val params = mapModulesInConstructor(
+            constructor = constructor,
+            modules = Module::class.java.declaredFields,
+        )
+        return constructor.newInstance(*params.toTypedArray()) as T
+    }
+
+    private fun mapModulesInConstructor(
+        constructor: Constructor<*>,
+        modules: Array<Field>,
+    ): List<Any?> = constructor.parameterTypes.map { parameterType ->
+        val module = getFieldWithType(fields = modules, type = parameterType)
+        module.isAccessible = true
+        module.get(modules)
+    }
+
+    private fun getFieldWithType(fields: Array<Field>, type: Class<*>): Field {
+        val module = fields.firstOrNull { it.type.simpleName == type.simpleName }
+        requireNotNull(module) { "No matching same type in fields | type : $type" }
+        return module
+    }
+}
