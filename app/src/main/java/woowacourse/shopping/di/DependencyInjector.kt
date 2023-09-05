d5e84124de9e1e474559543933b59c617fb16edc
Doyeop Kim <dygames34@gmail.com>
2023-09-05 15:14:51 +0900

    feat: 필드 주입

--- app/src/main/java/woowacourse/shopping/di/DependencyInjector.kt
+++ app/src/main/java/woowacourse/shopping/di/DependencyInjector.kt
@@ -1,5 +1,7 @@
 package woowacourse.shopping.di
 
+import woowacourse.shopping.di.annotation.Injectable
+import kotlin.reflect.KMutableProperty
 import kotlin.reflect.KParameter
 import kotlin.reflect.KType
 import kotlin.reflect.full.declaredMemberProperties
@@ -16,12 +18,13 @@ object DependencyInjector {
     }
 
     fun inject(type: KType): Any {
-        return findSingleton(type) ?: instantiate(type)
+        return findSingleton(type) ?: instantiate(type).apply {
+            injectFields(this)
+        }
     }
 
     private fun findSingleton(type: KType): Any? {
-        if (!::dependencies.isInitialized)
-            throw IllegalStateException("의존이 초기화되지 않았습니다.")
+        if (!::dependencies.isInitialized) throw IllegalStateException("의존이 초기화되지 않았습니다.")
         dependencies::class.declaredMemberProperties.forEach {
             if (type.isSupertypeOf(it.returnType)) {
                 return it.getter.call(dependencies)
@@ -42,5 +45,14 @@ object DependencyInjector {
         return parameters.map { parameter ->
             inject(parameter.type)
         }.toTypedArray()
+    }
+
+    private fun injectFields(instance: Any): Any {
+        val fields = instance::class.declaredMemberProperties
+        fields.forEach {
+            if (it.annotations.filterIsInstance<Injectable>().isEmpty()) return@forEach
+            if (it is KMutableProperty<*>) it.setter.call(instance, inject(it.returnType))
+        }
+        return instance
     }
 }
