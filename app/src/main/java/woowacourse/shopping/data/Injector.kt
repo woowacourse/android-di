fce096f983d6d5d211ba7d26f7dfc2cfe3cf2d4d
Yerin <myheaven_b@naver.com>
2023-09-05 17:48:30 +0900

    refactor: null인 경우 NullPointerException

--- app/src/main/java/woowacourse/shopping/data/Injector.kt
+++ app/src/main/java/woowacourse/shopping/data/Injector.kt
@@ -8,7 +8,8 @@ import kotlin.reflect.full.primaryConstructor
 object Injector {
 
     fun <T : ViewModel> inject(modelClass: Class<T>): T {
-        val constructor = modelClass.kotlin.primaryConstructor ?: throw IllegalStateException()
+        val constructor =
+            modelClass.kotlin.primaryConstructor ?: throw NullPointerException("주 생성자가 null 입니다")
         val args = constructor.parameters.map {
             getRepositoryWithType(it.type)
         }
@@ -19,6 +20,6 @@ object Injector {
     private fun getRepositoryWithType(type: KType): Any {
         return RepositoryContainer::class.declaredMemberProperties.find {
             it.returnType == type
-        }?.call(RepositoryContainer) ?: throw IllegalStateException()
+        }?.call(RepositoryContainer) ?: throw IllegalStateException("해당 타입을 찾을 수 없습니다.")
     }
 }
