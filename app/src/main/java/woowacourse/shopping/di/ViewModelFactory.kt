7510f072ed7a352b09293bb58d3eaa59e6675938
vrexpert <vrexpert2302@gmail.com>
2023-09-05 19:12:20 +0900

    refactor: ViewModelFactory kotlin 리플랙션 API 로 변경

--- app/src/main/java/woowacourse/shopping/di/ViewModelFactory.kt
+++ app/src/main/java/woowacourse/shopping/di/ViewModelFactory.kt
@@ -2,35 +2,23 @@ package woowacourse.shopping.di
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import java.lang.reflect.Constructor
-import java.lang.reflect.Field
+import kotlin.reflect.KFunction
+import kotlin.reflect.full.primaryConstructor
+import kotlin.reflect.jvm.jvmErasure
 
-class ViewModelFactory : ViewModelProvider.Factory {
+class ViewModelFactory(private val container: Container) : ViewModelProvider.Factory {
 
-    @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        val constructor = modelClass.declaredConstructors.first()
-        val params = mapModulesInConstructor(
-            constructor = constructor,
-            modules = Module::class.java.declaredFields,
-        )
-        return constructor.newInstance(*params.toTypedArray()) as T
+        val constructor = requireNotNull(modelClass.kotlin.primaryConstructor)
+        val params = mapModulesInConstructor(constructor)
+        return constructor.call(*params.toTypedArray())
     }
 
-    private fun mapModulesInConstructor(
-        constructor: Constructor<*>,
-        modules: Array<Field>,
-    ): List<Any?> {
-        return constructor.parameterTypes.map { parameterType ->
-            val module = getFieldWithType(fields = modules, type = parameterType)
-            module.isAccessible = true
-            module.get(modules)
+    private fun mapModulesInConstructor(constructor: KFunction<Any>): List<Any?> {
+        return constructor.parameters.map { param ->
+            requireNotNull(container.getInstance(param.type.jvmErasure)) {
+                "No matching same type in param | type : ${param.type}"
+            }
         }
-    }
-
-    private fun getFieldWithType(fields: Array<Field>, type: Class<*>): Field {
-        val module = fields.firstOrNull { it.type.simpleName == type.simpleName }
-        requireNotNull(module) { "No matching same type in fields | type : $type" }
-        return module
     }
 }
