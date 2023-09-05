83a5a22806b518cced53deb8abff1e753447dc2b
hyunji1203 <hj27720@gmail.com>
2023-09-05 21:34:42 +0900

    refactor: 에러 처리 및 함수 반환 타입 수정

--- app/src/main/java/woowacourse/shopping/di/inject/AutoDependencyInjector.kt
+++ app/src/main/java/woowacourse/shopping/di/inject/AutoDependencyInjector.kt
@@ -7,25 +7,25 @@ import kotlin.reflect.full.primaryConstructor
 
 class AutoDependencyInjector {
     fun <T : Any> inject(clazz: Class<T>): T {
-        return (
-            matchParameterType(clazz.kotlin)
-                ?: throw IllegalArgumentException(INJECT_ERROR_MESSAGE)
-            )
+        return matchParameterType(clazz.kotlin)
     }
 
-    private fun <T : Any> matchParameterType(clazz: KClass<T>): T? {
-        val arguments = mutableListOf<Any?>()
+    private fun <T : Any> matchParameterType(clazz: KClass<T>): T {
+        val arguments = mutableListOf<Any>()
         val constructor = clazz.primaryConstructor
+            ?: throw IllegalArgumentException(INJECT_ERROR_MESSAGE)
         val modules = DataModule::class.declaredMemberProperties
 
-        constructor?.parameters?.forEach { parameter ->
-            val argument = modules.first { it.returnType == parameter.type }
-            arguments.add(argument.getter.call(DataModule))
+        constructor.parameters.forEach { parameter ->
+            val argument = modules.firstOrNull { it.returnType == parameter.type }
+                ?: throw IllegalArgumentException(MATCH_MODULE_ERROR_MESSAGE)
+            arguments.add(argument.getter.call(DataModule)!!)
         }
-        return constructor?.call(*arguments.toTypedArray())
+        return constructor.call(*arguments.toTypedArray())
     }
 
     companion object {
+        private const val MATCH_MODULE_ERROR_MESSAGE = "필요한 모듈이 존재하지 않습니다."
         private const val INJECT_ERROR_MESSAGE = "주입 할 생성자가 존재하지 않습니다."
     }
 }
