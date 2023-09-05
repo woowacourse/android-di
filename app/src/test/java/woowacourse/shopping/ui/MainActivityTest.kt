ab37be0d48af625260ef199dcebe9f030af24a7f
vrexpert <vrexpert2302@gmail.com>
2023-09-05 19:11:36 +0900

    test: MainActivityTest 리플랙션 테스트 제거

--- app/src/test/java/woowacourse/shopping/ui/MainActivityTest.kt
+++ app/src/test/java/woowacourse/shopping/ui/MainActivityTest.kt
@@ -2,16 +2,12 @@ package woowacourse.shopping.ui
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.ViewModelProvider
-import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
-import org.assertj.core.api.SoftAssertions
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
-import woowacourse.shopping.repository.CartRepository
-import woowacourse.shopping.repository.ProductRepository
 
 @RunWith(RobolectricTestRunner::class)
 class MainActivityTest {
@@ -55,34 +51,5 @@ class MainActivityTest {
 
         // then
         assertThat(viewModel).isInstanceOf(MainViewModel::class.java)
-    }
-
-    @Test
-    fun `ViewModel 은 상품 저장소와 장바구니 저장소를 가진다`() {
-        // given
-        val activity = Robolectric
-            .buildActivity(MainActivity::class.java)
-            .create()
-            .get()
-
-        // when : ViewModel 생성
-        ViewModelProvider(activity)[MainViewModel::class.java]
-
-        val softly = SoftAssertions().apply {
-            // and
-            val hasCartRepository = MainViewModel::class.java.declaredFields.any {
-                CartRepository::class.java.isAssignableFrom(it.type)
-            }
-
-            Assertions.assertThat(hasCartRepository).isTrue
-
-            // and
-            val hasProductRepository = MainViewModel::class.java.declaredFields.any {
-                ProductRepository::class.java.isAssignableFrom(it.type)
-            }
-
-            Assertions.assertThat(hasProductRepository).isTrue
-        }
-        softly.assertAll()
     }
 }
