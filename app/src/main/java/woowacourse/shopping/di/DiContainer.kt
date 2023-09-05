f625d5dca2737b4389781107878ca55814ea1e90
re4rk <37167652+re4rk@users.noreply.github.com>
2023-09-05 10:32:12 +0900

    fix : diContainer 리팩토링 후 생긴 버그 개선

--- app/src/main/java/woowacourse/shopping/di/DiContainer.kt
+++ app/src/main/java/woowacourse/shopping/di/DiContainer.kt
@@ -4,20 +4,16 @@ import kotlin.reflect.KClass
 import kotlin.reflect.full.declaredFunctions
 import kotlin.reflect.full.declaredMemberProperties
 import kotlin.reflect.full.primaryConstructor
-import kotlin.reflect.jvm.isAccessible
+import kotlin.reflect.jvm.javaGetter
+import kotlin.reflect.jvm.javaMethod
 
 open class DiContainer(private val parentDiContainer: DiContainer? = null) {
 
     fun <T : Any> createInstance(clazz: KClass<T>): T {
         val constructor = clazz.primaryConstructor ?: throw IllegalArgumentException()
+        val args = constructor.parameters.map { param -> get(param.type.classifier as KClass<*>) }
 
-        constructor.isAccessible = true
-
-        return constructor.call(
-            *constructor.parameters.map { param ->
-                get(param.type.classifier as KClass<*>)
-            }.toTypedArray()
-        )
+        return constructor.call(*args.toTypedArray())
     }
 
     fun <T : Any> get(clazz: KClass<T>): T? {
@@ -26,15 +22,13 @@ open class DiContainer(private val parentDiContainer: DiContainer? = null) {
 
     private fun <T : Any> getFromMethod(clazz: KClass<T>): T? {
         return this::class.declaredFunctions.firstOrNull { method ->
-            method.isAccessible = true
-            method.returnType.javaClass.simpleName == clazz.simpleName
+            method.javaMethod?.returnType?.simpleName == clazz.simpleName
         }?.call(this) as T?
     }
 
     private fun <T : Any> getFromGetter(clazz: KClass<T>): T? {
         return this::class.declaredMemberProperties.firstOrNull { property ->
-            property.isAccessible = true
-            property.returnType.javaClass.simpleName == clazz.simpleName
+            property.javaGetter?.returnType?.simpleName == clazz.simpleName
         }?.getter?.call(this) as T?
     }
 }
