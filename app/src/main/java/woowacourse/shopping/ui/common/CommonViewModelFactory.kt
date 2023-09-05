552aa234e94cc96bb7a8733b57b35216de22f541
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-09-05 13:42:48 +0900

    refactor: application 클래스의 변수에 접근할 수 있도록 수정

--- app/src/main/java/woowacourse/shopping/ui/common/CommonViewModelFactory.kt
+++ app/src/main/java/woowacourse/shopping/ui/common/CommonViewModelFactory.kt
@@ -2,10 +2,14 @@ package woowacourse.shopping.ui.common
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewmodel.CreationExtras
 import woowacourse.shopping.ShoppingApplication
 
 object CommonViewModelFactory : ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return ShoppingApplication.inject.getInstance(modelClass)
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
+        val application = checkNotNull(extras[APPLICATION_KEY]) as ShoppingApplication
+        return application.inject.getInstance(modelClass)
     }
 }
