7768e082472d5cd336ee0b6d17b5caf08a3f5685
ippnsj <ippnsj@gmail.com>
2023-09-05 17:18:57 +0900

    refactor: 주입할 인스턴스가 없는 경우 디폴트 값을 가지도록 수정

--- app/src/main/java/woowacourse/shopping/di/AppContainer.kt
+++ app/src/main/java/woowacourse/shopping/di/AppContainer.kt
@@ -2,26 +2,24 @@ package woowacourse.shopping.di
 
 import woowacourse.shopping.data.DefaultCartRepository
 import woowacourse.shopping.data.DefaultProductRepository
-import kotlin.reflect.full.declaredMemberProperties
+import woowacourse.shopping.repository.CartRepository
+import woowacourse.shopping.repository.ProductRepository
+import kotlin.reflect.KClass
 import kotlin.reflect.full.primaryConstructor
-import kotlin.reflect.jvm.isAccessible
+import kotlin.reflect.jvm.jvmErasure
 
 class AppContainer {
-    private val productRepository = DefaultProductRepository()
-    private val cartRepository = DefaultCartRepository()
+    private val repositories: MutableMap<KClass<*>, Any> = mutableMapOf(
+        ProductRepository::class to DefaultProductRepository(),
+        CartRepository::class to DefaultCartRepository(),
+    )
 
     fun <T : Any> inject(clazz: Class<T>): T {
         val constructor = clazz.kotlin.primaryConstructor
             ?: throw NullPointerException("주입할 클래스의 주생성자가 존재하지 않습니다.")
-        val args = constructor.parameters.associateWith { parameter ->
-            this::class.declaredMemberProperties
-                .find { it.returnType == parameter.type }
-                ?.let {
-                    it.isAccessible = true
-                    it.getter.call(this)
-                } ?: throw NoSuchElementException("생성자 주입을 위한 프로퍼티의 인스턴스가 존재하지 않습니다.")
-        }
-
+        val args = constructor.parameters
+            .associateWith { repositories[it.type.jvmErasure] }
+            .filterValues { it != null }
         return constructor.callBy(args)
     }
 }
