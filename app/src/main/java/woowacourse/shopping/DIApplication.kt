c13400e27f1a9c5fcefa70e4ec52412f6ce0de6d
2chang5 <chang5hustle@gmail.com>
2023-09-05 16:21:10 +0900

    [feat]: AutoDi 인스턴스 생성 방법 등록

---
+++ app/src/main/java/woowacourse/shopping/DIApplication.kt
@@ -0,0 +1,24 @@
+package woowacourse.shopping
+
+import android.app.Application
+import woowacourse.shopping.data.repository.CartRepositoryImpl
+import woowacourse.shopping.data.repository.ProductRepositoryImpl
+import woowacourse.shopping.repository.CartRepository
+import woowacourse.shopping.repository.ProductRepository
+import woowacourse.shopping.ui.MainViewModel
+import woowacourse.shopping.util.autoDI.AutoDI
+
+class DIApplication : Application() {
+    override fun onCreate() {
+        initAutoDI()
+        super.onCreate()
+    }
+
+    private fun initAutoDI() {
+        AutoDI {
+            singleton<CartRepository>("singleton") { CartRepositoryImpl() }
+            singleton<ProductRepository>("singleton") { ProductRepositoryImpl() }
+            viewModel<MainViewModel> { MainViewModel(inject("singleton"), inject("singleton")) }
+        }
+    }
+}
