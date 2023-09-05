53b47d0d4218f1a9826bc4741e2118626386e231
hyemdooly <hyemdooly@gmail.com>
2023-09-05 17:12:08 +0900

    refactor: 함수 분리

---
+++ app/src/main/java/woowacourse/shopping/di/ViewModelInjector.kt
@@ -0,0 +1,26 @@
+package woowacourse.shopping.di
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import woowacourse.shopping.ui.common.Container
+import kotlin.reflect.full.primaryConstructor
+import kotlin.reflect.jvm.jvmErasure
+
+object ViewModelInjector {
+    inline fun <reified T : ViewModel> getInjectedViewModelFactory(): ViewModelProvider.Factory {
+        val constructor = T::class.primaryConstructor
+        requireNotNull(constructor) { "Unknown ViewModel Class ${T::class}" }
+
+        val instances = constructor.parameters.map {
+            Container.getInstance(it.type.jvmErasure)
+        }
+
+        return viewModelFactory {
+            initializer {
+                constructor.call(*instances.toTypedArray())
+            }
+        }
+    }
+}
