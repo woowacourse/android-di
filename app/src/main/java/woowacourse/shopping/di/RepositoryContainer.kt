bf5faa1dae43b294f3cc3105f1862ae501ed9d1d
krrong <khk9664@naver.com>
2023-09-05 14:53:26 +0900

    feat: 리포지터리를 싱글톤으로 관리하는 RepositoryContainer 구현

---
+++ app/src/main/java/woowacourse/shopping/di/RepositoryContainer.kt
@@ -0,0 +1,16 @@
+package woowacourse.shopping.di
+
+import kotlin.reflect.KClass
+import kotlin.reflect.full.createInstance
+
+object RepositoryContainer {
+    private val repositories = mutableMapOf<KClass<*>, Any>()
+
+    fun getInstance(type: KClass<*>): Any {
+        return repositories[type] ?: type.createInstance()
+    }
+
+    fun addInstance(type: KClass<*>, instance: Any) {
+        repositories[type] = instance
+    }
+}
