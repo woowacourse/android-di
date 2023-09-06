09fda33a7988b357e6c9a7576bd35e210e897cc8
whk06061 <gju06061@naver.com>
2023-09-06 21:29:27 +0900

    refactor: null 처리 및 함수 시그니쳐 변경

--- app/src/main/java/woowacourse/shopping/data/di/Injector.kt
+++ app/src/main/java/woowacourse/shopping/data/di/Injector.kt
@@ -1,6 +1,7 @@
 package woowacourse.shopping.data.di
 
-import java.lang.IllegalArgumentException
+import kotlin.reflect.KFunction
+import kotlin.reflect.KProperty
 import kotlin.reflect.KType
 import kotlin.reflect.full.declaredMemberProperties
 import kotlin.reflect.full.primaryConstructor
@@ -9,28 +10,30 @@ object Injector {
     var modules: List<Module> = listOf()
 
     inline fun <reified T : Any> inject(): T {
-        val constructor = T::class.primaryConstructor ?: throw IllegalArgumentException()
-        val dependencies:List<Any?> = provideDependencies<T>()
+        val constructor = requireNotNull(T::class.primaryConstructor)
+        val dependencies: List<Any> = provideDependencies(constructor)
         return constructor.call(*dependencies.toTypedArray())
     }
 
-    inline fun <reified T : Any> provideDependencies(): List<Any?> {
-        val constructorParametersType =
-            T::class.primaryConstructor?.parameters?.map { it.type } ?: mutableListOf()
-        val dependencies: List<Any?> = modules.flatMap { module ->
+    inline fun <reified T : Any> provideDependencies(constructor: KFunction<T>): List<Any> {
+        val constructorParametersType = constructor.parameters.map { it.type }
+        val dependencies: List<Any> = modules.flatMap { module ->
             findDependencies(module, constructorParametersType)
         }
         return dependencies
     }
 
     fun findDependencies(
-        module: Module,
-        constructorParametersType: List<KType>
-    ): List<Any?> {
-        return constructorParametersType.map { type ->
+        module: Module, constructorParametersType: List<KType>
+    ): List<Any> {
+        val foundProperties: List<KProperty<*>> = constructorParametersType.map { type ->
             module::class.declaredMemberProperties.first {
-                it.returnType == type }
-        }.map {
-            it.call(module) ?: throw NoSuchElementException() }
+                it.returnType == type
+            }
+        }
+        return foundProperties.map {
+            // 프로퍼티 값 가져오기
+            it.call(module) ?: throw NoSuchElementException()
+        }
     }
 }
