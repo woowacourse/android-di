aaebdc03054182f8f3a66149fdc15c84d6cc9a28
pingu244 <pingu244@naver.com>
2023-09-06 11:40:50 +0900

    [refactor] 코드 간소화

--- app/src/main/java/woowacourse/shopping/di/Injector.kt
+++ app/src/main/java/woowacourse/shopping/di/Injector.kt
@@ -9,25 +9,14 @@ class Injector(private val container: Container) {
     inline fun <reified T : Any> getInstance(): T {
         val constructor = T::class.primaryConstructor ?: throw IllegalStateException()
         val parameters = constructor.parameters
-        return constructor.call(*getArguments(parameters).toTypedArray())
+        val arguments = parameters.map { getArgument(it) }
+        return constructor.call(*arguments.toTypedArray())
     }
 
-    fun getArguments(parameters: List<KParameter>): MutableList<Any> {
-        val arguments = mutableListOf<Any>()
-
-        for (param in parameters) {
-            arguments.add(getArgument(param))
-        }
-        return arguments
-    }
-
-    private fun getArgument(param: KParameter): Any {
+    fun getArgument(param: KParameter): Any {
         container::class.declaredMemberProperties.forEach {
             if (param.type == it.returnType) {
-                val result = it.getter.call(container)
-                if (result != null) {
-                    return result
-                }
+                return it.getter.call(container) ?: throw java.lang.IllegalArgumentException()
             }
         }
         throw IllegalArgumentException()
