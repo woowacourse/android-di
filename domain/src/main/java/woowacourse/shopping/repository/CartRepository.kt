b5bfe63215ee22be262e388f4c461f965b982e89
briandr97 <briandr@naver.com>
2023-09-06 13:57:57 +0900

    feat: Flow를 적용해보고 Dao로부터 가져오도록 변경하기 위해 인터페이스 설계 수정

--- domain/src/main/java/woowacourse/shopping/repository/CartRepository.kt
+++ domain/src/main/java/woowacourse/shopping/repository/CartRepository.kt
@@ -1,9 +1,10 @@
 package woowacourse.shopping.repository
 
+import kotlinx.coroutines.flow.Flow
 import woowacourse.shopping.model.Product
 
 interface CartRepository {
-    fun addCartProduct(product: Product)
-    fun getAllCartProducts(): List<Product>
-    fun deleteCartProduct(id: Int)
+    suspend fun addCartProduct(product: Product)
+    suspend fun getAllCartProducts(): Flow<List<Product>>
+    suspend fun deleteCartProduct(id: Long)
 }
