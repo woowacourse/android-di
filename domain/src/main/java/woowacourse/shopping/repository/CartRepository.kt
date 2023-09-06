4f40420f7ec6445243972974d8f55acb4a1da718
briandr97 <briandr@naver.com>
2023-09-06 15:03:58 +0900

    feat: Flow가 필요없다고 생각되어 다시 Flow 제거

--- domain/src/main/java/woowacourse/shopping/repository/CartRepository.kt
+++ domain/src/main/java/woowacourse/shopping/repository/CartRepository.kt
@@ -1,10 +1,9 @@
 package woowacourse.shopping.repository
 
-import kotlinx.coroutines.flow.Flow
 import woowacourse.shopping.model.Product
 
 interface CartRepository {
     suspend fun addCartProduct(product: Product)
-    suspend fun getAllCartProducts(): Flow<List<Product>>
+    suspend fun getAllCartProducts(): List<Product>
     suspend fun deleteCartProduct(id: Long)
 }
