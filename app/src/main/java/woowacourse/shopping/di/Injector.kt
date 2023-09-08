ffa5c7b4bce0088d8c5bb89947db04109b768a9b
hyemdooly <hyemdooly@gmail.com>
2023-09-08 14:07:04 +0900

    feat: Recursive DI 구현

--- app/src/main/java/woowacourse/shopping/di/Injector.kt
+++ app/src/main/java/woowacourse/shopping/di/Injector.kt
@@ -1,18 +1,51 @@
 package woowacourse.shopping.di
 
-import androidx.lifecycle.ViewModel
+import kotlin.reflect.KClass
+import kotlin.reflect.KFunction
+import kotlin.reflect.full.declaredMemberProperties
+import kotlin.reflect.full.hasAnnotation
 import kotlin.reflect.full.primaryConstructor
+import kotlin.reflect.jvm.isAccessible
+import kotlin.reflect.jvm.javaField
 import kotlin.reflect.jvm.jvmErasure
 
 object Injector {
-    inline fun <reified T : ViewModel> inject(): T {
-        val constructor = T::class.primaryConstructor
-        requireNotNull(constructor) { "Unknown ViewModel Class ${T::class}" }
+    fun <T> inject(modelClass: KClass<*>): T {
+        val instance = Container.getInstance(modelClass) ?: createInstance(modelClass)
+        return instance as T
+    }
 
-        val instances = constructor.parameters.map {
-            Container.getInstance(it.type.jvmErasure)
+    private fun <T> createInstance(modelClass: KClass<*>): T {
+        val constructor = modelClass.primaryConstructor
+        requireNotNull(constructor) { "Unknown ViewModel Class $modelClass" }
+
+        val paramInstances = getParamInstances(constructor)
+        val instance = constructor.call(*paramInstances.toTypedArray()) as T
+        injectFields(instance)
+
+        return instance
+    }
+
+    private fun <T> getParamInstances(constructor: KFunction<T>): List<Any> {
+        val paramInstances = constructor.parameters.map { param ->
+            val type = param.type.jvmErasure
+            Container.getInstance(type) ?: inject(type)
         }
+        return paramInstances
+    }
 
-        return constructor.call(*instances.toTypedArray())
+    private fun <T> injectFields(instance: T) {
+        val notNullInstance = requireNotNull(instance) { "Instance should not null" }
+        val properties =
+            notNullInstance::class.declaredMemberProperties.filter { it.hasAnnotation<Inject>() }
+
+        properties.forEach { property ->
+            property.isAccessible = true
+            property.javaField?.let {
+                val type = it.type.kotlin
+                val fieldValue = Container.getInstance(type) ?: inject(type)
+                it.set(instance, fieldValue)
+            }
+        }
     }
 }
