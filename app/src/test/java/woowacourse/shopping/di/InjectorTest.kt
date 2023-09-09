0a7c6c89fea2e4a1212feb771c2a4f9a43ba6b80
krrong <khk9664@naver.com>
2023-09-10 01:45:16 +0900

    test: Injector test

--- app/src/test/java/woowacourse/shopping/di/InjectorTest.kt
+++ app/src/test/java/woowacourse/shopping/di/InjectorTest.kt
@@ -1,10 +1,15 @@
 package woowacourse.shopping.di
 
 import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertNotNull
 import org.junit.Test
 import woowacourse.shopping.data.DefaultProductRepository
 import woowacourse.shopping.data.annotation.Inject
+import woowacourse.shopping.data.annotation.Qualifier
 import woowacourse.shopping.repository.ProductRepository
+import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.jvm.isAccessible
 
 interface Fake
 
@@ -44,4 +49,80 @@ class InjectorTest {
         assertEquals(crew.name, "강석진")
         assertEquals(crew.nickName, "Krrong")
     }
+
+    @Test
+    fun `알루미늄, 플라스틱 하우징을 Container inject를 통해 추가하면 Qualifier를 통해 인스턴스를 얻을 수 있다`() {
+        // given & when
+        Container.addInstance(
+            Housing::class,
+            Injector.inject(Aluminum::class),
+        )
+        Container.addInstance(
+            Housing::class,
+            Injector.inject(Plastic::class),
+        )
+
+        val aluminumHousing = Container.getInstance(Aluminum::class.findAnnotation<Qualifier>()!!)
+        val plasticHousing = Container.getInstance(Plastic::class.findAnnotation<Qualifier>()!!)
+
+        assertNotNull(aluminumHousing)
+        assertNotNull(plasticHousing)
+    }
+
+    @Test
+    fun `알루미늄, 플라스틱 하우징을 Container inject를 통해 추가하면 Container의 annotationMap의 크기는 2이다`() {
+        // given
+        Container.addInstance(
+            Housing::class,
+            Injector.inject(Aluminum::class),
+        )
+        Container.addInstance(
+            Housing::class,
+            Injector.inject(Plastic::class),
+        )
+
+        // annotationMap 필드를 가져오기 위해 접근성을 변경
+        val annotationMapField = Container::class.memberProperties.find { it.name == "annotationMap" }
+        annotationMapField?.isAccessible = true
+
+        // when : annotationMap 필드의 값을 가져옴
+        val annotationMap = annotationMapField?.getter?.call() as Map<*, *>
+
+        // then
+        assertEquals(annotationMap.size, 2)
+    }
+
+    @Test
+    fun `as`() {
+    }
 }
+
+interface Housing
+
+@Qualifier("Aluminum")
+class Aluminum : Housing
+
+@Qualifier("Plastic")
+class Plastic : Housing
+
+interface Switch
+
+@Qualifier("Blue")
+class BlueSwitch : Switch
+
+@Qualifier("Red")
+class RedSwitch : Switch
+
+interface KeyCap
+
+@Qualifier("Oem")
+class Oem : KeyCap
+
+@Qualifier("Xda")
+class Xda : KeyCap
+
+class KeyBoard(
+    @Inject private val housing: Housing,
+    @Inject private val switch: Switch,
+    @Inject private val keyCap: KeyCap,
+)
