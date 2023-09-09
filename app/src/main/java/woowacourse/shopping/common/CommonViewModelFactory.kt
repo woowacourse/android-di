c0d2c80a930c9f20d176344f25b1f35e3426f065
Bandal <no1msh1217@gmail.com>
2023-09-09 16:02:43 +0900

    refactor: 이펙티브 코틀린 기반 코드 가독성 개선
    - 아이템 4 "inferred 타입으로 리턴하지 말라"
    - 아이템 15 "리시버를 명시적으로 참조하라"
    - 아이템 17 "이름 있는 아규먼트를 사용하라"

--- app/src/main/java/woowacourse/shopping/common/CommonViewModelFactory.kt
+++ app/src/main/java/woowacourse/shopping/common/CommonViewModelFactory.kt
@@ -4,39 +4,39 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import woowacourse.shopping.ShoppingApplication
+import kotlin.reflect.KFunction
 import kotlin.reflect.KParameter
+import kotlin.reflect.KProperty1
 import kotlin.reflect.full.declaredMemberProperties
 import kotlin.reflect.full.primaryConstructor
 import kotlin.reflect.jvm.isAccessible
 
 object CommonViewModelFactory : ViewModelProvider.Factory {
 
-    @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
-        val primaryConstructorParameters = getPrimaryConstructorParameters(modelClass)
-        val injectProperties = getPropertiesForInject(primaryConstructorParameters)
-
-        return modelClass.constructors.first().newInstance(*injectProperties.toTypedArray()) as T
-    }
-
-    private fun <T : ViewModel> getPrimaryConstructorParameters(modelClass: Class<T>): List<KParameter> {
-        return modelClass.kotlin.primaryConstructor?.parameters
+        val primaryConstructor: KFunction<T> = modelClass.kotlin.primaryConstructor
             ?: throw NullPointerException("주 생성자를 찾을 수 없습니다.")
+
+        val injectProperties = getPropertiesForInject(primaryConstructor.parameters)
+
+        return primaryConstructor.call(*injectProperties.toTypedArray())
     }
 
     private fun getPropertiesForInject(needParameters: List<KParameter>): List<Any?> {
-        val injectProperties = mutableListOf<Any?>()
-        val defaultProperties =
+        val defaultProperties: Collection<KProperty1<out AppContainer, *>> =
             ShoppingApplication.defaultAppContainer::class.declaredMemberProperties
 
-        needParameters.forEach { needParameter ->
-            val inject =
-                defaultProperties.find { it.returnType == needParameter.type }?.let {
-                    it.isAccessible = true
-                    it.getter.call(ShoppingApplication.defaultAppContainer)
-                }
-            injectProperties.add(inject)
+        return needParameters.map { needParameter ->
+            defaultProperties.find { defaultProperty ->
+                defaultProperty.returnType == needParameter.type
+            }?.let { matchedProperty ->
+                getDefaultProperty(matchedProperty)
+            }
         }
-        return injectProperties
+    }
+
+    private fun getDefaultProperty(matchedProperty: KProperty1<out AppContainer, *>): Any? {
+        matchedProperty.isAccessible = true
+        return matchedProperty.getter.call(ShoppingApplication.defaultAppContainer)
     }
 }
