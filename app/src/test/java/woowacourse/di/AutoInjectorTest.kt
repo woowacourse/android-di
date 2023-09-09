d64e53984cabb5aaddc0328a8c8f67c265a17d51
mendel <rhthrhrl0@naver.com>
2023-09-09 22:08:56 +0900

    test: 필드 주입 기능 테스트 코드 추가

--- app/src/test/java/woowacourse/di/AutoInjectorTest.kt
+++ app/src/test/java/woowacourse/di/AutoInjectorTest.kt
@@ -1,17 +1,53 @@
 package woowacourse.di
 
+import androidx.lifecycle.ViewModel
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertThrows
 import org.junit.Test
-import woowacourse.shopping.di.module.DefaultActivityModule
-import woowacourse.shopping.di.module.DefaultApplicationModule
-import woowacourse.shopping.ui.MainViewModel
+import woowacourse.shopping.data.CartRepository
+import woowacourse.shopping.data.ProductRepository
+import woowacourse.shopping.di.annotation.FieldInject
+import woowacourse.util.getFakeActivityModule
+import woowacourse.util.getFakeApplicationModule
 import kotlin.reflect.full.isSubclassOf
+
+class FakeViewModel(
+    private val cartRepository: CartRepository,
+) : ViewModel() {
+    @FieldInject
+    lateinit var productRepository: ProductRepository
+}
+
+class FakePrivateFieldInjectViewModel(
+    private val cartRepository: CartRepository,
+) : ViewModel() {
+    @FieldInject
+    private lateinit var productRepository: ProductRepository
+}
 
 internal class AutoInjectorTest {
     @Test
     fun `DefaultActivityModule로 MainViewModel을 만들 수 있다`() {
-        val activityModule = DefaultActivityModule(DefaultApplicationModule())
-        val instance = activityModule.provideInstance(MainViewModel::class.java)
-        assertEquals(true, instance::class.isSubclassOf(MainViewModel::class))
+        // given
+        val activityModule = getFakeActivityModule(getFakeApplicationModule())
+
+        // when
+        val instance = activityModule.provideInstance(FakeViewModel::class.java)
+
+        // then
+        assertEquals(true, instance::class.isSubclassOf(FakeViewModel::class))
+        assertNotNull(instance.productRepository)
+    }
+
+    @Test
+    fun `가시성이 공개되지 않은 필드는 주입받을 수 없다`() {
+        // given
+        val activityModule = getFakeActivityModule(getFakeApplicationModule())
+
+        // then
+        assertThrows(IllegalStateException::class.java) {
+            activityModule.provideInstance(FakePrivateFieldInjectViewModel::class.java)
+        }
     }
 }
