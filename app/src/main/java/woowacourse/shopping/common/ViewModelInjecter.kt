5b04c4684629a18ccf8bec0aee2840b1fa09be71
Bandal <no1msh1217@gmail.com>
2023-09-09 21:06:01 +0900

    refactor: viewModelInject가 다른 factory를 가질 수 있게 변경

--- app/src/main/java/woowacourse/shopping/common/ViewModelInjecter.kt
+++ app/src/main/java/woowacourse/shopping/common/ViewModelInjecter.kt
@@ -4,16 +4,23 @@ import androidx.activity.ComponentActivity
 import androidx.annotation.MainThread
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelLazy
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
+import woowacourse.shopping.ShoppingApplication
 
 @MainThread
 inline fun <reified VM : ViewModel> ComponentActivity.viewModelInject(
     noinline extrasProducer: (() -> CreationExtras)? = null,
+    noinline factoryProducer: (() -> ViewModelProvider.Factory)? = null,
 ): Lazy<VM> {
+    val factoryPromise = factoryProducer ?: {
+        CommonViewModelFactory(ShoppingApplication.defaultAppContainer)
+    }
+
     return ViewModelLazy(
         VM::class,
         { viewModelStore },
-        { CommonViewModelFactory },
+        factoryPromise,
         { extrasProducer?.invoke() ?: this.defaultViewModelCreationExtras },
     )
 }
