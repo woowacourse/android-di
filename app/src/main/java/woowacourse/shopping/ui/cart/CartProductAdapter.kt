158c48f7b79e380be127f78422b85ccfa9cd37fe
re4rk <37167652+re4rk@users.noreply.github.com>
2023-09-09 22:34:58 +0900

    feat: 장바구니 position에서 id를 리포지터리에 전달하도록 변경

--- app/src/main/java/woowacourse/shopping/ui/cart/CartProductAdapter.kt
+++ app/src/main/java/woowacourse/shopping/ui/cart/CartProductAdapter.kt
@@ -13,7 +13,7 @@ class CartProductAdapter(
     private val items: MutableList<CartProduct> = items.toMutableList()
 
     private val onClickDelete = { position: Int ->
-        onClickDelete(position)
+        onClickDelete(items[position].id)
         removeItem(position)
     }
 
