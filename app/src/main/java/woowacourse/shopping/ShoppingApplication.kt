afd4ffbab0e7aba0e93009979dcba1a1aa643704
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-09-09 20:54:03 +0900

    refactor: 변수명 수정 및 Application에서 클래스 인스턴스 넣도록 수정

--- app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
+++ app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
@@ -1,15 +1,22 @@
 package woowacourse.shopping
 
 import android.app.Application
+import woowacourse.shopping.data.DefaultCartRepository
+import woowacourse.shopping.data.DefaultProductRepository
+import woowacourse.shopping.di.container.DependencyContainer
 import woowacourse.shopping.di.inject.CustomInjector
+import woowacourse.shopping.repository.CartRepository
+import woowacourse.shopping.repository.ProductRepository
 
 class ShoppingApplication : Application() {
 
-    lateinit var inject: CustomInjector
+    lateinit var injector: CustomInjector
         private set
 
     override fun onCreate() {
         super.onCreate()
-        inject = CustomInjector()
+        injector = CustomInjector()
+        DependencyContainer.setInstance(ProductRepository::class, injector.inject(DefaultProductRepository::class))
+        DependencyContainer.setInstance(CartRepository::class, injector.inject(DefaultCartRepository::class))
     }
 }
