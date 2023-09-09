65c53c150749ba6ed009d353bdba64cc636a7f1b
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-09-09 20:52:41 +0900

    feat: field로 주입하는 기능 구현

--- app/src/main/java/woowacourse/shopping/di/inject/CustomInjector.kt
+++ app/src/main/java/woowacourse/shopping/di/inject/CustomInjector.kt
@@ -1,34 +1,43 @@
 package woowacourse.shopping.di.inject
 
-import woowacourse.shopping.di.container.RepositoryContainer
+import woowacourse.shopping.di.annotation.CustomInject
+import woowacourse.shopping.di.container.DependencyContainer
 import kotlin.reflect.KClass
-import kotlin.reflect.KType
+import kotlin.reflect.KMutableProperty
 import kotlin.reflect.full.declaredMemberProperties
+import kotlin.reflect.full.hasAnnotation
 import kotlin.reflect.full.primaryConstructor
 import kotlin.reflect.jvm.isAccessible
 
 class CustomInjector {
 
-    fun <T : Any> getInstanceFromClass(clazz: Class<T>): T {
-        return createInstanceFromKClass(clazz.kotlin)
+    fun <T : Any> inject(kClass: KClass<T>): Any {
+        return DependencyContainer.getInstance(kClass)
+            ?: createInstanceFromKClass(kClass)
     }
 
     private fun <T : Any> createInstanceFromKClass(kClass: KClass<T>): T {
         val constructor =
             kClass.primaryConstructor ?: throw IllegalArgumentException("주 생성자를 찾을 수 없습니다.")
 
-        val params = constructor.parameters
-        val args = params.map {
-            findPropertyAndGetValue(it.type)
-        }.toTypedArray()
+        val parameterValues =
+            constructor.parameters.associateWith { findPropertyAndGetValue(kClass) }
 
-        return constructor.call(*args)
+        return constructor.callBy(parameterValues).apply { injectFields(this) }
     }
 
-    private fun findPropertyAndGetValue(type: KType): Any {
-        return RepositoryContainer::class.declaredMemberProperties.find { it.returnType == type }?.let {
-            it.isAccessible = true
-            it.getter.call(RepositoryContainer)
-        } ?: throw IllegalArgumentException("같은 타입의 프로퍼티를 찾을 수 없습니다.")
+    private fun <T : Any> injectFields(instance: T) {
+        instance::class.declaredMemberProperties
+            .filter { it.hasAnnotation<CustomInject>() }
+            .forEach { prop ->
+                prop.isAccessible = true
+                val propertyType = prop.returnType.classifier as KClass<*>
+                val value = findPropertyAndGetValue(propertyType)
+                (prop as KMutableProperty<*>).setter.call(instance, value)
+            }
+    }
+
+    private fun findPropertyAndGetValue(kClass: KClass<*>): Any {
+        return DependencyContainer.getInstance(kClass) ?: inject(kClass)
     }
 }
