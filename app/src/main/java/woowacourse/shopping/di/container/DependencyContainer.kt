e69106504879ae2431e15f1f953d4680b5f5ea6c
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-09-10 17:04:54 +0900

    refactor: Container에서 Qualifier가 붙은 Annotation 관리

--- app/src/main/java/woowacourse/shopping/di/container/DependencyContainer.kt
+++ app/src/main/java/woowacourse/shopping/di/container/DependencyContainer.kt
@@ -1,14 +1,22 @@
 package woowacourse.shopping.di.container
 
+import woowacourse.shopping.di.annotation.Qualifier
 import kotlin.reflect.KClass
+import kotlin.reflect.full.findAnnotation
 
 object DependencyContainer {
-    private val instances: HashMap<KClass<*>, Any> = hashMapOf()
+    private val instances: HashMap<Pair<KClass<*>, List<Annotation>>, Any> = hashMapOf()
+
     fun setInstance(kClass: KClass<*>, instance: Any) {
-        instances[kClass] = instance
+        val key = Pair(
+            kClass,
+            kClass.annotations.filter { it.annotationClass.findAnnotation<Qualifier>() != null },
+        )
+        instances[key] = instance
     }
 
-    fun getInstance(kClass: KClass<*>): Any? {
-        return instances[kClass]
+    fun getInstance(kClass: KClass<*>, annotations: List<Annotation> = emptyList()): Any? {
+        val key = Pair(kClass, annotations)
+        return instances[key]
     }
 }
