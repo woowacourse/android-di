3284fe2089aa77e9775024ac1a8f6f13dd5a4681
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-09-10 13:29:57 +0900

    feat: database 연결 및 의존성 주입

--- app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
+++ app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
@@ -1,8 +1,11 @@
 package woowacourse.shopping
 
 import android.app.Application
+import androidx.room.Room
+import woowacourse.shopping.data.CartProductDao
 import woowacourse.shopping.data.DefaultCartRepository
 import woowacourse.shopping.data.DefaultProductRepository
+import woowacourse.shopping.data.ShoppingDatabase
 import woowacourse.shopping.di.container.DependencyContainer
 import woowacourse.shopping.di.inject.CustomInjector
 import woowacourse.shopping.repository.CartRepository
@@ -16,7 +19,19 @@ class ShoppingApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         injector = CustomInjector()
-        DependencyContainer.setInstance(ProductRepository::class, injector.inject(DefaultProductRepository::class))
-        DependencyContainer.setInstance(CartRepository::class, injector.inject(DefaultCartRepository::class))
+        val database = Room
+            .databaseBuilder(this, ShoppingDatabase::class.java, "shopping_database")
+            .build()
+        val cartProductDao = database.cartProductDao()
+
+        DependencyContainer.setInstance(CartProductDao::class, cartProductDao)
+        DependencyContainer.setInstance(
+            ProductRepository::class,
+            injector.inject(DefaultProductRepository::class),
+        )
+        DependencyContainer.setInstance(
+            CartRepository::class,
+            injector.inject(DefaultCartRepository::class),
+        )
     }
 }
