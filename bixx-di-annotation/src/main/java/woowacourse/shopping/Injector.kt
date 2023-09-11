17f3a2806d7afc4f16559c023ec106b4746f66a0
briandr97 <briandr@naver.com>
2023-09-12 04:50:21 +0900

    feat: 의존성 주입이 필요한 프로퍼티 혹은 생성자 파라미터를 구분한다. 추상체를 주입받는 경우 받고싶은 구현체의 클래스명을 어노테이션에 입력하면 해당 구현체를 주입해준다.

---
+++ bixx-di-annotation/src/main/java/woowacourse/shopping/Injector.kt
@@ -0,0 +1,61 @@
+package woowacourse.shopping
+
+import kotlin.reflect.KClass
+import kotlin.reflect.KFunction
+import kotlin.reflect.KParameter
+import kotlin.reflect.full.declaredMemberProperties
+import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.full.primaryConstructor
+import kotlin.reflect.jvm.jvmErasure
+
+@Suppress("UNCHECKED_CAST")
+object Injector {
+    fun <T> inject(target: KClass<*>, nameTag: String? = null): T {
+        return if (nameTag == null) {
+            (DependencyContainer.getInstance(target) ?: createInstance(target)) as T
+        } else {
+            (DependencyContainer.getInstance(target, nameTag) ?: createInstance(target)) as T
+        }
+    }
+
+    private fun createInstance(target: KClass<*>): Any {
+        val constructor = target.primaryConstructor
+            ?: throw NullPointerException("{${target.simpleName}}의 주 생성자를 찾을 수 없습니다")
+        val paramInstances = provideConstructorParameters(constructor)
+
+        val instance = constructor.call(*paramInstances.toTypedArray())
+        instance.injectProperties()
+        DependencyContainer.addInstance(target, instance)
+
+        return instance
+    }
+
+    private fun provideConstructorParameters(constructor: KFunction<*>): List<Any> {
+        val kParams: List<KParameter> = constructor.parameters
+        val paramInstances = mutableListOf<Any>()
+        kParams.forEach {
+            if (it.findAnnotation<Inject>() == null) return@forEach
+            val qualifier = it.findAnnotation<Qualifier>()
+            if (qualifier != null) {
+                val instance: Any = inject(it.type.jvmErasure, qualifier.className)
+                paramInstances.add(instance)
+            } else {
+                paramInstances.add(inject(it.type.jvmErasure))
+            }
+        }
+        return paramInstances
+    }
+
+    private fun Any.injectProperties() {
+        val properties = this@injectProperties::class.declaredMemberProperties
+        properties.forEach {
+            val inject = it.findAnnotation<Inject>()
+            if (inject != null) {
+                this@injectProperties::class.java.getDeclaredField(it.name).apply {
+                    isAccessible = true
+                    set(this@injectProperties, inject(it.returnType.jvmErasure))
+                }
+            }
+        }
+    }
+}
