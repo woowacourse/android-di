31f13b9cbf66e9f4d5ceec9e756e98c07debc35e
2chang5 <chang5hustle@gmail.com>
2023-09-11 14:18:13 +0900

    [test] AutoDITest 신규 기능테스트 추가 및 테스트 정상화

--- autoDI/src/test/java/com/angrypig/autodi/autoDI/AutoDITest.kt
+++ autoDI/src/test/java/com/angrypig/autodi/autoDI/AutoDITest.kt
@@ -1,56 +1,114 @@
 package com.angrypig.autodi.autoDI
 
+import com.angrypig.autodi.AutoDI
+import com.angrypig.autodi.autoDIModule.autoDIModule
 import com.google.common.truth.Truth.assertThat
+import org.junit.After
 import org.junit.Test
-import woowacourse.shopping.data.repository.CartRepositoryImpl
-import woowacourse.shopping.repository.CartRepository
-import com.angrypig.autodi.autoDIModule.AutoDIModule
+
+private class TestDependency(var testValue: String)
 
 class AutoDITest {
 
-    @Test
-    fun `singleton 객체를 Container에 등록한다`() {
-        // when
-        val testQualifier = "test"
-        com.angrypig.autodi.AutoDI {
-            singleton<CartRepository>(testQualifier) { CartRepositoryImpl() }
+    @After
+    fun tearDown() {
+        clearAutoDIContainer()
+    }
+
+    private fun clearAutoDIContainer() {
+        AutoDI.clearModuleContainer()
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun `clearModuleContainer가 잘 동작한다(autoDIContainer 비우기)`() {
+        // given
+        val noQualifierModule = autoDIModule {
+            singleton { TestDependency("meaningless") }
+        }
+        AutoDI {
+            registerModule(noQualifierModule)
         }
 
+        // when
+        AutoDI.clearModuleContainer()
+
         // then
-        val containerQualifier = com.angrypig.autodi.autoDIModule.AutoDIModule.singletons.value.first().qualifier
-        val containerInstanceType =
-            com.angrypig.autodi.autoDIModule.AutoDIModule.singletons.value.first().getInstance().javaClass.typeName
-        assertThat(containerQualifier).isEqualTo(containerQualifier)
-        assertThat(containerInstanceType).isEqualTo(CartRepositoryImpl().javaClass.typeName)
+        // 성공 시나리오시 exception 발생하지않음
+        AutoDI.inject<TestDependency>()
     }
 
     @Test
-    fun `disposable 객체를 Container에 등록한다`() {
+    fun `qualifier 없는 모듈을 등록한다`() {
+        // given
+        val noQualifierModule = autoDIModule {
+            singleton { TestDependency("meaningless") }
+        }
+
         // when
-        val testQualifier = "test"
-        com.angrypig.autodi.AutoDI {
-            disposable<CartRepository>(testQualifier) { CartRepositoryImpl() }
+        AutoDI {
+            registerModule(noQualifierModule)
         }
 
         // then
-        val containerQualifier = com.angrypig.autodi.autoDIModule.AutoDIModule.disposables.value.first().qualifier
-        val containerInstanceType =
-            com.angrypig.autodi.autoDIModule.AutoDIModule.disposables.value.first().getInstance().javaClass.typeName
-        assertThat(containerQualifier).isEqualTo(containerQualifier)
-        assertThat(containerInstanceType).isEqualTo(CartRepositoryImpl().javaClass.typeName)
+        // 성공 시나리오시 exception 발생하지않음
+        AutoDI.inject<TestDependency>()
+    }
+
+    @Test
+    fun `모듈을 등록할때 존재하는 qualifier인경우 덮어쓴다`() {
+        // given
+        val existingString = "existing"
+        val existingModule = autoDIModule(existingString) {
+            singleton { TestDependency(existingString) }
+        }
+        val overrideString = "override"
+        val overrideModule = autoDIModule(existingString) {
+            singleton { TestDependency(overrideString) }
+        }
+        AutoDI.registerModule(existingModule)
+
+        // when
+        AutoDI.registerModule(overrideModule)
+
+        // then
+        assertThat(AutoDI.inject<TestDependency>().testValue).isEqualTo(overrideString)
+    }
+
+    @Test
+    fun `모듈을 override한다`() {
+        // given
+        val existingString = "existing"
+        val existingModule = autoDIModule(existingString) {
+            singleton { TestDependency(existingString) }
+        }
+        val overrideString = "override"
+        val overrideModule = autoDIModule(overrideString) {
+            singleton { TestDependency(overrideString) }
+        }
+        AutoDI.registerModule(existingModule)
+
+        // when
+        AutoDI.overrideModule(existingString, overrideModule)
+
+        // then
+        assertThat(AutoDI.inject<TestDependency>().testValue).isEqualTo(overrideString)
     }
 
     @Test
     fun `String값 "멧돼지"를 주입받는 Pig 객체를 inject함수를 통해 주입받는다`() {
         // given
         class Pig(val name: String)
-        com.angrypig.autodi.AutoDI {
-            disposable { Pig(inject("pigName")) }
-            disposable("pigName") { "멧돼지" }
+        AutoDI {
+            registerModule(
+                autoDIModule {
+                    disposable { Pig(AutoDI.inject("pigName")) }
+                    disposable("pigName") { "멧돼지" }
+                },
+            )
         }
 
         // when
-        val injectedInstance = com.angrypig.autodi.AutoDI.inject<Pig>()
+        val injectedInstance = AutoDI.inject<Pig>()
 
         // then
         val pigName = "멧돼지"
