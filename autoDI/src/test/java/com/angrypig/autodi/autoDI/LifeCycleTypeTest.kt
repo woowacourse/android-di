5876a0f519c530eb930e33168d1d0828dc14ed48
2chang5 <chang5hustle@gmail.com>
2023-09-11 14:30:40 +0900

    [test] LifeCycleTypeTest 정상화

--- autoDI/src/test/java/com/angrypig/autodi/autoDI/LifeCycleTypeTest.kt
+++ autoDI/src/test/java/com/angrypig/autodi/autoDI/LifeCycleTypeTest.kt
@@ -1,9 +1,9 @@
 package com.angrypig.autodi.autoDI
 
+import com.angrypig.autodi.autoDI.dummys.FakeRepository
+import com.angrypig.autodi.autoDI.dummys.FakeRepositoryImpl
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
-import woowacourse.shopping.data.repository.CartRepositoryImpl
-import woowacourse.shopping.repository.CartRepository
 import kotlin.reflect.full.starProjectedType
 
 class LifeCycleTypeTest {
@@ -11,14 +11,14 @@ class LifeCycleTypeTest {
     @Test
     fun `LifeCycle 하위 객체 생성시 type 변수값을 생성한다`() {
         // Singleton
-        val singleton = com.angrypig.autodi.LifeCycleType.Singleton<CartRepository> {
-            CartRepositoryImpl()
+        val singleton = com.angrypig.autodi.LifeCycleType.Singleton<FakeRepository> {
+            FakeRepositoryImpl()
         }
-        assertThat(singleton.type).isEqualTo(CartRepository::class.starProjectedType)
+        assertThat(singleton.type).isEqualTo(FakeRepository::class.starProjectedType)
         // Disposable
-        val disposable = com.angrypig.autodi.LifeCycleType.Disposable<CartRepository> {
-            CartRepositoryImpl()
+        val disposable = com.angrypig.autodi.LifeCycleType.Disposable<FakeRepository> {
+            FakeRepositoryImpl()
         }
-        assertThat(singleton.type).isEqualTo(CartRepository::class.starProjectedType)
+        assertThat(singleton.type).isEqualTo(FakeRepository::class.starProjectedType)
     }
 }
