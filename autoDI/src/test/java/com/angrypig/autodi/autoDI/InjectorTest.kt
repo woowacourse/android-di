f8b096a59e4b48c4a1681c4c5a35468a19e99680
2chang5 <chang5hustle@gmail.com>
2023-09-11 14:26:42 +0900

    [test] injector 테스트 정상화

--- autoDI/src/test/java/com/angrypig/autodi/autoDI/InjectorTest.kt
+++ autoDI/src/test/java/com/angrypig/autodi/autoDI/InjectorTest.kt
@@ -1,79 +1,93 @@
 package com.angrypig.autodi.autoDI
 
-import com.google.common.truth.Truth
+import com.angrypig.autodi.AutoDI
+import com.angrypig.autodi.Injector
+import com.angrypig.autodi.autoDIModule.autoDIModule
 import com.google.common.truth.Truth.assertThat
+import org.junit.After
 import org.junit.Test
-import woowacourse.shopping.data.repository.CartRepositoryImpl
-import woowacourse.shopping.repository.CartRepository
 import kotlin.reflect.full.isSubclassOf
 
+private interface FakeRepository
+
+private class FakeRepositoryImpl : FakeRepository
+
 class InjectorTest {
+    @After
+    fun tearDown() {
+        AutoDI.clearModuleContainer()
+    }
+
     @Test
     fun `singleton으로 등록된 객체를 프로퍼티 위임을 통해 주입받는다(qualifier 받지않는 상태)`() {
         // when
         val testQualifier = "test"
-        com.angrypig.autodi.AutoDI {
-            singleton<CartRepository>(testQualifier) { CartRepositoryImpl() }
+        AutoDI {
+            registerModule(autoDIModule { singleton<FakeRepository>(testQualifier) { FakeRepositoryImpl() } })
         }
 
         // then
-        val cartRepository: CartRepository by com.angrypig.autodi.Injector()
-        assertThat(cartRepository::class.isSubclassOf(CartRepositoryImpl::class)).isTrue()
+        val fakeRepository: FakeRepository by Injector()
+        assertThat(fakeRepository::class.isSubclassOf(FakeRepositoryImpl::class)).isTrue()
     }
 
     @Test
     fun `singleton으로 등록된 객체를 프로퍼티 위임을 통해 주입받는다(qualifier 받는 상태)`() {
         // when
         val testQualifier = "test"
-        com.angrypig.autodi.AutoDI {
-            singleton<CartRepository>(testQualifier) { CartRepositoryImpl() }
+        AutoDI {
+            registerModule(autoDIModule { singleton<FakeRepository>(testQualifier) { FakeRepositoryImpl() } })
         }
 
         // then
-        val cartRepository: CartRepository by com.angrypig.autodi.Injector(testQualifier)
-        assertThat(cartRepository::class.isSubclassOf(CartRepositoryImpl::class)).isTrue()
+        val fakeRepository: FakeRepository by Injector(testQualifier)
+        assertThat(fakeRepository::class.isSubclassOf(FakeRepositoryImpl::class)).isTrue()
     }
 
     @Test
     fun `disposable 객체를 Container에 등록한다(qualifier 받지않는 상태)`() {
         // when
         val testQualifier = "test"
-        com.angrypig.autodi.AutoDI {
-            disposable<CartRepository>(testQualifier) { CartRepositoryImpl() }
+        AutoDI {
+            registerModule(autoDIModule { disposable<FakeRepository>(testQualifier) { FakeRepositoryImpl() } })
         }
 
         // then
-        val cartRepository: CartRepository by com.angrypig.autodi.Injector()
-        assertThat(cartRepository::class.isSubclassOf(CartRepositoryImpl::class)).isTrue()
+        val fakeRepository: FakeRepository by Injector()
+        assertThat(fakeRepository::class.isSubclassOf(FakeRepositoryImpl::class)).isTrue()
     }
 
     @Test
     fun `disposable 객체를 Container에 등록한다(qualifier 받는 상태)`() {
         // when
         val testQualifier = "test"
-        com.angrypig.autodi.AutoDI {
-            disposable<CartRepository>(testQualifier) { CartRepositoryImpl() }
+        AutoDI {
+            registerModule(autoDIModule { disposable<FakeRepository>(testQualifier) { FakeRepositoryImpl() } })
         }
 
         // then
-        val cartRepository: CartRepository by com.angrypig.autodi.Injector(testQualifier)
-        assertThat(cartRepository::class.isSubclassOf(CartRepositoryImpl::class)).isTrue()
+        val cartRepository: FakeRepository by Injector(testQualifier)
+        assertThat(cartRepository::class.isSubclassOf(FakeRepositoryImpl::class)).isTrue()
     }
 
     @Test
     fun `String값 "멧돼지"를 주입받는 Pig 객체를 inject함수를 통해 주입받는다`() {
         // given
         class Pig(val name: String)
-        com.angrypig.autodi.AutoDI {
-            disposable { Pig(inject("pigName")) }
-            disposable("pigName") { "멧돼지" }
+        AutoDI {
+            registerModule(
+                autoDIModule {
+                    disposable { Pig(inject("pigName")) }
+                    disposable("pigName") { "멧돼지" }
+                },
+            )
         }
 
         // when
-        val injectedInstance: Pig by com.angrypig.autodi.Injector()
+        val injectedInstance: Pig by Injector()
 
         // then
         val pigName = "멧돼지"
-        Truth.assertThat(injectedInstance.name).isEqualTo(pigName)
+        assertThat(injectedInstance.name).isEqualTo(pigName)
     }
 }
