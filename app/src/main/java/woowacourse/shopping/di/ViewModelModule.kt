7f214e271c06cde84fc6342671c0fae446cc5257
changhwan <chang5hustle@gmail.com>
2023-09-12 01:36:18 +0900

    [feat] CartViewModel 등록

--- app/src/main/java/woowacourse/shopping/di/ViewModelModule.kt
+++ app/src/main/java/woowacourse/shopping/di/ViewModelModule.kt
@@ -2,7 +2,9 @@ package woowacourse.shopping.di
 
 import com.angrypig.autodi.autoDIModule.autoDIModule
 import woowacourse.shopping.ui.MainViewModel
+import woowacourse.shopping.ui.cart.CartViewModel
 
 val viewModelModule = autoDIModule {
     viewModel { MainViewModel(inject(), inject()) }
+    viewModel { CartViewModel(inject()) }
 }
