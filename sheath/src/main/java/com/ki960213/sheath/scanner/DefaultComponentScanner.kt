f6066389b8a46acbf3917cd1287f28cf30f1aeb8
ki960213 <ki960213@gmail.com>
2023-09-13 17:04:29 +0900

    feat(DefaultComponentScanner): @Component 뿐만 아니라 @Module로 컴포넌트를 스캔하는 기능 구현

--- sheath/src/main/java/com/ki960213/sheath/scanner/DefaultComponentScanner.kt
+++ sheath/src/main/java/com/ki960213/sheath/scanner/DefaultComponentScanner.kt
@@ -5,10 +5,12 @@ import com.ki960213.sheath.annotation.Component
 import com.ki960213.sheath.annotation.Module
 import com.ki960213.sheath.component.SheathComponent
 import com.ki960213.sheath.component.SheathComponentByClass
+import com.ki960213.sheath.component.SheathComponentByFunction
 import dalvik.system.DexFile
 import dalvik.system.PathClassLoader
 import kotlin.reflect.KClass
 import kotlin.reflect.full.functions
+import kotlin.reflect.full.hasAnnotation
 
 class DefaultComponentScanner(private val context: Context) : ComponentScanner {
     override fun findAll(): List<SheathComponent> {
@@ -38,19 +40,13 @@ class DefaultComponentScanner(private val context: Context) : ComponentScanner {
         this.startsWith(context.packageName) && !this.contains("$")
 
     private fun Class<*>.isComponent(): Boolean =
-        this.annotations.any { it.annotationClass == Component::class || it.hasAnnotation(Component::class) }
+        this.annotations.any { it.annotationClass == Component::class || it.annotationClass.hasAnnotation<Component>() }
 
     private fun Class<*>.isModule(): Boolean =
         this.annotations.any { annotation -> annotation.annotationClass == Module::class }
 
-    private fun extractSheathComponent(clazz: KClass<*>): List<SheathComponent> {
-        clazz.functions.map { function ->
-            val returnTypeClass = function.returnType.classifier as KClass<*>
-            returnTypeClass.java
+    private fun extractSheathComponent(clazz: KClass<*>): List<SheathComponent> =
+        clazz.functions.mapNotNull { function ->
+            if (function.hasAnnotation<Component>()) SheathComponentByFunction(function) else null
         }
-        return listOf()
-    }
-
-    private fun Annotation.hasAnnotation(annotationClass: KClass<*>): Boolean =
-        this.annotationClass.annotations.any { it.annotationClass == annotationClass }
 }
