fd38add94bb77cbba4515b8a3f27acb4bbb43f55
krrong <khk9664@naver.com>
2023-09-14 02:47:26 +0900

    refactor: Container에서 인스턴스를 관리하는 방법 변경

--- Di/src/main/java/com/now/di/Container.kt
+++ Di/src/main/java/com/now/di/Container.kt
@@ -1,46 +1,19 @@
 package com.now.di
 
-import com.now.annotation.Qualifier
 import kotlin.reflect.KClass
-import kotlin.reflect.full.findAnnotation
-import kotlin.reflect.full.hasAnnotation
 
 object Container {
     private val dependency = mutableMapOf<DependencyType, Any>()
-
-    private val nonAnnotationMap = mutableMapOf<KClass<*>, Any>()
-
-    private val annotationMap = mutableMapOf<String, Any>()
-
-    const val defaultQualifier = ""
 
     fun getInstance(dependencyType: DependencyType): Any? {
         return dependency[dependencyType]
     }
 
-    fun getInstance(type: KClass<*>): Any? {
-        return nonAnnotationMap[type]
-    }
-
-    fun addInstance(type: KClass<*>, instance: Any) {
-        // 어노테이션이 있으면 Annotation맵에 저장
-        if (instance::class.hasAnnotation<Qualifier>()) {
-            val qualifier = instance::class.findAnnotation<Qualifier>()
-
-            // getInstance를 가능하게 하기 위해 Qualifier가 있는 경우 Default 값 저장
-            nonAnnotationMap[type] = defaultQualifier
-        } else {
-            // 어노테이션이 없으면 nonAnnotationMap에 저장
-            nonAnnotationMap[type] = instance
-        }
-    }
-
-    fun addInstance2(type: KClass<*>, instance: Any, annotation: Annotation?) {
+    fun addInstance(type: KClass<*>, instance: Any, annotation: Annotation?) {
         dependency[DependencyType(type, annotation)] = instance
     }
 
     fun clear() {
-        annotationMap.clear()
-        nonAnnotationMap.clear()
+        dependency.clear()
     }
 }
