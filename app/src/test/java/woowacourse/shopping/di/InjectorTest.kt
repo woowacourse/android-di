12323cc2652d9cb87ce430da05d5883f591c5076
pingu244 <pingu244@naver.com>
2023-09-13 15:49:40 +0900

    [refactor] 구조 변경에 따른 test 수정

--- app/src/test/java/woowacourse/shopping/di/InjectorTest.kt
+++ app/src/test/java/woowacourse/shopping/di/InjectorTest.kt
@@ -9,7 +9,6 @@ class InjectorTest {
 
     interface FakeViewModel2
     interface FakeRepository2
-    class DefaultFakeRepository2 : FakeRepository2
     class FakeViewModel3(val repository: FakeRepository2) : ViewModel()
 
     @Test
@@ -23,19 +22,19 @@ class InjectorTest {
         assertEquals(Fake.FakeViewModel::class, actual::class)
     }
 
-//    @Test(expected = IllegalStateException::class)
-//    fun `주어진 클래스 타입의 주생성자를 가져올 수 없다면 오류가 발생한다`() {
-//        val injector = Injector(Container)
-//        injector.createInstance(FakeViewModel3::class)
-//    }
-//
-//    @Test(expected = IllegalArgumentException::class)
-//    fun `container에 없는 타입을 요구할 경우 오류가 발생한다`() {
-//        // given
-//        Container.addInstance(Fake.DefaultFakeRepository::class, null)
-//        val injector = Injector(Container)
-//
-//        // when
-//        injector.createInstance(FakeViewModel3::class)
-//    }
+    @Test(expected = IllegalStateException::class)
+    fun `주어진 클래스 타입의 주생성자를 가져올 수 없다면 오류가 발생한다`() {
+        val injector = Injector(Container)
+        injector.createInstance(FakeViewModel2::class)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `container에 없는 타입을 요구할 경우 오류가 발생한다`() {
+        // given
+        Container.addInstance(Fake.DefaultFakeRepository::class, null)
+        val injector = Injector(Container)
+
+        // when
+        injector.createInstance(FakeViewModel3::class)
+    }
 }
