353d48b2872d5d095517dbc558c33ed3b0ad4509
hyemdooly <hyemdooly@gmail.com>
2023-09-13 16:46:55 +0900

    refactor: implementation 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
     implementation(project(":domain"))
 //    implementation("com.github.hyemdooly:android-di:v2.0.0")
     implementation(project(":di"))
-
+    implementation(project(mapOf("path" to ":di")))
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
