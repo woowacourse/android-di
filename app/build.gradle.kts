29fb4a3e6f495624f9629373479da96617852311
EmilyCh0 <emily9803@ewhain.net>
2023-09-13 13:44:23 +0900

    chore: 모듈 이름 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ android {
 
 dependencies {
     implementation(project(":domain"))
-    implementation(project(":hashdi"))
+    implementation(project(":hasydi"))
 
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
