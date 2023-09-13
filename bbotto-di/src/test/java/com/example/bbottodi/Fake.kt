07ca9bb3c5306dc31155ec8334ab4afaeaa2d367
hyunji1203 <hj27720@gmail.com>
2023-09-13 15:32:55 +0900

    test: Inject 테스트 작성

---
+++ bbotto-di/src/test/java/com/example/bbottodi/Fake.kt
@@ -0,0 +1,58 @@
+package com.example.bbottodi
+
+import com.example.bbottodi.di.annotation.InDisk
+import com.example.bbottodi.di.annotation.InMemory
+import com.example.bbottodi.di.annotation.Inject
+
+class FakeViewModelWithInjectOnSuccess(
+    @Inject
+    val productRepository: FakeProductRepository,
+)
+
+class FakeViewModelWithInjectOnFailure(
+    val productRepository: FakeProductRepository,
+)
+
+class FakeViewModelWithFieldInjectOnSuccess() {
+    @Inject
+    private lateinit var productRepository: FakeProductRepository
+}
+
+class FakeViewModelWithRecursiveInject(
+    @Inject
+    productRepository: FakeProductRepository,
+    @Inject
+    cartRepository: FakeInDiskCartRepository,
+)
+
+class FakeViewModelWithQualifier(
+    @Inject
+    productRepository: FakeProductRepository,
+    @Inject
+    @InDisk
+    cartRepository: FakeCartRepository,
+)
+
+class FakeViewModelWithQualifierFieldInject(
+    @Inject
+    productRepository: FakeProductRepository,
+) {
+    @Inject
+    @InDisk
+    private lateinit var cartRepository: FakeCartRepository
+}
+
+class FakeProductRepository()
+interface FakeCartRepository
+class FakeDefaultCartRepository() : FakeCartRepository
+
+@InMemory
+class FakeInMemoryCartRepository() : FakeCartRepository
+
+@InDisk
+class FakeInDiskCartRepository(
+    @Inject
+    private val cartDao: FakeCartDao,
+) : FakeCartRepository
+
+class FakeCartDao
---
+++ bbotto-di/src/test/java/com/example/bbottodi/InjectTest.kt
@@ -0,0 +1,85 @@
+package com.example.bbottodi
+
+import com.example.bbottodi.di.Container
+import com.example.bbottodi.di.inject.AutoDependencyInjector.inject
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+class InjectTest {
+
+    @Before
+    fun setUp() {
+        Container.addInstance(FakeCartDao::class, FakeCartDao())
+        Container.addInstance(FakeProductRepository::class, inject(FakeProductRepository::class))
+        Container.addInstance(FakeCartRepository::class, inject(FakeDefaultCartRepository::class))
+        Container.addInstance(FakeCartRepository::class, inject(FakeInMemoryCartRepository::class))
+        Container.addInstance(FakeCartRepository::class, inject(FakeInDiskCartRepository::class))
+    }
+
+    @Test
+    fun `inject 어노테이션 있을 때 의존성 주입 후 인스턴스 생성 성공`() {
+        // when
+        val viewModel = inject<FakeViewModelWithInjectOnSuccess>(FakeViewModelWithInjectOnSuccess::class)
+
+        // then
+        val expect = FakeViewModelWithInjectOnSuccess::class
+        val actual = viewModel::class
+        assertEquals(expect, actual)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `inject 어노테이션 없을 때 의존성 주입 후 인스턴스 생성 실패`() {
+        // when
+        val viewModel = inject<FakeViewModelWithInjectOnFailure>(FakeViewModelWithInjectOnFailure::class)
+
+        // then
+        val expect = FakeViewModelWithInjectOnFailure::class
+        val actual = viewModel::class
+        assertEquals(expect, actual)
+    }
+
+    @Test
+    fun `@inject 가 붙은 필드의 의존성 주입 성공`() {
+        // when
+        val viewModel = inject<FakeViewModelWithFieldInjectOnSuccess>(FakeViewModelWithFieldInjectOnSuccess::class)
+
+        // then
+        val expect = FakeViewModelWithFieldInjectOnSuccess::class
+        val actual = viewModel::class
+        assertEquals(expect, actual)
+    }
+
+    @Test
+    fun `재귀 의존성 주입 성공`() {
+        // when
+        val viewModel = inject<FakeViewModelWithRecursiveInject>(FakeViewModelWithRecursiveInject::class)
+
+        // then
+        val expect = FakeViewModelWithRecursiveInject::class
+        val actual = viewModel::class
+        assertEquals(expect, actual)
+    }
+
+    @Test
+    fun `Qualifier로 @InDisk 구분하여 의존성 주입 성공`() {
+        // when
+        val viewModel = inject<FakeViewModelWithQualifier>(FakeViewModelWithQualifier::class)
+
+        // then
+        val expect = FakeViewModelWithQualifier::class
+        val actual = viewModel::class
+        assertEquals(expect, actual)
+    }
+
+    @Test
+    fun `Qualifier로 @InDisk 구분하여 필드에 의존성 주입 성공`() {
+        // when
+        val viewModel = inject<FakeViewModelWithQualifierFieldInject>(FakeViewModelWithQualifierFieldInject::class)
+
+        // then
+        val expect = FakeViewModelWithQualifierFieldInject::class
+        val actual = viewModel::class
+        assertEquals(expect, actual)
+    }
+}
