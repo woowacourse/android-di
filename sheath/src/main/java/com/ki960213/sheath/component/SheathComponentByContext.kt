8b05871cd9963121da5e6a40322fc2a828204c85
ki960213 <ki960213@gmail.com>
2023-09-13 16:15:05 +0900

    feat(SheathComponentByContext): 컨텍스트로 SheathComponent를 생성하는 기능 구현

---
+++ sheath/src/main/java/com/ki960213/sheath/component/SheathComponentByContext.kt
@@ -0,0 +1,15 @@
+package com.ki960213.sheath.component
+
+import android.content.Context
+import kotlin.reflect.KClass
+
+class SheathComponentByContext(private val context: Context) : SheathComponent() {
+    override val clazz: KClass<*> = Context::class
+    override val name: String = Context::class.qualifiedName!!
+    override val isSingleton: Boolean = true
+    override val dependentCount: Int = 0
+
+    override fun isDependingOn(component: SheathComponent): Boolean = false
+
+    override fun instantiated(instances: List<Any>): Any = context
+}
