5df66e63923b66f14e1391f10ab0e34f9bc447e7
Sujin Kim <sujinkim919@gmail.com>
2023-09-13 13:44:09 +0900

    feat: di module 분리

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -6,6 +6,7 @@ java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
 }
+
 dependencies {
     implementation("javax.inject", "javax.inject", "1")
     implementation("org.jetbrains.kotlin", "kotlin-reflect", "1.8.21")
