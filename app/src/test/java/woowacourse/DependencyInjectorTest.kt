c28d68fa69803cec01bfa3d30eccf8616f91001f
buna <tmdgh1592@naver.com>
2023-09-13 15:34:42 +0900

    test: DependencyInjector 코드 변경에 따른 테스트 코드 수정

--- app/src/test/java/woowacourse/DependencyInjectorTest.kt
+++ app/src/test/java/woowacourse/DependencyInjectorTest.kt
@@ -1,11 +1,6 @@
 package woowacourse
 
-import woowacourse.fakeClasses.AConstructorDependency
-import woowacourse.fakeClasses.AFieldDependency
-import woowacourse.fakeClasses.ConstructorDependency
-import woowacourse.fakeClasses.ConstructorTestActivity
-import woowacourse.fakeClasses.FieldDependency
-import woowacourse.fakeClasses.FieldTestActivity
+import com.woowacourse.bunadi.injector.DependencyInjector
 import junit.framework.TestCase.assertFalse
 import junit.framework.TestCase.assertNotNull
 import junit.framework.TestCase.assertTrue
@@ -14,23 +9,22 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
+import woowacourse.fakeClasses.AConstructorDependency
+import woowacourse.fakeClasses.AFieldDependency
+import woowacourse.fakeClasses.ConstructorTestActivity
+import woowacourse.fakeClasses.FieldTestActivity
 
 @RunWith(RobolectricTestRunner::class)
 class DependencyInjectorTest {
 
     @Before
     fun setup() {
-        com.woowacourse.bunadi.injector.DependencyInjector.clear()
+        DependencyInjector.clear()
     }
 
     @Test
     fun 뷰모델의_생성자_종속_항목을_자동_주입해준다() {
         // given: 뷰모델 생성에 필요한 종속 항목을 주입한다.
-        com.woowacourse.bunadi.dsl.modules {
-            com.woowacourse.bunadi.dsl.types {
-                type(ConstructorDependency::class to AConstructorDependency::class)
-            }
-        }
 
         // when: 액티비티를 생성한다.
         val activity = Robolectric.buildActivity(ConstructorTestActivity::class.java)
@@ -47,11 +41,6 @@ class DependencyInjectorTest {
     @Test
     fun 뷰모델에서_Inject_애노테이션이_붙은_필드에_종속_항목을_자동_주입해준다() {
         // given: 뷰모델 생성과 필드에 필요한 종속 항목을 주입한다.
-        com.woowacourse.bunadi.dsl.modules {
-            com.woowacourse.bunadi.dsl.types {
-                type(FieldDependency::class to AFieldDependency::class)
-            }
-        }
 
         // when: 액티비티를 생성한다.
         val activity = Robolectric.buildActivity(FieldTestActivity::class.java)
@@ -68,11 +57,6 @@ class DependencyInjectorTest {
     @Test
     fun 뷰모델에서_Inject_애노테이션이_없는_필드에는_종속_항목을_주입하지_않는다() {
         // given: 뷰모델 생성과 필드에 필요한 종속 항목을 주입한다.
-        com.woowacourse.bunadi.dsl.modules {
-            com.woowacourse.bunadi.dsl.types {
-                type(FieldDependency::class to AFieldDependency::class)
-            }
-        }
 
         // when: 액티비티를 생성한다.
         val activity = Robolectric.buildActivity(FieldTestActivity::class.java)
