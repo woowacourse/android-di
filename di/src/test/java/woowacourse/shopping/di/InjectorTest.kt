95a4554e58ada603730f27bf5e54466aba58299f
inseonyun <yuninseon@naver.com>
2023-09-14 19:38:19 +0900

    refactor: 테스트 코드 수정

--- di/src/test/java/woowacourse/shopping/di/InjectorTest.kt
+++ di/src/test/java/woowacourse/shopping/di/InjectorTest.kt
@@ -8,26 +8,33 @@ import org.junit.Test
 
 interface FakeInterface
 
+// 주 생성자에 주입할 객체가 없는 객체
 class FakeFirst
 
+// 추상화된 객체 A
 class FakeImplOfInterface : FakeInterface
 
+// 추상화된 객체 B (동일한 인터페이스를 가질 때 Qualifier로 구분)
 class FakeImplOfInterface2 : FakeInterface
 
+// 추상화된 객체를 생성자 주입 받는 객체
 class FakeOnceConstructor(
     fakeInterface: FakeInterface,
 )
 
+// 추상화된 객체가 여러 개일 경우 Qualifier로 명시하여 생성자 주입 받는 객체
 class FakeOnceConstructorQualifier(
     @Qualifier(FakeImplOfInterface2::class)
     val fakeInterface: FakeInterface,
 )
 
+// 주입이 필요 없는 객체와 추상화된 객체를 생성자 주입 받는 객체(재귀) 두 개를 주입 받는 객체
 class FakeHaveTwiceConstructor(
     fakeFirst: FakeFirst,
     fakeOnceConstructor: FakeOnceConstructor,
 )
 
+// 추상화된 객체를 생성자 주입 받는 객체와 필드 주입 두 개를 받는 객체
 class FakeHaveFieldAndConstructor(
     fakeOnceConstructor: FakeOnceConstructor,
 ) {
@@ -74,11 +81,10 @@ class InjectorTest {
 
     @Test
     fun `주 생성자가 없으면 NullPointException이 발생한다`() {
-        assertThrows(NullPointerException::class.java) {
+        val actualException = assertThrows(NullPointerException::class.java) {
             injector.inject(FakeInterface::class.java)
-        }.also {
-            assertEquals("[ERROR] 주 생성자가 없습니다.", it.message)
         }
+        assertEquals("[ERROR] 주 생성자가 없습니다.", actualException.message)
     }
 
     @Test
@@ -101,10 +107,14 @@ class InjectorTest {
 
     @Test
     fun `추상화 된 객체를 @Qualifier 어노테이션으로 구분하여 FakeImplOfInterface2의 의존성을 주입한다`() {
+        // given
         // 어노테이션 Qualifier 테스트를 위한 모듈 적용
         injector = Injector(FakeQualifierModule())
 
+        // when
         val instance = injector.inject(FakeOnceConstructorQualifier::class.java)
+
+        // then
         assertThat(instance).isInstanceOf(FakeOnceConstructorQualifier::class.java)
         // 실제로 주입된 객체가 어노테이션으로 지정한 FakeImplOfInterface2 클래스가 맞는지 검증
         assertThat(instance.fakeInterface).isInstanceOf(FakeImplOfInterface2::class.java)
