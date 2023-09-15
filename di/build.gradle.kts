288c909207f804d0d351e11c091b12ea62895dad
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-09-15 17:13:41 +0900

    chore: di 모듈에서 domain 의존성 제거

--- di/build.gradle.kts
+++ di/build.gradle.kts
@@ -8,11 +8,9 @@ java {
     targetCompatibility = JavaVersion.VERSION_11
 }
 dependencies {
-    implementation(project(":domain"))
     implementation("javax.inject", "javax.inject", "1")
     implementation("org.jetbrains.kotlin", "kotlin-reflect", "1.8.21")
     testImplementation("org.junit.jupiter", "junit-jupiter", "5.8.2")
     testImplementation("org.assertj", "assertj-core", "3.22.0")
     testImplementation("junit:junit:4.13.2")
-    testImplementation(project(mapOf("path" to ":domain")))
 }
