809fdee55f367af2c0016d0b60b79459551bfef5
Bandal <no1msh1217@gmail.com>
2023-09-15 17:27:22 +0900

    test: ViewModelFactoryTest 수정

--- app/src/test/java/woowacourse/shopping/ViewModelFactoryTest.kt
+++ app/src/test/java/woowacourse/shopping/ViewModelFactoryTest.kt
@@ -3,10 +3,10 @@ package woowacourse.shopping
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.bandal.di.AppContainer
 import com.bandal.di.BandalInject
+import com.bandal.di.BandalInjectorAppContainer
+import com.bandal.di.DIError
 import com.bandal.di.Database
-import com.bandal.di.Qualifier
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertNotNull
 import org.junit.Before
@@ -15,7 +15,6 @@ import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
 import woowacourse.shopping.common.CommonViewModelFactory
-import kotlin.reflect.KClass
 
 interface FakeRepository
 
@@ -30,33 +29,6 @@ class FakeViewModelWithField : ViewModel() {
     @Database
     @BandalInject
     lateinit var repository: FakeRepository
-}
-
-object FakeAppContainer : AppContainer {
-    private val instances: HashMap<Pair<KClass<*>, List<Annotation>>, Any> = HashMap()
-
-    override fun getInstance(type: KClass<*>, annotations: List<Annotation>): Any? {
-        return instances.keys.firstOrNull { (clazz, annotationList) ->
-            clazz == type && annotationList.containsAll(annotations)
-        }?.let { instances[it] }
-    }
-
-    override fun addInstance(type: KClass<*>, instance: Any) {
-        val annotationWithQualifier = instance::class.annotations.filter {
-            it.annotationClass.java.isAnnotationPresent(
-                Qualifier::class.java,
-            )
-        }
-        val key = Pair(
-            type,
-            annotationWithQualifier,
-        )
-        instances[key] = instance
-    }
-
-    override fun clear() {
-        instances.clear()
-    }
 }
 
 class FakeActivityWithConstructor : AppCompatActivity() {
@@ -82,14 +54,14 @@ class ViewModelFactoryTest {
 
     @Before
     fun setUp() {
-        FakeAppContainer.clear()
+        BandalInjectorAppContainer.clear()
     }
 
     @Test
     fun `적절한 객체 인스턴스가 존재하면 ViewModel 의존성 주입이 성공한다`() {
         // given
         val defaultFakeRepository = DefaultFakeRepository()
-        FakeAppContainer.addInstance(
+        BandalInjectorAppContainer.addInstance(
             type = FakeRepository::class,
             instance = defaultFakeRepository,
         )
@@ -110,7 +82,7 @@ class ViewModelFactoryTest {
     fun `ViewModel 필드에 의존성을 주입할 수 있다`() {
         // given
         val defaultFakeRepository = DefaultFakeRepository()
-        FakeAppContainer.addInstance(
+        BandalInjectorAppContainer.addInstance(
             type = FakeRepository::class,
             instance = defaultFakeRepository,
         )
@@ -128,7 +100,7 @@ class ViewModelFactoryTest {
         assertEquals(viewModel?.repository, defaultFakeRepository)
     }
 
-    @Test(expected = com.bandal.di.DIError.NotFoundInstanceForInject::class)
+    @Test(expected = DIError.NotFoundQualifierOrInstance::class)
     fun `적절한 객체 인스턴스가 존재하지 않으면 ViewModel 의존성 주입에 실패한다`() {
         val activity = Robolectric
             .buildActivity(FakeActivityWithConstructor::class.java)
