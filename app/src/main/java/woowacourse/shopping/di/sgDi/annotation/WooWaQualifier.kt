91c9b48f0a12c095ad094d570bce372d2c70c0dc
SeHun <81347125+s9hn@users.noreply.github.com>
2023-09-15 14:41:40 +0900

    feat: InMemoryCartRepository 추가

---
+++ app/src/main/java/woowacourse/shopping/di/sgDi/annotation/WooWaQualifier.kt
@@ -0,0 +1,15 @@
+package woowacourse.shopping.di.sgDi.annotation
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(
+    AnnotationTarget.CONSTRUCTOR,
+    AnnotationTarget.CLASS,
+    AnnotationTarget.PROPERTY,
+    AnnotationTarget.VALUE_PARAMETER,
+)
+annotation class WooWaQualifier(val type: String) {
+    companion object {
+        const val DATABASE = "DATABASE"
+        const val IN_MEMORY = "IN_MEMORY"
+    }
+}
