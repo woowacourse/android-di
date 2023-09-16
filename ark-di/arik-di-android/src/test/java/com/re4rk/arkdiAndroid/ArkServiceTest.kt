c2b44ce681ea9eb3657a22697a93083f5e8f322d
re4rk <37167652+re4rk@users.noreply.github.com>
2023-09-16 23:37:06 +0900

    feat: ArkService 추가 및 테스트코드 케이스 추가

--- ark-di/arik-di-android/src/test/java/com/re4rk/arkdiAndroid/ArkServiceTest.kt
+++ ark-di/arik-di-android/src/test/java/com/re4rk/arkdiAndroid/ArkServiceTest.kt
@@ -2,11 +2,7 @@ package com.re4rk.arkdiAndroid
 
 import androidx.test.rule.ServiceTestRule
 import com.re4rk.arkdi.ArkInject
-import com.re4rk.arkdiAndroid.fakeDependency.FakeActivityDependency
-import com.re4rk.arkdiAndroid.fakeDependency.FakeApplicationDependency
-import com.re4rk.arkdiAndroid.fakeDependency.FakeRetainedActivityDependency
 import com.re4rk.arkdiAndroid.fakeDependency.FakeServiceDependency
-import com.re4rk.arkdiAndroid.fakeDependency.FakeViewModelDependency
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Rule
@@ -32,58 +28,20 @@ class ArkServiceTest {
     }
 
     @Test
-    fun `ArkService 클래스의 필드 주입은 @ArkInject 어노테이션을 사용한다`() {
+    fun `ArkService 클래스는 @ArkInject 어노테이션이 있는 필드에는 주입이 된다`() {
         // when
         `서비스를 생성했을 때`().apply {
             // then
-            `어플리케이션 의존성이 주입이 된다`()
-            `서비스 의존성이 주입이 된다`()
+            `@ArkInject 어노테이션이 있는 필드에는 주입이 된다`()
+        }
+    }
+
+    @Test
+    fun `ArkService 클래스는 @ArkInject 어노테이션이 없는 필드에는 주입이 안된다`() {
+        // when
+        `서비스를 생성했을 때`().apply {
+            // then
             `@ArkInject 어노테이션이 없는 필드에는 주입이 안된다`()
-        }
-    }
-
-    @Test
-    fun `ArkService 클래스는 ApplicationModule을 사용할 수 있다`() {
-        // when
-        `서비스를 생성했을 때`().apply {
-            // then
-            `어플리케이션 의존성이 주입이 된다`()
-        }
-    }
-
-    @Test
-    fun `ArkServie 클래스는 ServiceModule을 사용할 수 있다`() {
-        // when
-        `서비스를 생성했을 때`().apply {
-            // then
-            `서비스 의존성이 주입이 된다`()
-        }
-    }
-
-    @Test
-    fun `ArkService 클래스는 RetainedActivityModule을 모듈을 사용할 수 없다`() {
-        // when
-        `서비스를 생성했을 때`().apply {
-            // then
-            `리테인드 액티비티 의존성이 주입이 안된다`()
-        }
-    }
-
-    @Test
-    fun `ArkService 클래스는 ActivityModule을 사용할 수 없다`() {
-        // when
-        `서비스를 생성했을 때`().apply {
-            // then
-            `액티비티 의존성이 주입이 안된다`()
-        }
-    }
-
-    @Test
-    fun `ArkService 클래스는 ViewModelModule을 사용할 수 없다`() {
-        // when
-        `서비스를 생성했을 때`().apply {
-            // then
-            `뷰모델 의존성이 주입이 안된다`()
         }
     }
 
@@ -92,44 +50,17 @@ class ArkServiceTest {
     }
 
     class FakeService : ArkService() {
-        @ArkInject
-        lateinit var fakeApplicationDependency: FakeApplicationDependency
-
-        @ArkInject
-        lateinit var fakeServiceDependency: FakeServiceDependency
         lateinit var fakeServiceDependencyNoInject: FakeServiceDependency
 
         @ArkInject
-        lateinit var fakeRetainedActivityDependency: FakeRetainedActivityDependency
+        lateinit var fakeServiceDependency: FakeServiceDependency
 
-        @ArkInject
-        lateinit var fakeActivityDependency: FakeActivityDependency
-
-        @ArkInject
-        lateinit var fakeViewModelDependency: FakeViewModelDependency
-
-        fun `어플리케이션 의존성이 주입이 된다`() {
-            assertThat(::fakeApplicationDependency.isInitialized).isTrue
-        }
-
-        fun `서비스 의존성이 주입이 된다`() {
+        fun `@ArkInject 어노테이션이 있는 필드에는 주입이 된다`() {
             assertThat(::fakeServiceDependency.isInitialized).isTrue
         }
 
         fun `@ArkInject 어노테이션이 없는 필드에는 주입이 안된다`() {
             assertThat(::fakeServiceDependencyNoInject.isInitialized).isFalse
-        }
-
-        fun `리테인드 액티비티 의존성이 주입이 안된다`() {
-            assertThat(::fakeRetainedActivityDependency.isInitialized).isFalse
-        }
-
-        fun `액티비티 의존성이 주입이 안된다`() {
-            assertThat(::fakeActivityDependency.isInitialized).isFalse
-        }
-
-        fun `뷰모델 의존성이 주입이 안된다`() {
-            assertThat(::fakeViewModelDependency.isInitialized).isFalse
         }
     }
 }
