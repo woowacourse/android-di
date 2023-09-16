2635459fa858849aa7cdf44745472870541a9167
vrexpert <vrexpert2302@gmail.com>
2023-09-16 21:45:27 +0900

    refactor: Qualifier Target 축소

--- berdi/src/main/java/com/di/berdi/annotation/Qualifier.kt
+++ berdi/src/main/java/com/di/berdi/annotation/Qualifier.kt
@@ -1,5 +1,5 @@
 package com.di.berdi.annotation
 
-@Target(AnnotationTarget.ANNOTATION_CLASS, AnnotationTarget.CLASS, AnnotationTarget.TYPE)
+@Target(AnnotationTarget.ANNOTATION_CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class Qualifier
