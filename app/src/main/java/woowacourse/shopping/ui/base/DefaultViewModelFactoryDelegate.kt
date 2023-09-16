c4a21901d64cd6695c00fcb937446f3226f64f8c
briandr97 <briandr@naver.com>
2023-09-17 04:14:51 +0900

    refactor: 쓸데없는 인자 삭제

--- app/src/main/java/woowacourse/shopping/ui/base/DefaultViewModelFactoryDelegate.kt
+++ app/src/main/java/woowacourse/shopping/ui/base/DefaultViewModelFactoryDelegate.kt
@@ -2,12 +2,11 @@ package woowacourse.shopping.ui.base
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewmodel.CreationExtras
 import woowacourse.shopping.Injector
 
 class DefaultViewModelFactoryDelegate : ViewModelFactoryDelegate {
     override val Factory = object : ViewModelProvider.Factory {
-        override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
             return Injector.inject(modelClass.kotlin)
         }
     }
