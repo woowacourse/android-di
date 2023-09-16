5a784e4d2a18d1706c7bead42a821cd43586715d
buna <tmdgh1592@naver.com>
2023-09-16 22:38:34 +0900

    test: NonConfigurationActivityInjectorManager 테스트 코드 작성

---
+++ androiddi/src/test/java/com/mission/androiddi/NonConfigurationActivityInjectorManagerTest.kt
@@ -0,0 +1,106 @@
+package com.mission.androiddi
+
+import com.mission.androiddi.component.activity.InjectableActivity
+import com.mission.androiddi.component.activity.NonConfigurationActivityInjectorManager
+import com.mission.androiddi.component.application.InjectableApplication
+import com.woowacourse.bunadi.injector.Injector
+import io.mockk.every
+import io.mockk.mockk
+import junit.framework.TestCase.assertFalse
+import org.junit.Test
+
+class NonConfigurationActivityInjectorManagerTest {
+
+    @Test
+    fun 인젝터를_액티비티_인젝터_매니저에_저장한다() {
+        // given: Injector와 매니저가 주어졌을 때
+        val injectors = HashMap<String, Injector>()
+        val injector = mockk<Injector>()
+        val sut = NonConfigurationActivityInjectorManager(injectors)
+
+        // when: Injector를 매니저에 저장하면
+        sut.saveInjector("KEY", injector)
+
+        // then: 매니저에 Injector가 저장된다.
+        assert(injectors["KEY"] == injector)
+    }
+
+    @Test
+    fun 인젝터를_액티비티_인젝터_매니저에서_제거한다() {
+        // given: 인젝터와 매니저가 주어졌을 때
+        val injectorKey = "KEY"
+        val injector = mockk<Injector>()
+
+        val injectors = HashMap<String, Injector>().apply {
+            put(injectorKey, injector)
+        }
+        val sut = NonConfigurationActivityInjectorManager(injectors)
+
+        // when: Injector를 매니저에서 제거하면
+        sut.removeInjector(injectorKey)
+
+        // then: 매니저에서 Injector가 제거된다.
+        assert(injectors[injectorKey] == null)
+    }
+
+    @Test
+    fun 잘못된_키로_인젝터를_제거하면_액티비티_인젝터_매니저에서_인젝터가_제거되지_않는다() {
+        // given: 인젝터와 매니저가 주어졌을 때
+        val injectorKey = "KEY"
+        val injector = mockk<Injector>()
+
+        val injectors = HashMap<String, Injector>().apply {
+            put(injectorKey, injector)
+        }
+        val sut = NonConfigurationActivityInjectorManager(injectors)
+
+        // when: 잘못된 키로 인젝터를 제거하면
+        val invalidKey = "INVALID_KEY"
+        sut.removeInjector(invalidKey)
+
+        // then: 매니저에서 인젝터가 제거되지 않는다.
+        assert(injectors[injectorKey] == injector)
+    }
+
+    @Test
+    fun 저장된_인젝터_키가_없으면_액티비티_인젝터_매니저에_새로운_인젝터를_반환한다() {
+        // given: 인젝터와 매니저가 주어졌을 때
+        val injectorKey = "KEY"
+        val injector = mockk<Injector>()
+        val injectors = HashMap<String, Injector>().apply {
+            put(injectorKey, injector)
+        }
+        val sut = NonConfigurationActivityInjectorManager(injectors)
+        // given:
+        val injectableActivity = mockk<InjectableActivity>()
+        every { injectableActivity.application } returns InjectableApplication()
+
+        // when: 저장되지 않은 키로 인젝터를 가져오면
+        val invalidKey = "INVALID_KEY"
+        val actual = sut.getInjector(injectableActivity, invalidKey)
+
+        // then: 새로운 인젝터를 반환한다.
+        assertFalse(injector === actual)
+    }
+
+    @Test
+    fun 저장된_인젝터_키가_있으면_액티비티_인젝터_매니저에_저장된_인젝터를_반환한다() {
+        // given: 인젝터와 매니저가 주어졌을 때
+        val injectorKey = "KEY"
+        val injector = mockk<Injector>()
+        val injectors = HashMap<String, Injector>().apply {
+            put(injectorKey, injector)
+        }
+        val sut = NonConfigurationActivityInjectorManager(injectors)
+
+        // given: 저장된 키로 인젝터를 가져오면
+        val injectableActivity = mockk<InjectableActivity>()
+        every { injectableActivity.application } returns InjectableApplication()
+
+        // when: 저장된 키로 인젝터를 가져오면
+        val actual = sut.getInjector(injectableActivity, injectorKey)
+
+        // then: 저장된 인젝터를 반환한다.
+        assert(injector === actual)
+    }
+}
