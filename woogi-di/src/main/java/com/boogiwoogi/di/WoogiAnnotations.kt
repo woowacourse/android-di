8fcf85411fc6ec1b0391df2c9775baad90af0a8a
woogi <kimjinuk1999@gmail.com>
2023-09-17 19:02:55 +0900

    refactor: Woogi 이름을 제외하고 Qualifier의 인자로 String 형태의 값을 삽입한다.

--- woogi-di/src/main/java/com/boogiwoogi/di/WoogiAnnotations.kt
+++ woogi-di/src/main/java/com/boogiwoogi/di/WoogiAnnotations.kt
@@ -3,25 +3,10 @@ package com.boogiwoogi.di
 import kotlin.reflect.KClass
 
 @Target(AnnotationTarget.PROPERTY, AnnotationTarget.VALUE_PARAMETER)
-annotation class WoogiProperty
+annotation class Inject
 
-@Target(AnnotationTarget.PROPERTY, AnnotationTarget.VALUE_PARAMETER)
-annotation class WoogiQualifier(val clazz: KClass<*>)
-
-@Target(AnnotationTarget.PROPERTY, AnnotationTarget.VALUE_PARAMETER)
-annotation class WoogiWholeLifeCycle
-
-@Target(AnnotationTarget.PROPERTY, AnnotationTarget.VALUE_PARAMETER)
-annotation class WoogiActivityContext
-
-@Target(AnnotationTarget.CLASS)
-annotation class WoogiActivity
-
-@Target(AnnotationTarget.CLASS)
-annotation class WoogiApplication
-
-@Target(AnnotationTarget.CLASS)
-annotation class WoogiViewModel
+@Target(AnnotationTarget.PROPERTY, AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.FUNCTION)
+annotation class Qualifier(val simpleName: String)
 
 @Target(AnnotationTarget.CLASS)
 annotation class Module
