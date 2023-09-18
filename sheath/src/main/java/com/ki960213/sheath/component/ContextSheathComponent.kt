46fdb194a6ce0aae1f24f559bae1e8da60e6c417
ki960213 <ki960213@gmail.com>
2023-09-18 17:01:38 +0900

    feat(ContextSheathComponent): ContextSheathComponent 클래스 구현

---
+++ sheath/src/main/java/com/ki960213/sheath/component/ContextSheathComponent.kt
@@ -0,0 +1,23 @@
+package com.ki960213.sheath.component
+
+import android.content.Context
+import kotlin.reflect.KType
+
+class ContextSheathComponent(
+    type: KType,
+    name: String,
+    isSingleton: Boolean,
+    dependentConditions: Map<KType, DependentCondition>,
+    private val context: Context,
+) : SheathComponent1(
+    type = type,
+    name = name,
+    isSingleton = isSingleton,
+    dependentConditions = dependentConditions,
+) {
+    override fun instantiate(components: List<SheathComponent1>) {
+        instance = context
+    }
+
+    override fun getNewInstance(): Any = context
+}
