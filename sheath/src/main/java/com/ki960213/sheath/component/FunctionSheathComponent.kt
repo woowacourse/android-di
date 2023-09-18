47360c3c3016e6ef6a3d593ca348756b3b332f74
ki960213 <ki960213@gmail.com>
2023-09-18 17:01:00 +0900

    feat(FunctionSheathComponent): FunctionSheathComponent 클래스 구현

---
+++ sheath/src/main/java/com/ki960213/sheath/component/FunctionSheathComponent.kt
@@ -0,0 +1,55 @@
+package com.ki960213.sheath.component
+
+import kotlin.reflect.KFunction
+import kotlin.reflect.KType
+import kotlin.reflect.full.isSupertypeOf
+import kotlin.reflect.full.valueParameters
+import kotlin.reflect.jvm.javaMethod
+
+class FunctionSheathComponent(
+    type: KType,
+    name: String,
+    isSingleton: Boolean,
+    dependentConditions: Map<KType, DependentCondition>,
+    private val function: KFunction<*>,
+) : SheathComponent1(
+    type = type,
+    name = name,
+    isSingleton = isSingleton,
+    dependentConditions = dependentConditions,
+) {
+    private val cache: MutableMap<KType, SheathComponent1> = mutableMapOf()
+
+    override fun instantiate(components: List<SheathComponent1>) {
+        val obj = function.javaMethod?.declaringClass?.kotlin?.objectInstance
+        instance = function.call(*function.getArguments(components).toTypedArray())
+            ?: throw IllegalArgumentException("null을 생성하는 함수는 SheathComponent가 될 수 없습니다.")
+    }
+
+    override fun getNewInstance(): Any {
+        val obj = function.javaMethod?.declaringClass?.kotlin?.objectInstance
+        val arguments = function.valueParameters.map { param ->
+            if (dependentConditions[param.type]!!.isNewInstance) {
+                cache[param.type]?.getNewInstance()
+            } else {
+                if (cache[param.type]!!.isSingleton) {
+                    cache[param.type]!!.instance
+                } else {
+                    cache[param.type]!!.getNewInstance()
+                }
+            }
+        }
+        return function.call(*arguments.toTypedArray())!!
+    }
+
+    private fun KFunction<*>.getArguments(components: List<SheathComponent1>): List<Any> {
+        return valueParameters.map { param ->
+            val component = components.find { param.type.isSupertypeOf(it.type) }
+                ?: throw IllegalArgumentException("$name 함수의 종속 항목이 존재하지 않아 인스턴스화 할 수 없습니다.")
+
+            cache[param.type] = component
+
+            component.instance
+        }
+    }
+}
