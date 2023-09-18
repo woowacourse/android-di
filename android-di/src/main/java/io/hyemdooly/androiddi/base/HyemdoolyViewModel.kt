84fe1b9d9e979fe86ccac6761f19a13644bad083
hyemdooly <hyemdooly@gmail.com>
2023-09-19 03:16:32 +0900

    refactor: 안쓰는 프로퍼티 삭제

--- android-di/src/main/java/io/hyemdooly/androiddi/base/HyemdoolyViewModel.kt
+++ android-di/src/main/java/io/hyemdooly/androiddi/base/HyemdoolyViewModel.kt
@@ -4,10 +4,9 @@ import androidx.lifecycle.ViewModel
 import io.hyemdooly.di.Module
 
 open class HyemdoolyViewModel : ViewModel() {
-    lateinit var activityRetainedModule: Module
     lateinit var activityModule: Module
     lateinit var viewModelModule: Module
 
     val isInitailzeModule: Boolean
-        get() = ::activityRetainedModule.isInitialized && ::activityModule.isInitialized && ::viewModelModule.isInitialized
+        get() = ::activityModule.isInitialized && ::viewModelModule.isInitialized
 }
