163dd4ec45129174aa96b7d29dbe91f4e4c2fccd
krrong <khk9664@naver.com>
2023-09-19 01:07:42 +0900

    test: 실패하는 테스트 수정

--- di/src/test/java/com/now/di/InjectorTest.kt
+++ di/src/test/java/com/now/di/InjectorTest.kt
@@ -2,23 +2,29 @@ package com.now.di
 
 import com.now.annotation.Qualifier
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import kotlin.reflect.full.declaredFunctions
 import kotlin.reflect.full.hasAnnotation
 import kotlin.reflect.jvm.jvmErasure
 
 class InjectorTest {
-    @AfterEach
-    fun tearDown() {
-        Container.clear()
+    lateinit var injector: Injector
+
+    @BeforeEach
+    fun setup() {
+        injector = Injector(null, Container())
     }
 
     @Test
     fun `addModule 함수를 실행하면 모듈에 있는 함수가 실행되어 인스턴스들이 Container에 저장된다`() {
+        // given
         val module = KeyboardModule()
-        Injector.addModule(module)
 
+        // when
+        injector.addModule(module)
+
+        // then
         KeyboardModule::class.declaredFunctions.map {
             val klass = it.returnType.jvmErasure
             val annotation = it.annotations.firstOrNull {
@@ -26,49 +32,61 @@ class InjectorTest {
             }
             val dependencyType = DependencyType(klass, annotation)
 
-            val instance = Container.getInstance(dependencyType) ?: throw NullPointerException()
+            val instance = injector.getCurrentContainer().getInstance(dependencyType) ?: throw NullPointerException()
             assertThat(instance).isNotNull
         }
     }
 
     @Test
     fun `하나의 인터페이스를 구현하는 구현체가 하나일 경우 인스턴스 생성에 성공한다`() {
+        // given
         val module = KeyboardModuleOneInterfaceOneImplement()
-        Injector.addModule(module)
+        injector.addModule(module)
 
-        val keyboard = Injector.inject<Keyboard>(Keyboard::class)
+        // when
+        val keyboard = injector.inject<Keyboard>(Keyboard::class)
 
+        // then
         assertThat(keyboard).isNotNull
     }
 
     @Test
     fun `하나의 인터페이스를 구현하는 구현체가 두개일 경우 Qualifier를 사용하지 않으면 잘못된 주입이 발생한다`() {
+        // given
         val module = KeyboardModuleOneInterfaceTwoImplementWithOutQualifier()
-        Injector.addModule(module)
+        injector.addModule(module)
 
-        val keyboard = Injector.inject<Keyboard>(Keyboard::class)
+        // when
+        val keyboard = injector.inject<Keyboard>(Keyboard::class)
 
+        // then
         assertThat(keyboard.housing).isNotExactlyInstanceOf(PlasticHousing::class.java)
     }
 
     @Test
     fun `하나의 인터페이스를 구현하는 구현체가 두개일 경우 Qualifier를 사용하면 알맞게 주입할 수 있다`() {
+        // given
         val module = KeyboardModuleOneInterfaceTwoImplementWithQualifier()
-        Injector.addModule(module)
+        injector.addModule(module)
 
-        val keyboard = Injector.inject<KeyboardWithQualifier1>(KeyboardWithQualifier1::class)
+        // when
+        val keyboard = injector.inject<KeyboardWithQualifier1>(KeyboardWithQualifier1::class)
 
+        // then
         assertThat(keyboard.housing).isInstanceOf(AluminumHousing::class.java)
         assertThat(keyboard.housing).isNotExactlyInstanceOf(PlasticHousing::class.java)
     }
 
     @Test
     fun `하나의 인터페이스를 구현하는 구현체가 두개일 경우 Qualifier를 사용하면 알맞게 주입할 수 있다2`() {
+        // given
         val module = KeyboardModuleTwoInterfaceTwoImplementWithQualifier()
-        Injector.addModule(module)
+        injector.addModule(module)
 
-        val keyboard = Injector.inject<KeyboardWithQualifier2>(KeyboardWithQualifier2::class)
+        // when
+        val keyboard = injector.inject<KeyboardWithQualifier2>(KeyboardWithQualifier2::class)
 
+        // then
         assertThat(keyboard.housing).isInstanceOf(AluminumHousing::class.java)
         assertThat(keyboard.housing).isNotExactlyInstanceOf(PlasticHousing::class.java)
 
