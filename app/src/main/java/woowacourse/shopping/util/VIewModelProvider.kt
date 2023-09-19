abeb38d814843973bbe7343feeedf67e5319d407
inseonyun <yuninseon@naver.com>
2023-09-19 15:16:01 +0900

    refactor: VIewModelProvider 수정

--- app/src/main/java/woowacourse/shopping/util/VIewModelProvider.kt
+++ app/src/main/java/woowacourse/shopping/util/VIewModelProvider.kt
@@ -2,26 +2,26 @@ package woowacourse.shopping.util
 
 import android.content.Context
 import androidx.activity.ComponentActivity
-import androidx.annotation.MainThread
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelLazy
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import woowacourse.shopping.di.DefaultViewModelModule
 
-@MainThread
 inline fun <reified VM : ViewModel> ComponentActivity.injectViewModel(): Lazy<VM> {
     return ViewModelLazy(
         VM::class,
         { viewModelStore },
         {
-            viewModelFactory(applicationContext)
+            viewModelFactory<VM>(applicationContext)
         },
     )
 }
 
-fun viewModelFactory(context: Context): ViewModelProvider.Factory =
-    object : ViewModelProvider.Factory {
-        override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            return DefaultViewModelModule(context).createViewModel(modelClass)
+inline fun <reified VM : ViewModel> viewModelFactory(context: Context): ViewModelProvider.Factory =
+    viewModelFactory {
+        initializer {
+            DefaultViewModelModule.getViewModelModule(context).createViewModel(VM::class.java)
         }
     }
