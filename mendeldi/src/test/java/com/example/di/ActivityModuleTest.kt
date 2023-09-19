c2230e1c2274e3bd0922cf5ba7c1dd820beeefa9
mendel <rhthrhrl0@naver.com>
2023-09-19 20:22:18 +0900

    test: ActivityModule 테스트 코드 수정

--- mendeldi/src/test/java/com/example/di/ActivityModuleTest.kt
+++ mendeldi/src/test/java/com/example/di/ActivityModuleTest.kt
@@ -1,5 +1,6 @@
 package com.example.di
 
+import android.os.Bundle
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.example.di.activity.DiEntryPointActivity
 import com.example.di.annotation.FieldInject
@@ -7,7 +8,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.Robolectric
+import org.robolectric.Robolectric.buildActivity
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 
@@ -18,30 +19,22 @@ internal class ActivityModuleTest {
     var instantTaskExecutorRule = InstantTaskExecutorRule()
 
     @Test
-    fun `액티비티 모듈에 정의된 메소드로 만들어진 객체는 액티비티 별로 다른 객체를 받는다`() {
+    fun `액티비티 모듈에 정의된 메소드로 만들어진 객체는 구성변경시 유지되지 않는다`() {
         // given
         class FakeFirstActivity : DiEntryPointActivity() {
             @FieldInject
             lateinit var counter: NumberCounter
+
+            override fun onCreate(savedInstanceState: Bundle?) {
+                super.onCreate(savedInstanceState)
+                setTheme(androidx.appcompat.R.style.Theme_AppCompat_DayNight_NoActionBar)
+            }
         }
 
-        class FakeSecondActivity : DiEntryPointActivity() {
-            @FieldInject
-            lateinit var counter: NumberCounter
-        }
-
-        // when
-        val firstActivity = Robolectric
-            .buildActivity(FakeFirstActivity::class.java)
-            .create()
-            .get()
-
-        val secondActivity = Robolectric
-            .buildActivity(FakeSecondActivity::class.java)
-            .create()
-            .get()
-
-        // then
-        assertThat(firstActivity.counter === secondActivity.counter).isFalse
+        val controller = buildActivity(FakeFirstActivity::class.java).setup() // resume까지
+        val firstCounter = controller.get().counter
+        controller.recreate() // 구성 변경 후 재시작
+        val secondCounter = controller.get().counter
+        assertThat(firstCounter === secondCounter).isFalse
     }
 }
