24afe153f7b1db2d220f2cdf186a84a910f080b4
woogi <kimjinuk1999@gmail.com>
2023-09-19 10:20:33 +0900

    refactor: module 이름을 변경한다.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ android {
 
 dependencies {
     implementation(project(":domain"))
-    implementation(project(":woogi-di"))
+    implementation(project(":di"))
 
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
