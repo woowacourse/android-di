65d4312756f5a07c92c010a081d3ec10d3081c11
ippnsj <ippnsj@gmail.com>
2023-09-19 13:51:34 +0900

    feat: DiApplication 정의 및 ApplicationLifecycleObserver 제거

--- app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
+++ app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
@@ -1,43 +1,22 @@
 package woowacourse.shopping
 
-import android.app.Application
-import androidx.lifecycle.ProcessLifecycleOwner
-import woowacourse.shopping.container.DiContainer
 import woowacourse.shopping.data.CartProductDao
 import woowacourse.shopping.data.ShoppingDatabase
-import woowacourse.shopping.lifecycleobserver.ApplicationLifecycleObserver
-import woowacourse.shopping.lifecycleobserver.DefaultApplicationLifecycleObserver
+import woowacourse.shopping.ui.DiApplication
 import woowacourse.shopping.ui.cart.DateFormatter
 import woowacourse.shopping.ui.cart.createDateFormatter
 
-class ShoppingApplication :
-    Application(),
-    ApplicationLifecycleObserver by DefaultApplicationLifecycleObserver() {
-    private lateinit var diContainer: DiContainer
-    lateinit var injector: Injector
-        private set
-
-    init {
-        setupContainer()
-        setupInjector()
-        setupLifecycleObserver()
+class ShoppingApplication : DiApplication() {
+    override fun onCreate() {
+        super.onCreate()
+        setupProviders()
     }
 
-    private fun setupContainer() {
-        diContainer = DiContainer()
-        diContainer.registerProviders {
+    private fun setupProviders() {
+        registerProviders {
             provider(ShoppingDatabase::class to ShoppingDatabase::getInstance)
             provider(CartProductDao::class to ShoppingDatabase::cartProductDao)
             provider(DateFormatter::class to ::createDateFormatter)
         }
-    }
-
-    private fun setupInjector() {
-        injector = Injector(diContainer)
-    }
-
-    private fun setupLifecycleObserver() {
-        val lifecycle = ProcessLifecycleOwner.get().lifecycle
-        setupLifecycleObserver(lifecycle, this, injector)
     }
 }
