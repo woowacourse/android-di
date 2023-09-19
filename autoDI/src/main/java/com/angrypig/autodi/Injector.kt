359b25e276c7845316041f7b4e1f620f427f1a98
2chang5 <chang5hustle@gmail.com>
2023-09-19 17:20:28 +0900

    [fix] injector 접근제한자 관련 버그 수정

--- autoDI/src/main/java/com/angrypig/autodi/Injector.kt
+++ autoDI/src/main/java/com/angrypig/autodi/Injector.kt
@@ -3,7 +3,7 @@ package com.angrypig.autodi
 import kotlin.reflect.KProperty
 
 class Injector(val qualifier: String? = null) {
-    internal inline operator fun <reified T : Any> getValue(thisRef: Any?, property: KProperty<*>): T {
+    inline operator fun <reified T : Any> getValue(thisRef: Any?, property: KProperty<*>): T {
         @Suppress("UNCHECKED_CAST")
         return AutoDI.inject(qualifier)
     }
