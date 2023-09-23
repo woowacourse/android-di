a5ab3ecc384f671f5d53675c0a392c57d1402a4f
krrong <khk9664@naver.com>
2023-09-23 16:50:29 +0900

    refactor: Injector와 Container의 관심사 분리

--- di/src/main/java/com/now/di/Container.kt
+++ di/src/main/java/com/now/di/Container.kt
@@ -1,6 +1,13 @@
 package com.now.di
 
-import kotlin.reflect.KClass
+import com.now.annotation.Qualifier
+import kotlin.reflect.KFunction
+import kotlin.reflect.KParameter
+import kotlin.reflect.full.declaredFunctions
+import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.full.hasAnnotation
+import kotlin.reflect.full.valueParameters
+import kotlin.reflect.jvm.jvmErasure
 
 class Container {
     private val dependency = mutableMapOf<DependencyType, Any>()
@@ -9,11 +16,50 @@ class Container {
         return dependency[dependencyType]
     }
 
-    fun addInstance(type: KClass<*>, instance: Any, annotation: Annotation?) {
-        dependency[DependencyType(type, annotation)] = instance
+    fun addInstance(receiver: Module, kFunction: KFunction<*>) {
+        val dependencyType = getDependencyTypeFromKFunction(kFunction)
+
+        // dependency에 이미 있다면 추가할 필요가 없기 때문에 종료
+        if (getInstance(dependencyType) != null) return
+
+        // dependency에 없는 경우
+        if (kFunction.valueParameters.isEmpty()) {
+            // provide 함수에 파라미터가 없는 경우 인스턴스를 생성하고 dependency에 추가 후 종료
+            kFunction.call(receiver)?.let { addDependency(dependencyType, it) }
+                ?: throw IllegalArgumentException("문제 생김")
+            return
+        }
+
+        // provide 함수에 파라미터가 있는 경우 dependency에서 가져오거나 생성해야 함
+        val requiredParams = kFunction.valueParameters.map { kParameter ->
+            val klass = kParameter.type.jvmErasure
+            val annotation = kParameter.findAnnotation<Qualifier>()
+            getInstance(DependencyType(klass, annotation)) ?: createParameter(receiver, kParameter)
+        }
+
+        // 인스턴스를 생성하여 dependency에 저장
+        kFunction.call(receiver, *requiredParams.toTypedArray())?.let {
+            addDependency(dependencyType, it)
+        }
     }
 
-    fun clear() {
-        dependency.clear()
+    private fun getDependencyTypeFromKFunction(kFunction: KFunction<*>): DependencyType {
+        return DependencyType(
+            kFunction.returnType.jvmErasure,
+            kFunction.annotations.firstOrNull { it.annotationClass.hasAnnotation<Qualifier>() },
+        )
+    }
+
+    private fun createParameter(receiver: Module, kParameter: KParameter): Any {
+        val kFunction = receiver::class.declaredFunctions.firstOrNull { kFunction ->
+            kFunction.returnType == kParameter.type
+        } ?: throw NullPointerException("생성할 수 없습니다!")
+        addInstance(receiver, kFunction)
+        // 재귀적 호출 이후라 무조건 있음
+        return getInstance(DependencyType(kParameter.type.jvmErasure, kParameter.findAnnotation<Qualifier>()))!!
+    }
+
+    private fun addDependency(dependencyType: DependencyType, instance: Any) {
+        dependency[dependencyType] = instance
     }
 }
--- di/src/main/java/com/now/di/Injector.kt
+++ di/src/main/java/com/now/di/Injector.kt
@@ -3,16 +3,13 @@ package com.now.di
 import com.now.annotation.Inject
 import com.now.annotation.Qualifier
 import kotlin.reflect.KClass
-import kotlin.reflect.KFunction
 import kotlin.reflect.KMutableProperty1
-import kotlin.reflect.KParameter
 import kotlin.reflect.KProperty1
 import kotlin.reflect.full.declaredFunctions
 import kotlin.reflect.full.declaredMemberProperties
 import kotlin.reflect.full.findAnnotation
 import kotlin.reflect.full.hasAnnotation
 import kotlin.reflect.full.primaryConstructor
-import kotlin.reflect.full.valueParameters
 import kotlin.reflect.jvm.isAccessible
 import kotlin.reflect.jvm.jvmErasure
 
@@ -24,56 +21,8 @@ class Injector(
     fun addModule(module: Module) {
         val kFunctions = module::class.declaredFunctions
         kFunctions.forEach { kFunction ->
-            createOrAdd(module, kFunction)
+            container.addInstance(module, kFunction)
         }
-    }
-
-    private fun createOrAdd(receiver: Module, kFunction: KFunction<*>) {
-        val functionKlass = kFunction.returnType.jvmErasure
-        val qualifier = kFunction.annotations.firstOrNull {
-            it.annotationClass.hasAnnotation<Qualifier>()
-        }
-
-        val dependencyType = DependencyType(functionKlass, qualifier)
-
-        // 컨테이너에 이미 있다면 종료
-        if (container.getInstance(dependencyType) != null) return
-
-        // Container에 없는 경우
-        // 함수에 파라미터가 없는 경우 인스턴스를 생성하고 Container에 추가 후 종료
-        if (kFunction.valueParameters.isEmpty()) {
-            val kclass = kFunction.call(receiver)
-            kclass?.let { container.addInstance(functionKlass, it, qualifier) }
-                ?: throw IllegalArgumentException("문제 생김")
-            return
-        }
-
-        // 함수에 파라미터가 있는 경우 컨테이너에서 가져오거나 생성해야 함
-        val requiredParams = kFunction.valueParameters.map { kParameter ->
-            getParameterInstance(receiver, kFunction, kParameter)
-        }
-
-        kFunction.call(receiver, *requiredParams.toTypedArray())?.let {
-            container.addInstance(functionKlass, it, qualifier)
-        }
-    }
-
-    private fun getParameterInstance(
-        receiver: Module,
-        kFunction: KFunction<*>,
-        kParameter: KParameter,
-    ): Any {
-        val klass = kParameter.type.jvmErasure
-        val annotation = kParameter.findAnnotation<Qualifier>()
-        val dependencyType = DependencyType(klass, annotation)
-
-        // 파라미터가 컨테이너에 없는 경우 재귀적 호출을 통해 컨테이너에 추가로 저장
-        if (container.getInstance(dependencyType) == null) {
-            createOrAdd(receiver, kFunction)
-        }
-
-        // 재귀적 호출을 통해 컨테이너에 저장했기 때문에 무조건 있음
-        return container.getInstance(dependencyType)!!
     }
 
     private fun getPropertyInstance(
@@ -83,7 +32,7 @@ class Injector(
         val annotation = kProperty1.findAnnotation<Qualifier>()
         val dependencyType = DependencyType(klass, annotation)
 
-        return container.getInstance(dependencyType) ?: NullPointerException()
+        return container.getInstance(dependencyType) ?: NullPointerException("컨테이너에 저장되어 있지 않습니다.")
     }
 
     // klass의 인스턴스를 생성하여 반환한다
@@ -108,19 +57,11 @@ class Injector(
         }
 
         val instance = primaryConstructor.callBy(insertedParameters) as T
-
-        // 주입이 필요하지만 생성자에 위치하지 않은 프로퍼티
-        val properties =
-            klass.declaredMemberProperties.filterIsInstance<KMutableProperty1<*, *>>()
-                .filter { it.hasAnnotation<Inject>() }
-        properties.forEach {
-            it.isAccessible = true
-            it.setter.call(instance, getPropertyInstance(it))
-        }
-
+        injectParams(klass, instance)
         return instance
     }
 
+    // 주입이 필요하지만 생성자에 위치하지 않은 프로퍼티
     fun injectParams(klass: KClass<*>, instance: Any) {
         val properties =
             klass.declaredMemberProperties.filterIsInstance<KMutableProperty1<*, *>>()
