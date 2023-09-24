4b9dd24eca64aa9969a34d7662c8fa8c96f2be60
hyunji1203 <hj27720@gmail.com>
2023-09-25 00:46:27 +0900

    feat: 기존 모듈 제거 후 Application과 Activity로 구분되는 모듈 생성

--- app/src/main/java/woowacourse/shopping/di/module/ActivityModule.kt
+++ app/src/main/java/woowacourse/shopping/di/module/ActivityModule.kt
@@ -1,10 +1,17 @@
 package woowacourse.shopping.di.module
 
+import android.content.Context
+import com.example.bbottodi.di.Module
 import woowacourse.shopping.data.repository.DefaultProductRepository
 import woowacourse.shopping.model.repository.ProductRepository
+import woowacourse.shopping.ui.cart.DateFormatter
 
-object MainActivityModule {
+class ActivityModule(private val context: Context) : Module {
     fun provideProductRepository(): ProductRepository {
         return DefaultProductRepository()
+    }
+
+    fun provideDateFormatter(): DateFormatter {
+        return DateFormatter(context)
     }
 }
