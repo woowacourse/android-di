aeac9a6b336210355009f25e8022a6d46fb65fa7
hyunji1203 <hj27720@gmail.com>
2023-09-25 00:47:11 +0900

    feat: DiActivity 생성

--- bbotto-di/src/main/java/com/example/bbottodi/di/DiActivity.kt
+++ bbotto-di/src/main/java/com/example/bbottodi/di/DiActivity.kt
@@ -1,9 +1,21 @@
 package com.example.bbottodi.di
 
+import android.content.Context
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.bbottodi.di.inject.AutoDependencyInjector
 
 open class DiActivity : AppCompatActivity() {
-    val container: ContainerWithContext by lazy {
-        ContainerWithContext((application as DiApplication).container, this)
+    lateinit var container: Container
+    lateinit var module: (Context) -> Module
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        container = ContainerWithContext(
+            (application as DiApplication).container,
+            module(this),
+            this,
+        )
+        AutoDependencyInjector.injectField(container, this)
     }
 }
