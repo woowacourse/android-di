33b4654d368608134fd0e2cfbe5df822d81e460e
hyunji1203 <hj27720@gmail.com>
2023-09-25 00:50:24 +0900

    refactor: 뷰모델 생성 클래스 이름 변경 및 inject 함수 변경

--- bbotto-di/src/main/java/com/example/bbottodi/di/common/ViewModelLazy.kt
+++ bbotto-di/src/main/java/com/example/bbottodi/di/common/ViewModelLazy.kt
@@ -1,22 +1,20 @@
 package com.example.bbottodi.di.common
 
-import androidx.annotation.MainThread
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelLazy
 import androidx.lifecycle.ViewModelProvider
 import com.example.bbottodi.di.DiActivity
-import com.example.bbottodi.di.inject.AutoDependencyInjector
+import com.example.bbottodi.di.inject.AutoDependencyInjector.createInstance
 
-@MainThread
-inline fun <reified viewModel : ViewModel> DiActivity.viewModel(): Lazy<viewModel> {
+inline fun <reified viewModel : ViewModel> DiActivity.viewModels(): Lazy<viewModel> {
     return ViewModelLazy(
         viewModel::class,
         { viewModelStore },
         {
             object : ViewModelProvider.Factory {
                 override fun <T : ViewModel> create(modelClass: Class<T>): T {
-                    return AutoDependencyInjector.inject(
-                        this@viewModel.container,
+                    return createInstance(
+                        this@viewModels.container,
                         modelClass.kotlin,
                     )
                 }
