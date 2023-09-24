6a7712cd1da37cb7fd324974b5bf3ca58c8dee12
hyunji1203 <hj27720@gmail.com>
2023-09-25 00:53:10 +0900

    refactor: InstanceIdentifier의 변수 타입 변경에 따라 해당되는 annotation 로직 변경

--- bbotto-di/src/main/java/com/example/bbottodi/di/inject/AutoDependencyInjector.kt
+++ bbotto-di/src/main/java/com/example/bbottodi/di/inject/AutoDependencyInjector.kt
@@ -2,6 +2,7 @@ package com.example.bbottodi.di.inject
 
 import com.example.bbottodi.di.Container
 import com.example.bbottodi.di.annotation.Inject
+import com.example.bbottodi.di.annotation.Qualifier
 import kotlin.reflect.KClass
 import kotlin.reflect.full.declaredMemberProperties
 import kotlin.reflect.full.hasAnnotation
@@ -11,17 +12,32 @@ import kotlin.reflect.jvm.javaField
 import kotlin.reflect.jvm.jvmErasure
 
 object AutoDependencyInjector {
-
     private const val INJECT_ERROR_MESSAGE = "주입 할 생성자가 존재 하지 않습니다."
     private const val NONE_INSTANCE_ERROR_MESSAGE = "해당 인스턴스가 존재하지 않습니다."
 
     fun <T : Any> inject(container: Container, clazz: KClass<*>): T {
-        var instance = container.getInstance(clazz, clazz.annotations)
+        val annotation =
+            clazz.annotations.firstOrNull { it.annotationClass.hasAnnotation<Qualifier>() }
+        var instance = container.getInstance(clazz, annotation)
         if (instance == null) instance = createInstance(container, clazz)
         return instance as T
     }
 
-    private fun <T : Any> createInstance(container: Container, clazz: KClass<*>): T {
+    fun <T : Any> injectField(container: Container, instance: T) {
+        val properties = instance::class.declaredMemberProperties.filter { property ->
+            property.hasAnnotation<Inject>()
+        }
+        properties.forEach { property ->
+            property.isAccessible = true
+            val type = property.returnType.jvmErasure
+            val annotation =
+                property.annotations.firstOrNull { it.annotationClass.hasAnnotation<Qualifier>() }
+            val argument = container.getInstance(type, annotation)
+            property.javaField?.set(instance, argument)
+        }
+    }
+
+    fun <T : Any> createInstance(container: Container, clazz: KClass<*>): T {
         val constructor = clazz.primaryConstructor
             ?: throw IllegalArgumentException(INJECT_ERROR_MESSAGE)
         val parameters = constructor.parameters.filter { parameter ->
@@ -29,26 +45,14 @@ object AutoDependencyInjector {
         }
         val arguments = parameters.map { parameter ->
             val type = parameter.type.jvmErasure
-            container.getInstance(type, parameter.annotations) ?: inject(container, type)
+            val annotation =
+                parameter.annotations.firstOrNull { it.annotationClass.hasAnnotation<Qualifier>() }
+            container.getInstance(type, annotation)
+                ?: inject(container, type)
                 ?: throw IllegalStateException(NONE_INSTANCE_ERROR_MESSAGE)
         }
         val instance = constructor.call(*arguments.toTypedArray()) as T
         injectField(container, instance)
         return instance
-    }
-
-    private fun <T : Any> injectField(container: Container, instance: T) {
-        val properties = instance::class.declaredMemberProperties.filter { property ->
-            property.hasAnnotation<Inject>()
-        }
-        properties.forEach { property ->
-            property.isAccessible = true
-            property.javaField?.let {
-                val type = it.type.kotlin
-                val fieldValue = container.getInstance(type, it.annotations.toList())
-                    ?: throw IllegalStateException(NONE_INSTANCE_ERROR_MESSAGE)
-                it.set(instance, fieldValue)
-            }
-        }
     }
 }
