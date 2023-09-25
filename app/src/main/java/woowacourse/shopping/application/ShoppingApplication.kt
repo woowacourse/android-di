69e4ba5e7d4136022870f841d62d7ae133081da0
Sujin Kim <sujinkim919@gmail.com>
2023-09-25 15:12:17 +0900

    refactor: DiApplication에서 모듈 인스턴스 주입을 하도록 수정

--- app/src/main/java/woowacourse/shopping/application/ShoppingApplication.kt
+++ app/src/main/java/woowacourse/shopping/application/ShoppingApplication.kt
@@ -3,11 +3,4 @@ package woowacourse.shopping.application
 import com.ssu.androidi.application.DiApplication
 import woowacourse.shopping.di.module.ApplicationModule
 
-class ShoppingApplication : DiApplication() {
-
-    override fun onCreate() {
-        super.onCreate()
-
-        injector.addModuleInstances(ApplicationModule(this@ShoppingApplication))
-    }
-}
+class ShoppingApplication : DiApplication(ApplicationModule::class)
