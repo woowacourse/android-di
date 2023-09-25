656b547632567c0d9b0f213577ed77bf7a4a2e7e
whk06061 <gju06061@naver.com>
2023-09-26 01:40:13 +0900

    refactor: annotation target 설정

--- di/src/main/java/com/hyegyeong/di/annotations/Inject.kt
+++ di/src/main/java/com/hyegyeong/di/annotations/Inject.kt
@@ -1,4 +1,10 @@
 package com.hyegyeong.di.annotations
 
 @Retention(AnnotationRetention.RUNTIME)
+@Target(
+    AnnotationTarget.CONSTRUCTOR,
+    AnnotationTarget.FIELD,
+    AnnotationTarget.PROPERTY,
+    AnnotationTarget.VALUE_PARAMETER
+)
 annotation class Inject
--- di/src/main/java/com/hyegyeong/di/annotations/Singleton.kt
+++ di/src/main/java/com/hyegyeong/di/annotations/Singleton.kt
@@ -1,4 +1,11 @@
 package com.hyegyeong.di.annotations
 
 @Retention(AnnotationRetention.RUNTIME)
+@Target(
+    AnnotationTarget.CONSTRUCTOR,
+    AnnotationTarget.FIELD,
+    AnnotationTarget.PROPERTY,
+    AnnotationTarget.VALUE_PARAMETER,
+    AnnotationTarget.FUNCTION
+)
 annotation class Singleton
