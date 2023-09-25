7c3adc13bbb270fdb051035f4f38a59741dcd219
briandr97 <briandr@naver.com>
2023-09-25 17:32:42 +0900

    test: 테스트 코드 중복 코드 제거 및 여러 생명주기에 따른 테스트 추가

--- bixby-android/src/test/java/woowacourse/shopping/AndroidDiTest.kt
+++ bixby-android/src/test/java/woowacourse/shopping/AndroidDiTest.kt
@@ -7,16 +7,25 @@ import androidx.appcompat.app.AppCompatActivity
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertNotNull
 import junit.framework.TestCase.assertNotSame
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.android.controller.ActivityController
 import org.robolectric.annotation.Config
 import woowacourse.shopping.ActivityInjectManager.ActivityLifecycle
 
 @RunWith(RobolectricTestRunner::class)
 @Config(application = AndroidDiTest.TestApplication::class)
 class AndroidDiTest {
+    private lateinit var activityController: ActivityController<TestActivity>
+
+    @Before
+    fun setup() {
+        activityController = Robolectric.buildActivity(TestActivity::class.java)
+    }
+
     internal class MockObject(context: Context)
 
     internal class TestApplication : Application() {
@@ -64,8 +73,7 @@ class AndroidDiTest {
     fun `onCreate가 호출되면 Inject 어노테이션을 가진 액티비티의 프로퍼티들이 주입된다`() {
         // given
         // when
-        val activity = Robolectric
-            .buildActivity(TestActivity::class.java)
+        val activity = activityController
             .create()
             .get()
 
@@ -76,14 +84,12 @@ class AndroidDiTest {
     @Test
     fun `액티비티 라이프사이클을 따르는 프로퍼티는 화면 회전에서 살아남지 못한다`() {
         // when
-        val activityBeforeRotation = Robolectric
-            .buildActivity(TestActivity::class.java)
+        val activityBeforeRotation = activityController
             .create()
             .get()
         val mockBeforeRotation = activityBeforeRotation.disposableMock
 
-        val activityAfterRotation = Robolectric
-            .buildActivity(TestActivity::class.java)
+        val activityAfterRotation = activityController
             .recreate()
             .get()
         val mockAfterRotation = activityAfterRotation.disposableMock
@@ -95,19 +101,45 @@ class AndroidDiTest {
     @Test
     fun `Provider에서 싱글턴으로 저장된 타입의 프로퍼티는 화면 회전에서도 살아남는다`() {
         // when
-        val activityBeforeRotation = Robolectric
-            .buildActivity(TestActivity::class.java)
+        val activityBeforeRotation = activityController
             .create()
             .get()
         val mockBeforeRotation = activityBeforeRotation.singletonMock
 
-        val activityAfterRotation = Robolectric
-            .buildActivity(TestActivity::class.java)
+        val activityAfterRotation = activityController
             .recreate()
             .get()
         val mockAfterRotation = activityAfterRotation.singletonMock
 
         // then
         assertEquals(mockBeforeRotation, mockAfterRotation)
+    }
+
+    @Test
+    fun `액티비티 라이프 사이클을 따르는 프로퍼티는 액티비티가 일시중지 후 재개돼도 살아있다`() {
+        // given
+        val activity = activityController.setup()
+        val originalMock = activity.get().disposableMock
+
+        // when
+        val resumedActivity: ActivityController<TestActivity> = activity.pause().resume()
+        val resumedMock = resumedActivity.get().disposableMock
+
+        // then
+        assertEquals(originalMock, resumedMock)
+    }
+
+    @Test
+    fun `액티비티 라이프 사이클을 따르는 프로퍼티는 액티비티가 중지 후 시작돼도 살아있다`() {
+        // given
+        val activity = activityController.setup()
+        val originalMock = activity.get().disposableMock
+
+        // when
+        val resumedActivity: ActivityController<TestActivity> = activity.stop().start()
+        val resumedMock = resumedActivity.get().disposableMock
+
+        // then
+        assertEquals(originalMock, resumedMock)
     }
 }
