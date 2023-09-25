144ffef902512ba133212e9dcd4dbf3e262c418a
briandr97 <briandr@naver.com>
2023-09-25 15:34:32 +0900

    feat: Creator를 추가하여 Provider가 없을 때 자체 생성이 가능하도록 함

--- bixby/src/main/java/woowacourse/shopping/Injector.kt
+++ bixby/src/main/java/woowacourse/shopping/Injector.kt
@@ -7,16 +7,22 @@ import kotlin.reflect.KClass
 @Suppress("UNCHECKED_CAST")
 open class Injector(val dependencyContainer: DependencyContainer = DependencyContainer.getSingletonInstance()) {
     open val providerContainer = ProviderContainer.getSingletonInstance(this, dependencyContainer)
+    val creator = Creator(this)
     fun <T : Any> inject(target: KClass<T>, qualifierTag: String? = null): T {
         return getFromContainer(target, qualifierTag) ?: createByProvider(target, qualifierTag)
+            ?: createInstance(target, qualifierTag)
     }
 
-    private fun <T : Any> createByProvider(target: KClass<T>, qualifierTag: String?): T {
-        return providerContainer.getInstance(target, qualifierTag) as T
+    private fun <T : Any> createByProvider(target: KClass<T>, qualifierTag: String?): T? {
+        return providerContainer.getInstance(target, qualifierTag) as T?
     }
 
     private fun <T : Any> getFromContainer(target: KClass<T>, qualifierTag: String?): T? {
         return dependencyContainer.getInstance(target, qualifierTag) as T?
+    }
+
+    private fun <T : Any> createInstance(target: KClass<T>, qualifierTag: String?): T {
+        return creator.createInstance(target, qualifierTag) as T
     }
 
     companion object {
