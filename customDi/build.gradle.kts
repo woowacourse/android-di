238e8641869fc4b8cca2cd154125d3f1d2488693
s9hn <tnghqn@naver.com>
2023-09-25 14:21:06 +0900

    chore: 머터리얼 의존성 제거

--- customDi/build.gradle.kts
+++ customDi/build.gradle.kts
@@ -43,5 +43,4 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.21")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
 }
