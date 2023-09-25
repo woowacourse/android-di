7b99242d3ee0cedb3c55ce2c219e12c82cc22ec4
woogi <kimjinuk1999@gmail.com>
2023-09-25 17:32:19 +0900

    refactor: 지금 상황에서는 필요하지 않은 어노테이션을 제거한다.

--- app/src/main/java/woowacourse/shopping/di/module/ProductRepositoryModule.kt
+++ app/src/main/java/woowacourse/shopping/di/module/ProductRepositoryModule.kt
@@ -4,7 +4,6 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
-import dagger.hilt.android.scopes.ViewModelScoped
 import woowacourse.shopping.data.DefaultProductRepository
 import woowacourse.shopping.model.ProductRepository
 
@@ -13,7 +12,6 @@ import woowacourse.shopping.model.ProductRepository
 abstract class ProductRepositoryModule {
 
     @Binds
-    @ViewModelScoped
     abstract fun bindProductRepository(
         defaultProductRepository: DefaultProductRepository
     ): ProductRepository
