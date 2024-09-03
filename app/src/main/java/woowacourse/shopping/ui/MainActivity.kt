5ba2fed0fc0bc3d5a7ddd46cf58e20af2b71e850
sh1mj1 <7wlgns@gmail.com>
2024-09-03 22:21:52 +0900

    feat(MainActivity): 뷰모델을 lazy 한 위임자 api 로 생성한다

--- app/src/main/java/woowacourse/shopping/ui/MainActivity.kt
+++ app/src/main/java/woowacourse/shopping/ui/MainActivity.kt
@@ -4,17 +4,22 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
 import woowacourse.shopping.R
+import woowacourse.shopping.data.CartRepository
+import woowacourse.shopping.data.ProductRepository
 import woowacourse.shopping.databinding.ActivityMainBinding
 import woowacourse.shopping.ui.cart.CartActivity
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
-    private val viewModel by lazy {
-        ViewModelProvider(this)[MainViewModel::class.java]
+    private val viewModel by viewModels<MainViewModel> {
+        MainViewModel.factory(
+            productRepository = ProductRepository(),
+            cartRepository = CartRepository(),
+        )
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
