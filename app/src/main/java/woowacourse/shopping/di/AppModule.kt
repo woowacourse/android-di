bda133fa433f0ea72f2c9f8d677a58b5ccb08371
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-09-03 22:21:51 +0900

    feat: ViewModel DI에 필요한 객체들을 전역적으로 선언한 Module 추가

---
+++ app/src/main/java/woowacourse/shopping/di/AppModule.kt
@@ -0,0 +1,11 @@
+package woowacourse.shopping.di
+
+import woowacourse.shopping.data.CartRepository
+import woowacourse.shopping.data.ProductRepository
+
+interface AppModule {
+    val productRepository: ProductRepository
+    
+    val cartRepository: CartRepository
+}
+
---
+++ app/src/main/java/woowacourse/shopping/di/DefaultAppModule.kt
@@ -0,0 +1,13 @@
+package woowacourse.shopping.di
+
+import android.content.Context
+import woowacourse.shopping.data.CartRepository
+import woowacourse.shopping.data.CartRepositoryImpl
+import woowacourse.shopping.data.ProductRepository
+import woowacourse.shopping.data.ProductRepositoryImpl
+
+class DefaultAppModule(private val appContext: Context) : AppModule {
+    override val productRepository: ProductRepository = ProductRepositoryImpl()
+    
+    override val cartRepository: CartRepository = CartRepositoryImpl()
+}
