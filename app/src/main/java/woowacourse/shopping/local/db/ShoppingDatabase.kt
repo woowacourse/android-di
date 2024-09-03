528a0f96b0f6d0ad95c27deb7f0d093625dc466d
dpcks0509 <dpcks0509@gmail.com>
2024-09-04 00:38:45 +0900

    refactor: ShoppingDatabase 싱글톤 패턴 적용

--- app/src/main/java/woowacourse/shopping/local/db/ShoppingDatabase.kt
+++ app/src/main/java/woowacourse/shopping/local/db/ShoppingDatabase.kt
@@ -1,6 +1,8 @@
 package woowacourse.shopping.local.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import woowacourse.shopping.local.dao.CartProductDao
 import woowacourse.shopping.local.entity.CartProductEntity
@@ -8,4 +10,18 @@ import woowacourse.shopping.local.entity.CartProductEntity
 @Database(entities = [CartProductEntity::class], version = 1, exportSchema = false)
 abstract class ShoppingDatabase : RoomDatabase() {
     abstract fun cartProductDao(): CartProductDao
+
+    companion object {
+        private var instance: ShoppingDatabase? = null
+
+        fun getInstance(context: Context): ShoppingDatabase {
+            return instance ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context.applicationContext,
+                    ShoppingDatabase::class.java,
+                    "shopping_database",
+                ).fallbackToDestructiveMigration().build().also { instance = it }
+            }
+        }
+    }
 }
