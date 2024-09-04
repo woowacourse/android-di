d64732e8feb925e0544cadd208bf3f8be7850c45
김진우 <jinwoo3661@naver.com>
2024-09-04 20:16:41 +0900

    feat: SingletonComponent 구현

---
+++ app/src/main/java/woowacourse/shopping/data/di/component/DiSingletonComponent.kt
@@ -0,0 +1,18 @@
+package woowacourse.shopping.data.di.component
+
+object DiSingletonComponent {
+    private val binds: MutableMap<Class<*>, Any> = mutableMapOf()
+    private const val ERROR_DI_MATCH = "No binding Singleton Component %s"
+
+    fun <T : Any> bind(
+        bindClassType: Class<T>,
+        bindClass: Any,
+    ) {
+        binds[bindClassType] = bindClass
+    }
+
+    fun <T : Any> match(bindClassType: Class<T>): T {
+        return binds[bindClassType] as? T
+            ?: throw IllegalArgumentException(ERROR_DI_MATCH.format(bindClassType))
+    }
+}
