a91f99483179c424e6b5cbef9c47a7370a24cc01
sh1mj1 <7wlgns@gmail.com>
2024-09-04 10:18:27 +0900

    refactor(CartViewModel): BaseViewModelFactory 로 뷰모델을 생성한다

--- app/src/main/java/woowacourse/shopping/ui/cart/CartViewModel.kt
+++ app/src/main/java/woowacourse/shopping/ui/cart/CartViewModel.kt
@@ -3,8 +3,10 @@ package woowacourse.shopping.ui.cart
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import woowacourse.shopping.data.CartRepository
 import woowacourse.shopping.model.Product
+import woowacourse.shopping.ui.base.BaseViewModelFactory
 
 class CartViewModel(
     private val cartRepository: CartRepository,
@@ -23,5 +25,12 @@ class CartViewModel(
     fun deleteCartProduct(id: Int) {
         cartRepository.deleteCartProduct(id)
         _onCartProductDeleted.value = true
+    }
+
+    companion object {
+        fun factory(cartRepository: CartRepository): ViewModelProvider.Factory =
+            BaseViewModelFactory {
+                CartViewModel(cartRepository)
+            }
     }
 }
