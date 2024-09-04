61eddfc98894e38a05d5ef2f9f94b35fa96cff43
hxeyexn <kck85075@kumoh.ac.kr>
2024-09-04 22:04:21 +0900

    feat: CartViewModelFactory 구현

--- app/src/main/java/woowacourse/shopping/ui/cart/CartActivity.kt
+++ app/src/main/java/woowacourse/shopping/ui/cart/CartActivity.kt
@@ -2,18 +2,15 @@ package woowacourse.shopping.ui.cart
 
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
 import woowacourse.shopping.R
 import woowacourse.shopping.databinding.ActivityCartBinding
 
 class CartActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityCartBinding.inflate(layoutInflater) }
-
-    private val viewModel by lazy {
-        ViewModelProvider(this)[CartViewModel::class.java]
-    }
+    private val viewModel by viewModels<CartViewModel> { CartViewModelFactory() }
 
     private lateinit var dateFormatter: DateFormatter
 
