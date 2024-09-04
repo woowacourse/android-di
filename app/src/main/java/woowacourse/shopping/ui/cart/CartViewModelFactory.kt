1f53c2d37358b56fd543def5e853e6f5c4565589
aprilgom <aprilgom@gmail.com>
2024-09-04 13:48:06 +0900

    style: ktLintFormatˆ

--- app/src/main/java/woowacourse/shopping/ui/cart/CartViewModelFactory.kt
+++ app/src/main/java/woowacourse/shopping/ui/cart/CartViewModelFactory.kt
@@ -5,8 +5,8 @@ import androidx.lifecycle.ViewModelProvider
 import woowacourse.shopping.data.CartRepository
 
 class CartViewModelFactory(
-    private val cartRepository: CartRepository
-):ViewModelProvider.Factory {
+    private val cartRepository: CartRepository,
+) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(CartViewModel::class.java)) {
             return CartViewModel(cartRepository) as T
