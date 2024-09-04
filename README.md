20fe09a372ad02006a3fc9096181468b370af2d2
hxeyexn <kck85075@kumoh.ac.kr>
2024-09-04 19:59:31 +0900

    refactor: CartRepository가 CartProductDao를 참조하도록 수정

--- README.md
+++ README.md
@@ -13,4 +13,4 @@ Repository 객체를 교체하기 위해 또다른 객체를 만들어 바꿔줘
   - **원인**
     - CartRepository 가 메모리가 아닌 Room DB 의 데이터에 접근하게 되면 ViewModel에 직접적인 변경사항이 발생
   - **해결방안**
-    - [ ] CartProductDao를 수동 주입
+    - [x] CartProductDao를 수동 주입
--- app/src/main/java/woowacourse/shopping/data/CartDefaultRepository.kt
+++ app/src/main/java/woowacourse/shopping/data/CartDefaultRepository.kt
@@ -3,8 +3,9 @@ package woowacourse.shopping.data
 import woowacourse.shopping.model.CartRepository
 import woowacourse.shopping.model.Product
 
-// TODO: Step2 - CartProductDao를 참조하도록 변경
-class CartDefaultRepository: CartRepository {
+class CartDefaultRepository(
+    private val cartProductDao: CartProductDao
+) : CartRepository {
 
     private val cartProducts: MutableList<Product> = mutableListOf()
     override fun addCartProduct(product: Product) {
