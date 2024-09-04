cbc4c50299cc220f259f0d32f397ed4e293f0784
JunJangE <wnswkd486@gmail.com>
2024-09-04 23:31:26 +0900

    feat: AppModule

---
+++ app/src/main/java/woowacourse/shopping/shoppingapp/di/AppModule.kt
@@ -0,0 +1,62 @@
+package woowacourse.shopping.shoppingapp.di
+
+import androidx.lifecycle.SavedStateHandle
+import woowacourse.shopping.data.di.RepositoryModule
+import kotlin.reflect.KClass
+import kotlin.reflect.KFunction
+import kotlin.reflect.full.declaredFunctions
+import kotlin.reflect.jvm.jvmErasure
+
+class AppModule {
+    private val instances = mutableMapOf<KClass<*>, Any>()
+
+    init {
+        initInstances(RepositoryModule::class.declaredFunctions)
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    fun <T : Any> resolve(
+        kClass: KClass<T>,
+        savedStateHandle: SavedStateHandle? = null,
+    ): T {
+        return instances[kClass] as? T ?: createInstance(kClass, savedStateHandle)
+    }
+
+    private fun initInstances(functions: Collection<KFunction<*>>) {
+        functions.forEach { function ->
+            val kClass = function.returnType.jvmErasure
+            val instance = function.call(RepositoryModule)
+            instances[kClass] = instance ?: throw IllegalArgumentException("인스턴스를 생성하지 못했습니다")
+        }
+    }
+
+    private fun <T : Any> createInstance(
+        kClass: KClass<T>,
+        savedStateHandle: SavedStateHandle? = null,
+    ): T {
+        if (savedStateHandle != null) {
+            instances[savedStateHandle::class] = savedStateHandle
+        }
+
+        val constructor = kClass.constructors.first()
+        val params =
+            constructor.parameters.map { parameter ->
+                instances[parameter.type.jvmErasure] ?: throw IllegalArgumentException("인스턴스가 없습니다")
+            }.toTypedArray()
+
+        val instance = constructor.call(*params)
+        instances[kClass] = instance
+
+        return instance
+    }
+
+    companion object {
+        private var instance: AppModule? = null
+
+        fun setInstance() {
+            instance = AppModule()
+        }
+
+        fun getInstance(): AppModule = requireNotNull(instance)
+    }
+}
