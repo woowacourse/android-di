1d884ca09c4c656d7fb797ff9b34e78341e3828d
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-09-04 09:55:12 +0900

    feat: 자동 의존성 주입을 위한 Injector 추가

---
+++ app/src/main/java/woowacourse/shopping/ui/Injector.kt
@@ -0,0 +1,43 @@
+package woowacourse.shopping.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.reflect.KMutableProperty
+import kotlin.reflect.full.declaredMemberProperties
+import kotlin.reflect.jvm.isAccessible
+import kotlin.reflect.typeOf
+import woowacourse.shopping.data.CartRepository
+import woowacourse.shopping.data.ProductRepository
+import woowacourse.shopping.di.AppModule
+
+class Injector(
+    private val appModule: AppModule = ShoppingApplication.appModule,
+) {
+    fun inject(activity: AppCompatActivity) {
+        val properties = activity::class.declaredMemberProperties
+            .filterIsInstance<KMutableProperty<*>>()
+            .filter { it.isLateinit }
+
+        properties.forEach { property ->
+            injectProperty(property, activity)
+        }
+    }
+
+    private fun injectProperty(
+        property: KMutableProperty<*>,
+        activity: AppCompatActivity
+    ) {
+        property.isAccessible = true
+
+        findValueForProperty(property, appModule)?.let { value ->
+            property.setter.call(activity, value)
+        }
+    }
+
+    private fun findValueForProperty(property: KMutableProperty<*>, appModule: AppModule): Any? {
+        return when (property.returnType) {
+            typeOf<CartRepository>() -> appModule.cartRepository
+            typeOf<ProductRepository>() -> appModule.productRepository
+            else -> null
+        }
+    }
+}
