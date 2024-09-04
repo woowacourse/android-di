a5b4914d094cac12d578fe43351cf5940cecfcb7
김진우 <jinwoo3661@naver.com>
2024-09-04 20:16:54 +0900

    feat: AppModule 구현

---
+++ app/src/main/java/woowacourse/shopping/data/di/RepositoryModule.kt
@@ -0,0 +1,29 @@
+package woowacourse.shopping.data.di
+
+import woowacourse.shopping.data.CartRepositoryImpl
+import woowacourse.shopping.data.ProductRepositoryImpl
+import woowacourse.shopping.data.di.component.DiSingletonComponent
+import woowacourse.shopping.domain.CartRepository
+import woowacourse.shopping.domain.ProductRepository
+
+object RepositoryModule {
+
+    fun install() {
+        bindCartRepository()
+        bindProductRepository()
+    }
+
+    private fun bindCartRepository() {
+        DiSingletonComponent.bind(
+            CartRepository::class.java,
+            CartRepositoryImpl(),
+        )
+    }
+
+    private fun bindProductRepository() {
+        DiSingletonComponent.bind(
+            ProductRepository::class.java,
+            ProductRepositoryImpl(),
+        )
+    }
+}
