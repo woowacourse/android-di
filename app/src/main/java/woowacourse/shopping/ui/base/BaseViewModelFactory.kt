7fe48c637369d477e0eee2b651a95de7b691aad9
sh1mj1 <7wlgns@gmail.com>
2024-09-04 17:53:09 +0900

    build(Manifest): shoppingapp 추가

--- app/src/main/java/woowacourse/shopping/ui/base/BaseViewModelFactory.kt
+++ app/src/main/java/woowacourse/shopping/ui/base/BaseViewModelFactory.kt
@@ -3,6 +3,7 @@ package woowacourse.shopping.ui.base
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
+// TODO: 사용하고 있지 않음. 삭제해야 함.
 class BaseViewModelFactory<VM : ViewModel>(
     private val creator: () -> VM,
 ) : ViewModelProvider.Factory {
