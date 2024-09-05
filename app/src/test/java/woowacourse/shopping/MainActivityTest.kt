7668cf4683ab16df2ec432065f3a28f3406e9bf3
sh1mj1 <7wlgns@gmail.com>
2024-09-05 15:34:53 +0900

    test(MainActivity): assert injected repository

--- app/src/test/java/woowacourse/shopping/MainActivityTest.kt
+++ app/src/test/java/woowacourse/shopping/MainActivityTest.kt
@@ -8,10 +8,14 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+import woowacourse.shopping.data.DefaultProductRepository
+import woowacourse.shopping.data.ProductRepository
 import woowacourse.shopping.ui.MainActivity
 import woowacourse.shopping.ui.MainViewModel
 
 @RunWith(RobolectricTestRunner::class)
+@Config(application = ShoppingApplication::class)
 class MainActivityTest {
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
@@ -43,26 +47,46 @@ class MainActivityTest {
         assertThat(viewModel).isNotNull()
     }
 
-    @Test
-    fun `메인 액티비티가 제거되면 주입되었던 DateFormatter 객체에 대한 참조도 사라진다`() {
-        // given
-        val activity =
-            Robolectric
-                .buildActivity(MainActivity::class.java)
-                .create()
-                .get()
-
-        // when
-
-        // then
-    }
 
     @Test
-    fun `메인 액티비티가 회전되어도 DateFormatter 객체에 대한 참조는 사라지면 안된다`() {
+    fun injected_productRepository_is_DefaultProductRepository() {
         // given
+        val activity = Robolectric
+            .buildActivity(MainActivity::class.java)
+            .create()
+            .get()
 
         // when
+        val viewModel = ViewModelProvider(activity)[MainViewModel::class.java]
 
         // then
+        val productRepositoryField = MainViewModel::class.java.getDeclaredField("productRepository")
+        productRepositoryField.isAccessible = true
+        val productRepository = productRepositoryField.get(viewModel) as ProductRepository
+
+        assertThat(productRepository).isInstanceOf(DefaultProductRepository::class.java)
     }
+
+
+    @Test
+    fun injected_cartRepository_is_DefaultCartRepository() {
+        // given
+        val activity = Robolectric
+            .buildActivity(MainActivity::class.java)
+            .create()
+            .get()
+
+        // when
+        val viewModel = ViewModelProvider(activity)[MainViewModel::class.java]
+
+        // then
+        val cartRepositoryField = MainViewModel::class.java.getDeclaredField("productRepository")
+        cartRepositoryField.isAccessible = true
+        val cartRepository = cartRepositoryField.get(viewModel) as ProductRepository
+
+        assertThat(cartRepository).isInstanceOf(DefaultProductRepository::class.java)
+    }
+
+
+
 }
