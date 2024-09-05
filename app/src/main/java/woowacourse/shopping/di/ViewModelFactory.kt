85fbcf9a1bf2e8e0fcd75fbd8ecfcc69b32592ac
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-09-05 16:04:48 +0900

    feat: ViewModel을 생성하고 관리하는 ViewModelFactory 구현

---
+++ app/src/main/java/woowacourse/shopping/di/ViewModelFactory.kt
@@ -0,0 +1,10 @@
+package woowacourse.shopping.di
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class ViewModelFactory : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return DIContainer.getInstance(modelClass.kotlin)
+    }
+}
