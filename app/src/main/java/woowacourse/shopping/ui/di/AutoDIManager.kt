e31ce51d944fe1b05893d72078d1ec5ad5de8b6c
songpink <jejeje04@naver.com>
2024-09-05 14:47:29 +0900

    feat: DI를 자동으로 해주는 객체 AutoDIManager 구현

---
+++ app/src/main/java/woowacourse/shopping/ui/di/AutoDIManager.kt
@@ -0,0 +1,45 @@
+package woowacourse.shopping.ui.di
+
+import androidx.lifecycle.ViewModel
+import woowacourse.shopping.data.CartRepositoryImpl
+import woowacourse.shopping.data.ProductRepositoryImpl
+import kotlin.reflect.KClass
+import kotlin.reflect.full.primaryConstructor
+import kotlin.reflect.jvm.isAccessible
+
+object AutoDIManager {
+    private val repositories: MutableMap<String, Any> = mutableMapOf()
+
+    init {
+        registerRepository(ProductRepositoryImpl)
+        registerRepository(CartRepositoryImpl)
+    }
+
+    private fun registerRepository(repository: Any) {
+        repositories[repository::class.typeParameters.toString()] = repository
+    }
+
+    private fun <T : Any> createInstanceWithParameters(
+        clazz: KClass<T>,
+        parameters: Map<String, Any?>
+    ): T? {
+        val constructor = clazz.primaryConstructor ?: return null
+
+        val args = constructor.parameters.associateWith { parameter ->
+            parameters[parameter.name]
+        }
+        constructor.isAccessible = true
+        return constructor.callBy(args)
+    }
+
+    fun <T : ViewModel> createViewModelFactory(viewModelClass: KClass<T>): GenericViewModelFactory<ViewModel> {
+        val args = mapOf(
+            "productRepository" to ProductRepositoryImpl,
+            "cartRepository" to CartRepositoryImpl,
+        )
+
+        return GenericViewModelFactory(viewModelClass) {
+            createInstanceWithParameters(viewModelClass, args) ?: error("")
+        }
+    }
+}
