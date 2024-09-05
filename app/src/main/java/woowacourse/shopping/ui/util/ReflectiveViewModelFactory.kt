1a9a36f49bf9322ba4e7dbadf6f689bd57cfc4cd
Junyoung-WON <wjyo60@gmail.com>
2024-09-05 15:37:24 +0900

    feat: Application 생성 후 개선된 DependencyContainer 적용
    - ShoppingApplication 작성 및 ViewModelDependencyContainer 인스턴스 생성
    - ReflectiveViewModelFactory에 개선된 ViewModelDependencyContainer 적용
    - Manifest에 ShoppingApplication 등록
    - 1단계 기능 요구 사항 확인

--- app/src/main/java/woowacourse/shopping/ui/util/ReflectiveViewModelFactory.kt
+++ app/src/main/java/woowacourse/shopping/ui/util/ReflectiveViewModelFactory.kt
@@ -3,9 +3,11 @@ package woowacourse.shopping.ui.util
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
+import woowacourse.shopping.ui.ShoppingApplication.Companion.viewModelDependencyContainer
 
 class ReflectiveViewModelFactory : ViewModelProvider.Factory {
-    private val dependencyInjector = DependencyInjector(ViewModelDependencyContainer)
+    private val dependencyInjector =
+        DependencyInjector(viewModelDependencyContainer)
 
     override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
         return dependencyInjector.createInstanceFromConstructor(modelClass)
