01ba28b4761b0561e8e88e2650a9c7ad5581d3b6
Junyoung-WON <wjyo60@gmail.com>
2024-09-05 15:30:17 +0900

    feat: DependencyContainer와 구현체 개선
    DependencyContainer 개선
    - 동적으로 의존성을 추가하는 setDependency 메서드 추가
    
    ViewModelDependencyContainer 개선
    - object -> class로 타입 변경
    - ViewModelDependencyContainer에 최초 한번 생성된 인스턴스를 저장하는 cachedInstances 프로퍼티를 추가
    - 이후 의존성을 확인할 때 캐싱된 인스턴스가 있다면 이를 반환한다.
    - 캐싱된 것이 없다면 저장된 의존성을 찾아 reflection을 활용하여 인스턴스를 생성, 저장 후 반환한다.

--- app/src/main/java/woowacourse/shopping/ui/util/DependencyContainer.kt
+++ app/src/main/java/woowacourse/shopping/ui/util/DependencyContainer.kt
@@ -1,7 +1,10 @@
 package woowacourse.shopping.ui.util
 
+import kotlin.reflect.KClass
 import kotlin.reflect.KClassifier
 
 interface DependencyContainer {
     fun <T : Any> getInstance(kClassifier: KClassifier): T
+
+    fun <T : Any> setDependency(kClassifier: KClassifier, kClass: KClass<T>)
 }
--- app/src/main/java/woowacourse/shopping/ui/util/ViewModelDependencyContainer.kt
+++ app/src/main/java/woowacourse/shopping/ui/util/ViewModelDependencyContainer.kt
@@ -1,22 +1,25 @@
 package woowacourse.shopping.ui.util
 
-import woowacourse.shopping.data.repository.CartDefaultRepository
-import woowacourse.shopping.data.repository.ProductDefaultRepository
-import woowacourse.shopping.domain.repository.CartRepository
-import woowacourse.shopping.domain.repository.ProductRepository
 import kotlin.reflect.KClass
 import kotlin.reflect.KClassifier
 import kotlin.reflect.full.createInstance
 
-object ViewModelDependencyContainer : DependencyContainer {
-    private val dependencies: Map<KClassifier, KClass<*>> =
-        mapOf(
-            ProductRepository::class to ProductDefaultRepository::class,
-            CartRepository::class to CartDefaultRepository::class
-        )
+class ViewModelDependencyContainer : DependencyContainer {
+    private val dependencies: MutableMap<KClassifier, KClass<*>> =
+        mutableMapOf()
+
+    private val cachedInstances: MutableMap<KClassifier, Any> =
+        mutableMapOf()
 
     override fun <T : Any> getInstance(kClassifier: KClassifier): T {
-        val kClass = dependencies[kClassifier] ?: throw IllegalArgumentException("$kClassifier : 알 수 없는 클래스 지정자 입니다.")
-        return kClass.createInstance() as T
+        return cachedInstances.getOrPut(kClassifier) {
+            val kClass = dependencies[kClassifier]
+                ?: throw IllegalArgumentException("$kClassifier : 알 수 없는 클래스 지정자 입니다.")
+            kClass.createInstance()
+        } as T
+    }
+
+    override fun <T : Any> setDependency(kClassifier: KClassifier, kClass: KClass<T>) {
+        dependencies[kClassifier] = kClass
     }
 }
