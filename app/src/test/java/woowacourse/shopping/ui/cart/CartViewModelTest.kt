4d40ad0921dd268d0768ef47584504a453cb8de3
sh1mj1 <7wlgns@gmail.com>
2024-09-05 16:28:31 +0900

    test(CartViewModelTest): 장바구니에 있는 상품 삭제 이벤트

---
+++ app/src/test/java/woowacourse/shopping/ui/cart/CartViewModelTest.kt
@@ -0,0 +1,41 @@
+package woowacourse.shopping.ui.cart
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import woowacourse.shopping.ProductFixture
+import woowacourse.shopping.data.CartRepository
+import woowacourse.shopping.data.FakeCartRepository
+import woowacourse.shopping.getOrAwaitValue
+
+class CartViewModelTest {
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var cartRepository: CartRepository
+    private lateinit var vm: CartViewModel
+
+    @Before
+    fun setUp() {
+        cartRepository = FakeCartRepository(
+            ProductFixture(1),
+            ProductFixture(2),
+        )
+        vm = CartViewModel(cartRepository)
+    }
+
+    @Test
+    fun `장바구니에 있는 상품 삭제 이벤트`() {
+        // when
+        vm.deleteCartProduct(1)
+
+        // then
+        assertThat(vm.onCartProductDeleted.getOrAwaitValue()).isEqualTo(true)
+
+    }
+
+
+}
