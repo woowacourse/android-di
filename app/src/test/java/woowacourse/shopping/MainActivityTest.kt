81c27af2117e9439d609fed1602bb9eb35eda5b1
gaeun5744 <geun5744@gmail.com>
2024-09-05 16:09:33 +0900

    test: 변경된 코드에 맞춰 테스트코드 수정

--- app/src/test/java/woowacourse/shopping/MainActivityTest.kt
+++ app/src/test/java/woowacourse/shopping/MainActivityTest.kt
@@ -1,42 +1,46 @@
 package woowacourse.shopping
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModelProvider
 import com.google.common.truth.Truth.assertThat
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.android.controller.ActivityController
 import woowacourse.shopping.ui.MainActivity
 import woowacourse.shopping.ui.MainViewModel
+import woowacourse.shopping.ui.injection.RepositoryModule
 
 
 @RunWith(RobolectricTestRunner::class)
 class MainActivityTest {
 
+    private lateinit var activity: MainActivity
+
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
 
+    @Before
+    fun setUp() {
+        val controller = Robolectric.buildActivity(MainActivity::class.java)
+        RepositoryModule.setLifeCycle(controller.get())
+        RepositoryModule.getInstance().onCreate(controller.get() as LifecycleOwner)
+        activity = controller.create().get()
+    }
+
+
     @Test
     fun `Activity 실행 테스트`() {
-        // given
-        val activity = Robolectric
-            .buildActivity(MainActivity::class.java)
-            .create()
-            .get()
-
-        // then
         assertThat(activity).isNotNull()
     }
 
     @Test
     fun `ViewModel 주입 테스트`() {
         // given
-        val activity = Robolectric
-            .buildActivity(MainActivity::class.java)
-            .create()
-            .get()
         val viewModel = ViewModelProvider(activity)[MainViewModel::class.java]
 
         // then
