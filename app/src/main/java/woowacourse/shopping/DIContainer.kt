964b362234c48c569c3c5dfc0b850a8630b5d8f9
YECHAN KIM <74256335+ii2001@users.noreply.github.com>
2024-09-06 14:41:44 +0900

    [하디] 1단계 자동 DI 미션 제출합니다 (#82)
    고생하셨습니다~~ 👍

---
+++ app/src/main/java/woowacourse/shopping/DIContainer.kt
@@ -0,0 +1,25 @@
+package woowacourse.shopping
+
+object DIContainer {
+    private val instances = mutableMapOf<Class<*>, Any>()
+
+    fun <T : Any> register(
+        clazz: Class<T>,
+        instance: T,
+    ) {
+        instances[clazz] = instance
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    fun <T : Any> resolve(clazz: Class<T>): T {
+        return instances[clazz] as? T ?: createInstance(clazz)
+    }
+
+    private fun <T : Any> createInstance(clazz: Class<T>): T {
+        val constructor = clazz.constructors.first()
+        val params = constructor.parameterTypes.map { resolve(it) }.toTypedArray()
+        val instance = constructor.newInstance(*params) as T
+        register(clazz, instance)
+        return instance
+    }
+}
---
+++ app/src/main/java/woowacourse/shopping/ui/GenericViewModelFactory.kt
@@ -0,0 +1,11 @@
+package woowacourse.shopping.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import woowacourse.shopping.DIContainer
+
+class GenericViewModelFactory : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return DIContainer.resolve(modelClass)
+    }
+}
