98cc1fa3eba8b02117de91949acb145c05b3bc04
Hogu59 <dbstjdguswkd@naver.com>
2024-09-08 01:14:40 +0900

    refactor(DIContainer): make throw npe when there is no instance
    - about getInstance method

--- app/src/main/java/woowacourse/shopping/di/DIContainer.kt
+++ app/src/main/java/woowacourse/shopping/di/DIContainer.kt
@@ -1,12 +1,11 @@
 package woowacourse.shopping.di
 
 import kotlin.reflect.KClass
-import kotlin.reflect.full.createInstance
 
 object DIContainer {
     private val instances = mutableMapOf<KClass<*>, Any>()
 
-    fun getInstance(type: KClass<*>): Any = instances[type] ?: type.createInstance()
+    fun getInstance(type: KClass<*>): Any = instances[type] ?: throw NullPointerException("No Instance Found")
 
     fun setInstance(
         type: KClass<*>,
