944dadd0bdbe4cf7fb8d17f3ccc89f4aafcf6873
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-09-07 19:04:25 +0900

    refactor: ViewModelFactory 생성 방식 변경

--- app/src/main/java/woowacourse/shopping/ui/util/ViewModelFactory.kt
+++ app/src/main/java/woowacourse/shopping/ui/util/ViewModelFactory.kt
@@ -2,6 +2,35 @@ package woowacourse.shopping.ui.util
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import kotlin.reflect.KClass
+import kotlin.reflect.full.primaryConstructor
+import woowacourse.shopping.di.InstanceContainer
+
+class ViewModelFactory(
+    private val viewModelClass: KClass<out ViewModel>,
+    private val instanceContainer: InstanceContainer,
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        require(modelClass.isAssignableFrom(viewModelClass.java)) { EXCEPTION_VIEWMODEL_NOT_FOUND }
+
+        val primaryConstructor = viewModelClass.primaryConstructor
+        requireNotNull(primaryConstructor) { EXCEPTION_NO_PRIMARY_CONSTRUCTOR.format(viewModelClass.simpleName) }
+
+        val primaryConstructorParameters = primaryConstructor.parameters
+        val instance = primaryConstructor.callBy(
+            primaryConstructorParameters.associateWith { parameter ->
+                instanceContainer.instanceOf(parameter.type)
+            }
+        )
+
+        return instance as T
+    }
+
+    companion object {
+        private const val EXCEPTION_VIEWMODEL_NOT_FOUND = "ViewModel class not found"
+        private const val EXCEPTION_NO_PRIMARY_CONSTRUCTOR = "No primary constructor found for %s"
+    }
+}
 
 fun <VM : ViewModel> viewModelFactory(creator: () -> VM): ViewModelProvider.Factory {
     return object : ViewModelProvider.Factory {
