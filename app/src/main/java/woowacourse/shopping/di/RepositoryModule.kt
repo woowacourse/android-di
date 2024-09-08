f10b742a318842a1348507cdaeaaad4a9f9503ec
김진우 <85734140+jinuemong@users.noreply.github.com>
2024-09-08 23:49:07 +0900

    [누누] 1단계 자동 DI 미션 제출합니다 (#83)
    * docs: 기능 요구사항 작성
    
    * feat: 테스트하기 어려운 코드를 수정
    
    * feat: ViewModel에 직접적인 변경 사항 수정
    
    * feat: SingletonComponent 구현
    
    * feat: AppModule 구현
    
    * feat: Application 생성
    
    * feat: ViewModelComponent 생성
    
    * feat: ViewModel 적용
    
    * docs: 기능 요구 사항 점검
    
    * style: 코드 포맷 수정 & 린트 체크
    
    * refactor : type message fix
    
    * refactor : change di layer package
    
    * style: code format fix & line check

---
+++ app/src/main/java/woowacourse/shopping/di/RepositoryModule.kt
@@ -0,0 +1,28 @@
+package woowacourse.shopping.di
+
+import woowacourse.shopping.data.CartRepositoryImpl
+import woowacourse.shopping.data.ProductRepositoryImpl
+import woowacourse.shopping.di.component.DiSingletonComponent
+import woowacourse.shopping.domain.CartRepository
+import woowacourse.shopping.domain.ProductRepository
+
+object RepositoryModule {
+    fun install() {
+        bindCartRepository()
+        bindProductRepository()
+    }
+
+    private fun bindCartRepository() {
+        DiSingletonComponent.bind(
+            CartRepository::class.java,
+            CartRepositoryImpl(),
+        )
+    }
+
+    private fun bindProductRepository() {
+        DiSingletonComponent.bind(
+            ProductRepository::class.java,
+            ProductRepositoryImpl(),
+        )
+    }
+}
