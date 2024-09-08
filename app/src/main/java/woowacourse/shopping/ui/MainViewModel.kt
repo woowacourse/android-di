4b41bfff1c99e1f07c1522b565f60c69df0fa767
gaeun5744 <geun5744@gmail.com>
2024-09-08 21:46:50 +0900

    feat: DI 주입 로직 인터페이스 & 검사 로직 구현

--- app/src/main/java/woowacourse/shopping/ui/MainViewModel.kt
+++ app/src/main/java/woowacourse/shopping/ui/MainViewModel.kt
@@ -6,16 +6,21 @@ import androidx.lifecycle.ViewModel
 import woowacourse.shopping.model.Product
 import woowacourse.shopping.model.repository.CartRepository
 import woowacourse.shopping.model.repository.ProductRepository
+import woowacourse.shopping.ui.injection.DIInjection
 
 class MainViewModel(
     private val productRepository: ProductRepository,
     private val cartRepository: CartRepository,
-) : ViewModel() {
+) : ViewModel(), DIInjection {
     private val _products: MutableLiveData<List<Product>> = MutableLiveData(emptyList())
     val products: LiveData<List<Product>> get() = _products
 
     private val _onProductAdded: MutableLiveData<Boolean> = MutableLiveData(false)
     val onProductAdded: LiveData<Boolean> get() = _onProductAdded
+
+    init {
+        checkConstructor()
+    }
 
     fun addCartProduct(product: Product) {
         cartRepository.addCartProduct(product)
--- app/src/main/java/woowacourse/shopping/ui/cart/CartViewModel.kt
+++ app/src/main/java/woowacourse/shopping/ui/cart/CartViewModel.kt
@@ -5,10 +5,15 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import woowacourse.shopping.model.Product
 import woowacourse.shopping.model.repository.CartRepository
+import woowacourse.shopping.ui.injection.DIInjection
 
 class CartViewModel(
     private val cartRepository: CartRepository,
-) : ViewModel() {
+) : ViewModel(), DIInjection {
+    init {
+        checkConstructor()
+    }
+
     private val _cartProducts: MutableLiveData<List<Product>> =
         MutableLiveData(emptyList())
     val cartProducts: LiveData<List<Product>> get() = _cartProducts
