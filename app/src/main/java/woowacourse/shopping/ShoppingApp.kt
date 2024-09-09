3c902d3a26c1333e8ca31904722f46526b279981
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-09-09 16:15:32 +0900

    style: ktlint 적용

--- app/src/main/java/woowacourse/shopping/ShoppingApp.kt
+++ app/src/main/java/woowacourse/shopping/ShoppingApp.kt
@@ -1,9 +1,8 @@
 package woowacourse.shopping
 
 import android.app.Application
-import woowacourse.shopping.di.RepositoryModule
 import woowacourse.shopping.di.DIContainer
-import woowacourse.shopping.di.DependencyInjector
+import woowacourse.shopping.di.RepositoryModule
 
 class ShoppingApp : Application() {
     override fun onCreate() {
