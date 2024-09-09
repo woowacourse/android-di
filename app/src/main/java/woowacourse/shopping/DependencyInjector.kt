2e21fffbbef56f7ffe55701872e80e072825022a
hxeyexn <kck85075@kumoh.ac.kr>
2024-09-10 01:01:13 +0900

    feat: 의존성 주입 순환 참조 로직 제거 및 instances 의 key 값 타입 변경

--- app/src/main/java/woowacourse/shopping/DependencyInjector.kt
+++ app/src/main/java/woowacourse/shopping/DependencyInjector.kt
@@ -4,41 +4,49 @@ import android.content.Context
 import woowacourse.shopping.data.ShoppingDatabase
 import woowacourse.shopping.model.CartRepository
 import woowacourse.shopping.model.ProductRepository
-import java.lang.reflect.Constructor
+import kotlin.reflect.KClass
+import kotlin.reflect.KFunction
+import kotlin.reflect.full.primaryConstructor
+
+typealias instance = Any
 
 object DependencyInjector {
-    private val instances = mutableMapOf<Class<*>, Any>()
+    private val instances = mutableMapOf<KClass<*>, instance>()
 
     private const val CONSTRUCTOR_NOT_FOUND = "적합한 생성자를 찾을 수 없습니다."
-    private const val MINIMUM_PARAMETER_COUNT = 0
+    private const val DEPENDENCY_TYPE_IS_INVALID = "의존성 클래스 타입이 올바르지 않습니다."
 
     fun initialize(context: Context) {
         val cartProductDao = ShoppingDatabase.initialize(context).cartProductDao()
-        instances[ProductRepository::class.java] = RepositoryModule.provideProductRepository()
-        instances[CartRepository::class.java] = RepositoryModule.provideCartRepository(cartProductDao)
+        instances[ProductRepository::class] = RepositoryModule.provideProductRepository()
+        instances[CartRepository::class] = RepositoryModule.provideCartRepository(cartProductDao)
     }
 
-    fun <T> getInstance(clazz: Class<T>): T {
-        instances[clazz]?.let { instance -> return instance as T }
+    fun <T : Any> getInstance(clazz: Class<T>): T {
+        val kClass: KClass<T> = clazz.kotlin
+        instances[kClass]?.let { instance -> return instance as T }
 
-        val instance = createInstance(clazz)
-        instances[clazz] = instance as Any
+        val instance: T = createInstance(kClass)
+        instances[kClass] = instance
 
         return instance
     }
 
-    private fun <T> createInstance(clazz: Class<T>): T {
-        val constructor: Constructor<*> =
-            clazz.declaredConstructors.firstOrNull { it.parameterCount > MINIMUM_PARAMETER_COUNT }
-                ?: throw IllegalArgumentException(CONSTRUCTOR_NOT_FOUND)
+    private fun <T : Any> createInstance(clazz: KClass<T>): T {
+        val constructor: KFunction<T> = getPrimaryConstructor(clazz)
+        val dependencies: List<Any?> = constructor.extractDependencies()
 
-        constructor.isAccessible = true
-
-        val dependencies =
-            constructor.parameterTypes.map { paramClass ->
-                getInstance(paramClass)
-            }
-
-        return constructor.newInstance(*dependencies.toTypedArray()) as T
+        return constructor.call(*dependencies.toTypedArray())
     }
+
+    private fun <T : Any> getPrimaryConstructor(clazz: KClass<T>): KFunction<T> =
+        clazz.primaryConstructor ?: throw IllegalArgumentException(CONSTRUCTOR_NOT_FOUND)
+
+    private fun <T : Any> KFunction<T>.extractDependencies(): List<Any?> =
+        parameters.map { parameter ->
+            when (val classifier = parameter.type.classifier) {
+                is KClass<*> -> instances[classifier]
+                else -> throw IllegalArgumentException(DEPENDENCY_TYPE_IS_INVALID)
+            }
+        }
 }
