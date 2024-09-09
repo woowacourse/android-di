4150da6848afa4ba7ff875b42e9ced68e9db2e0e
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-09-09 15:22:40 +0900

    refactor: module 이름 변경

--- app/src/main/java/woowacourse/shopping/ShoppingApp.kt
+++ app/src/main/java/woowacourse/shopping/ShoppingApp.kt
@@ -1,12 +1,12 @@
 package woowacourse.shopping
 
 import android.app.Application
-import woowacourse.shopping.di.AppModule
+import woowacourse.shopping.di.RepositoryModule
 import woowacourse.shopping.di.DIContainer
 
 class ShoppingApp : Application() {
     override fun onCreate() {
         super.onCreate()
-        DIContainer.loadModule(AppModule())
+        DIContainer.loadModule(RepositoryModule())
     }
 }
