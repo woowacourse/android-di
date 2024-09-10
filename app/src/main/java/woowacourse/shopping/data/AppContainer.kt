e17aff983ff17be7d22dba59eeeb829a73607115
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-09-10 17:18:55 +0900

    refactor: 사용하지 않는 파일 삭제

--- app/src/main/java/woowacourse/shopping/data/AppContainer.kt
+++
@@ -1,9 +0,0 @@
-package woowacourse.shopping.data
-
-import woowacourse.shopping.domain.CartRepository
-import woowacourse.shopping.domain.ProductRepository
-
-interface AppContainer {
-    val productRepository: ProductRepository
-    val cartRepository: CartRepository
-}
