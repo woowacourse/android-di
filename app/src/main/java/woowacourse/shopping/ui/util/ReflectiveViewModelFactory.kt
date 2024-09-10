c2d5d03643f9cc45026aa78adc908fd5dc23b3c4
Junyoung-WON <wjyo60@gmail.com>
2024-09-10 14:03:21 +0900

    test: 의존성 주입 테스트 작성
    - Fake 객체 추가
    - ReflectiveViewModelFactory 의존성 주입이 가능하도록 수정
    - DependencyInjector 테스트 작성

--- app/src/main/java/woowacourse/shopping/ui/util/ReflectiveViewModelFactory.kt
+++ app/src/main/java/woowacourse/shopping/ui/util/ReflectiveViewModelFactory.kt
@@ -5,10 +5,11 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import woowacourse.shopping.ui.ShoppingApplication.Companion.viewModelDependencyContainer
 
-class ReflectiveViewModelFactory : ViewModelProvider.Factory {
-    private val dependencyInjector =
-        DependencyInjector(viewModelDependencyContainer)
-
+class ReflectiveViewModelFactory(
+    private val dependencyInjector: DependencyInjector = DependencyInjector(
+        viewModelDependencyContainer
+    ),
+) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
         return dependencyInjector.createInstanceFromConstructor(modelClass)
     }
--- app/src/test/java/woowacourse/shopping/Fake.kt
+++ app/src/test/java/woowacourse/shopping/Fake.kt
@@ -1,8 +1,17 @@
 package woowacourse.shopping
 
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
 import woowacourse.shopping.domain.model.Product
 import woowacourse.shopping.domain.repository.CartRepository
 import woowacourse.shopping.domain.repository.ProductRepository
+import woowacourse.shopping.ui.util.DependencyContainer
+import woowacourse.shopping.ui.util.DependencyInjector
+import woowacourse.shopping.ui.util.ReflectiveViewModelFactory
+import kotlin.reflect.KClass
+import kotlin.reflect.KClassifier
+import kotlin.reflect.full.createInstance
 
 class FakeCartRepository(
     private val cartProducts: MutableList<Product>,
@@ -46,3 +55,46 @@ val fakeProducts: MutableList<Product> =
             imageUrl = "https://cdn-mart.baemin.com/sellergoods/main/e703c53e-5d01-4b20-bd33-85b5e778e73f.jpg?h=700&w=700"
         ),
     )
+
+interface FakeRepository
+
+class FakeRepositoryImpl : FakeRepository
+
+class FakeViewModel(
+    val fakeRepository: FakeRepository,
+) : ViewModel()
+
+class FakeActivity : AppCompatActivity() {
+    val viewModel: FakeViewModel by viewModels {
+        ReflectiveViewModelFactory(DependencyInjector(fakeDependencyContainer))
+    }
+}
+
+class FakeDependencyContainer : DependencyContainer {
+    private val dependencies: MutableMap<KClassifier, KClass<*>> =
+        mutableMapOf()
+
+    private val cachedInstances: MutableMap<KClassifier, Any> =
+        mutableMapOf()
+
+    init {
+        setDependency(
+            FakeRepository::class,
+            FakeRepositoryImpl::class,
+        )
+    }
+
+    override fun <T : Any> getInstance(kClassifier: KClassifier): T {
+        return cachedInstances.getOrPut(kClassifier) {
+            val kClass = dependencies[kClassifier]
+                ?: throw IllegalArgumentException("$kClassifier : 알 수 없는 클래스 지정자 입니다.")
+            kClass.createInstance()
+        } as T
+    }
+
+    override fun <T : Any> setDependency(kClassifier: KClassifier, kClass: KClass<T>) {
+        dependencies[kClassifier] = kClass
+    }
+}
+
+val fakeDependencyContainer: DependencyContainer = FakeDependencyContainer()
