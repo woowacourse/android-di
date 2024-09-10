36d1a743bde0bc6d6ada412cf4fc30bb283db1c9
haeum808 <haeumueah@gmail.com>
2024-09-10 14:17:31 +0900

    refactor: dependencies 사용 안하게 변경

--- app/src/main/java/woowacourse/shopping/DIContainer.kt
+++ app/src/main/java/woowacourse/shopping/DIContainer.kt
@@ -9,13 +9,9 @@ import kotlin.reflect.KClass
 import kotlin.reflect.full.createInstance
 import kotlin.reflect.full.hasAnnotation
 import kotlin.reflect.full.primaryConstructor
+import kotlin.reflect.jvm.internal.impl.metadata.jvm.deserialization.JvmMemberSignature.Method
 
-class DIContainer(
-    val dependencies: Map<KClass<*>, Any> = mapOf(
-        ProductRepository::class to DefaultProductRepository,
-        CartRepository::class to DefaultCartRepository,
-    )
-) {
+class DIContainer {
     inline fun <reified T : Any> createViewModel(
         modelClass: KClass<T>,
         vararg any: Any? = emptyArray()
@@ -25,7 +21,11 @@ class DIContainer(
         val constructorArgs = primaryConstructor.parameters.map { parameter ->
             if (parameter.hasAnnotation<Inject>()) {
                 val dependencyClass = parameter.type.classifier as? KClass<*>
-                dependencies[dependencyClass]
+
+                val getMethod = Module::class.java.declaredMethods.find {
+                    it.returnType == dependencyClass?.java
+                }
+                getMethod?.invoke(Module)
             } else {
                 any[index++]
             }
---
+++ app/src/main/java/woowacourse/shopping/Module.kt
@@ -0,0 +1,16 @@
+package woowacourse.shopping
+
+import woowacourse.shopping.data.CartRepository
+import woowacourse.shopping.data.DefaultCartRepository
+import woowacourse.shopping.data.DefaultProductRepository
+import woowacourse.shopping.data.ProductRepository
+
+object Module {
+    fun provideCartRepository(): CartRepository {
+        return DefaultCartRepository
+    }
+
+    fun provideProductRepository(): ProductRepository {
+        return DefaultProductRepository
+    }
+}
