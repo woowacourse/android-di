9235adb12486f3e30852eb0fc564ce01fd31b6b0
haeum808 <haeumueah@gmail.com>
2024-09-10 11:41:28 +0900

    chore: 필요없는 어노테이션 삭제

--- app/src/main/java/woowacourse/shopping/DIAnnotations.kt
+++ app/src/main/java/woowacourse/shopping/DIAnnotations.kt
@@ -1,6 +1,4 @@
 package woowacourse.shopping
 
-annotation class Provides
-
 @Target(AnnotationTarget.CONSTRUCTOR, AnnotationTarget.PROPERTY, AnnotationTarget.VALUE_PARAMETER)
 annotation class Inject
