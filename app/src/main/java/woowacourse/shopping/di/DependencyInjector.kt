258af6da5302ee2b88cff7f0d1142e3de6e1cb4a
somin <salth0601@gmail.com>
2024-09-10 16:10:23 +0900

    refactor: DependencyInjector의 코드 중복 수정

--- app/src/main/java/woowacourse/shopping/di/DependencyInjector.kt
+++ app/src/main/java/woowacourse/shopping/di/DependencyInjector.kt
@@ -8,8 +8,8 @@ import kotlin.reflect.full.primaryConstructor
 class DependencyInjector(private val registry: DependencyRegistry) {
     fun <T : Any> createWithAutoDI(classType: KClass<T>): T {
         val constructor = getPrimaryConstructor(classType)
-        val constructorArgs = resolveDependencies(constructor.parameters)
-        val instance = createInstance(classType, constructorArgs)
+        val dependencies = resolveDependencies(constructor.parameters)
+        val instance = constructor.callBy(dependencies)
         registry.addInstance(classType, instance)
         return instance
     }
@@ -18,19 +18,11 @@ class DependencyInjector(private val registry: DependencyRegistry) {
         return classType.primaryConstructor ?: throw IllegalArgumentException("주생성자가 존재하지 않습니다.")
     }
 
-    private fun resolveDependencies(parameters: List<KParameter>): List<Any> {
-        return parameters.map { parameter ->
+    private fun resolveDependencies(parameters: List<KParameter>): Map<KParameter, Any> {
+        return parameters.associateWith { parameter ->
             val parameterType = parameter.type.classifier as KClass<*>
             registry.getInstanceOrNull(parameterType)
                 ?: throw IllegalArgumentException("해당 파라미터 타입에 해당하는 인스턴스를 찾을 수 없습니다: $parameterType")
         }
-    }
-
-    private fun <T : Any> createInstance(
-        classType: KClass<T>,
-        constructorArgs: List<Any>,
-    ): T {
-        return classType.primaryConstructor?.call(*constructorArgs.toTypedArray())
-            ?: throw IllegalArgumentException("인스턴스를 생성할 수 없습니다.")
     }
 }
