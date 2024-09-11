00cbdd2fe307beee5978707a4e7d0644773106b3
JoYehyun <jo990714@naver.com>
2024-09-12 02:33:48 +0900

    feat: 필드 주입 구현

--- app/src/main/java/woowacourse/shopping/di/Injector.kt
+++ app/src/main/java/woowacourse/shopping/di/Injector.kt
@@ -1,15 +1,19 @@
 package woowacourse.shopping.di
 
 import kotlin.reflect.KClass
+import kotlin.reflect.KMutableProperty
+import kotlin.reflect.full.declaredMemberProperties
 import kotlin.reflect.full.hasAnnotation
 import kotlin.reflect.full.primaryConstructor
+import kotlin.reflect.jvm.isAccessible
 import kotlin.reflect.jvm.jvmErasure
 
 fun <T : Any> inject(
     modelClass: KClass<T>,
     dependencies: DependencyProvider,
 ): T {
-    val constructor = requireNotNull(modelClass.primaryConstructor) { "No suitable constructor found for $modelClass" }
+    val constructor =
+        requireNotNull(modelClass.primaryConstructor) { "No suitable constructor found for $modelClass" }
     val parameters =
         constructor.parameters
             .filter { it.hasAnnotation<Inject>() }
@@ -18,5 +22,20 @@ fun <T : Any> inject(
                 dependencies.getInstance(paramsTypes) ?: inject(paramsTypes, dependencies)
             }
 
-    return constructor.callBy(parameters)
+    return constructor.callBy(parameters).also { injectFields(it, dependencies) }
+}
+
+private fun <T : Any> injectFields(
+    instance: T,
+    dependencies: DependencyProvider,
+) {
+    val properties =
+        instance::class.declaredMemberProperties
+            .filterIsInstance<KMutableProperty<*>>()
+            .filter { it.hasAnnotation<Inject>() }
+
+    properties.forEach { property ->
+        property.isAccessible = true
+        property.setter.call(instance, dependencies.getInstance(property.returnType.jvmErasure))
+    }
 }
