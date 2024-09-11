2caa141e74bc5ec6da58683799b72be537002e55
Junyoung-WON <wjyo60@gmail.com>
2024-09-12 05:10:14 +0900

    feat: ShoppingDatabase의 인스턴스 생성 및 저장 로직 추가

--- app/src/main/java/woowacourse/shopping/data/local/ShoppingDatabase.kt
+++ app/src/main/java/woowacourse/shopping/data/local/ShoppingDatabase.kt
@@ -1,10 +1,29 @@
 package woowacourse.shopping.data.local
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
-
 
 @Database(entities = [CartProductEntity::class], version = 1, exportSchema = false)
 abstract class ShoppingDatabase : RoomDatabase() {
     abstract fun cartProductDao(): CartProductDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: ShoppingDatabase? = null
+
+        fun getInstance(context: Context): ShoppingDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance =
+                    Room.databaseBuilder(
+                        context.applicationContext,
+                        ShoppingDatabase::class.java,
+                        "shopping_database"
+                    ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
 }
