0b02d23fa3ce4dc18071022eafb5d6a48a2d99e8
Junyoung-WON <wjyo60@gmail.com>
2024-09-11 09:55:07 +0900

    refactor: requireNotNull 활용 및 에러 메시지 상수화

--- app/src/main/java/woowacourse/shopping/ui/util/DependencyInjector.kt
+++ app/src/main/java/woowacourse/shopping/ui/util/DependencyInjector.kt
@@ -6,14 +6,21 @@ import kotlin.reflect.full.primaryConstructor
 class DependencyInjector(private val dependencyContainer: DependencyContainer) {
     fun <T : Any> createInstanceFromConstructor(modelClass: Class<T>): T {
         val kClass: KClass<T> = modelClass.kotlin
-        val primaryConstructor = kClass.primaryConstructor
-            ?: throw IllegalArgumentException("${kClass.java.simpleName}에 대한 주 생성자가 없음.")
+        val primaryConstructor =
+            requireNotNull(kClass.primaryConstructor) {
+                "${kClass.java.simpleName} : $ERROR_MESSAGE_NO_PRIMARY_CONSTRUCTOR"
+            }
 
-        val constructorParams = primaryConstructor.parameters.map { param ->
-            val kParamType = param.type.classifier as KClass<*>
-            dependencyContainer.getInstance<Any>(kParamType)
-        }.toTypedArray()
+        val constructorParams =
+            primaryConstructor.parameters.map { param ->
+                val kParamType = param.type.classifier as KClass<*>
+                dependencyContainer.getInstance<Any>(kParamType)
+            }.toTypedArray()
 
         return primaryConstructor.call(*constructorParams)
+    }
+
+    companion object {
+        private const val ERROR_MESSAGE_NO_PRIMARY_CONSTRUCTOR = "해당 클래스의 주 생성자가 없습니다."
     }
 }
--- app/src/main/java/woowacourse/shopping/ui/util/ViewModelDependencyContainer.kt
+++ app/src/main/java/woowacourse/shopping/ui/util/ViewModelDependencyContainer.kt
@@ -13,13 +13,19 @@ class ViewModelDependencyContainer : DependencyContainer {
 
     override fun <T : Any> getInstance(kClassifier: KClassifier): T {
         return cachedInstances.getOrPut(kClassifier) {
-            val kClass = dependencies[kClassifier]
-                ?: throw IllegalArgumentException("$kClassifier : 알 수 없는 클래스 지정자 입니다.")
+            val kClass =
+                requireNotNull(dependencies[kClassifier]) {
+                    "$kClassifier : $ERROR_MESSAGE_UNKNOWN_CLASSIFIER"
+                }
             kClass.createInstance()
         } as T
     }
 
     override fun <T : Any> setDependency(kClassifier: KClassifier, kClass: KClass<T>) {
         dependencies[kClassifier] = kClass
+    }
+
+    companion object {
+        private const val ERROR_MESSAGE_UNKNOWN_CLASSIFIER = "알 수 없는 클래스 지정자 입니다."
     }
 }
