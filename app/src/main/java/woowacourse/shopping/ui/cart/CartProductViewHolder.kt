f0e40584e64b7635f676a1710e667598bd6ba87e
Junyoung-WON <wjyo60@gmail.com>
2024-09-12 05:23:43 +0900

    refactor: 장바구니의 아이템 삭제 및 바인딩 로직 변경
    - 삭제에 대한 콜백 함수 수정
    - 장바구니에 추가된 시간 바인딩하여 표시
    - 데이터바인딩 객체를 Product 에서 CartProduct 로 변경

--- app/src/main/java/woowacourse/shopping/ui/cart/CartProductViewHolder.kt
+++ app/src/main/java/woowacourse/shopping/ui/cart/CartProductViewHolder.kt
@@ -4,31 +4,31 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import woowacourse.shopping.databinding.ItemCartProductBinding
-import woowacourse.shopping.domain.model.Product
+import woowacourse.shopping.domain.model.CartProduct
 
 class CartProductViewHolder(
     private val binding: ItemCartProductBinding,
     private val dateFormatter: DateFormatter,
-    onClickDelete: (position: Int) -> Unit,
+    onClickDelete: (position: Int, id: Long) -> Unit,
 ) : RecyclerView.ViewHolder(binding.root) {
 
     init {
         binding.ivCartProductDelete.setOnClickListener {
             val position = adapterPosition
-            onClickDelete(position)
+            binding.item?.id?.let { onClickDelete(position, it) }
         }
     }
 
-    fun bind(product: Product) {
-        binding.item = product
-        // TODO: Step2 - dateFormatter를 활용하여 상품이 담긴 날짜와 시간을 출력하도록 변경
+    fun bind(cartProduct: CartProduct) {
+        binding.item = cartProduct
+        binding.tvCartProductCreatedAt.text = dateFormatter.formatDate(cartProduct.addedAt)
     }
 
     companion object {
         fun from(
             parent: ViewGroup,
             dateFormatter: DateFormatter,
-            onClickDelete: (position: Int) -> Unit,
+            onClickDelete: (Int, Long) -> Unit,
         ): CartProductViewHolder {
             val binding = ItemCartProductBinding
                 .inflate(LayoutInflater.from(parent.context), parent, false)
