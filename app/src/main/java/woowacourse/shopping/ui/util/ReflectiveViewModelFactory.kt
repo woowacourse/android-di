20d0b18a39a11529ab544fd777644a36e1fd95e1
Junyoung-WON <wjyo60@gmail.com>
2024-09-12 05:18:08 +0900

    refactor: DependencyInjector 변경에 따른 ViewModelFactory 수정

--- app/src/main/java/woowacourse/shopping/ui/util/ReflectiveViewModelFactory.kt
+++ app/src/main/java/woowacourse/shopping/ui/util/ReflectiveViewModelFactory.kt
@@ -3,14 +3,9 @@ package woowacourse.shopping.ui.util
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
-import woowacourse.shopping.ui.ShoppingApplication.Companion.viewModelDependencyContainer
 
-class ReflectiveViewModelFactory(
-    private val dependencyInjector: DependencyInjector = DependencyInjector(
-        viewModelDependencyContainer
-    ),
-) : ViewModelProvider.Factory {
+class ReflectiveViewModelFactory : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
-        return dependencyInjector.createInstanceFromConstructor(modelClass)
+        return DependencyInjector.createInstanceFromConstructor(modelClass)
     }
 }
