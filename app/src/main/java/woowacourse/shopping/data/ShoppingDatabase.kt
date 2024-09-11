32a5bb36ce7da7b5dc45f63526a52189764e5529
Namyunsuk <yunsuk0328@naver.com>
2024-09-11 11:51:01 +0900

    feat: ShoppingDatabase생성 코드 작성

--- app/src/main/java/woowacourse/shopping/data/ShoppingDatabase.kt
+++ app/src/main/java/woowacourse/shopping/data/ShoppingDatabase.kt
@@ -1,9 +1,28 @@
 package woowacourse.shopping.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [CartProductEntity::class], version = 1, exportSchema = false)
 abstract class ShoppingDatabase : RoomDatabase() {
     abstract fun cartProductDao(): CartProductDao
+
+    companion object {
+        @Volatile
+        private var instance: ShoppingDatabase? = null
+
+        fun getInstance(context: Context): ShoppingDatabase {
+            return instance ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context.applicationContext,
+                    ShoppingDatabase::class.java,
+                    "shopping_database",
+                ).addCallback(
+                    object : Callback() {},
+                ).build().also { instance = it }
+            }
+        }
+    }
 }
