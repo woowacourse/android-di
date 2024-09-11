c12d9b31b4d70b8612f24e3ec8d9ed53409d263d
sh1mj1 <7wlgns@gmail.com>
2024-09-11 16:57:20 +0900

    feat(ShoppingDatabase): 싱글톤 객체 생성

--- app/src/main/java/woowacourse/shopping/data/ShoppingDatabase.kt
+++ app/src/main/java/woowacourse/shopping/data/ShoppingDatabase.kt
@@ -1,9 +1,27 @@
 package woowacourse.shopping.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [CartProductEntity::class], version = 1, exportSchema = false)
 abstract class ShoppingDatabase : RoomDatabase() {
     abstract fun cartProductDao(): CartProductDao
+
+    companion object {
+        private const val DATABASE_NAME = "cart.db"
+
+        @Volatile
+        private var instance: ShoppingDatabase? = null
+
+        fun instance(context: Context): ShoppingDatabase =
+            instance ?: synchronized(this) {
+                instance ?: Room.databaseBuilder(
+                    context,
+                    ShoppingDatabase::class.java,
+                    DATABASE_NAME,
+                ).build().also { instance = it }
+            }
+    }
 }
