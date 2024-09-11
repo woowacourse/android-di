74631ccbd2deae3c072b798fd92d0bf37a305b23
somin <salth0601@gmail.com>
2024-09-11 11:33:40 +0900

    feat : CartProduct 추가 및 장바구니에 담은 날짜/시간 표시

--- app/src/main/java/woowacourse/shopping/data/DefaultCartRepository.kt
+++ app/src/main/java/woowacourse/shopping/data/DefaultCartRepository.kt
@@ -1,21 +1,23 @@
 package woowacourse.shopping.data
 
+import woowacourse.shopping.data.mapper.toCartProduct
+import woowacourse.shopping.model.CartProduct
 import woowacourse.shopping.model.CartRepository
 import woowacourse.shopping.model.Product
 
 // TODO: Step2 - CartProductDao를 참조하도록 변경
 class DefaultCartRepository : CartRepository {
-    private val cartProducts: MutableList<Product> = mutableListOf()
+    private val cartProducts: MutableList<CartProduct> = mutableListOf()
 
-    override fun addCartProduct(product: Product) {
-        cartProducts.add(product)
+    override suspend fun addCartProduct(product: Product) {
+        cartProducts.add(product.toCartProduct(cartProducts.size.toLong()))
     }
 
-    override fun getAllCartProducts(): List<Product> {
+    override suspend fun getAllCartProducts(): List<CartProduct> {
         return cartProducts.toList()
     }
 
-    override fun deleteCartProduct(id: Int) {
-        cartProducts.removeAt(id)
+    override suspend fun deleteCartProduct(id: Long) {
+        cartProducts.removeAt(id.toInt())
     }
 }
