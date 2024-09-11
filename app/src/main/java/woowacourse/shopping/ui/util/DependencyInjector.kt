4568725ac3ea665935778342df84c47341d98232
Junyoung-WON <wjyo60@gmail.com>
2024-09-12 05:17:46 +0900

    refactor: DependencyInjector 개선
    - object 싱글톤 객체로 변환
    - 재귀적으로 의존성을 찾아 주입시키며 인스턴스를 생성 및 반환하도록 수정

--- app/src/main/java/woowacourse/shopping/ui/util/DependencyInjector.kt
+++ app/src/main/java/woowacourse/shopping/ui/util/DependencyInjector.kt
@@ -1,26 +1,67 @@
 package woowacourse.shopping.ui.util
 
 import kotlin.reflect.KClass
+import kotlin.reflect.KMutableProperty
+import kotlin.reflect.KParameter
+import kotlin.reflect.KProperty
+import kotlin.reflect.full.declaredMemberProperties
+import kotlin.reflect.full.hasAnnotation
 import kotlin.reflect.full.primaryConstructor
+import kotlin.reflect.jvm.isAccessible
 
-class DependencyInjector(private val dependencyContainer: DependencyContainer) {
+object DependencyInjector {
+    lateinit var dependencyContainer: DependencyContainer
+        private set
+
+    fun initDependencyContainer(container: DependencyContainer) {
+        dependencyContainer = container
+    }
+
     fun <T : Any> createInstanceFromConstructor(modelClass: Class<T>): T {
         val kClass: KClass<T> = modelClass.kotlin
-        val primaryConstructor =
-            requireNotNull(kClass.primaryConstructor) {
-                "${kClass.java.simpleName} : $ERROR_MESSAGE_NO_PRIMARY_CONSTRUCTOR"
+        val targetInstance: T
+        val primaryConstructor = kClass.primaryConstructor
+
+        if (primaryConstructor == null) {
+            val instanceFromContainer = dependencyContainer.getInstance<T>(kClass)
+            if (instanceFromContainer != null) {
+                targetInstance = instanceFromContainer
+            } else {
+                val implementKClass = requireNotNull(dependencyContainer.getImplement<T>(kClass))
+                targetInstance = createInstanceFromConstructor(implementKClass.java)
             }
+        } else {
+            val constructorParams = getDependencyOfParameters(primaryConstructor.parameters)
+            targetInstance = primaryConstructor.call(*constructorParams)
 
-        val constructorParams =
-            primaryConstructor.parameters.map { param ->
-                val kParamType = param.type.classifier as KClass<*>
-                dependencyContainer.getInstance<Any>(kParamType)
-            }.toTypedArray()
+            setDependencyOfProperties(kClass, targetInstance)
+        }
 
-        return primaryConstructor.call(*constructorParams)
+        dependencyContainer.setInstance(kClass, targetInstance)
+        return targetInstance
     }
 
-    companion object {
-        private const val ERROR_MESSAGE_NO_PRIMARY_CONSTRUCTOR = "해당 클래스의 주 생성자가 없습니다."
+    private fun getDependencyOfParameters(parameters: List<KParameter>): Array<Any> {
+        return parameters.map { param ->
+            val classifier = param.type.classifier as KClass<*>
+            createInstanceFromConstructor(classifier.java)
+        }.toTypedArray()
     }
+
+    private fun <T : Any> setDependencyOfProperties(kClass: KClass<T>, targetInstance: T) {
+        kClass.declaredMemberProperties.forEach { kProperty ->
+            if (checkNeedsDependencyInject(kProperty)) {
+                val isAccessible = kProperty.isAccessible
+                val classifier = kProperty.returnType.classifier as KClass<*>
+                val dependency: Any = createInstanceFromConstructor(classifier.java)
+                if (!isAccessible) {
+                    kProperty.isAccessible = true
+                }
+                (kProperty as KMutableProperty<*>).setter.call(targetInstance, dependency)
+            }
+        }
+    }
+
+    private fun checkNeedsDependencyInject(kProperty: KProperty<*>): Boolean =
+        kProperty.hasAnnotation<FieldInject>() && kProperty.isLateinit && kProperty is KMutableProperty
 }
