675c459a0b035307680ecd35f4845825f75d5c97
sh1mj1 <7wlgns@gmail.com>
2024-09-12 08:29:35 +0900

    test(ProductFixture): 타입 맞추기

--- app/src/test/java/woowacourse/shopping/ProductFixture.kt
+++ app/src/test/java/woowacourse/shopping/ProductFixture.kt
@@ -3,9 +3,9 @@ package woowacourse.shopping
 import woowacourse.shopping.model.Product
 
 fun ProductFixture(
-    id: Int = 1,
+    id: Long = 1,
     name: String = "product $id",
-    price: Int = 1000 * id,
+    price: Int = 1000 * id.toInt(),
     imageUrl: String = "image $id",
     createdAt: Long = System.currentTimeMillis()
-) = Product(name, price, imageUrl, createdAt)
+) = Product(id, name, price, imageUrl, createdAt)
