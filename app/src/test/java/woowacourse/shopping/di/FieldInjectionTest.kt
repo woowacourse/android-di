851aaf515940c3c1b1d8fe2cb0919eb2f7a3e460
Namyunsuk <yunsuk0328@naver.com>
2024-09-11 17:18:15 +0900

    test: 필드 주입 테스트 작성

---
+++ app/src/test/java/woowacourse/shopping/di/FieldInjectionTest.kt
@@ -0,0 +1,15 @@
+package woowacourse.shopping.di
+
+import com.google.common.truth.Truth
+import org.junit.Test
+import woowacourse.shopping.ui.MainViewModel
+
+class FieldInjectionTest {
+    @Test
+    fun `FiledInject어노테이션이 붙은 필드에 접근할 수 있다`()  {
+        val mainViewModelClass = MainViewModel::class
+        val fields = mainViewModelClass.java.fields.filter { it.annotations.contains(FieldInject()) }
+
+        Truth.assertThat(fields).hasSize(1)
+    }
+}
