43a02891d0510f4831da76542774f9f70d3a8f20
songpink <jejeje04@naver.com>
2024-09-12 01:11:01 +0900

    test: MainViewModel이 상품을 가져올 수 있는지 여부 테스트 작성

--- app/src/test/java/woowacourse/shopping/MainActivityTest.kt
+++ app/src/test/java/woowacourse/shopping/MainActivityTest.kt
@@ -3,6 +3,7 @@ package woowacourse.shopping
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.ViewModelProvider
 import com.google.common.truth.Truth.assertThat
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -16,30 +17,37 @@ class MainActivityTest {
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    @Test
-    fun `Activity 실행 테스트`() {
-        // given
-        val activity =
+    private lateinit var activity: MainActivity
+    private lateinit var viewModel: MainViewModel
+
+    @Before
+    fun setUp() {
+        activity =
             Robolectric
                 .buildActivity(MainActivity::class.java)
                 .create()
                 .get()
 
+        viewModel = ViewModelProvider(activity)[MainViewModel::class.java]
+    }
+
+    @Test
+    fun `Activity 실행 테스트`() {
         // then
         assertThat(activity).isNotNull()
     }
 
     @Test
     fun `ViewModel 주입 테스트`() {
-        // given
-        val activity =
-            Robolectric
-                .buildActivity(MainActivity::class.java)
-                .create()
-                .get()
-        val viewModel = ViewModelProvider(activity)[MainViewModel::class.java]
-
         // then
         assertThat(viewModel).isNotNull()
+    }
+
+    @Test
+    fun `MainViewModel이 상품들을 가져올 수 있어야 한다`() {
+        // then
+        assertThat(viewModel.products.getOrAwaitValue()[0].name).isEqualTo("우테코 과자")
+        assertThat(viewModel.products.getOrAwaitValue()[1].name).isEqualTo("우테코 쥬스")
+        assertThat(viewModel.products.getOrAwaitValue()[2].name).isEqualTo("우테코 아이스크림")
     }
 }
