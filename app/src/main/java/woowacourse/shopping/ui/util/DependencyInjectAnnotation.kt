36bf0127a9f927f6175b6c6b1064f7acb7e26ad1
Junyoung-WON <wjyo60@gmail.com>
2024-09-12 11:11:09 +0900

    feat: Qualifier 어노테이션 추가

--- app/src/main/java/woowacourse/shopping/ui/util/DependencyInjectAnnotation.kt
+++ app/src/main/java/woowacourse/shopping/ui/util/DependencyInjectAnnotation.kt
@@ -1,6 +1,14 @@
 package woowacourse.shopping.ui.util
 
+@Target(AnnotationTarget.PROPERTY)
+annotation class FieldInject
+
+@Target(AnnotationTarget.VALUE_PARAMETER)
+annotation class ParamInject
+
 @Target(
     AnnotationTarget.PROPERTY,
+    AnnotationTarget.VALUE_PARAMETER,
+    AnnotationTarget.FIELD,
 )
-annotation class FieldInject
+annotation class Qualifier(val name: String = "")
