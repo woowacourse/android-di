8c4a408cf9351a2198be2b4fbfa55eb26ebaeb2f
JunJangE <wnswkd486@gmail.com>
2024-09-12 17:04:28 +0900

    refactor: CartRepositoryImpl 생성자 추가

--- app/src/main/java/woowacourse/shopping/data/di/RepositoryModule.kt
+++ app/src/main/java/woowacourse/shopping/data/di/RepositoryModule.kt
@@ -1,5 +1,6 @@
 package woowacourse.shopping.data.di
 
+import woowacourse.shopping.data.CartProductDao
 import woowacourse.shopping.data.CartRepository
 import woowacourse.shopping.data.CartRepositoryImpl
 import woowacourse.shopping.data.ProductRepository
@@ -8,5 +9,5 @@ import woowacourse.shopping.data.ProductRepositoryImpl
 class RepositoryModule {
     fun provideProductRepository(): ProductRepository = ProductRepositoryImpl()
 
-    fun provideCartRepository(): CartRepository = CartRepositoryImpl()
+    fun provideCartRepository(dao: CartProductDao): CartRepository = CartRepositoryImpl(dao = dao)
 }
