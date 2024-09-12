4f40e81d5fb6ff24112246585b2ae3b551663574
sh1mj1 <7wlgns@gmail.com>
2024-09-12 14:00:12 +0900

    feat(Qualifier): 애노테이션

--- app/src/main/java/woowacourse/shopping/di/InjectAnnotation.kt
+++ app/src/main/java/woowacourse/shopping/di/InjectAnnotation.kt
@@ -3,10 +3,5 @@ package woowacourse.shopping.di
 @Retention(AnnotationRetention.RUNTIME)
 annotation class Inject
 
-annotation class Qualifier
-
-@Qualifier
-annotation class RoomDao
-
-@Qualifier
-annotation class InMemoryDao
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Qualifier(val value: String)
