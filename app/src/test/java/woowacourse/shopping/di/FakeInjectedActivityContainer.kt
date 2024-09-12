88f2fd1c9245375287dcb65ddb97d67c4d708866
sh1mj1 <7wlgns@gmail.com>
2024-09-12 16:48:51 +0900

    feat(FakeInjected~~Container): qualifier 를 가지는 함수 구현

--- app/src/test/java/woowacourse/shopping/di/FakeInjectedActivityContainer.kt
+++ app/src/test/java/woowacourse/shopping/di/FakeInjectedActivityContainer.kt
@@ -1,12 +1,15 @@
 package woowacourse.shopping.di
 
+import com.example.sh1mj1.InjectedActivityContainer
+import com.example.sh1mj1.InjectedComponent
+import com.example.sh1mj1.Qualifier
 import kotlin.reflect.KClass
 import kotlin.reflect.full.isSuperclassOf
 
 class FakeInjectedActivityContainer(
-    private val components: MutableList<com.example.sh1mj1.InjectedComponent.InjectedActivityComponent> = mutableListOf(),
-) : com.example.sh1mj1.InjectedActivityContainer {
-    override fun add(component: com.example.sh1mj1.InjectedComponent.InjectedActivityComponent) {
+    private val components: MutableList<InjectedComponent.InjectedActivityComponent> = mutableListOf(),
+) : InjectedActivityContainer {
+    override fun add(component: InjectedComponent.InjectedActivityComponent) {
         components.add(component)
     }
 
@@ -14,6 +17,13 @@ class FakeInjectedActivityContainer(
         components.find {
             clazz.isSuperclassOf(it.injectedClass)
         }?.instance
+
+    override fun find(clazz: KClass<*>, qualifier: Qualifier): Any? = components.find {
+        clazz.isSuperclassOf(it.injectedClass) &&
+                it::class.annotations.find { it.annotationClass == qualifier.annotationClass } != null
+    }?.instance?.let {
+        find(clazz)
+    }
 
     override fun clear() {
         components.clear()
--- app/src/test/java/woowacourse/shopping/di/FakeInjectedSingletonContainer.kt
+++ app/src/test/java/woowacourse/shopping/di/FakeInjectedSingletonContainer.kt
@@ -1,17 +1,25 @@
 package woowacourse.shopping.di
 
+import com.example.sh1mj1.InjectedComponent
+import com.example.sh1mj1.InjectedSingletonContainer
+import com.example.sh1mj1.Qualifier
 import kotlin.reflect.KClass
 import kotlin.reflect.full.isSuperclassOf
 
 class FakeInjectedSingletonContainer(
-    private val components: MutableList<com.example.sh1mj1.InjectedComponent.InjectedSingletonComponent> = mutableListOf(),
-) : com.example.sh1mj1.InjectedSingletonContainer {
-    override fun add(component: com.example.sh1mj1.InjectedComponent.InjectedSingletonComponent) {
+    private val components: MutableList<InjectedComponent.InjectedSingletonComponent> = mutableListOf(),
+) : InjectedSingletonContainer {
+    override fun add(component: InjectedComponent.InjectedSingletonComponent) {
         components.add(component)
     }
 
     override fun find(clazz: KClass<*>): Any? =
         components.find {
             clazz.isSuperclassOf(it.injectedClass)
+        }?.instance
+
+    override fun find(clazz: KClass<*>, qualifier: Qualifier): Any? =
+        components.find {
+            clazz.isSuperclassOf(it.injectedClass) && qualifier.value == it.qualifier?.value
         }?.instance
 }
