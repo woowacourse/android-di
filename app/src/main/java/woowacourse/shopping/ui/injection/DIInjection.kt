28b7604733c8822d463525930d86cf6fc31fa37a
gaeun5744 <geun5744@gmail.com>
2024-09-12 10:42:28 +0900

    refactor: 불필요한 어노테이션 제거

--- app/src/main/java/woowacourse/shopping/ui/injection/DIInjection.kt
+++ app/src/main/java/woowacourse/shopping/ui/injection/DIInjection.kt
@@ -5,7 +5,3 @@ import kotlin.reflect.KClass
 @Target(AnnotationTarget.PROPERTY, AnnotationTarget.VALUE_PARAMETER)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class DIInjection
-
-@Target(AnnotationTarget.CLASS)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class Binder(val returnType: KClass<out Any>)
--- app/src/main/java/woowacourse/shopping/ui/injection/repository/RepositoryDI.kt
+++ app/src/main/java/woowacourse/shopping/ui/injection/repository/RepositoryDI.kt
@@ -4,7 +4,6 @@ import woowacourse.shopping.data.CartRepositoryImpl
 import woowacourse.shopping.data.ProductRepositoryImpl
 import woowacourse.shopping.model.repository.CartRepository
 import woowacourse.shopping.model.repository.ProductRepository
-import woowacourse.shopping.ui.injection.Binder
 import kotlin.reflect.KClass
 import kotlin.reflect.KVisibility
 import kotlin.reflect.full.declaredFunctions
@@ -13,27 +12,25 @@ import kotlin.reflect.full.isSubclassOf
 
 interface RepositoryDI
 
-@Binder(RepositoryDI::class)
 class RepositoryBinder {
     init {
-        validateReturnTypes()
+        require(
+            validateReturnTypes()
+        ) {
+            "모든 함수의 반환 타입은 RepositoryDI여야 합니다."
+        }
     }
 
     fun provideProductRepository(): ProductRepository = ProductRepositoryImpl()
 
     fun provideCartRepository(): CartRepository = CartRepositoryImpl()
 
-    private fun validateReturnTypes() {
-        val annotation = this::class.findAnnotation<Binder>()
-        annotation?.let {
-            this::class.declaredFunctions.filter { it.visibility == KVisibility.PUBLIC }.forEach { function ->
+    private fun validateReturnTypes(): Boolean {
+        return this::class.declaredFunctions.filter { it.visibility == KVisibility.PUBLIC }
+            .all { function ->
                 val returnTypeClassifier = function.returnType.classifier as? KClass<*>
-                if (returnTypeClassifier == null || !returnTypeClassifier.isSubclassOf(it.returnType)) {
-                    throw IllegalArgumentException(
-                        "클래스 ${this::class.simpleName}의 함수 ${function.name}의 반환 타입은 ${it.returnType.simpleName}이어야 합니다.",
-                    )
-                }
+                returnTypeClassifier != null && returnTypeClassifier.isSubclassOf(RepositoryDI::class)
             }
-        }
+
     }
 }
