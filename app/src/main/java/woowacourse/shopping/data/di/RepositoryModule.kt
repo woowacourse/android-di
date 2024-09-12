25c27a105068b9d46441058aa3a58967c83d7f15
JunJangE <wnswkd486@gmail.com>
2024-09-12 13:40:03 +0900

    refactor: 인터페이스와 구현체 분리

--- app/src/main/java/woowacourse/shopping/data/di/RepositoryModule.kt
+++ app/src/main/java/woowacourse/shopping/data/di/RepositoryModule.kt
@@ -1,10 +1,12 @@
 package woowacourse.shopping.data.di
 
 import woowacourse.shopping.data.CartRepository
+import woowacourse.shopping.data.CartRepositoryImpl
 import woowacourse.shopping.data.ProductRepository
+import woowacourse.shopping.data.ProductRepositoryImpl
 
 class RepositoryModule {
-    fun provideProductRepository(): ProductRepository = ProductRepository()
+    fun provideProductRepository(): ProductRepository = ProductRepositoryImpl()
 
-    fun provideCartRepository(): CartRepository = CartRepository()
+    fun provideCartRepository(): CartRepository = CartRepositoryImpl()
 }
