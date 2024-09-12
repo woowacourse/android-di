8b0a5e012bf4cbd45e768ee8fb50dddb83774eba
sh1mj1 <7wlgns@gmail.com>
2024-09-12 14:01:01 +0900

    feat(InjectedComponent): Qualifier 타입 프로퍼티 추가

--- app/src/main/java/woowacourse/shopping/di/InjectedComponent.kt
+++ app/src/main/java/woowacourse/shopping/di/InjectedComponent.kt
@@ -5,15 +5,18 @@ import kotlin.reflect.KClass
 sealed class InjectedComponent {
     abstract val injectedClass: KClass<*>
     abstract val instance: Any?
+    abstract val qualifier: Qualifier?
 
     class InjectedSingletonComponent(
         override val injectedClass: KClass<*>,
-        override val instance: Any,
+        override val instance: Any? = null,
+        override val qualifier: Qualifier? = null,
     ) : InjectedComponent()
 
     class InjectedActivityComponent(
         override val injectedClass: KClass<*>,
-        override val instance: Any,
+        override val instance: Any? = null,
         val activityClass: KClass<*>,
+        override val qualifier: Qualifier? = null,
     ) : InjectedComponent()
 }
