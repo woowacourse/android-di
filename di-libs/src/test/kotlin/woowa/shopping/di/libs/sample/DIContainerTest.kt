20bf5eeb1f855353e07923b295b54337b930e51d
murjune <sis9221302@gmail.com>
2024-09-12 15:54:52 +0900

    test: Qualifier 테스트

--- di-libs/src/test/kotlin/woowa/shopping/di/libs/sample/DIContainerTest.kt
+++ di-libs/src/test/kotlin/woowa/shopping/di/libs/sample/DIContainerTest.kt
@@ -6,9 +6,10 @@ import io.kotest.matchers.shouldNotBe
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 import woowa.shopping.di.libs.container.Containers
-import woowa.shopping.di.libs.container.container
 import woowa.shopping.di.libs.container.startDI
+import woowa.shopping.di.libs.factory.Lifecycle
 import woowa.shopping.di.libs.inject.inject
+import woowa.shopping.di.libs.qualify.qualifier
 
 class DIContainerTest {
     class Service
@@ -25,7 +26,9 @@ class DIContainerTest {
     fun `singleton 객체인지 테스트`() {
         //given
         startDI {
-            single { Service() }
+            container {
+                single { Service() }
+            }
         }
         //when
         val service1 by inject<Service>()
@@ -38,11 +41,13 @@ class DIContainerTest {
     fun `프로토타입 객체인지 테스트`() {
         //given
         startDI {
-            proto { Service() }
+            container {
+                proto { Service() }
+            }
         }
         // when
-        val service1 by inject<Service>()
-        val service2 by inject<Service>()
+        val service1 by inject<Service>(lifecycle = Lifecycle.PROTOTYPE)
+        val service2 by inject<Service>(lifecycle = Lifecycle.PROTOTYPE)
         //then
         service1 shouldNotBe service2
     }
@@ -51,8 +56,10 @@ class DIContainerTest {
     fun `Respository 싱글톤 구현체`() {
         //given
         startDI {
-            single { Service() }
-            single<Repository> { RepositoryImpl(get()) }
+            container {
+                single { Service() }
+                single<Repository> { RepositoryImpl(get()) }
+            }
         }
         // when
         val repository1 by inject<Repository>()
@@ -65,13 +72,16 @@ class DIContainerTest {
     fun `startDI 이후 컨테이너 추가 시 에러 발생 테스트`() {
         // given
         startDI {
-            single { Service() }
+            container {
+                single { Service() }
+            }
         }
         // when & then
         shouldThrow<IllegalStateException> {
-            container {
-                single { Service() }
-                single { RepositoryImpl(get()) }
+            startDI {
+                container {
+                    single { Service() }
+                }
             }
         }
     }
@@ -88,5 +98,56 @@ class DIContainerTest {
         // then
         val repository by inject<Repository>()
         repository shouldNotBe null
+    }
+
+    @Test
+    fun `중복되는 타입과 Qualifier로 객체를 가져올 때 에러 발생`() {
+        // given
+        shouldThrow<IllegalStateException> {
+            startDI {
+                container {
+                    single(qualifier("오둥")) { Service() }
+                }
+                container {
+                    single(qualifier("오둥")) { Service() }
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `Qualifier로 원하는 객체 가져오기`() {
+        // given
+        startDI {
+            container {
+                single { Service() }
+                single<Repository>(qualifier("안녕")) { RepositoryImpl(get()) }
+            }
+
+            container {
+                single<Repository>(qualifier("오둥아")) { RepositoryImpl(get()) }
+            }
+        }
+        // when
+        val repository1 by inject<Repository>(qualifier("안녕"))
+        val repository2 by inject<Repository>(qualifier("오둥아"))
+        // then
+        repository1 shouldNotBe repository2
+    }
+
+    @Test
+    fun `Qualifier로 원하는 객체 가져오기2`() {
+        // given
+        startDI {
+            container {
+                single(qualifier("둥가둥가")) { Service() }
+                single(qualifier("오둥이")) { RepositoryImpl(get(qualifier("둥가둥가"))) }
+            }
+        }
+        // when
+        val repository1 by inject<RepositoryImpl>(qualifier("오둥이"))
+        val service by inject<Service>(qualifier("둥가둥가"))
+        // then
+        repository1.service shouldBe service
     }
 }
