689c73edfda2a32e0b295dfd3cdd8c684563df8d
haeum808 <haeumueah@gmail.com>
2024-09-12 17:22:38 +0900

    feat: haeum2 모듈 추가

---
+++ haeum2/src/test/java/com/bignerdranch/android/haeum2/ExampleUnitTest.kt
@@ -0,0 +1,14 @@
+package com.bignerdranch.android.haeum2
+
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+    }
+}
