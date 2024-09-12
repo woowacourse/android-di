bf2cc7ea6c55b23acd1efe19f5549412fb44ab77
sh1mj1 <7wlgns@gmail.com>
2024-09-12 15:36:57 +0900

    chore: 주석 제거

--- app/src/main/java/woowacourse/shopping/BaseViewModelFactory.kt
+++ app/src/main/java/woowacourse/shopping/BaseViewModelFactory.kt
@@ -10,46 +10,5 @@ class BaseViewModelFactory(
 ) : ViewModelProvider.Factory {
     val di = DependencyInjector(appContainer)
 
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return di.createInstance(modelClass)
-//        val kClass = modelClass.kotlin
-//
-//        val constructor =
-//            kClass.primaryConstructor
-//                ?: throw IllegalArgumentException("ViewModel must have a primary constructor: ${kClass.simpleName}")
-//
-//        val injectedFields = kClass.memberProperties.filter { it.hasAnnotation<Inject>() }
-//
-//        val constructorArgs =
-//            constructor.parameters.map { parameter ->
-//                appContainer.find(parameter.type.classifier as KClass<*>)
-//            }.toTypedArray()
-//
-//        val viewModel = constructor.call(*constructorArgs)
-//
-//        injectedFields.forEach { field ->
-//            field.isAccessible = true // private 필드에 접근할 수 있도록 설정
-//
-//            val qualifier = field.annotations.filterIsInstance<Qualifier>().firstOrNull().also {
-//                Log.d(TAG, "qualifier $it")
-//            }
-//
-//            val dependency =
-//                if (qualifier != null) {
-//                    appContainer.find(field.returnType.classifier as KClass<*>, qualifier)
-//                } else {
-//                    appContainer.find(field.returnType.classifier as KClass<*>)
-//                } ?: throw IllegalArgumentException("Unresolved dependency for field: ${field.name}")
-//
-//            if (field is KMutableProperty<*>) {
-//                field.setter.call(viewModel, dependency)
-//            } else {
-//                throw IllegalArgumentException("Field ${field.name} is not mutable and cannot be injected")
-//            }
-//        }
-//
-//        return viewModel
-    }
+    override fun <T : ViewModel> create(modelClass: Class<T>): T = di.createInstance(modelClass)
 }
-
-private const val TAG = "BaseViewModelFactory"
