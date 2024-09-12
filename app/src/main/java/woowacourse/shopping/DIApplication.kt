bbbdbf546cb0f4fd64b9a2ef40a785e1abcee640
gaeun5744 <geun5744@gmail.com>
2024-09-12 13:49:21 +0900

    feat: 생성한 di 모듈 저장 로직 구현

--- app/src/main/java/woowacourse/shopping/DIApplication.kt
+++ app/src/main/java/woowacourse/shopping/DIApplication.kt
@@ -1,12 +1,19 @@
 package woowacourse.shopping
 
 import android.app.Application
-import woowacourse.shopping.ui.injection.dao.DaoModule
-import woowacourse.shopping.ui.injection.repository.RepositoryModule
+import com.woowa.di.injection.ModuleRegistry
+import woowacourse.shopping.di.DaoDI
+import woowacourse.shopping.di.DaoModule
+import woowacourse.shopping.di.RepositoryModule
+import woowacourse.shopping.di.repository.RepositoryDI
 
 class DIApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        ModuleRegistry.registerModule(RepositoryDI::class, RepositoryModule::class)
+        ModuleRegistry.registerModule(
+            DaoDI::class, DaoModule::class
+        )
         DaoModule.initLifeCycle(this)
         RepositoryModule.initLifeCycle(this)
     }
