bddc00eebf908a2c1c1344410b087f5d6b2b8511
김진우 <jinwoo3661@naver.com>
2024-09-12 12:13:14 +0900

    feat: @Inject 어노테이션 적용

--- app/src/main/java/woowacourse/shopping/data/CartRepositoryImpl.kt
+++ app/src/main/java/woowacourse/shopping/data/CartRepositoryImpl.kt
@@ -2,11 +2,15 @@ package woowacourse.shopping.data
 
 import woowacourse.shopping.data.mapper.toDomain
 import woowacourse.shopping.data.mapper.toEntity
+import woowacourse.shopping.di.annotation.InMemoryDatabase
+import woowacourse.shopping.di.annotation.Qualifier
+import woowacourse.shopping.di.annotation.RoomDatabase
 import woowacourse.shopping.domain.CartRepository
 import woowacourse.shopping.model.Product
 
-
 class CartRepositoryImpl(
+    @RoomDatabase
+//    @InMemoryDatabase :: can use
     private val dao: CartProductDao,
 ) : CartRepository {
 
