3d95be5b4b2ef3f4a4e48f92698b31c15edbd039
dpcks0509 <dpcks0509@gmail.com>
2024-09-14 03:20:42 +0900

    test: Qualifier 테스트 추가

--- di/src/test/java/com/example/di/DIInjectorTest.kt
+++ di/src/test/java/com/example/di/DIInjectorTest.kt
@@ -1,17 +1,31 @@
 package com.example.di
 
+import androidx.lifecycle.ViewModel
 import com.example.di.annotation.Inject
+import com.example.di.annotation.Qualifier
 import com.google.common.truth.Truth.assertThat
 import org.junit.Before
 import org.junit.Test
 
 class FakeModule : Module {
-    fun provideFakeRepository(): FakeRepository {
-        return FakeRepository(FakeCartRepository(), FakeProductRepository())
+    fun provideFakeCartDao(): FakeCartDao {
+        return FakeCartDao()
     }
 
-    fun provideFakeCartRepository(): FakeCartRepository {
-        return FakeCartRepository()
+    @Qualifier(FakeDatabaseCartRepository::class)
+    fun provideFakeDatabaseCartRepository(
+        fakeCartDao: FakeCartDao,
+        fakeProductRepository: FakeProductRepository,
+    ): FakeCartRepository {
+        return FakeDatabaseCartRepository(fakeCartDao, fakeProductRepository)
+    }
+
+    fun provideFakeInMemoryCartRepository(): FakeCartRepository {
+        return FakeInMemoryCartRepository()
+    }
+
+    fun provideFakeProductRepository(): FakeProductRepository {
+        return FakeProductRepository()
     }
 
     fun provideFakeFieldRepository(): FakeFieldRepository {
@@ -19,38 +33,58 @@ class FakeModule : Module {
     }
 }
 
-class FakeRepository(
-    @Inject private val fakeCartRepository: FakeCartRepository,
-    private val fakeProductRepository: FakeProductRepository,
-)
+class FakeCartDao
 
-class FakeCartRepository {
-    @Inject
-    val fakeFieldRepository: FakeFieldRepository? = null
-}
+interface FakeCartRepository
+
+class FakeDatabaseCartRepository(
+    @Inject val fakeCartDao: FakeCartDao,
+    val fakeProductRepository: FakeProductRepository? = null,
+) : FakeCartRepository
+
+class FakeInMemoryCartRepository : FakeCartRepository
 
 class FakeProductRepository
 
 class FakeFieldRepository
 
+class FakeViewModel(
+    @Qualifier(FakeDatabaseCartRepository::class) @Inject val fakeDatabaseCartRepository: FakeCartRepository,
+) : ViewModel() {
+    @Inject
+    lateinit var fakeFieldRepository: FakeFieldRepository
+}
+
 class DIInjectorTest {
+    private lateinit var viewModel: FakeViewModel
+
     @Before
     fun setup() {
         DIContainer.clear()
         DIInjector.injectModule(FakeModule())
+        viewModel = DIInjector.createInstance(FakeViewModel::class)
+    }
+
+    @Test
+    fun `Inject Annotation이 붙은 생성자는 자동으로 의존성이 주입된다`() {
+        val cartDao = DIContainer.getInstance(FakeCartDao::class)
+        assertThat(cartDao).isNotNull()
     }
 
     @Test
     fun `Inject Annotation이 붙은 필드는 자동으로 의존성이 주입된다`() {
-        val cartRepository = DIContainer.getInstance(FakeCartRepository::class)
-
-        assertThat(cartRepository).isNotNull()
-        assertThat(cartRepository?.fakeFieldRepository).isNotNull()
+        assertThat(viewModel.fakeFieldRepository).isNotNull()
     }
 
     @Test
-    fun `Inject Annotation이 붙은 필드는 의존성이 주입되지 않는다`() {
-        val productRepository = DIContainer.getInstance(FakeProductRepository::class)
-        assertThat(productRepository).isNull()
+    fun `Qualifier Annotation이 붙은 생성자는 정확한 타입의 의존성이 주입된다`() {
+        val databaseCartRepository = viewModel.fakeDatabaseCartRepository
+        assertThat(databaseCartRepository).isInstanceOf(FakeDatabaseCartRepository::class.java)
+    }
+
+    @Test
+    fun `Qualifier Annotation이 없는 경우 기본 구현체는 주입되지 않는다`() {
+        val inMemoryCartRepository = DIContainer.getInstance(FakeInMemoryCartRepository::class)
+        assertThat(inMemoryCartRepository).isNull()
     }
 }
