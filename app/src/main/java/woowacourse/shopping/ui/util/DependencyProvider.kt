0d8808583b38a5ccb423b5b49eb6ffe93d1098dd
songpink <jejeje04@naver.com>
2024-09-16 03:32:33 +0900

    refactor: 임시커밋

--- app/src/main/java/woowacourse/shopping/ui/util/DependencyProvider.kt
+++ app/src/main/java/woowacourse/shopping/ui/util/DependencyProvider.kt
@@ -1,15 +1,17 @@
 package woowacourse.shopping.ui.util
 
+import com.example.alsonglibrary2.di.AutoDIManager
 import com.example.alsonglibrary2.di.AutoDIManager.createAutoDIInstance
 import com.example.alsonglibrary2.di.LibraryDependencyProvider
 import woowacourse.shopping.data.CartRepository
 import woowacourse.shopping.data.DefaultCartRepository
+import woowacourse.shopping.data.InMemoryCartRepository
 
 annotation class SharedCartRepository
 
 object DependencyProvider : LibraryDependencyProvider {
     @SharedCartRepository
     fun provideCartRepository(): CartRepository {
-        return createAutoDIInstance<DefaultCartRepository>()
+        return AutoDIManager.dependencies[CartRepository::class] as CartRepository
     }
 }
