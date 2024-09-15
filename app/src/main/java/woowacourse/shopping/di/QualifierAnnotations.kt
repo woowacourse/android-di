c8bf98c4fbd1be3a46bd88c107ce19e8dd4ed825
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-09-15 23:17:24 +0900

    chore: 불필요한 프로덕션 코드 상의 documentation 제거

--- app/src/main/java/woowacourse/shopping/di/QualifierAnnotations.kt
+++ app/src/main/java/woowacourse/shopping/di/QualifierAnnotations.kt
@@ -2,17 +2,6 @@ package woowacourse.shopping.di
 
 import javax.inject.Qualifier
 
-/**
- * Qualifier for distinguishing instances that are the same type but have different implementations.
- *
- * Two additional meta annotations are needed : `@Qualifier`, `@Retention`.
- * ```
- * @Qualifier
- * @Retention(AnnotationRetention.RUNTIME)
- * annotation class CustomRepository
- * ```
- */
-
 @Qualifier
 annotation class DatabaseRepository
 
