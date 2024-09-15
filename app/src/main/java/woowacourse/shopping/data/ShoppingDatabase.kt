43bc3c2c9eeaac7a2e3f70f2477de26b48280869
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-09-15 22:20:05 +0900

    refactor: 데이터베이스 초기화 로직과 인스턴스 반환 로직을 별도의 함수로 분리

--- app/src/main/java/woowacourse/shopping/data/ShoppingDatabase.kt
+++ app/src/main/java/woowacourse/shopping/data/ShoppingDatabase.kt
@@ -10,18 +10,28 @@ abstract class ShoppingDatabase : RoomDatabase() {
     abstract fun cartProductDao(): CartProductDao
 
     companion object {
+        @Volatile
         private var instance: ShoppingDatabase? = null
 
-        fun getInstance(context: Context): ShoppingDatabase =
-            instance ?: synchronized(this) {
-                Room
+        fun getInstance(context: Context): ShoppingDatabase {
+            if (instance == null) initializeDatabase(context)
+            return instance
+                ?: throw IllegalStateException(EXCEPTION_UNINITIALIZED_DATABASE_ACCESS)
+        }
+
+        private fun initializeDatabase(context: Context) {
+            synchronized(this) {
+                instance = Room
                     .databaseBuilder(
                         context.applicationContext,
                         ShoppingDatabase::class.java,
                         "shopping.db",
                     )
                     .build()
-                    .also { instance = it }
             }
+        }
+
+        private const val EXCEPTION_UNINITIALIZED_DATABASE_ACCESS =
+            "Database has not been initialized."
     }
 }
