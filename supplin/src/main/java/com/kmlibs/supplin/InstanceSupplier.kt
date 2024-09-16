025b9abdff64b0ec3eba7dd2104958b0950633ff
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-09-17 00:21:08 +0900

    chore: 불필요한 주석 제거

--- supplin/src/main/java/com/kmlibs/supplin/InstanceSupplier.kt
+++ supplin/src/main/java/com/kmlibs/supplin/InstanceSupplier.kt
@@ -34,37 +34,4 @@ object InstanceSupplier {
     private fun findInstanceOf(property: KProperty<*>): Any {
         return Injector.instanceContainer.instanceOf(property)
     }
-
-    //    TODO 추후 생성자 주입 + 필드 주입 혼합하여 사용할 수 있도록 수정
-//    fun <T : Any> injectedInstance(clazz: KClass<T>): T {
-//        val targetConstructor = targetConstructor(clazz)
-//        val constructorParameters = targetConstructor.parameters
-//        val parameterValues =
-//            constructorParameters.map { parameter ->
-//                val parameterClass = parameter.type
-//                Injector.instanceContainer.instanceOf(parameterClass)
-//            }.toTypedArray<Any>()
-//
-//        val instance = targetConstructor.newInstance(*parameterValues)
-//        injectFields(clazz, instance)
-//
-//        return instance as T
-//    }
-//
-//    private fun <T : Any> targetConstructor(clazz: KClass<T>): KFunction<*> {
-//        val constructors = clazz.constructors
-//        val targetConstructor =
-//            constructors.firstOrNull { constructor ->
-//                constructor.annotations.any { annotation ->
-//                    annotation.annotationClass == Supply::class
-//                }
-//            }
-//
-//        return checkNotNull(targetConstructor) {
-//            EXCEPTION_NO_TARGET_CONSTRUCTOR.format(clazz.simpleName)
-//        }
-//    }
-//    private const val EXCEPTION_PROPERTY_NOT_FOUND = "Property that needs to be injected not found"
-//    private const val EXCEPTION_NO_TARGET_CONSTRUCTOR =
-//        "No constructor with @Supply annotation found for %s"
 }
