bb2fbf9bab2c377f9d5a616f576c1c158bd60391
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-09-18 16:09:25 +0900

    docs: supplinInjection 문서 추가

--- supplin/src/main/java/com/kmlibs/supplin/SupplinInjection.kt
+++ supplin/src/main/java/com/kmlibs/supplin/SupplinInjection.kt
@@ -1,5 +1,14 @@
 package com.kmlibs.supplin
 
+import androidx.activity.ComponentActivity
+import androidx.fragment.app.Fragment
+
+/**
+ * For supplying dependencies that don't utilize other dependencies stored in `Modules`.
+ *
+ * If you want to supply dependencies that utilize the dependencies stored in `Modules`,
+ * use field injection with late initialization and `@Supply` annotation instead.
+ */
 inline fun <reified T : Any> supplinInjection(): Lazy<T> =
     lazy {
         val instance = Injector.instanceContainer.instanceOf(T::class)
