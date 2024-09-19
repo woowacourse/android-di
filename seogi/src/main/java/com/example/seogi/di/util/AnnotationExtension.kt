1044e59b3e047cb57c4910f10989af07b60bdb56
Namyunsuk <yunsuk0328@naver.com>
2024-09-19 10:22:32 +0900

    refactor: 메서드 접근제한자 변경

--- seogi/src/main/java/com/example/seogi/di/util/AnnotationExtension.kt
+++ seogi/src/main/java/com/example/seogi/di/util/AnnotationExtension.kt
@@ -5,6 +5,8 @@ import kotlin.reflect.KParameter
 import kotlin.reflect.KProperty1
 import kotlin.reflect.full.hasAnnotation
 
-fun KParameter.getAnnotationIncludeQualifier() = annotations.firstOrNull { it.annotationClass.hasAnnotation<Qualifier>() }
+internal fun KParameter.getAnnotationIncludeQualifier() =
+    annotations.firstOrNull { it.annotationClass.hasAnnotation<Qualifier>() }
 
-fun <T> KProperty1<T, *>.getAnnotationIncludeQualifier() = annotations.firstOrNull { it.annotationClass.hasAnnotation<Qualifier>() }
+internal fun <T> KProperty1<T, *>.getAnnotationIncludeQualifier() =
+    annotations.firstOrNull { it.annotationClass.hasAnnotation<Qualifier>() }
