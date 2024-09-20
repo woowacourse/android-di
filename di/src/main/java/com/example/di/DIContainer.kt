860ec16c8d4200af002fb46258c8446f07dfa2ce
Hogu59 <dbstjdguswkd@naver.com>
2024-09-20 19:36:36 +0900

    refactor: make create instance when viewmodel is called

--- di/src/main/java/com/example/di/DIContainer.kt
+++ di/src/main/java/com/example/di/DIContainer.kt
@@ -5,6 +5,7 @@ import com.example.di.annotations.FieldInject
 import com.example.di.annotations.Qualifier
 import kotlin.reflect.KClass
 import kotlin.reflect.full.declaredMemberProperties
+import kotlin.reflect.full.primaryConstructor
 import kotlin.reflect.jvm.isAccessible
 import kotlin.reflect.jvm.javaField
 
@@ -24,7 +25,7 @@ object DIContainer {
         instances[type to qualifier] = instance
     }
 
-    inline fun <reified T : Any> inject(qualifier: String? = null): T = getInstance(T::class, qualifier) as T
+    inline fun <reified T : Any> inject(qualifier: String? = null): T = createInstance(T::class, qualifier)
 
     fun injectFieldDependencies(target: Any) {
         val injectionProperties =
@@ -38,6 +39,31 @@ object DIContainer {
             val value = getInstance(property.returnType.classifier as KClass<*>, qualifier)
             property.javaField?.set(target, value)
         }
+    }
+
+    fun <T : Any> createInstance(
+        type: KClass<T>,
+        qualifier: String?,
+    ): T {
+        val constructor =
+            type.primaryConstructor
+                ?: throw IllegalArgumentException("No suitable constructor found for ${type.simpleName}")
+
+        val parameters =
+            constructor.parameters
+                .map { parameter ->
+                    val paramType = parameter.type.classifier as KClass<*>
+                    val paramQualifier =
+                        parameter.annotations
+                            .filterIsInstance<Qualifier>()
+                            .firstOrNull()
+                            ?.value
+                    setInstance(type, paramType, paramQualifier)
+                }.toTypedArray()
+
+        val instance = constructor.call(*parameters) as T
+        setInstance(type, instance, qualifier)
+        return instance
     }
 
     fun <T : ViewModel> setViewModelInstance(
