597eb5525b66223eeedd4174401dc5794a828084
Hogu59 <dbstjdguswkd@naver.com>
2024-09-20 19:32:52 +0900

    style: erase un-using codes

--- app/src/main/java/woowacourse/shopping/di/AppModule.kt
+++ app/src/main/java/woowacourse/shopping/di/AppModule.kt
@@ -1,10 +1,3 @@
 package woowacourse.shopping.di
 
-import woowacourse.shopping.data.repository.CartRepository
-import woowacourse.shopping.data.repository.ProductRepository
-
-interface AppModule {
-    val productRepository: ProductRepository
-
-    val cartRepository: CartRepository
-}
+interface AppModule
--- app/src/main/java/woowacourse/shopping/di/DefaultAppModule.kt
+++ app/src/main/java/woowacourse/shopping/di/DefaultAppModule.kt
@@ -1,17 +1,10 @@
 package woowacourse.shopping.di
 
 import android.content.Context
-import com.example.di.DIContainer.inject
 import woowacourse.shopping.data.ShoppingDatabase
-import woowacourse.shopping.data.repository.CartRepository
-import woowacourse.shopping.data.repository.ProductRepository
 
 class DefaultAppModule(
     appContext: Context,
 ) : AppModule {
     val database = ShoppingDatabase.getInstance(appContext)
-
-    override val productRepository: ProductRepository by lazy { inject<ProductRepository>() }
-
-    override val cartRepository: CartRepository by lazy { inject<CartRepository>() }
 }
