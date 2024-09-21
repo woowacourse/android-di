6cf14ee8b99323b827701eceac8f64d5ab84c9aa
sh1mj1 <7wlgns@gmail.com>
2024-09-21 15:22:37 +0900

    feat(InjectAnnotation): Qualifier 에 generate 필드 추가

--- sh1mj1/src/main/java/com/example/sh1mj1/InjectAnnotation.kt
+++ sh1mj1/src/main/java/com/example/sh1mj1/InjectAnnotation.kt
@@ -4,4 +4,4 @@ package com.example.sh1mj1
 annotation class Inject
 
 @Retention(AnnotationRetention.RUNTIME)
-annotation class Qualifier(val value: String)
+annotation class Qualifier(val value: String, val generate: Boolean = false)
