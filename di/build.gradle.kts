12ab8b4b926b2118c1f0de2c00d70f5958db5aaa
kkosang <eiwoqp77@gmail.com>
2024-09-22 17:06:52 +0900

    refactor: di모듈 android 의존성 갖도록 수정

--- di/build.gradle.kts
+++ di/build.gradle.kts
@@ -1,16 +1,48 @@
 plugins {
-    id("java-library")
-    id("org.jetbrains.kotlin.jvm")
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+android {
+    namespace = "woowacourse.shopping.di"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 26
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    packaging {
+        resources {
+            excludes += "META-INF/**"
+            excludes += "win32-x86*/**"
+        }
+    }
 }
 
 dependencies {
     // Reflection
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.23")
+    // lifecycle
+    implementation("androidx.lifecycle:lifecycle-common-jvm:2.8.6")
+    implementation("androidx.activity:activity-ktx:1.9.2")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     testImplementation("junit:junit:4.13.2")
     testImplementation("com.google.truth:truth:1.1.3")
     testImplementation("org.assertj:assertj-core:3.22.0")
