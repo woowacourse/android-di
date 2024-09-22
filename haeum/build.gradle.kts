02daa0f6706d42541b1441324ac877f5497f537d
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-09-22 13:41:32 +0900

    config: 테스트에 필요한 의존성들 추가

--- haeum/build.gradle.kts
+++ haeum/build.gradle.kts
@@ -37,4 +37,13 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.9.1")
     implementation("javax.inject:javax.inject:1")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.23")
+
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("com.google.truth:truth:1.1.3")
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0")
+    testImplementation("org.robolectric:robolectric:4.13")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("com.google.truth:truth:1.1.3")
 }
