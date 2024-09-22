6ca1bba626f9a71c4960748c839b7d3d1b39d9b6
sh1mj1 <7wlgns@gmail.com>
2024-09-22 20:45:08 +0900

    build: sh1mj1 모듈에 안드로이드 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("org.jlleitschuh.gradle.ktlint") version "12.1.0" apply false
     id("org.jetbrains.kotlin.jvm") version "1.9.0" apply false
+    id("com.android.library") version "8.3.1" apply false
 }
 
 allprojects {
--- sh1mj1/build.gradle.kts
+++ sh1mj1/build.gradle.kts
@@ -1,14 +1,38 @@
 plugins {
-    id("java-library")
-    id("org.jetbrains.kotlin.jvm")
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+android {
+    namespace = "com.example.mylibrary"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 26
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 dependencies {
+    // android
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.0")
 
     testImplementation("junit:junit:4.13.2")
@@ -17,4 +41,7 @@ dependencies {
     testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.8.2")
     testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
     testImplementation("de.mannodermaus.junit5:android-test-runner:1.4.0")
+
+    // Robolectric
+    testImplementation("org.robolectric:robolectric:4.13")
 }
