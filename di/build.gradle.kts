31da925508b38a7fb5a6c42505e7794a2bb4ad44
Junyoung-WON <wjyo60@gmail.com>
2024-09-23 17:52:34 +0900

    build: di 모듈에 안드로이드 의존성 추가

--- di/build.gradle.kts
+++ di/build.gradle.kts
@@ -1,19 +1,46 @@
 plugins {
-    id("java-library")
-    id("org.jetbrains.kotlin.jvm")
+//    id("java-library")
+//    id("org.jetbrains.kotlin.jvm")
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+android {
+    namespace = "woowacourse.shopping"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 26
+        targetSdk = 34
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 }
+
+//java {
+//    sourceCompatibility = JavaVersion.VERSION_17
+//    targetCompatibility = JavaVersion.VERSION_17
+//}
 
 dependencies {
     implementation("javax.inject:javax.inject:1")
-    testImplementation("junit:junit:4.13.2")
-    testImplementation("com.google.truth:truth:1.1.3")
     // Reflection
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.23")
+    // Android LifeCycle
+    implementation("androidx.lifecycle:lifecycle-common-java8:2.5.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4")
+    implementation("androidx.activity:activity-ktx:1.9.2")
+    implementation("androidx.fragment:fragment-ktx:1.8.3")
+
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("com.google.truth:truth:1.1.3")
+
     // Robolectric
     testImplementation("org.robolectric:robolectric:4.13")
 }
