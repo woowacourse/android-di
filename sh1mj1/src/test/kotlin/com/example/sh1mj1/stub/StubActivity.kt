a93ddd2efcc2ff404d2bab9718e75d88669b772d
sh1mj1 <7wlgns@gmail.com>
2024-09-23 13:59:55 +0900

    feat(StubActivity, StubViewModel, StubRepo): StubActivity 는 ComponentActivity 를 상속받는다

--- sh1mj1/src/test/kotlin/com/example/sh1mj1/stub/StubActivity.kt
+++ sh1mj1/src/test/kotlin/com/example/sh1mj1/stub/StubActivity.kt
@@ -1,15 +1,26 @@
 package com.example.sh1mj1.stub
 
-import android.app.Activity
 import android.content.Context
+import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ViewModel
+import com.example.sh1mj1.annotation.Inject
+import com.example.sh1mj1.annotation.ViewModelScope
 import com.example.sh1mj1.component.activityscope.injectActivityScopeComponent
+import com.example.sh1mj1.injectedSh1mj1ViewModel
 import woowacourse.shopping.ui.cart.DateFormatter
 
-class StubActivity : Activity() {
+class StubActivity : ComponentActivity() {
+    // TODO: StubActivity 가 Activity 를 상속받을 때는 injectedSh1mj1ViewModel 를 사용할 수 없지만, AppCompatActivity 상속받으니 ㄱㅊ
+    val viewModel by injectedSh1mj1ViewModel<StubViewModel>()
     val dateFormatter by injectActivityScopeComponent<DateFormatter>()
+
+    fun useViewModelFunction() {
+        viewModel.printAll()
+    }
 }
 
 class StubDateFormatter(context: Context) : LifecycleEventObserver, DateFormatter {
@@ -24,4 +35,23 @@ class StubDateFormatter(context: Context) : LifecycleEventObserver, DateFormatte
     companion object {
         private const val STUB_FORMAT = "Stub"
     }
+}
+
+// TODO: [ ] ProductRepository 는 ViewModel LifeCycle 동안 유지되도록 구현한다.
+class StubViewModel : ViewModel() {
+    @Inject
+    @ViewModelScope
+    lateinit var repo: StubRepo
+
+    fun printAll() {
+        repo.all().forEach(::println)
+    }
+}
+
+interface StubRepo {
+    fun all(): List<String>
+}
+
+class DefaultStubRepo : StubRepo {
+    override fun all(): List<String> = listOf("11", "22")
 }
--- sh1mj1/src/test/kotlin/com/example/sh1mj1/stub/StubApplication.kt
+++ sh1mj1/src/test/kotlin/com/example/sh1mj1/stub/StubApplication.kt
@@ -2,11 +2,16 @@ package com.example.sh1mj1.stub
 
 import com.example.sh1mj1.DiApplication
 import com.example.sh1mj1.component.activityscope.activityScopeComponent
+import com.example.sh1mj1.component.singleton.singletonComponent
 import woowacourse.shopping.ui.cart.DateFormatter
 
 class StubApplication : DiApplication() {
     override fun onCreate() {
         super.onCreate()
+
+        container.add(
+            singletonComponent<StubRepo>(DefaultStubRepo()),
+        )
 
         activityContainer.add(
             activityScopeComponent<DateFormatter>(::StubDateFormatter),
