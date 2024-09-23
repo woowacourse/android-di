36c0ff29fd6079aac4dfe0bc8ee5fa137545e70f
김진우 <jinwoo3661@naver.com>
2024-09-23 15:40:18 +0900

    feat : viewModel Injector 구현

---
+++ di-android/src/main/java/com/android/di_android/ViewModelInjector.kt
@@ -0,0 +1,31 @@
+package com.android.di_android
+
+import androidx.lifecycle.ViewModel
+import com.android.di.component.DiContainer
+import com.android.di.component.DiInjector
+import com.android.di.component.Module
+
+abstract class ViewModelInjector(
+    private val applicationInjector: ApplicationInjector,
+) : ViewModel() {
+    private val diInjector: DiInjector = createViewModelInjector()
+
+    init {
+        applicationInjector.saveInjector(extractKey(), diInjector)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        applicationInjector.removeInjector(extractKey())
+    }
+
+    private fun extractKey(): String = this::class.java.name
+
+    private fun createViewModelInjector(): DiInjector {
+        return DiInjector(loadViewModelContainer())
+    }
+
+    private fun loadViewModelContainer(): DiContainer {
+        return DiContainer(parentContainer = applicationInjector.getApplicationContainer())
+    }
+}
