dbe40bf3ce02803805533a87a6402e741bea3be1
Junyoung-WON <wjyo60@gmail.com>
2024-09-24 03:22:15 +0900

    feat: 뷰모델의 생명주기 관찰 시 onCreate 상태도 확인
    - onCreate 일 때 onCreate 콜백 메서드를 수행

--- di/src/main/java/woowacourse/shopping/di/ViewModelLifecycleObserver.kt
+++ di/src/main/java/woowacourse/shopping/di/ViewModelLifecycleObserver.kt
@@ -1,29 +1,38 @@
 package woowacourse.shopping.di
 
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 
 class ViewModelLifecycleObserver(
+    private val onCreate: () -> Unit,
     private val onFinish: () -> Unit,
 ) : DefaultLifecycleObserver {
+    override fun onCreate(owner: LifecycleOwner) {
+        println("hodu")
+        println("\nhodu: owner = $owner")
+        println("hodu: ViewModel Lifecycle Observer onCreate")
+        onCreate()
+    }
+
     override fun onDestroy(owner: LifecycleOwner) {
-        Log.d("hodu", "ViewModel Lifecycle Observer onDestroy")
+        println("hodu")
+        println("\nhodu: owner = $owner")
+        println("hodu: ViewModel Lifecycle Observer onDestroy")
         when (owner) {
             is ComponentActivity -> {
-                Log.d("hodu", "onDestory of Activity")
+                println("hodu: onDestory of Activity")
                 if (owner.isFinishing) {
-                    Log.d("hodu", "Activity is finished")
+                    println("hodu: Activity is finished")
                     onFinish()
                 }
             }
 
             is Fragment -> {
-                Log.d("hodu", "onDestory of Fragment")
+                println("hodu: onDestory of Fragment")
                 if (owner.isRemoving || owner.isDetached) {
-                    Log.d("hodu", "Fragment is finished")
+                    println("hodu: Fragment is finished")
                     onFinish()
                 }
             }
