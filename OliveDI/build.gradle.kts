acd9431e3999c97d75966f5065b2db9f05fe2390
kimhyemin <kimhm0728@naver.com>
2024-09-23 16:09:29 +0900

    chore: 불필요한 의존성 삭제

--- OliveDI/build.gradle.kts
+++ OliveDI/build.gradle.kts
@@ -38,8 +38,6 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     testImplementation("junit:junit:4.13.2")
     testImplementation("com.google.truth:truth:1.1.3")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     // Reflection
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.23")
     // Mockk
