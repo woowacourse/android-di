13645f279b4ba799fc294aef2fca8c82dd399f95
murjune <sis9221302@gmail.com>
2024-09-23 14:52:59 +0900

    [feat] Scope 와 Scope Instance를 다루는 ScopeRegistry

---
+++ di-libs/src/main/java/woowa/shopping/di/libs/scope/ScopeRegistry.kt
@@ -0,0 +1,92 @@
+package woowa.shopping.di.libs.scope
+
+import woowa.shopping.di.libs.annotation.InternalApi
+import woowa.shopping.di.libs.factory.ScopedInstanceFactory
+import woowa.shopping.di.libs.qualify.Qualifier
+import kotlin.reflect.KClass
+
+/**
+ * [registerInstanceFactories], [registerInstanceFactory] : ScopeRegistry 에 InstanceFactory 를 등록합니다
+ * [unLock] : ScopeRegistry 에서 Lock 된 Scope 를 UnLock 합니다. 이 때부터 해당 Scope 에서 Instance 를 가져올 수 있습니다
+ * [clearScope] : ScopeRegistry 에서 해당 Scope 에 등록된 InstanceFactory 내 Instance 를 모두 제거합니다
+ * [resolve] : unLock 된 Scope 에서 Instance 를 가져옵니다.
+ * */
+@InternalApi
+class ScopeRegistry {
+    private val scopedFactoriesMap =
+        mutableMapOf<Qualifier, MutableMap<FactoryKey<*>, ScopedInstanceFactory<*>>>()
+    private val lockedScope: MutableSet<Qualifier> = mutableSetOf()
+
+    fun unlockScope(scopeQualifier: Qualifier) {
+        require(scopeQualifier in lockedScope) {
+            "$scopeQualifier 에 해당하는 Scope 가 이미 Unlocked 상태입니다"
+        }
+        require(scopedFactoriesMap[scopeQualifier] != null) {
+            "$scopeQualifier 에 해당하는 Scope 가 존재하지 않습니다"
+        }
+        lockedScope.remove(scopeQualifier)
+    }
+
+    fun registerInstanceFactories(
+        scopeQualifier: Qualifier,
+        factories: List<ScopedInstanceFactory<*>>
+    ) {
+        factories.forEach { registerInstanceFactory(scopeQualifier, it) }
+    }
+
+    fun <T : Any> registerInstanceFactory(
+        scopeQualifier: Qualifier,
+        factory: ScopedInstanceFactory<T>
+    ) {
+        lockedScope.add(scopeQualifier) // Lock
+        val factories = scopedFactoriesMap[scopeQualifier]
+        if (factories == null) {
+            scopedFactoriesMap[scopeQualifier] = mutableMapOf(factory.toKey() to factory)
+            return
+        }
+        val newFactoryKey = factory.toKey()
+        require(newFactoryKey !in factories.keys) {
+            "$newFactoryKey 에 해당하는 ScopeInstanceFactory 가 이미 존재합니다"
+        }
+        factories[newFactoryKey] = factory
+    }
+
+    @InternalApi
+    fun <T : Any> resolve(
+        scopeQualifier: Qualifier,
+        instanceClazz: KClass<T>,
+        instanceQualifier: Qualifier? = null,
+    ): T {
+        val factoryKey = FactoryKey(instanceClazz, instanceQualifier)
+        val factories = scopedFactoriesMap[scopeQualifier]
+        require(factories != null) {
+            "$scopeQualifier 에 해당하는 Scope 가 없습니다"
+        }
+        require(isUnLocked(scopeQualifier)) {
+            "$scopeQualifier 에 해당하는 Scope 를 만들어주세요"
+        }
+        require(factoryKey in factories.keys) {
+            "$factoryKey 에 해당하는 ScopeInstance 가 존재하지 않습니다"
+        }
+        val factory: ScopedInstanceFactory<*> = requireNotNull(factories[factoryKey])
+        return factory.instance() as T
+    }
+
+    private fun isUnLocked(scopeQualifier: Qualifier): Boolean {
+        return scopeQualifier !in lockedScope
+    }
+
+    @InternalApi
+    fun clearScope(scopeQualifier: Qualifier) {
+        val factories = scopedFactoriesMap[scopeQualifier]
+        requireNotNull(factories) {
+            "$scopeQualifier 에 해당하는 Scope 가 존재하지 않습니다"
+        }
+        factories.forEach { (key, factory) -> factory.clear() }
+        lockedScope.add(scopeQualifier)
+    }
+
+    private fun ScopedInstanceFactory<*>.toKey() = FactoryKey(instanceClazz, qualifier)
+
+    private data class FactoryKey<T : Any>(val clazz: KClass<T>, val qualifier: Qualifier? = null)
+}
---
+++ di-libs/src/test/kotlin/woowa/shopping/di/libs/scope/ScopeRegistryTest.kt
@@ -0,0 +1,163 @@
+package woowa.shopping.di.libs.scope
+
+import io.kotest.assertions.throwables.shouldNotThrow
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+import woowa.shopping.di.libs.annotation.InternalApi
+import woowa.shopping.di.libs.factory.ScopedInstanceFactory
+import woowa.shopping.di.libs.qualify.qualifier
+
+@OptIn(InternalApi::class)
+class ScopeRegistryTest {
+
+    @Test
+    fun `Scope 와 Scope의 생명주기를 따르는 InstanceFactory 를 등록한다`() {
+        // given
+        val scopeRegistry = ScopeRegistry()
+        val scopeQualifier = qualifier("scope")
+        val instanceFactory = ScopedInstanceFactory(
+            instanceClazz = String::class,
+            factory = { "string" }
+        )
+        // when & then
+        shouldNotThrow<IllegalArgumentException> {
+            scopeRegistry.registerInstanceFactory(scopeQualifier, instanceFactory)
+        }
+    }
+
+    @Test
+    fun `Scope 의 생명주기를 따르는 InstanceFactory 가 이미 있을 경우 예외 반환`() {
+        // given
+        val scopeRegistry = ScopeRegistry()
+        val scopeQualifier = qualifier("scope")
+        val instanceFactory = ScopedInstanceFactory(
+            instanceClazz = String::class,
+            factory = { "string" }
+        )
+        // when & then
+        scopeRegistry.registerInstanceFactory(scopeQualifier, instanceFactory)
+        shouldThrow<IllegalArgumentException> {
+            scopeRegistry.registerInstanceFactory(scopeQualifier, instanceFactory)
+        }
+    }
+
+    @Test
+    fun `Lock 된 Scope 에 해당하는 객체를 가져올 경우 예외가 발생`() {
+        // given
+        val scopeRegistry = ScopeRegistry()
+        val scopeQualifier = qualifier("scope")
+        val factories = ScopedInstanceFactory(
+            instanceClazz = String::class,
+            factory = { "string" }
+        )
+        // when & then
+        scopeRegistry.registerInstanceFactory(scopeQualifier, factories)
+        shouldThrow<IllegalArgumentException> {
+            scopeRegistry.resolve(scopeQualifier, String::class)
+        }
+    }
+
+    @Test
+    fun `UnLock 된 Scope 에 해당하는 객체만 가져올 수 있다`() {
+        // given
+        val scopeRegistry = ScopeRegistry()
+        val scopeQualifier = qualifier("scope")
+        val factories = ScopedInstanceFactory(
+            instanceClazz = String::class,
+            factory = { "string" }
+        )
+        // when
+        scopeRegistry.registerInstanceFactory(scopeQualifier, factories)
+        scopeRegistry.unlockScope(scopeQualifier)
+        val result = scopeRegistry.resolve(scopeQualifier, String::class)
+        // then
+        result shouldBe "string"
+    }
+
+    @Test
+    fun `객체를 가져올 때, Scope 가 등록되어 있지 않으면 예외 발생`() {
+        // given
+        val scopeRegistry = ScopeRegistry()
+        val scopeQualifier = qualifier("scope")
+        val scopeQualifer2 = qualifier("scope2")
+        val factories = ScopedInstanceFactory(
+            instanceClazz = String::class,
+            factory = { "string" }
+        )
+        // when
+        scopeRegistry.registerInstanceFactory(scopeQualifier, factories)
+        scopeRegistry.unlockScope(scopeQualifier)
+        shouldThrow<IllegalArgumentException> {
+            scopeRegistry.resolve(scopeQualifer2, String::class)
+        }
+    }
+
+    @Test
+    fun `객체를 가져올 때, Scope 의 생명주기를 따르는 객체가 등록되어 있지 않으면 예외 발생`() {
+        // given
+        val scopeRegistry = ScopeRegistry()
+        val scopeQualifier = qualifier("scope")
+        val factories = ScopedInstanceFactory(
+            instanceClazz = String::class,
+            factory = { "string" }
+        )
+        // when
+        scopeRegistry.registerInstanceFactory(scopeQualifier, factories)
+        scopeRegistry.unlockScope(scopeQualifier)
+        shouldThrow<IllegalArgumentException> {
+            scopeRegistry.resolve(scopeQualifier, Int::class)
+        }
+    }
+
+    @Test
+    fun `Qualifier 를 통해 같은 타입의 객체도 등록할 수 있다`() {
+        // given
+        val scopeRegistry = ScopeRegistry()
+        val scopeQualifier = qualifier("scope")
+        val factories = listOf(
+            ScopedInstanceFactory(
+                qualifier = qualifier("string1"),
+                instanceClazz = String::class,
+                factory = { "string1" }
+            ),
+            ScopedInstanceFactory(
+                qualifier = qualifier("string2"),
+                instanceClazz = String::class,
+                factory = { "string2" }
+            ),
+        )
+        // when
+        scopeRegistry.registerInstanceFactories(scopeQualifier, factories)
+        scopeRegistry.unlockScope(scopeQualifier)
+        val result1 = scopeRegistry.resolve(scopeQualifier, String::class, qualifier("string1"))
+        val result2 = scopeRegistry.resolve(scopeQualifier, String::class, qualifier("string2"))
+        // then
+        result1 shouldBe "string1"
+        result2 shouldBe "string2"
+    }
+
+    @Test
+    fun `Scope 의 생명주기가 끝나면, Scope 에 등록된 객체들을 모두 삭제한다`() {
+        // given
+        val scopeRegistry = ScopeRegistry()
+        val scopeQualifier = qualifier("scope")
+        val factories = listOf(
+            ScopedInstanceFactory(
+                qualifier = qualifier("string1"),
+                instanceClazz = String::class,
+                factory = { "string1" }
+            ),
+            ScopedInstanceFactory(
+                qualifier = qualifier("string2"),
+                instanceClazz = String::class,
+                factory = { "string2" }
+            ),
+        )
+        // when
+        val scope = scopeRegistry.registerInstanceFactories(scopeQualifier, factories)
+        scopeRegistry.unlockScope(scopeQualifier)
+        scopeRegistry.clearScope(scopeQualifier)
+        // then
+    }
+}
