cedc696aaccc5b6de23013896c07826e196248f9
Junyoung-WON <wjyo60@gmail.com>
2024-09-24 03:30:45 +0900

    refactor: DI Application을 상속받은 ShoppingApplication
    - 어플리케이션 전역에서 사용되는 의존성 컨테이너를 초기화 및 관리
    - 하나의 어플리케이션 인스턴스를 제공하기 위한 companion object 설정

--- app/src/main/java/woowacourse/shopping/ui/ShoppingApplication.kt
+++ app/src/main/java/woowacourse/shopping/ui/ShoppingApplication.kt
@@ -1,24 +1,21 @@
 package woowacourse.shopping.ui
 
-import android.app.Application
 import woowacourse.shopping.data.InMemory
 import woowacourse.shopping.data.RoomDB
 import woowacourse.shopping.data.local.CartProductDao
 import woowacourse.shopping.data.local.ShoppingDatabase
 import woowacourse.shopping.data.repository.DefaultCartRepository
 import woowacourse.shopping.data.repository.DefaultProductRepository
-import woowacourse.shopping.di.ApplicationDependencyContainer
-import woowacourse.shopping.di.LifecycleAwareDependencyInjector
-import woowacourse.shopping.di.LifecycleDependencyContainer
+import woowacourse.shopping.di.DependencyInjectedApplication
 import woowacourse.shopping.domain.repository.CartRepository
 import woowacourse.shopping.domain.repository.ProductRepository
 
-class ShoppingApplication : Application() {
+class ShoppingApplication : DependencyInjectedApplication() {
     val database: ShoppingDatabase by lazy { ShoppingDatabase.getInstance(this) }
 
     override fun onCreate() {
         super.onCreate()
-        applicationDependencyContainer.setLifecycle(this)
+        instance = this
         applicationDependencyContainer.setDependency(
             ProductRepository::class,
             DefaultProductRepository::class,
@@ -37,12 +34,16 @@ class ShoppingApplication : Application() {
             CartProductDao::class,
             database.cartProductDao(),
         )
-        LifecycleAwareDependencyInjector().initDependencyContainer(applicationDependencyContainer)
     }
 
     companion object {
-        val applicationDependencyContainer: LifecycleDependencyContainer by lazy {
-            ApplicationDependencyContainer()
+        private var instance: ShoppingApplication? = null
+
+        fun getApplication(): ShoppingApplication {
+            if (instance == null) {
+                instance = ShoppingApplication()
+            }
+            return instance!!
         }
     }
 }
