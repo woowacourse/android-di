e83b01801fc0d129330edf341ed6539563b48a12
김진우 <jinwoo3661@naver.com>
2024-09-23 15:40:25 +0900

    feat : activity Injector 구현

---
+++ di-android/src/main/java/com/android/di_android/ActivityInjector.kt
@@ -0,0 +1,62 @@
+package com.android.di_android
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.android.di.component.DiComponent
+import com.android.di.component.DiContainer
+import com.android.di.component.DiInjector
+import com.android.di.component.Module
+import kotlin.reflect.KClass
+
+abstract class ActivityInjector : AppCompatActivity() {
+    private lateinit var applicationInjector: ApplicationInjector
+    lateinit var diInjector: DiInjector
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        applicationInjector = application as ApplicationInjector
+        diInjector = applicationInjector.getInjector(extractKey())
+            ?: createActivityInjector()
+    }
+
+    fun injectModule(module: Module){
+        if(!DiComponent.hasActivityAnnotation(javaClass.kotlin)){
+            throw IllegalArgumentException(ERROR_ANNOTATION)
+        }
+        diInjector.addModule(module)
+        DiComponent.injectFields(diInjector.diContainer, this@ActivityInjector)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        when {
+            isChangingConfigurations -> {
+                applicationInjector.saveInjector(extractKey(), diInjector)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        when {
+            isFinishing -> {
+                applicationInjector.removeInjector(extractKey())
+            }
+        }
+    }
+
+    private fun extractKey(): String = this::class.java.name
+
+    private fun createActivityInjector(): DiInjector {
+        return DiInjector(loadActivityContainer())
+    }
+
+    private fun loadActivityContainer(): DiContainer {
+        return DiContainer(parentContainer = applicationInjector.getApplicationContainer())
+    }
+
+    companion object {
+        private const val ERROR_CONSTRUCTOR = "No suitable constructor for %s"
+        private const val ERROR_ANNOTATION = " AppCompatActivity class must be annotated with @DiActivity"
+    }
+}
