4132aa47541864110f2cced927ac30eb03ddd85a
sh1mj1 <7wlgns@gmail.com>
2024-09-23 16:21:24 +0900

    chore: 로그 제거

--- sh1mj1/src/main/java/com/example/sh1mj1/component/activityscope/ActivityScopeComponentFactory.kt
+++ sh1mj1/src/main/java/com/example/sh1mj1/component/activityscope/ActivityScopeComponentFactory.kt
@@ -14,14 +14,12 @@ class ActivityScopeComponentFactory<T : Any>(
         thisRef: Activity,
         property: KProperty<*>,
     ): T {
-        println("TAG, getValue: thisRef: $thisRef, property: $property app: ${thisRef.application}")
         val container = (thisRef.application as DiApplication).activityContainer
         return container.find(injectedClass, thisRef)
             ?: throw IllegalStateException("${injectedClass.simpleName} not found in container")
     }
 }
 
-// 커스텀 프로퍼티 위임 함수
 inline fun <reified T : Any> injectActivityScopeComponent() = ActivityScopeComponentFactory(T::class)
 
 private const val TAG = "ActivityScopeComponentF"
