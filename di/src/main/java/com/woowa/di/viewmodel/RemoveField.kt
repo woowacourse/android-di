ab2d1aa4b9b3858f8446604d2ab90934f8008580
gaeun5744 <geun5744@gmail.com>
2024-09-23 14:02:38 +0900

    refactor: 개별 객체 삭제 -> 모든 DI 객체 삭제로 수정

--- di/src/main/java/com/woowa/di/viewmodel/RemoveField.kt
+++ di/src/main/java/com/woowa/di/viewmodel/RemoveField.kt
@@ -1,20 +1,10 @@
 package com.woowa.di.viewmodel
 
 import androidx.lifecycle.ViewModel
-import com.woowa.di.findQualifierClassOrNull
-import javax.inject.Inject
-import kotlin.reflect.jvm.kotlinProperty
 
 inline fun <reified T : ViewModel> removeInstancesOnCleared(instance: T) {
     instance.addCloseable {
-        T::class.java.declaredFields.onEach { field ->
-            field.isAccessible = true
-        }.filter { it.isAnnotationPresent(Inject::class.java) }.forEach { field ->
-            ViewModelComponentManager.getComponentInstance(T::class).deleteDIInstance(
-                field.type.kotlin,
-                field.kotlinProperty?.findQualifierClassOrNull(),
-            )
-        }
+        ViewModelComponentManager.getComponentInstance(T::class).deleteAllDIInstance()
         ViewModelComponent.deleteInstance(T::class)
     }
 }
