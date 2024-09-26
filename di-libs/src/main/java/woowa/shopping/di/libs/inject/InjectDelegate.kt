b340190beead1144e6d436ba4965c95ee2cb25e5
murjune <sis9221302@gmail.com>
2024-09-26 15:28:59 +0900

    feat: Global injectScopeComponent, inject 함수 추가

--- di-libs/src/main/java/woowa/shopping/di/libs/inject/InjectDelegate.kt
+++ di-libs/src/main/java/woowa/shopping/di/libs/inject/InjectDelegate.kt
@@ -4,13 +4,30 @@ import woowa.shopping.di.libs.annotation.InternalApi
 import woowa.shopping.di.libs.container.Containers
 import woowa.shopping.di.libs.factory.Lifecycle
 import woowa.shopping.di.libs.qualify.Qualifier
+import woowa.shopping.di.libs.qualify.qualifier
+import kotlin.reflect.KClass
 
 inline fun <reified T : Any> inject(
-    qualifier: Qualifier? = null,
+    instanceQualifier: Qualifier? = null,
+    scopeQualifier: Qualifier? = null,
     lifecycle: Lifecycle = Lifecycle.SINGLETON,
 ): Lazy<T> {
+    @OptIn(InternalApi::class)
     return lazy {
-        @OptIn(InternalApi::class)
-        Containers.resolve(T::class, qualifier, lifecycle)
+        if (lifecycle == Lifecycle.SCOPED) {
+            requireNotNull(scopeQualifier) {
+                "Scope에서만 사용 가능한 qualifier입니다."
+            }
+            Containers.resolveScopedInstance(scopeQualifier, T::class, instanceQualifier)
+        } else {
+            Containers.resolve(T::class, instanceQualifier, lifecycle)
+        }
     }
+}
+
+inline fun <reified T : Any> injectScopeComponent(
+    scopeClazz: KClass<*>,
+    instanceQualifier: Qualifier? = null,
+): Lazy<T> {
+    return inject(instanceQualifier, qualifier(scopeClazz), Lifecycle.SCOPED)
 }
