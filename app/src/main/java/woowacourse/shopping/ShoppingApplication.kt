5e0485c83df1fe26ff1698ae7f3852d06b9f8b56
Namyunsuk <yunsuk0328@naver.com>
2024-09-26 13:33:31 +0900

    refactor: appDatabase를 DiContainer에 저장하도록 리팩토링

--- app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
+++ app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
@@ -2,19 +2,12 @@ package woowacourse.shopping
 
 import com.example.seogi.di.DiApplication
 import com.example.seogi.di.DiContainer
-import woowacourse.shopping.data.ShoppingDatabase
 import woowacourse.shopping.di.DiModule
 
 class ShoppingApplication : DiApplication() {
     override fun onCreate() {
         super.onCreate()
-        appDatabase = ShoppingDatabase.getInstance(this)
         module = DiModule
-        diContainer = DiContainer(DiModule, this)
-    }
-
-    companion object {
-        lateinit var appDatabase: ShoppingDatabase
-            private set
+        diContainer = DiContainer(DiModule, this@ShoppingApplication)
     }
 }
--- app/src/main/java/woowacourse/shopping/di/DiModule.kt
+++ app/src/main/java/woowacourse/shopping/di/DiModule.kt
@@ -3,15 +3,15 @@ package woowacourse.shopping.di
 import android.content.Context
 import com.example.seogi.di.Module
 import com.example.seogi.di.annotation.ActivityScoped
-import com.example.seogi.di.annotation.DaoInstance
+import com.example.seogi.di.annotation.Provides
 import com.example.seogi.di.annotation.Qualifier
 import com.example.seogi.di.annotation.SingleTone
 import com.example.seogi.di.annotation.ViewModelScoped
-import woowacourse.shopping.ShoppingApplication
 import woowacourse.shopping.data.CartProductDao
 import woowacourse.shopping.data.CartRepositoryInMemory
 import woowacourse.shopping.data.CartRepositoryOnDisk
 import woowacourse.shopping.data.ProductRepositoryImpl
+import woowacourse.shopping.data.ShoppingDatabase
 import woowacourse.shopping.model.repository.CartRepository
 import woowacourse.shopping.model.repository.ProductRepository
 import woowacourse.shopping.ui.cart.DateFormatter
@@ -37,6 +37,11 @@ object DiModule : Module {
     @ActivityScoped
     fun provideDateFormatter(context: Context): DateFormatter = DateFormatter(context)
 
-    @DaoInstance
-    fun provideCartProductDao(): CartProductDao = ShoppingApplication.appDatabase.cartProductDao()
+    @Provides
+    @SingleTone
+    fun provideAppDatabase(context: Context): ShoppingDatabase = ShoppingDatabase.getInstance(context)
+
+    @Provides
+    @SingleTone
+    fun provideCartProductDao(appDatabase: ShoppingDatabase): CartProductDao = appDatabase.cartProductDao()
 }
