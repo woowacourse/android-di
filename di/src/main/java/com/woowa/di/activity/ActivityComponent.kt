24929a65103cd83bae2b5ba6c07af4d83e0cee78
gaeun5744 <geun5744@gmail.com>
2024-09-26 13:44:28 +0900

    refactor: ComponentActivity가 아닐 경우, 예외 발생

--- di/src/main/java/com/woowa/di/activity/ActivityComponent.kt
+++ di/src/main/java/com/woowa/di/activity/ActivityComponent.kt
@@ -18,96 +18,97 @@ import kotlin.reflect.jvm.jvmErasure
 
 class ActivityComponent<T : ComponentActivity> private constructor(private val targetClazz: KClass<T>) :
     Component, DefaultLifecycleObserver {
-        private val diFunc: MutableMap<KFunction<*>, Any> = mutableMapOf()
-        private val diInstances: MutableMap<String, Any?> = mutableMapOf()
-        private var context: Context? = null
+    private val diFunc: MutableMap<KFunction<*>, Any> = mutableMapOf()
+    private val diInstances: MutableMap<String, Any?> = mutableMapOf()
+    private var context: Context? = null
 
-        override fun onCreate(owner: LifecycleOwner) {
-            super.onCreate(owner)
-            context = (owner as ComponentActivity).baseContext
-            injectActivityComponentFields(owner)
-        }
+    override fun onCreate(owner: LifecycleOwner) {
+        super.onCreate(owner)
+        context = (owner as? ComponentActivity)?.baseContext
+            ?: error("ComponentActivity에서만 DI를 주입할 수 있습니다.")
+        injectActivityComponentFields(owner)
+    }
 
-        override fun onDestroy(owner: LifecycleOwner) {
-            super.onDestroy(owner)
-            context = null
-            owner.lifecycle.removeObserver(this)
-        }
+    override fun onDestroy(owner: LifecycleOwner) {
+        super.onDestroy(owner)
+        context = null
+        owner.lifecycle.removeObserver(this)
+    }
 
-        override fun getDIInstance(
-            type: KClass<*>,
-            qualifier: KClass<out Annotation>?,
-        ): Any? {
-            qualifier?.let {
-                return diInstances.getOrPut((type.simpleName + it.simpleName)) {
-                    createDIInstance(type, qualifier)
-                }
-            }
-            return diInstances.getOrPut(
-                type.simpleName ?: error("익명 객체와 같이, 이름이 없는 객체는 di 주입을 할 수 없습니다."),
-            ) {
-                createDIInstance(type)
+    override fun getDIInstance(
+        type: KClass<*>,
+        qualifier: KClass<out Annotation>?,
+    ): Any? {
+        qualifier?.let {
+            return diInstances.getOrPut((type.simpleName + it.simpleName)) {
+                createDIInstance(type, qualifier)
             }
         }
-
-        override fun registerDIInstance(
-            binder: Any,
-            kFunc: KFunction<*>,
+        return diInstances.getOrPut(
+            type.simpleName ?: error("익명 객체와 같이, 이름이 없는 객체는 di 주입을 할 수 없습니다."),
         ) {
-            diFunc[kFunc] = binder
-        }
-
-        override fun deleteAllDIInstance() {
-            diInstances.clear()
-        }
-
-        private fun createDIInstance(
-            type: KClass<*>,
-            qualifier: KClass<out Annotation>? = null,
-        ): Any? {
-            val kFunc =
-                diFunc.keys.find { it.returnType.jvmErasure == type && it.findQualifierClassOrNull() == qualifier }
-                    ?: error("${type.simpleName}이 component에 등록되지 않았습니다.")
-
-            val parameters =
-                kFunc.parameters.filter { it.kind == KParameter.Kind.VALUE }.map {
-                    when {
-                        it.hasAnnotation<ApplicationContext>() -> SingletonComponent.applicationContext
-                        it.hasAnnotation<ActivityContext>() -> context
-                        else ->
-                            SingletonComponentManager.getDIInstance(
-                                it.type.jvmErasure,
-                                it.findQualifierClassOrNull(),
-                            )
-                    }
-                }
-
-            val instance =
-                if (parameters.isEmpty()) {
-                    kFunc.call(diFunc[kFunc])
-                } else {
-                    kFunc.call(diFunc[kFunc], *parameters.toTypedArray())
-                }
-
-            instance?.let {
-                injectActivityComponentFields(it)
-            }
-            return instance
-        }
-
-        companion object {
-            private val instances = mutableMapOf<KClass<*>, ActivityComponent<*>>()
-
-            fun <binder : ComponentActivity> getInstance(binderClazz: KClass<binder>): ActivityComponent<binder> {
-                return instances.getOrPut(binderClazz) {
-                    val newInstance = ActivityComponent(binderClazz)
-                    instances[binderClazz] = newInstance
-                    newInstance
-                } as ActivityComponent<binder>
-            }
-
-            fun deleteInstance(binderClazz: KClass<*>) {
-                instances.remove(binderClazz)
-            }
+            createDIInstance(type)
         }
     }
+
+    override fun registerDIInstance(
+        binder: Any,
+        kFunc: KFunction<*>,
+    ) {
+        diFunc[kFunc] = binder
+    }
+
+    override fun deleteAllDIInstance() {
+        diInstances.clear()
+    }
+
+    private fun createDIInstance(
+        type: KClass<*>,
+        qualifier: KClass<out Annotation>? = null,
+    ): Any? {
+        val kFunc =
+            diFunc.keys.find { it.returnType.jvmErasure == type && it.findQualifierClassOrNull() == qualifier }
+                ?: error("${type.simpleName}이 component에 등록되지 않았습니다.")
+
+        val parameters =
+            kFunc.parameters.filter { it.kind == KParameter.Kind.VALUE }.map {
+                when {
+                    it.hasAnnotation<ApplicationContext>() -> SingletonComponent.applicationContext
+                    it.hasAnnotation<ActivityContext>() -> context
+                    else ->
+                        SingletonComponentManager.getDIInstance(
+                            it.type.jvmErasure,
+                            it.findQualifierClassOrNull(),
+                        )
+                }
+            }
+
+        val instance =
+            if (parameters.isEmpty()) {
+                kFunc.call(diFunc[kFunc])
+            } else {
+                kFunc.call(diFunc[kFunc], *parameters.toTypedArray())
+            }
+
+        instance?.let {
+            injectActivityComponentFields(it)
+        }
+        return instance
+    }
+
+    companion object {
+        private val instances = mutableMapOf<KClass<*>, ActivityComponent<*>>()
+
+        fun <binder : ComponentActivity> getInstance(binderClazz: KClass<binder>): ActivityComponent<binder> {
+            return instances.getOrPut(binderClazz) {
+                val newInstance = ActivityComponent(binderClazz)
+                instances[binderClazz] = newInstance
+                newInstance
+            } as ActivityComponent<binder>
+        }
+
+        fun deleteInstance(binderClazz: KClass<*>) {
+            instances.remove(binderClazz)
+        }
+    }
+}
