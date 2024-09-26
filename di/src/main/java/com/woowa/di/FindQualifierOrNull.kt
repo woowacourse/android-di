c1d6b5293af780780b960f8de4e9ae9b5595bb43
gaeun5744 <geun5744@gmail.com>
2024-09-26 13:29:44 +0900

    refactor: 공통된 로직 메서드 통일

--- di/src/main/java/com/woowa/di/FindQualifierOrNull.kt
+++ di/src/main/java/com/woowa/di/FindQualifierOrNull.kt
@@ -1,17 +1,13 @@
 package com.woowa.di
 
 import javax.inject.Qualifier
+import kotlin.reflect.KAnnotatedElement
 import kotlin.reflect.KCallable
 import kotlin.reflect.KClass
 import kotlin.reflect.KParameter
 import kotlin.reflect.full.findAnnotation
 
-fun KCallable<*>.findQualifierClassOrNull(): KClass<out Annotation>? =
-    this.annotations.firstOrNull { annotation ->
-        annotation.annotationClass.findAnnotation<Qualifier>() != null
-    }?.annotationClass
-
-fun KParameter.findQualifierClassOrNull(): KClass<out Annotation>? =
+fun KAnnotatedElement.findQualifierClassOrNull(): KClass<out Annotation>? =
     this.annotations.firstOrNull { annotation ->
         annotation.annotationClass.findAnnotation<Qualifier>() != null
     }?.annotationClass
