f681d7264fb107d7be92c595a2d5dc4186831685
yrsel <xxknu88@gmail.com>
2025-10-01 08:36:46 +0900

    refactor: 뷰모델 생성자 기본값을 제공하여 테스트 통과하도록 수정

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # android-di
+
+## 🚀만들면서 배우는 DI - 0.5단계 생성자 주입(수동)
+
+### 기능 요구 사항
+
+- [x] 테스트하기 어렵다.
+- [x] Repository 객체를 교체하기 위해 또다른 객체를 만들어 바꿔줘야 한다. 즉, ViewModel에 직접적인 변경사항이 발생한다.
+
+### 접근 방향
+
+```
+테스트 하기 어렵다. & Repository 객체를 교체하기 위해 또다른 객체를 만들어 바꿔줘야 한다.
+- Repository 내부에서 사용중인 데이터를 제어할 수 없어서 테스트하기 어렵다.
+- Repository를 interface로 추상화하고, ViewModel에서는 추상화된 Repository 타입을 생성자 파라미터로 받는다. 
+  -> interface를 구현하는 테스트용 Repository를 생성 후 넣어준다.
+  -> ViewModel에서는 직접적인 변경사항 없이 외부에서 해당 Repository 인터페이스를 구현한 객체들을 자유롭게 변경할 수 있다. 
+```
--- app/src/main/java/woowacourse/shopping/ui/MainViewModel.kt
+++ app/src/main/java/woowacourse/shopping/ui/MainViewModel.kt
@@ -3,21 +3,20 @@ package woowacourse.shopping.ui
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import woowacourse.shopping.data.CartRepository
-import woowacourse.shopping.data.ProductRepository
+import woowacourse.shopping.di.RepositoryModule
+import woowacourse.shopping.domain.CartRepository
+import woowacourse.shopping.domain.ProductRepository
 import woowacourse.shopping.model.Product
 
 class MainViewModel(
-    private val productRepository: ProductRepository,
-    private val cartRepository: CartRepository,
+    private val productRepository: ProductRepository = RepositoryModule.provideProductRepository,
+    private val cartRepository: CartRepository = RepositoryModule.provideCartRepository,
 ) : ViewModel() {
-
     private val _products: MutableLiveData<List<Product>> = MutableLiveData(emptyList())
     val products: LiveData<List<Product>> get() = _products
 
     private val _onProductAdded: MutableLiveData<Boolean> = MutableLiveData(false)
     val onProductAdded: LiveData<Boolean> get() = _onProductAdded
-
 
     fun addCartProduct(product: Product) {
         cartRepository.addCartProduct(product)
