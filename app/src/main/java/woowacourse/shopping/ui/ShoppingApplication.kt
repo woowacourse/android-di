53efd51a66862a600989a8f211465fb969bf7673
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-09-30 22:09:29 +0900

    refactor: `AppContainer` 를 통해 `ViewModel` 생성 시 의존성 주입
    `ShoppingApplication` 클래스에 `AppContainer` 인터페이스를 구현하여 의존성 주입 컨테이너 역할을 하도록 변경했습니다.
    `ViewModelStoreOwner` 의 확장 함수 `viewModel` 을 추가하여, `ViewModelProvider.Factory` 를 통해 `ViewModel` 생성 시 `AppContainer` 로부터 필요한 의존성을 주입받도록 구현했습니다.
    `AndroidManifest.xml` 에 `ShoppingApplication` 을 등록했습니다.

---
+++ app/src/main/java/woowacourse/shopping/ui/ShoppingApplication.kt
@@ -0,0 +1,25 @@
+package woowacourse.shopping.ui
+
+import android.app.Application
+import woowacourse.shopping.data.DefaultCartRepository
+import woowacourse.shopping.data.DefaultProductRepository
+import woowacourse.shopping.model.CartRepository
+import woowacourse.shopping.model.ProductRepository
+import kotlin.reflect.KType
+import kotlin.reflect.full.createType
+
+class ShoppingApplication :
+    Application(),
+    AppContainer {
+    private val dependencies: Map<KType, Any> =
+        mapOf(
+            ProductRepository::class.createType() to DefaultProductRepository(),
+            CartRepository::class.createType() to DefaultCartRepository(),
+        )
+
+    override fun dependency(type: KType): Any = dependencies[type] ?: error("Don't know how to inject $type")
+}
+
+interface AppContainer {
+    fun dependency(type: KType): Any
+}
---
+++ app/src/main/java/woowacourse/shopping/ui/ViewModelStoreOwner.kt
@@ -0,0 +1,39 @@
+package woowacourse.shopping.ui
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.ViewModelStoreOwner
+import androidx.lifecycle.viewmodel.CreationExtras
+import kotlin.reflect.KClass
+import kotlin.reflect.KFunction
+import kotlin.reflect.KParameter
+import kotlin.reflect.full.primaryConstructor
+
+fun <VM : ViewModel> ViewModelStoreOwner.viewModel(clazz: KClass<VM>): Lazy<VM> =
+    lazy { ViewModelProvider(this, viewModelFactory(clazz))[clazz.java] }
+
+private fun <VM : ViewModel> viewModelFactory(viewModelKClass: KClass<VM>): ViewModelProvider.Factory =
+    object : ViewModelProvider.Factory {
+        @Suppress("UNCHECKED_CAST")
+        override fun <T : ViewModel> create(
+            modelClass: Class<T>,
+            extras: CreationExtras,
+        ): T {
+            val application: Application = checkNotNull(extras[APPLICATION_KEY])
+
+            val primaryConstructor: KFunction<VM> =
+                viewModelKClass.primaryConstructor
+                    ?: error("${viewModelKClass.qualifiedName} doesn't have primary constructor")
+
+            val appContainer: AppContainer = application as AppContainer
+
+            val parameters: List<Any> =
+                primaryConstructor.parameters.map { parameter: KParameter ->
+                    appContainer.dependency(parameter.type)
+                }
+
+            return primaryConstructor.call(*parameters.toTypedArray()) as T
+        }
+    }
