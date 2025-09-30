30785d7c94a7fe838b8b4b92bb7d077e29d4a8b8
Ham BeomJoon <hbj0802@naver.com>
2025-09-30 17:04:50 +0900

    refactor: ViewModelFactory에 Reflection 적용

--- app/src/main/java/woowacourse/shopping/ui/ViewModelFactory.kt
+++ app/src/main/java/woowacourse/shopping/ui/ViewModelFactory.kt
@@ -3,19 +3,23 @@ package woowacourse.shopping.ui
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import woowacourse.shopping.di.AppContainer
-import woowacourse.shopping.ui.cart.CartViewModel
+import woowacourse.shopping.domain.CartRepository
+import woowacourse.shopping.domain.ProductRepository
 
 class ViewModelFactory : ViewModelProvider.Factory {
     @Suppress("UNCHECKED_CAST")
-    override fun <T : ViewModel> create(modelClass: Class<T>): T =
-        when (modelClass) {
-            MainViewModel::class.java ->
-                MainViewModel(
-                    AppContainer.cartRepository,
-                    AppContainer.productRepository,
-                ) as T
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        val constructor = modelClass.constructors.first()
+        val params =
+            constructor.parameters
+                .map { param ->
+                    when (param.type.kotlin) {
+                        CartRepository::class -> AppContainer.cartRepository
+                        ProductRepository::class -> AppContainer.productRepository
+                        else -> throw IllegalArgumentException("${param.type} 타입에 대한 provider가 없습니다")
+                    }
+                }.toTypedArray()
 
-            CartViewModel::class.java -> CartViewModel(AppContainer.cartRepository) as T
-            else -> throw IllegalArgumentException("Unknown ViewModel class: $modelClass")
-        }
+        return constructor.newInstance(*params) as T
+    }
 }
