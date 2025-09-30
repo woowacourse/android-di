1be015dfbe7f068e620de46cc242625207e3dacd
hwannow <vnfdlvgid061@naver.com>
2025-09-30 19:58:25 +0900

    refactor: domain 레이어 추가 및 코드 포맷팅

--- app/src/main/java/woowacourse/shopping/ui/cart/CartProductAdapter.kt
+++ app/src/main/java/woowacourse/shopping/ui/cart/CartProductAdapter.kt
@@ -9,7 +9,6 @@ class CartProductAdapter(
     onClickDelete: (position: Int) -> Unit,
     private val dateFormatter: DateFormatter,
 ) : RecyclerView.Adapter<CartProductViewHolder>() {
-
     private val items: MutableList<Product> = items.toMutableList()
 
     private val onClickDelete = { position: Int ->
@@ -17,11 +16,17 @@ class CartProductAdapter(
         removeItem(position)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CartProductViewHolder {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int,
+    ): CartProductViewHolder {
         return CartProductViewHolder.from(parent, dateFormatter, onClickDelete)
     }
 
-    override fun onBindViewHolder(holder: CartProductViewHolder, position: Int) {
+    override fun onBindViewHolder(
+        holder: CartProductViewHolder,
+        position: Int,
+    ) {
         holder.bind(items[position])
     }
 
