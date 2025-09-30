87916302e2ffd087aeea86c6a3837060e553e95d
Ham BeomJoon <hbj0802@naver.com>
2025-09-30 18:31:26 +0900

    test: MainActivity Robolectric 테스트

--- app/src/test/java/woowacourse/shopping/MainActivityTest.kt
+++ app/src/test/java/woowacourse/shopping/MainActivityTest.kt
@@ -3,6 +3,7 @@ package woowacourse.shopping
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.ViewModelProvider
 import com.google.common.truth.Truth.assertThat
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -15,16 +16,19 @@ import woowacourse.shopping.ui.MainViewModel
 class MainActivityTest {
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
+    private lateinit var activity: MainActivity
 
-    @Test
-    fun `Activity 실행 테스트`() {
-        // given
-        val activity =
+    @Before
+    fun setUp() {
+        activity =
             Robolectric
                 .buildActivity(MainActivity::class.java)
                 .create()
                 .get()
+    }
 
+    @Test
+    fun `Activity 실행 테스트`() {
         // then
         assertThat(activity).isNotNull()
     }
@@ -32,14 +36,16 @@ class MainActivityTest {
     @Test
     fun `ViewModel 주입 테스트`() {
         // given
-        val activity =
-            Robolectric
-                .buildActivity(MainActivity::class.java)
-                .create()
-                .get()
         val viewModel = ViewModelProvider(activity)[MainViewModel::class.java]
 
         // then
         assertThat(viewModel).isNotNull()
+    }
+
+    @Test
+    fun `툴바가_설정되어_있는지_확인`() {
+        val toolbar = activity.findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        assertThat(toolbar).isNotNull()
+        assertThat(activity.supportActionBar).isNotNull()
     }
 }
