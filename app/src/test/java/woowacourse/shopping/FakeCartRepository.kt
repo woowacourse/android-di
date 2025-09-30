dd6b490d94f776316a314e40a156b84b9a46ad41
Ham BeomJoon <hbj0802@naver.com>
2025-09-30 18:31:07 +0900

    test: 테스트를 위한 FakeRepository 구현

---
+++ app/src/test/java/woowacourse/shopping/FakeCartRepository.kt
@@ -0,0 +1,25 @@
+package woowacourse.shopping
+
+import woowacourse.shopping.domain.CartRepository
+import woowacourse.shopping.domain.Product
+
+class FakeCartRepository : CartRepository {
+    private val cartProducts: MutableList<Product> = mutableListOf()
+
+    fun setCartProducts(products: List<Product>) {
+        cartProducts.clear()
+        cartProducts.addAll(products)
+    }
+
+    override fun addCartProduct(product: Product) {
+        cartProducts.add(product)
+    }
+
+    override fun getAllCartProducts(): List<Product> = cartProducts.toList()
+
+    override fun deleteCartProduct(id: Int) {
+        if (id in cartProducts.indices) {
+            cartProducts.removeAt(id)
+        }
+    }
+}
