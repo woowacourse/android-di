b969ff6251191b98a62e1183e543ab935f70d131
oungsi2000 <oungsi1000@gmail.com>
2025-10-01 00:46:20 +0900

    feat: DI 컨테이너에 순환 참조 감지 기능 추가

--- app/src/main/java/woowacourse/shopping/di/AppContainer.kt
+++ app/src/main/java/woowacourse/shopping/di/AppContainer.kt
@@ -13,18 +13,40 @@ class AppContainer {
 
     operator fun get(clazz: KClass<*>): Any? = cache[clazz]
 
-    fun instantiate(clazz: KClass<*>): Any? {
+    private fun instantiate(
+        clazz: KClass<*>,
+        inProgress: MutableSet<KClass<*>>,
+    ): Any? {
         if (cache.containsKey(clazz)) return this[clazz]
+
+        if (inProgress.contains(clazz)) {
+            throw IllegalArgumentException(
+                "$ERR_CIRCULAR_DEPENDENCY_DETECTED : ${clazz.simpleName}",
+            )
+        }
+
         return clazz.primaryConstructor?.let { constructor ->
+            inProgress.add(clazz)
             val arguments =
                 constructor.parameters
                     .filter { !it.isOptional }
                     .associateWith { parameter ->
-                        instantiate(parameter.type.jvmErasure)
+                        val instance = instantiate(parameter.type.jvmErasure, inProgress)
+                        instance
                     }
+            inProgress.remove(clazz)
             cache[clazz] = constructor.callBy(arguments)
             cache[clazz]
         } ?: clazz.createInstance()
+    }
+
+    fun instantiate(clazz: KClass<*>): Any? {
+        val inProgress = mutableSetOf<KClass<*>>()
+        return instantiate(clazz, inProgress)
+    }
+
+    companion object {
+        private const val ERR_CIRCULAR_DEPENDENCY_DETECTED = "순환 참조가 발견되었습니다"
     }
 }
 
