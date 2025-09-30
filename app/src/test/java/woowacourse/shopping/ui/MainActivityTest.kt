84ea0a250d1b2ed0cfacd0db0c405fc66a411440
cucumber99 <dlsguq1@gmail.com>
2025-09-30 20:02:37 +0900

    test: 테스트 패키지 변경

--- app/src/test/java/woowacourse/shopping/ui/MainActivityTest.kt
+++ app/src/test/java/woowacourse/shopping/ui/MainActivityTest.kt
@@ -1,20 +1,16 @@
-package woowacourse.shopping
+package woowacourse.shopping.ui
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.ViewModelProvider
-import com.google.common.truth.Truth.assertThat
+import com.google.common.truth.Truth
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
-import woowacourse.shopping.ui.MainActivity
-import woowacourse.shopping.ui.MainViewModel
-
 
 @RunWith(RobolectricTestRunner::class)
 class MainActivityTest {
-
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
 
@@ -27,7 +23,7 @@ class MainActivityTest {
             .get()
 
         // then
-        assertThat(activity).isNotNull()
+        Truth.assertThat(activity).isNotNull()
     }
 
     @Test
@@ -40,6 +36,6 @@ class MainActivityTest {
         val viewModel = ViewModelProvider(activity)[MainViewModel::class.java]
 
         // then
-        assertThat(viewModel).isNotNull()
+        Truth.assertThat(viewModel).isNotNull()
     }
 }
