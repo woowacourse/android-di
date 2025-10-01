87765b21c5328c624def52cd4db824c88781aef2
Ham BeomJoon <hbj0802@naver.com>
2025-10-01 16:46:36 +0900

    test: CartActivity Robolectric 테스트

---
+++ app/src/test/java/woowacourse/shopping/ui/CartActivityTest.kt
@@ -0,0 +1,45 @@
+package woowacourse.shopping.ui
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.ViewModelProvider
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import woowacourse.shopping.ui.cart.CartActivity
+import woowacourse.shopping.ui.cart.CartViewModel
+
+@RunWith(RobolectricTestRunner::class)
+class CartActivityTest {
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var activity: CartActivity
+
+    @Before
+    fun setUp() {
+        activity =
+            Robolectric
+                .buildActivity(CartActivity::class.java)
+                .create()
+                .get()
+    }
+
+    @Test
+    fun `Activity 실행 테스트`() {
+        // then
+        assertThat(activity).isNotNull()
+    }
+
+    @Test
+    fun `ViewModel 주입 테스트`() {
+        // given
+        val viewModel = ViewModelProvider(activity)[CartViewModel::class.java]
+
+        // then
+        assertThat(viewModel).isNotNull()
+    }
+}
