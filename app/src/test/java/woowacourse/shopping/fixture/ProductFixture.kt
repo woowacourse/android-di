5d11b28c4585289db49011a569c1f1acdbd4ffb4
hwannow <vnfdlvgid061@naver.com>
2025-10-01 22:06:47 +0900

    test: FakeRepository 및 Fixture 추가

---
+++ app/src/test/java/woowacourse/shopping/fixture/ProductFixture.kt
@@ -0,0 +1,9 @@
+package woowacourse.shopping.fixture
+
+import woowacourse.shopping.model.Product
+
+val TONKATSU = Product("돈가스", 11000, "image_url")
+
+val MALATANG = Product("마라탕", 14000, "image_url")
+
+val GOBCHANG = Product("곱창", 40000, "image_url")
