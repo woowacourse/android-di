39d6be48bd3df997aa7821d1a03b0baba488c6e0
chanho0908 <uni070@naver.com>
2025-10-01 19:42:28 +0900

    refactor: 사용하지 않는 파일 제거

--- app/src/main/java/woowacourse/shopping/AppContainer.kt
+++
@@ -1,8 +0,0 @@
-package woowacourse.shopping
-
-import android.content.Context
-import woowacourse.shopping.data.di.RepositoryModule
-
-class AppContainer(context: Context) {
-    val repositoryModule = RepositoryModule()
-}
