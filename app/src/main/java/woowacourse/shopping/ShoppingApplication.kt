a931c9145d7a02d115cf710d6227d6b3e171f44c
devfeijoa <devfeijoa@gmail.com>
2025-10-02 17:09:01 +0900

    feat: Application 클래스 구현 및 DI 초기화

---
+++ app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
@@ -0,0 +1,15 @@
+package woowacourse.shopping
+
+import android.app.Application
+import woowacourse.shopping.di.RepositoryModule
+
+class ShoppingApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        initializeDi()
+    }
+
+    private fun initializeDi() {
+        RepositoryModule.init()
+    }
+}
