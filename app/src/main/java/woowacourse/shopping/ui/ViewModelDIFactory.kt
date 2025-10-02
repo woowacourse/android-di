f20b9fdc26c55a5241c9ffc511b0f0f71e4c68ab
윤동현 <donghyunyoon81@gmail.com>
2025-10-02 16:52:36 +0900

    feat: ViewModel 생성 시 SavedStateHandle 주입

--- app/src/main/java/woowacourse/shopping/ui/ViewModelDIFactory.kt
+++ app/src/main/java/woowacourse/shopping/ui/ViewModelDIFactory.kt
@@ -1,12 +1,28 @@
 package woowacourse.shopping.ui
 
+import android.os.Bundle
+import androidx.lifecycle.AbstractSavedStateViewModelFactory
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.savedstate.SavedStateRegistryOwner
 import woowacourse.shopping.di.AppContainer
+import kotlin.reflect.KClass
+import kotlin.reflect.full.primaryConstructor
 
 class ViewModelDIFactory(
 ) : ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return AppContainer.getProvider(modelClass.kotlin) as T
+    override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
+        val constructor =
+            modelClass.kotlin.primaryConstructor ?: return create(modelClass)
+        val args = constructor.parameters.associateWith { parameter ->
+            when (val clazz = parameter.type.classifier as KClass<*>) {
+                SavedStateHandle::class -> extras.createSavedStateHandle()
+                else -> AppContainer.getProvider(clazz)
+            }
+        }
+        return constructor.callBy(args) as T
     }
 }
