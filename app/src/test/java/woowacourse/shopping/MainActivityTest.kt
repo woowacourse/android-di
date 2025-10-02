32150949fca8259f2f7b153b8ffd47ff09644cb4
yrsel <xxknu88@gmail.com>
2025-10-02 17:00:07 +0900

    test: 메인화면 상품 클릭 토스트 메시지 테스트 추가

--- app/src/test/java/woowacourse/shopping/MainActivityTest.kt
+++ app/src/test/java/woowacourse/shopping/MainActivityTest.kt
@@ -2,12 +2,14 @@ package woowacourse.shopping
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.ViewModelProvider
-import com.google.common.truth.Truth.assertThat
+import androidx.recyclerview.widget.RecyclerView
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.shadows.ShadowToast
 import woowacourse.shopping.ui.MainActivity
 import woowacourse.shopping.ui.MainViewModel
 
@@ -19,12 +21,7 @@ class MainActivityTest {
     @Test
     fun `Activity 실행 테스트`() {
         // given
-        val activity =
-            Robolectric
-                .buildActivity(MainActivity::class.java)
-                .create()
-                .get()
-
+        val activity = Robolectric.buildActivity(MainActivity::class.java).create().get()
         // then
         assertThat(activity).isNotNull()
     }
@@ -32,14 +29,25 @@ class MainActivityTest {
     @Test
     fun `ViewModel 주입 테스트`() {
         // given
-        val activity =
-            Robolectric
-                .buildActivity(MainActivity::class.java)
-                .create()
-                .get()
+        val activity = Robolectric.buildActivity(MainActivity::class.java).create().get()
         val viewModel = ViewModelProvider(activity)[MainViewModel::class.java]
 
         // then
         assertThat(viewModel).isNotNull()
+    }
+
+    @Test
+    fun `상품 하나를 클릭하면 토스트 메시지가 나온다`() {
+        // given
+        val activity = Robolectric.buildActivity(MainActivity::class.java).setup().get()
+        val recyclerView = activity.findViewById<RecyclerView>(R.id.rv_products)
+        val expected = "장바구니에 추가되었습니다."
+
+        // when
+        recyclerView.layoutManager?.findViewByPosition(0)?.performClick()
+
+        // then
+        val actual = ShadowToast.getTextOfLatestToast()
+        assertThat(actual).isEqualTo(expected)
     }
 }
