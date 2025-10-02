a6c022c1993b968684818420daa66b905dc53e6f
jerry8282 <ka06196@naver.com>
2025-10-02 15:00:10 +0900

    refactor: DI 컨테이너에서 getInstance 호출 시에 인스턴스를 생성하게 하도록 수정

--- app/src/main/java/woowacourse/shopping/di/DiContainer.kt
+++ app/src/main/java/woowacourse/shopping/di/DiContainer.kt
@@ -1,21 +1,28 @@
 package woowacourse.shopping.di
 
+import woowacourse.shopping.data.DefaultCartRepository
+import woowacourse.shopping.data.DefaultProductRepository
+import woowacourse.shopping.model.CartRepository
+import woowacourse.shopping.model.ProductRepository
 import kotlin.reflect.KClass
+import kotlin.reflect.full.createInstance
 
 object DiContainer {
     private const val ERROR_MESSAGE = "%s 인스턴스가 생성되지 않았습니다"
 
-    private val instances: MutableMap<KClass<*>, Any> = mutableMapOf()
+    private val instancePool: MutableMap<KClass<*>, Any> = mutableMapOf()
 
-    fun <T : Any> postInstance(
-        kClass: KClass<T>,
-        instance: Any,
-    ) {
-        instances[kClass] = instance
-    }
+    private val implementationMappings: MutableMap<KClass<*>, KClass<*>> = mutableMapOf(
+        CartRepository::class to DefaultCartRepository::class,
+        ProductRepository::class to DefaultProductRepository::class,
+    )
 
     @Suppress("UNCHECKED_CAST")
-    fun <T : Any> getInstance(kClass: KClass<T>): T =
-        instances[kClass] as? T
-            ?: throw IllegalArgumentException(ERROR_MESSAGE.format(kClass.simpleName))
+    fun <T : Any> getInstance(kClass: KClass<T>): T {
+        return instancePool.getOrPut(kClass) {
+            val implClass: KClass<*> = implementationMappings[kClass]
+                ?: throw IllegalArgumentException(ERROR_MESSAGE.format(kClass.simpleName))
+            implClass.createInstance()
+        } as T
+    }
 }
