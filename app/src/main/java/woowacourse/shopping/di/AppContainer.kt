54c21abde13737c10d45add8f1212eaf12de80c2
JunHyuk Im <imjh0417@gmail.com>
2025-10-02 15:11:20 +0900

    refactor: Reflection을 통해 AppContainer에서 의존성 탐색 및 주입

--- app/src/main/java/woowacourse/shopping/di/AppContainer.kt
+++ app/src/main/java/woowacourse/shopping/di/AppContainer.kt
@@ -1,9 +1,11 @@
 package woowacourse.shopping.di
 
+import woowacourse.shopping.data.CartRepository
 import woowacourse.shopping.data.DefaultCartRepository
 import woowacourse.shopping.data.DefaultProductRepository
+import woowacourse.shopping.data.ProductRepository
 
 class AppContainer {
-    val productRepository = DefaultProductRepository()
-    val cartRepository = DefaultCartRepository()
+    val productRepository: ProductRepository = DefaultProductRepository()
+    val cartRepository: CartRepository = DefaultCartRepository()
 }
--- app/src/main/java/woowacourse/shopping/di/DependencyInjector.kt
+++ app/src/main/java/woowacourse/shopping/di/DependencyInjector.kt
@@ -1,11 +1,12 @@
 package woowacourse.shopping.di
 
 import androidx.lifecycle.ViewModel
-import woowacourse.shopping.data.CartRepository
-import woowacourse.shopping.data.ProductRepository
 import kotlin.reflect.KClass
+import kotlin.reflect.KClassifier
 import kotlin.reflect.KParameter
+import kotlin.reflect.KProperty1
 import kotlin.reflect.full.createInstance
+import kotlin.reflect.full.memberProperties
 import kotlin.reflect.full.primaryConstructor
 
 class DependencyInjector(
@@ -26,11 +27,18 @@ class DependencyInjector(
         }
     }
 
-    // Parameter에 해당하는 Instance를 주입
-    fun injectInstance(kParameter: KParameter): Any? =
-        when (kParameter.type.classifier) {
-            ProductRepository::class -> appContainer.productRepository
-            CartRepository::class -> appContainer.cartRepository
-            else -> null
-        }
+    // Parameter에 해당하는 Instance를 appContainer에서 찾아 반환
+    fun injectInstance(kParameter: KParameter): Any? {
+        val classifier: KClassifier = kParameter.type.classifier ?: return null
+        val paramKClass = classifier as? KClass<*> ?: return null
+
+        // appContainer의 모든 프로퍼티를 탐색
+        val matchedProperty: KProperty1<out AppContainer, *>? =
+            appContainer::class
+                .memberProperties
+                .firstOrNull { prop -> prop.returnType.classifier == paramKClass }
+
+        // 매칭되는 프로퍼티의 값 반환
+        return matchedProperty?.getter?.call(appContainer)
+    }
 }
