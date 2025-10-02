5e8e237436b5644c148c1782a2f1da7f5508b929
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-10-02 17:22:35 +0900

    refactor: 불필요한 public 함수 viewModelFactory() 제거

--- app/src/main/java/woowacourse/shopping/ui/ViewModelStoreOwner.kt
+++ app/src/main/java/woowacourse/shopping/ui/ViewModelStoreOwner.kt
@@ -12,27 +12,30 @@ import kotlin.reflect.KParameter
 import kotlin.reflect.full.primaryConstructor
 
 inline fun <reified VM : ViewModel> ViewModelStoreOwner.viewModel(): Lazy<VM> =
-    lazy { ViewModelProvider(this, viewModelFactory<VM>())[VM::class.java] }
+    lazy {
+        ViewModelProvider(
+            owner = this,
+            factory =
+                object : ViewModelProvider.Factory {
+                    @Suppress("UNCHECKED_CAST")
+                    override fun <T : ViewModel> create(
+                        modelClass: Class<T>,
+                        extras: CreationExtras,
+                    ): T {
+                        val primaryConstructor: KFunction<VM> =
+                            VM::class.primaryConstructor
+                                ?: error("${VM::class.qualifiedName} doesn't have primary constructor")
 
-inline fun <reified VM : ViewModel> viewModelFactory(): ViewModelProvider.Factory =
-    object : ViewModelProvider.Factory {
-        @Suppress("UNCHECKED_CAST")
-        override fun <T : ViewModel> create(
-            modelClass: Class<T>,
-            extras: CreationExtras,
-        ): T {
-            val primaryConstructor: KFunction<VM> =
-                VM::class.primaryConstructor
-                    ?: error("${VM::class.qualifiedName} doesn't have primary constructor")
+                        val application: Application = checkNotNull(extras[APPLICATION_KEY])
+                        val appContainer: AppContainer = application as AppContainer
 
-            val application: Application = checkNotNull(extras[APPLICATION_KEY])
-            val appContainer: AppContainer = application as AppContainer
+                        val parameters: List<Any> =
+                            primaryConstructor.parameters.map { parameter: KParameter ->
+                                appContainer.dependency(parameter.type)
+                            }
 
-            val parameters: List<Any> =
-                primaryConstructor.parameters.map { parameter: KParameter ->
-                    appContainer.dependency(parameter.type)
-                }
-
-            return primaryConstructor.call(*parameters.toTypedArray()) as T
-        }
+                        return primaryConstructor.call(*parameters.toTypedArray()) as T
+                    }
+                },
+        )[VM::class.java]
     }
