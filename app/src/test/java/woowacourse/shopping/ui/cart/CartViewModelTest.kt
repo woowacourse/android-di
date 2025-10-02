c287c5263d5b30612f8b6cc07443b21ad3acb299
jiyunlee <02leejiyun@naver.com>
2025-10-02 15:35:26 +0900

    test: CartViewModel 테스트 코드 작성

---
+++ app/src/test/java/woowacourse/shopping/ui/cart/CartViewModelTest.kt
@@ -0,0 +1,47 @@
+package woowacourse.shopping.ui.cart
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import woowacourse.shopping.domain.CartRepository
+import woowacourse.shopping.fixture.PRODUCTS_FIXTURE
+import woowacourse.shopping.fixture.repository.FakeCartRepository
+import woowacourse.shopping.getOrAwaitValue
+import woowacourse.shopping.model.Product
+
+class CartViewModelTest {
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var viewModel: CartViewModel
+    private lateinit var cartRepository: CartRepository
+
+    @Before
+    fun setup() {
+        cartRepository = FakeCartRepository(PRODUCTS_FIXTURE.toMutableList())
+        viewModel = CartViewModel(cartRepository)
+    }
+
+    @Test
+    fun `전체 장바구니 상품 조회 테스트`() {
+        // when
+        viewModel.getAllCartProducts()
+
+        // then
+        val products: List<Product> = viewModel.cartProducts.getOrAwaitValue()
+        assertThat(products).hasSize(3)
+        assertThat(products).isEqualTo(PRODUCTS_FIXTURE)
+    }
+
+    @Test
+    fun `장바구니 상품 삭제 테스트`() {
+        // when
+        viewModel.deleteCartProduct(0)
+
+        // then
+        val onProductAdded: Boolean = viewModel.onCartProductDeleted.getOrAwaitValue()
+        assertThat(onProductAdded).isTrue
+    }
+}
