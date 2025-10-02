4a159315bb0a95067d3b59a64cd74833fbc23271
Dongjoo Seo <83579348+etama123@users.noreply.github.com>
2025-10-02 14:47:02 +0900

    refactor: ViewModelFactory에서 리플랙션을 사용해 인스턴스를 생성하도록 개선

--- app/src/main/java/woowacourse/shopping/AppContainer.kt
+++ app/src/main/java/woowacourse/shopping/AppContainer.kt
@@ -1,9 +1,20 @@
 package woowacourse.shopping
 
+import woowacourse.shopping.data.CartRepository
 import woowacourse.shopping.data.CartRepositoryImpl
+import woowacourse.shopping.data.ProductRepository
 import woowacourse.shopping.data.ProductRepositoryImpl
+import kotlin.reflect.KClass
 
 class AppContainer {
-    val cartRepository by lazy { CartRepositoryImpl() }
-    val productRepository by lazy { ProductRepositoryImpl() }
+    private val providers = mutableMapOf<KClass<*>, Any>()
+
+    init {
+        providers[CartRepository::class] = CartRepositoryImpl()
+        providers[ProductRepository::class] = ProductRepositoryImpl()
+    }
+
+    fun <T : Any> get(clazz: KClass<T>): T =
+        providers[clazz] as? T
+            ?: throw IllegalArgumentException("${clazz.simpleName} provider를 찾을 수 없습니다")
 }
--- app/src/main/java/woowacourse/shopping/util/ShoppingViewModelFactory.kt
+++ app/src/main/java/woowacourse/shopping/util/ShoppingViewModelFactory.kt
@@ -3,22 +3,27 @@ package woowacourse.shopping.util
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import woowacourse.shopping.AppContainer
-import woowacourse.shopping.ui.MainViewModel
-import woowacourse.shopping.ui.cart.CartViewModel
+import kotlin.reflect.KClass
+import kotlin.reflect.full.primaryConstructor
 
 class ShoppingViewModelFactory(
     private val appContainer: AppContainer,
 ) : ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(modelClass: Class<T>): T =
-        when (modelClass) {
-            MainViewModel::class.java -> {
-                MainViewModel(appContainer.productRepository, appContainer.cartRepository) as T
-            }
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        val kClass: KClass<out ViewModel> = modelClass.kotlin
 
-            CartViewModel::class.java -> {
-                CartViewModel(appContainer.cartRepository) as T
-            }
+        // 생성할 ViewModel 클래스의 주 생성자 정보
+        val constructor =
+            kClass.primaryConstructor
+                ?: throw IllegalArgumentException("ViewModel은 주 생성자를 가지고 있지 않습니다")
 
-            else -> throw IllegalArgumentException("Unknown ViewModel class")
-        }
+        // 생성자 파라미터 타입에 맞춰 AppContainer에서 가져오기
+        val parameterTypes = constructor.parameters.map { it.type.classifier as KClass<*> }
+
+        // appContainer에서 생성된 의존성 인스턴스 가져오기
+        val dependencies = parameterTypes.map { appContainer.get(it) }
+        val argumentMap = constructor.parameters.zip(dependencies).toMap()
+
+        return constructor.callBy(argumentMap) as T
+    }
 }
