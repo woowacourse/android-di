563999464fbd5f4ff53231465c3e2856cb09b4be
DoableTuple <doabletuple@gmail.com>
2025-10-05 22:01:04 +0900

    test: Fake repository 추가

---
+++ app/src/test/java/woowacourse/shopping/fixture/FakeCartRepository.kt
@@ -0,0 +1,18 @@
+package woowacourse.shopping.fixture
+
+import woowacourse.shopping.domain.CartRepository
+import woowacourse.shopping.model.Product
+
+class FakeCartRepository : CartRepository {
+    val products: MutableList<Product> = mutableListOf()
+
+    override fun addCartProduct(product: Product) {
+        products.add(product)
+    }
+
+    override fun getAllCartProducts(): List<Product> = products.toList()
+
+    override fun deleteCartProduct(id: Int) {
+        products.removeAt(id)
+    }
+}
