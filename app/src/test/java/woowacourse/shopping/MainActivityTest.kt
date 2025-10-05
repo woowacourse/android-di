752d7f08c1bb08f34e5af3d014c1091b99ba2b6f
DoableTuple <doabletuple@gmail.com>
2025-10-05 21:03:32 +0900

    refactor: MainActivity 테스트 추가

--- app/src/test/java/woowacourse/shopping/MainActivityTest.kt
+++ app/src/test/java/woowacourse/shopping/MainActivityTest.kt
@@ -1,13 +1,16 @@
 package woowacourse.shopping
 
+import android.view.View
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.google.common.truth.Truth.assertThat
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.shadows.ShadowToast
 import woowacourse.shopping.ui.MainActivity
 import woowacourse.shopping.ui.MainViewModel
 
@@ -19,7 +22,7 @@ class MainActivityTest {
     @Test
     fun `Activity 실행 테스트`() {
         // given
-        val activity =
+        val activity: MainActivity =
             Robolectric
                 .buildActivity(MainActivity::class.java)
                 .create()
@@ -32,7 +35,7 @@ class MainActivityTest {
     @Test
     fun `ViewModel 주입 테스트`() {
         // given
-        val activity =
+        val activity: MainActivity =
             Robolectric
                 .buildActivity(MainActivity::class.java)
                 .create()
@@ -41,5 +44,26 @@ class MainActivityTest {
 
         // then
         assertThat(viewModel).isNotNull()
+    }
+
+    @Test
+    fun `상품을 클릭하면 토스트 메세지가 표시된다`() {
+        // given
+        val expected = "장바구니에 추가되었습니다."
+        val activity: MainActivity =
+            Robolectric
+                .buildActivity(MainActivity::class.java)
+                .setup()
+                .get()
+        val recyclerView: RecyclerView = activity.findViewById(R.id.rv_products)
+        val firstItem: View =
+            recyclerView.layoutManager?.findViewByPosition(0) ?: error("상품을 찾지 못했습니다.")
+
+        // when
+        firstItem.performClick()
+
+        // then
+        val actual: String = ShadowToast.getTextOfLatestToast()
+        assertThat(expected).isEqualTo(actual)
     }
 }
