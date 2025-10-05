8addf03c587abd9f08f1e0da7909effba9c48206
박지민 <pjm5489@naver.com>
2025-10-06 01:08:06 +0900

    fix: `SavedStateHandle`을 `ViewModel`에 주입하도록 `AutoViewModelFactory` 수정

--- app/src/main/java/woowacourse/shopping/ui/AutoViewModelFactory.kt
+++ app/src/main/java/woowacourse/shopping/ui/AutoViewModelFactory.kt
@@ -2,21 +2,33 @@ package woowacourse.shopping.ui
 
 import androidx.activity.ComponentActivity
 import androidx.annotation.MainThread
+import androidx.lifecycle.AbstractSavedStateViewModelFactory
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelLazy
 import androidx.lifecycle.ViewModelProvider.Factory
 import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.savedstate.SavedStateRegistryOwner
 import woowacourse.shopping.di.DiContainer
 import kotlin.reflect.KClass
 import kotlin.reflect.full.primaryConstructor
 
-class AutoViewModelFactory : Factory {
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+class AutoViewModelFactory(
+    owner: SavedStateRegistryOwner,
+) : AbstractSavedStateViewModelFactory(owner, null) {
+    override fun <T : ViewModel> create(
+        key: String,
+        modelClass: Class<T>,
+        handle: SavedStateHandle,
+    ): T {
         val kClass = modelClass.kotlin
         val constructor = requireNotNull(kClass.primaryConstructor) { kClass.java.simpleName }
         val params =
             constructor.parameters.associateWith { param ->
-                DiContainer.getInstance(param.type.classifier as KClass<*>)
+                when (param.type.classifier) {
+                    SavedStateHandle::class -> handle
+                    else -> DiContainer.getInstance(param.type.classifier as KClass<*>)
+                }
             }
         return constructor.callBy(params)
     }
@@ -25,7 +37,7 @@ class AutoViewModelFactory : Factory {
 @MainThread
 inline fun <reified VM : ViewModel> ComponentActivity.autoViewModel(
     noinline extrasProducer: (() -> CreationExtras)? = null,
-    noinline factoryProducer: (() -> Factory)? = { AutoViewModelFactory() },
+    noinline factoryProducer: (() -> Factory)? = { AutoViewModelFactory(this) },
 ): Lazy<VM> =
     ViewModelLazy(
         VM::class,
