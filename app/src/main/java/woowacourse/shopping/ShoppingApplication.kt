b1b88c8906e124be13cf875b70385359f877853b
gahyunkim <mapledt001@naver.com>
2025-10-05 21:04:57 +0900

    feat: RoomDatabase 및 CartProductDao 의존성 주입

--- app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
+++ app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
@@ -4,5 +4,5 @@ import android.app.Application
 import woowacourse.shopping.di.AppContainer
 
 class ShoppingApplication : Application() {
-    val container by lazy { AppContainer() }
+    val container by lazy { AppContainer(this) }
 }
--- app/src/main/java/woowacourse/shopping/di/AppContainer.kt
+++ app/src/main/java/woowacourse/shopping/di/AppContainer.kt
@@ -1,11 +1,24 @@
 package woowacourse.shopping.di
 
+import android.content.Context
+import woowacourse.shopping.data.CartProductDao
 import woowacourse.shopping.data.DefaultCartRepository
 import woowacourse.shopping.data.DefaultProductRepository
+import woowacourse.shopping.data.ShoppingDatabase
 import woowacourse.shopping.domain.CartRepository
 import woowacourse.shopping.domain.ProductRepository
 
-class AppContainer {
+class AppContainer(
+    private val applicationContext: Context,
+) {
+    private val database: ShoppingDatabase by lazy {
+        ShoppingDatabase.getInstance(applicationContext)
+    }
+
+    val cartProductDao: CartProductDao by lazy {
+        database.cartProductDao()
+    }
+
     val productRepository: ProductRepository by lazy { DefaultProductRepository() }
-    val cartRepository: CartRepository by lazy { DefaultCartRepository() }
+    val cartRepository: CartRepository by lazy { DefaultCartRepository(cartProductDao) }
 }
