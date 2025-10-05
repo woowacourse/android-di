9a76b96755d24ce52836f957da219bc23631d509
DoableTuple <doabletuple@gmail.com>
2025-10-05 22:00:42 +0900

    test: Fixtures 추가 및 테스트 수정

--- app/src/test/java/woowacourse/shopping/DefaultCartRepositoryTest.kt
+++ app/src/test/java/woowacourse/shopping/DefaultCartRepositoryTest.kt
@@ -3,6 +3,9 @@ package woowacourse.shopping
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 import woowacourse.shopping.data.DefaultCartRepository
+import woowacourse.shopping.fixture.PRODUCT_1
+import woowacourse.shopping.fixture.PRODUCT_2
+import woowacourse.shopping.fixture.PRODUCT_3
 import woowacourse.shopping.model.Product
 
 class DefaultCartRepositoryTest {
@@ -24,19 +27,18 @@ class DefaultCartRepositoryTest {
     @Test
     fun `장바구니에서 특정 인덱스에 있는 상품을 제거할 수 있다`() {
         // given
-        val product1 = Product("Test product 1", 1, "Test product image URL 1")
-        val product2 = Product("Test product 2", 2, "Test product image URL 2")
         val repository =
             DefaultCartRepository().apply {
-                addCartProduct(product1)
-                addCartProduct(product2)
+                addCartProduct(PRODUCT_1)
+                addCartProduct(PRODUCT_2)
+                addCartProduct(PRODUCT_3)
             }
 
         // when
-        repository.deleteCartProduct(0)
+        repository.deleteCartProduct(1)
 
         // then
-        val expected: List<Product> = listOf(product2)
+        val expected: List<Product> = listOf(PRODUCT_1, PRODUCT_3)
         val actual: List<Product> = repository.getAllCartProducts()
         assertThat(expected).isEqualTo(actual)
     }
---
+++ app/src/test/java/woowacourse/shopping/fixture/Fixtures.kt
@@ -0,0 +1,7 @@
+package woowacourse.shopping.fixture
+
+import woowacourse.shopping.model.Product
+
+val PRODUCT_1 = Product("Product fixture 1", 1_000, "URL 1")
+val PRODUCT_2 = Product("Product fixture 2", 2_000, "URL 2")
+val PRODUCT_3 = Product("Product fixture 3", 3_000, "URL 3")
