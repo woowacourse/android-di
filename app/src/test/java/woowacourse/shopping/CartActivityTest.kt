3235f7b8715bdc35ab0604082ac87416321bbb83
지원 <cheerfulboys12@gmail.com>
2025-10-07 23:28:52 +0900

    test: CartActivity 테스트 코드 추가

---
+++ app/src/test/java/woowacourse/shopping/CartActivityTest.kt
@@ -0,0 +1,39 @@
+package woowacourse.shopping
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.google.common.truth.Truth.assertThat
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import woowacourse.shopping.ui.MainViewModel
+import woowacourse.shopping.ui.cart.CartActivity
+
+@RunWith(RobolectricTestRunner::class)
+class CartActivityTest {
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Test
+    fun `Activity 실행 테스트`() {
+        // given
+        val activity =
+            Robolectric
+                .buildActivity(CartActivity::class.java)
+                .create()
+                .get()
+
+        // then
+        assertThat(activity).isNotNull()
+    }
+
+    @Test
+    fun `ViewModel 주입 테스트`() {
+        // given
+        val viewModel = DependencyFactory.create(MainViewModel::class.java)
+
+        // then
+        assertThat(viewModel).isNotNull()
+    }
+}
