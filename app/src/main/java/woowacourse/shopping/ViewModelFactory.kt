95b43ad40cc0df6b3dacd7102bff541ebdf1d791
지원 <cheerfulboys12@gmail.com>
2025-10-07 23:10:32 +0900

    refactor: 타입 캐스팅에 의한 NPE 대응

--- app/src/main/java/woowacourse/shopping/ViewModelFactory.kt
+++ app/src/main/java/woowacourse/shopping/ViewModelFactory.kt
@@ -10,11 +10,13 @@ object ViewModelFactory : ViewModelProvider.Factory {
         val kClass = modelClass.kotlin
         val constructor =
             kClass.primaryConstructor
-                ?: throw IllegalArgumentException("No primary constructor for ${modelClass.simpleName}")
+                ?: throw IllegalArgumentException("주 생성자가 없습니다: ${modelClass.simpleName}")
 
         val params =
             constructor.parameters.associateWith { param ->
-                Dependency.get(param.type.classifier as KClass<*>)
+                val mKClass = param.type.classifier as? KClass<*>
+                    ?: throw IllegalArgumentException("KClass<*>로 캐스팅할 수 없습니다: ${param.type.classifier}")
+                Dependency.get(mKClass)
             }
 
         return constructor.callBy(params)
