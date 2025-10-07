5e9726c48588d5a44722483c3a848d01415fdf98
jerry8282 <ka06196@naver.com>
2025-10-07 16:35:03 +0900

    feat: DiContainer 생성 실패 시 에러 메시지 추가

--- app/src/main/java/woowacourse/shopping/di/DiContainer.kt
+++ app/src/main/java/woowacourse/shopping/di/DiContainer.kt
@@ -40,13 +40,25 @@ object DiContainer {
 
     private fun <T : Any> createInstance(kClass: KClass<T>): T {
         val implementClass: KClass<out Any> = implementationMappings[kClass] ?: kClass
-        val constructor: KFunction<Any> = implementClass.primaryConstructor ?: error("")
+        val constructor: KFunction<Any> = implementClass.primaryConstructor
+            ?: error(ERROR_MESSAGE_NOT_HAVE_DEFAULT_CONSTRUCTOR.format(implementClass.simpleName))
 
         val arguments: Map<KParameter, Any> =
             constructor.parameters.associateWith { param ->
-                getInstance(param.type.classifier as? KClass<*> ?: error(""))
+                getInstance(
+                    param.type.classifier as? KClass<*>
+                        ?: error(
+                            ERROR_MESSAGE_CANNOT_GET_INSTANCE.format(
+                                implementClass.simpleName,
+                                param
+                            )
+                        )
+                )
             }
 
         return kClass.cast(constructor.callBy(arguments))
     }
+
+    private const val ERROR_MESSAGE_NOT_HAVE_DEFAULT_CONSTRUCTOR = "%s 클래스에 기본 생성자가 없습니다."
+    private const val ERROR_MESSAGE_CANNOT_GET_INSTANCE = "생성자 %s의 파라미터 %s 타입을 가져올 수 없습니다."
 }
