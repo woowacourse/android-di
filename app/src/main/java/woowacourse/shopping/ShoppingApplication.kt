33e9365a3335cd176a87d28f184d466bae622a41
devfeijoa <devfeijoa@gmail.com>
2025-10-07 22:59:20 +0900

    refactor: AppContainer를 적용하여 DI 방식 변경

--- app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
+++ app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
@@ -1,15 +1,9 @@
 package woowacourse.shopping
 
 import android.app.Application
+import woowacourse.shopping.di.AppContainer
 import woowacourse.shopping.di.RepositoryModule
 
 class ShoppingApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        initializeDi()
-    }
-
-    private fun initializeDi() {
-        RepositoryModule.init()
-    }
+    val container = AppContainer()
 }
