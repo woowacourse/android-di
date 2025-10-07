35e77fc3a2cf327524dce381a453b0fe9687fa64
yrsel <xxknu88@gmail.com>
2025-10-07 18:25:42 +0900

    feat: 클래스 정보를 판단하기 위한 정의 클래스 생성

---
+++ app/src/main/java/woowacourse/shopping/di/definition/DefinitionInformation.kt
@@ -0,0 +1,21 @@
+package woowacourse.shopping.di.definition
+
+import kotlin.reflect.KClass
+import woowacourse.shopping.di.AppInjector
+import woowacourse.shopping.di.Provider
+
+/**
+ * 클래스 타입을 어떻게 생성할지에 대한 정보 담는 클래스
+ *
+ * @param T 생성 대상 타입
+ * @property kclass 생성 대상 클래스 타입
+ * @property qualifier 객체 구별용 추가 식별자
+ * @property kind 생성 방식
+ * @property provider 인스턴스를 제공하기 위한 Provider 생성 함수
+ */
+data class DefinitionInformation<T : Any>(
+    val kclass: KClass<T>,
+    val qualifier: Qualifier? = null,
+    val kind: Kind = Kind.FACTORY,
+    val provider: (AppInjector) -> Provider<T>,
+)
