d27d8d857fd8a902b8ea8129e6012037c307aec9
yrsel <xxknu88@gmail.com>
2025-10-07 21:25:50 +0900

    test: DI 테스트 코드 추가

---
+++ app/src/test/java/woowacourse/shopping/fixture/InjectFixture.kt
@@ -0,0 +1,19 @@
+package woowacourse.shopping.fixture
+
+interface FixtureCar
+
+data class ElectricFixtureCarImpl(
+    val name: String,
+) : FixtureCar
+
+data class EngineFixtureCarImpl(
+    val name: String,
+) : FixtureCar
+
+data class SingletonFixtureCar(
+    val name: String,
+)
+
+data class FactoryFixtureCar(
+    val name: String,
+)
