255d6ff85a90f28aeb67561e9c036c7d3aeb8b4c
devfeijoa <devfeijoa@gmail.com>
2025-10-07 23:24:42 +0900

    feat: ViewModelFactory 의존성 주입을 위한 Application 속성 추가

--- app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
+++ app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
@@ -3,7 +3,9 @@ package woowacourse.shopping
 import android.app.Application
 import woowacourse.shopping.di.AppContainer
 import woowacourse.shopping.di.RepositoryModule
+import woowacourse.shopping.di.ViewModelFactory
 
 class ShoppingApplication : Application() {
     val container = AppContainer()
+    val injectViewModelFactory by lazy { ViewModelFactory(container) }
 }
