e890726675f56fe1335a3d0a719f10aaf8b2f87e
Ham BeomJoon <hbj0802@naver.com>
2025-10-09 17:04:20 +0900

    refactor: AppContainer가 싱글턴 인스턴스를 반환하도록 수정

--- app/src/main/java/woowacourse/shopping/di/AppContainer.kt
+++ app/src/main/java/woowacourse/shopping/di/AppContainer.kt
@@ -7,15 +7,26 @@ import woowacourse.shopping.domain.ProductRepository
 import kotlin.reflect.KClass
 
 class AppContainer {
-    private val providers = mutableMapOf<KClass<*>, Any>()
+    private val providers = mutableMapOf<KClass<*>, () -> Any>()
 
     init {
-        providers[CartRepository::class] = DefaultCartRepository()
-        providers[ProductRepository::class] = DefaultProductRepository()
+        providers[CartRepository::class] = { DefaultCartRepository() }
+        providers[ProductRepository::class] = { DefaultProductRepository() }
     }
 
+    private val instances = mutableMapOf<KClass<*>, Any>()
+
     @Suppress("UNCHECKED_CAST")
-    fun <T : Any> resolve(clazz: KClass<T>): T =
-        providers[clazz] as? T
-            ?: throw IllegalArgumentException("${clazz.simpleName} provider 없음")
+    fun <T : Any> resolve(clazz: KClass<T>): T {
+        instances[clazz]?.let { return it as T }
+
+        val factory =
+            providers[clazz]
+                ?: throw IllegalArgumentException("${clazz.simpleName} provider 없음")
+
+        // 인스턴스 생성 및 캐싱
+        val instance = factory() as T
+        instances[clazz] = instance
+        return instance
+    }
 }
