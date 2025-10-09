e2403f437a5a0d14af6f2ebcd874791a3338ce9c
Ham BeomJoon <hbj0802@naver.com>
2025-10-09 16:51:30 +0900

    feat: 주 생성자 관련 예외 처리 추가

--- app/src/main/java/woowacourse/shopping/di/ViewModelFactory.kt
+++ app/src/main/java/woowacourse/shopping/di/ViewModelFactory.kt
@@ -2,21 +2,28 @@ package woowacourse.shopping.di
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import kotlin.reflect.KClass
+import kotlin.reflect.full.primaryConstructor
 
 class ViewModelFactory(
     private val appContainer: AppContainer,
 ) : ViewModelProvider.Factory {
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        val constructor = modelClass.constructors.first()
+        val constructor =
+            modelClass.kotlin.primaryConstructor
+                ?: throw IllegalArgumentException(
+                    "${modelClass.simpleName}에 주 생성자가 없습니다",
+                )
 
-        val params =
-            constructor.parameters
-                .map { param ->
-                    appContainer.resolve(param.type.kotlin)
-                        ?: throw IllegalArgumentException("AppContainer에 ${param.type} 의존성이 없습니다")
-                }.toTypedArray()
+        val args =
+            constructor.parameters.associateWith { param ->
+                val clazz =
+                    param.type.classifier as? KClass<*>
+                        ?: throw IllegalArgumentException("${param.name} 타입 정보를 가져올 수 없습니다")
+                appContainer.resolve(clazz)
+            }
 
-        return constructor.newInstance(*params) as T
+        return constructor.callBy(args)
     }
 }
