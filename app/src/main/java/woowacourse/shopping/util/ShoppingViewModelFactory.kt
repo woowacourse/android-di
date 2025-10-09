4a93f10c9876a1e23225dbf536e154298c4e9f21
Dongjoo Seo <83579348+etama123@users.noreply.github.com>
2025-10-09 18:47:05 +0900

    refactor: ViewModelFactory 변수명 변경 및로직 개선
    - ViewModel 생성 로직을 별도의 private 메서드로 추출
    - 가독성을 높이기 위해 변수 이름 변경
    - 리플렉션 로직에 주석 추가

--- app/src/main/java/woowacourse/shopping/util/ShoppingViewModelFactory.kt
+++ app/src/main/java/woowacourse/shopping/util/ShoppingViewModelFactory.kt
@@ -10,20 +10,26 @@ class ShoppingViewModelFactory(
     private val appContainer: AppContainer,
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        val kClass: KClass<out ViewModel> = modelClass.kotlin
+        val viewModelKClass: KClass<out ViewModel> = modelClass.kotlin
 
+        @Suppress("UNCHECKED_CAST")
+        return createViewModel(viewModelKClass as KClass<T>)
+    }
+
+    private fun <T : ViewModel> createViewModel(viewModelKClass: KClass<T>): T {
         // 생성할 ViewModel 클래스의 주 생성자 정보
-        val constructor =
-            kClass.primaryConstructor
+        val primaryConstructor =
+            viewModelKClass.primaryConstructor
                 ?: throw IllegalArgumentException("ViewModel은 주 생성자를 가지고 있지 않습니다")
 
         // 생성자 파라미터 타입에 맞춰 AppContainer에서 가져오기
-        val parameterTypes = constructor.parameters.map { it.type.classifier as KClass<*> }
+        val parameterTypes = primaryConstructor.parameters.map { it.type.classifier as KClass<*> }
 
         // appContainer에서 생성된 의존성 인스턴스 가져오기
-        val dependencies = parameterTypes.map { appContainer.get(it) }
-        val argumentMap = constructor.parameters.zip(dependencies).toMap()
+        val dependencyInstances = parameterTypes.map { appContainer.get(it) }
+        val constructorArguments = primaryConstructor.parameters.zip(dependencyInstances).toMap()
 
-        return constructor.callBy(argumentMap) as T
+        // 리플랙션을 이용해 ViewModel 인스턴스 생성 및 반환
+        return primaryConstructor.callBy(constructorArguments)
     }
 }
