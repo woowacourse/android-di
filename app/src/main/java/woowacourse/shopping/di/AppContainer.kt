162240903c5bb95c9c0d784561db5d3be1cc8f48
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-10-09 16:53:39 +0900

    refactor: 모든 클래스를 커버할 수 있도록 주입 함수 생성

--- app/src/main/java/woowacourse/shopping/di/AppContainer.kt
+++ app/src/main/java/woowacourse/shopping/di/AppContainer.kt
@@ -1,7 +1,23 @@
 package woowacourse.shopping.di
 
+import kotlin.reflect.KFunction
+import kotlin.reflect.KParameter
 import kotlin.reflect.KType
+import kotlin.reflect.full.primaryConstructor
 
 interface AppContainer {
     fun dependency(type: KType): Any
+}
+
+inline fun <reified T : Any> AppContainer.instance(): T {
+    val primaryConstructor: KFunction<T> =
+        T::class.primaryConstructor
+            ?: error("${T::class.qualifiedName} doesn't have primary constructor")
+
+    val parameters: List<Any> =
+        primaryConstructor.parameters.map { parameter: KParameter ->
+            dependency(parameter.type)
+        }
+
+    return primaryConstructor.call(*parameters.toTypedArray())
 }
--- app/src/main/java/woowacourse/shopping/ui/ShoppingApplication.kt
+++ app/src/main/java/woowacourse/shopping/ui/ShoppingApplication.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import woowacourse.shopping.data.DefaultCartRepository
 import woowacourse.shopping.data.DefaultProductRepository
 import woowacourse.shopping.di.AppContainer
+import woowacourse.shopping.di.instance
 import woowacourse.shopping.model.CartRepository
 import woowacourse.shopping.model.ProductRepository
 import kotlin.reflect.KType
@@ -15,12 +16,10 @@ class ShoppingApplication :
     private val dependencies: MutableMap<KType, Any> = mutableMapOf()
 
     override fun dependency(type: KType): Any =
-        dependencies.getOrPut(
-            type,
-        ) {
+        dependencies.getOrPut(type) {
             when (type) {
-                ProductRepository::class.createType() -> DefaultProductRepository()
-                CartRepository::class.createType() -> DefaultCartRepository()
+                ProductRepository::class.createType() -> instance<DefaultProductRepository>()
+                CartRepository::class.createType() -> instance<DefaultCartRepository>()
                 else -> error("Don't know how to inject $type")
             }
         }
