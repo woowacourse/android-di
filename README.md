c847dc58e39be601ce087cf716ed39f71748ddf8
yrsel <xxknu88@gmail.com>
2025-10-11 01:20:29 +0900

    refactor: 데이터베이스 초기화 및 DI 매핑 정보 담아주기

--- README.md
+++ README.md
@@ -80,7 +80,7 @@ Annotation
 Recursive DI
 
 - [x] CartRepository가 다음과 같이 DAO 객체를 참조하도록 변경한다.
-- [ ] CartProductViewHolder의 bind 함수에 다음 구문을 추가하여 뷰에서도 날짜 정보를 확인할 수 있도록 한다.
+- [x] CartProductViewHolder의 bind 함수에 다음 구문을 추가하여 뷰에서도 날짜 정보를 확인할 수 있도록 한다.
 
 ### 선택 요구 사항
 
--- app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
+++ app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
@@ -2,11 +2,14 @@ package woowacourse.shopping
 
 import android.app.Application
 import woowacourse.shopping.di.AppInjector
+import woowacourse.shopping.di.module.DatabaseModule
 import woowacourse.shopping.di.module.RepositoryModule
 
 class ShoppingApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        AppInjector.init(modules = listOf(RepositoryModule()))
+
+        DatabaseModule.init(applicationContext)
+        AppInjector.init(modules = listOf(RepositoryModule(), DatabaseModule()))
     }
 }
