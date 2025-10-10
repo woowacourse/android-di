6eb0a8313fe81e4f7aa3540b2ad1ac268d484b78
yrsel <xxknu88@gmail.com>
2025-10-11 01:19:36 +0900

    feat: 데이터베이스 모듈 추가

---
+++ app/src/main/java/woowacourse/shopping/di/module/DatabaseModule.kt
@@ -0,0 +1,42 @@
+package woowacourse.shopping.di.module
+
+import android.content.Context
+import androidx.room.Room
+import woowacourse.shopping.data.CartProductDao
+import woowacourse.shopping.data.ShoppingDatabase
+import woowacourse.shopping.di.Provider
+import woowacourse.shopping.di.definition.DefinitionInformation
+import woowacourse.shopping.di.definition.Kind
+
+class DatabaseModule : InjectionModule {
+    override fun provideDefinitions(): List<DefinitionInformation<*>> =
+        listOf(
+            DefinitionInformation(
+                kclass = ShoppingDatabase::class,
+                qualifier = null,
+                kind = Kind.SINGLETON,
+                provider = { _ -> Provider { localDatabase } },
+            ),
+            DefinitionInformation(
+                kclass = CartProductDao::class,
+                qualifier = null,
+                kind = Kind.SINGLETON,
+                provider = { _ -> Provider { localDatabase.cartProductDao() } },
+            ),
+        )
+
+    companion object {
+        lateinit var localDatabase: ShoppingDatabase
+            private set
+
+        fun init(context: Context) {
+            localDatabase =
+                Room
+                    .databaseBuilder(
+                        context,
+                        ShoppingDatabase::class.java,
+                        "shopping_db",
+                    ).build()
+        }
+    }
+}
