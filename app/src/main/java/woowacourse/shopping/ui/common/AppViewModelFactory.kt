2a84879806e0bbf346563149351d28deac2843c2
yrsel <xxknu88@gmail.com>
2025-10-10 13:00:24 +0900

    refactor: ViewModelFactory 메서드 분리

--- app/src/main/java/woowacourse/shopping/ui/common/AppViewModelFactory.kt
+++ app/src/main/java/woowacourse/shopping/ui/common/AppViewModelFactory.kt
@@ -8,8 +8,9 @@ import androidx.savedstate.SavedStateRegistryOwner
 import kotlin.reflect.KClass
 import kotlin.reflect.KFunction
 import kotlin.reflect.KParameter
-import kotlin.reflect.full.primaryConstructor
 import woowacourse.shopping.di.AppInjector
+import woowacourse.shopping.di.common.castKclassOrThrow
+import woowacourse.shopping.di.common.getPrimaryConstructorOrThrow
 import woowacourse.shopping.di.definition.Qualifier
 
 class AppViewModelFactory(
@@ -22,47 +23,43 @@ class AppViewModelFactory(
         modelClass: Class<T>,
         handle: SavedStateHandle,
     ): T {
-        val constructor: KFunction<T> =
-            requireNotNull(modelClass.kotlin.primaryConstructor) {
-                ERROR_NO_PRIMARY_CONSTRUCTOR.format(modelClass.name)
-            }
+        val constructor: KFunction<T> = modelClass.getPrimaryConstructorOrThrow()
 
         val arguments: Map<KParameter, Any> =
             constructor.parameters
                 .mapNotNull { parameter: KParameter ->
-                    val kclass: KClass<*> =
-                        checkNotNull(parameter.type.classifier as? KClass<*>) {
-                            ERROR_CAN_NOT_CONVERT_CLASS.format(parameter.name)
-                        }
-
-                    val value: Any? =
-                        when {
-                            kclass == SavedStateHandle::class -> handle
-                            else -> getValueOrThrow(kclass, parameter)
-                        }
-
+                    val kclass: KClass<*> = parameter.castKclassOrThrow()
+                    val value: Any? = resolveInjectValue(kclass, handle, parameter)
                     if (value != null) parameter to value else null
                 }.toMap()
 
         return constructor.callBy(arguments)
     }
 
-    private fun getValueOrThrow(
+    private fun resolveInjectValue(
+        kclass: KClass<*>,
+        handle: SavedStateHandle,
+        parameter: KParameter,
+    ) = when {
+        kclass == SavedStateHandle::class -> handle
+        else -> resolveInjectedValue(kclass, parameter)
+    }
+
+    private fun resolveInjectedValue(
         kclass: KClass<*>,
         kParameter: KParameter,
     ): Any? {
-        if (kParameter.isOptional) return null
-
         val qualifier: Qualifier? = qualifiers[kclass]
         if (AppInjector.hasDefinition(kclass, qualifier)) return AppInjector.get(kclass, qualifier)
+
+        if (hasDefaultValue(kParameter)) return null
 
         throw IllegalArgumentException(ERROR_NO_DEFINITION_INFORMATION.format(kParameter.name))
     }
 
+    private fun hasDefaultValue(kParameter: KParameter) = kParameter.isOptional
+
     companion object {
-        private const val ERROR_NO_PRIMARY_CONSTRUCTOR = "ViewModel 주 생성자가 필요합니다, className = %s"
-        private const val ERROR_CAN_NOT_CONVERT_CLASS =
-            "parameter 클래스 타입으로 변환 실패, parameterName = %s"
         private const val ERROR_NO_DEFINITION_INFORMATION = "주입된 정보가 존재하지 않습니다, parameterName = %s"
     }
 }
