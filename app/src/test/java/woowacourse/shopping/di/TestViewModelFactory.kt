339c8054c0bc2886256abbb0fd2db36964ee2ea2
Ham BeomJoon <hbj0802@naver.com>
2025-10-11 01:45:08 +0900

    test: Field 주입 테스트 팩토리로 변경

---
+++ app/src/test/java/woowacourse/shopping/di/TestViewModelFactory.kt
@@ -0,0 +1,26 @@
+package woowacourse.shopping.di
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import woowacourse.shopping.fixture.TestAppContainer
+
+class TestViewModelFactory(
+    private val appContainer: TestAppContainer,
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        val vm = modelClass.getDeclaredConstructor().newInstance()
+
+        modelClass.declaredFields.forEach { field ->
+            if (field.isAnnotationPresent(Inject::class.java)) {
+                field.isAccessible = true
+
+                val dependency = appContainer.resolve(field.type.kotlin)
+                if (dependency != null) {
+                    field.set(vm, dependency)
+                }
+            }
+        }
+
+        return vm
+    }
+}
