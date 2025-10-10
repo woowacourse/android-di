3beedebb4f4f2ecfd30aa4ca198ea95604dbb522
박지민 <pjm5489@naver.com>
2025-10-10 21:20:30 +0900

    feat: Room Database 구현

--- app/src/main/java/woowacourse/shopping/ShoppingApp.kt
+++ app/src/main/java/woowacourse/shopping/ShoppingApp.kt
@@ -1,14 +1,24 @@
 package woowacourse.shopping
 
 import android.app.Application
+import woowacourse.shopping.data.CartRepositoryImpl
+import woowacourse.shopping.data.ProductRepositoryImpl
+import woowacourse.shopping.data.ShoppingDatabase
 import woowacourse.shopping.di.DependencyInjectorImpl
 import woowacourse.shopping.domain.repository.CartRepository
 import woowacourse.shopping.domain.repository.ProductRepository
 
 class ShoppingApp : Application() {
+    private val database: ShoppingDatabase by lazy {
+        ShoppingDatabase.getInstance(this)
+    }
+
     override fun onCreate() {
         super.onCreate()
-        DependencyInjectorImpl.setInstance(CartRepository::class, AppContainer.cartRepository)
-        DependencyInjectorImpl.setInstance(ProductRepository::class, AppContainer.productRepository)
+        DependencyInjectorImpl.setInstance(ProductRepository::class, ProductRepositoryImpl())
+        DependencyInjectorImpl.setInstance(
+            CartRepository::class,
+            CartRepositoryImpl(database.cartProductDao()),
+        )
     }
 }
