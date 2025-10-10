9fd834f24e9148662bd0d1f0e99f6ce41f4197be
DoableTuple <doabletuple@gmail.com>
2025-10-11 01:14:05 +0900

    feat: 액티비티에 ViewModel을 주입하는 기능 구현

--- app/src/main/java/woowacourse/shopping/di/DependencyProvider.kt
+++ app/src/main/java/woowacourse/shopping/di/DependencyProvider.kt
@@ -1,10 +1,19 @@
 package woowacourse.shopping.di
 
+import android.app.Activity
+import androidx.lifecycle.ViewModel
 import woowacourse.shopping.data.DefaultCartRepository
 import woowacourse.shopping.data.DefaultProductRepository
 import woowacourse.shopping.domain.CartRepository
 import woowacourse.shopping.domain.ProductRepository
+import kotlin.reflect.KClass
+import kotlin.reflect.KMutableProperty1
+import kotlin.reflect.KProperty1
 import kotlin.reflect.KType
+import kotlin.reflect.full.createInstance
+import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.full.isSubtypeOf
+import kotlin.reflect.full.memberProperties
 import kotlin.reflect.typeOf
 
 object DependencyProvider {
@@ -15,4 +24,25 @@ object DependencyProvider {
         )
 
     fun dependency(key: KType): Any = Dependencies[key]?.value ?: error("${key}에 대한 의존성이 정의되지 않았습니다.")
+
+    fun injectToActivity(activity: Activity) {
+        activity::class.memberProperties.forEach { property ->
+            if (property.findAnnotation<InjectableViewModel>() != null && property is KMutableProperty1) {
+                if (!property.returnType.isSubtypeOf(typeOf<ViewModel>())) error("${property.returnType}은(는) ViewModel이 아닙니다.")
+                val kClass: KClass<*> = property.returnType.classifier as KClass<*>
+                val viewModel: Any = kClass.createInstance()
+                inject(viewModel)
+                property.setter.call(activity, viewModel)
+            }
+        }
+    }
+
+    fun inject(target: Any) {
+        val kClass = target::class
+        kClass.memberProperties.forEach { property: KProperty1<*, *> ->
+            if (property.findAnnotation<Inject>() != null && property is KMutableProperty1) {
+                property.setter.call(target, dependency(property.returnType))
+            }
+        }
+    }
 }
--- app/src/main/java/woowacourse/shopping/ui/DiApplication.kt
+++ app/src/main/java/woowacourse/shopping/ui/DiApplication.kt
@@ -1,5 +1,38 @@
 package woowacourse.shopping.ui
 
+import android.app.Activity
 import android.app.Application
+import android.os.Bundle
+import woowacourse.shopping.di.DependencyProvider
 
-class DiApplication : Application()
+class DiApplication : Application() {
+    val lifeCycleCallbacks =
+        object : ActivityLifecycleCallbacks {
+            override fun onActivityCreated(
+                activity: Activity,
+                bundle: Bundle?,
+            ) {
+                DependencyProvider.injectToActivity(activity)
+            }
+
+            override fun onActivityDestroyed(activity: Activity) = Unit
+
+            override fun onActivityPaused(activity: Activity) = Unit
+
+            override fun onActivityResumed(activity: Activity) = Unit
+
+            override fun onActivitySaveInstanceState(
+                activity: Activity,
+                bundle: Bundle,
+            ) = Unit
+
+            override fun onActivityStarted(activity: Activity) = Unit
+
+            override fun onActivityStopped(activity: Activity) = Unit
+        }
+
+    override fun onCreate() {
+        super.onCreate()
+        registerActivityLifecycleCallbacks(lifeCycleCallbacks)
+    }
+}
