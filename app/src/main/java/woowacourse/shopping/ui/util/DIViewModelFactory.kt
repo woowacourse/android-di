e3b3fae025338dda5d08757376dfd4291a64a380
moondev03 <moondev03@gmail.com>
2025-10-11 19:25:59 +0900

    refactor: 뷰모델 생성 로직 개선

--- app/src/main/java/woowacourse/shopping/ui/util/DIViewModelFactory.kt
+++ app/src/main/java/woowacourse/shopping/ui/util/DIViewModelFactory.kt
@@ -6,36 +6,14 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import woowacourse.shopping.di.AppContainer
-import kotlin.reflect.KClass
-import kotlin.reflect.full.primaryConstructor
 
 class DIViewModelFactory : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(
         modelClass: Class<T>,
         extras: CreationExtras,
-    ): T {
-        val kClass = modelClass.kotlin
-        val constructor =
-            requireNotNull(kClass.primaryConstructor) {
-                NO_PUBLIC_CONSTRUCTOR_ERROR_MESSAGE.format(kClass.simpleName ?: "Anonymous")
-            }
-
-        return constructor.parameters
-            .associateWith { param ->
-                resolveDependency(param.type.classifier as KClass<*>, extras)
-            }.let(constructor::callBy)
-    }
-
-    private fun resolveDependency(
-        clazz: KClass<*>,
-        extras: CreationExtras,
-    ): Any =
-        when (clazz) {
-            SavedStateHandle::class -> extras.createSavedStateHandle()
-            else -> AppContainer.resolve(clazz)
-        }
-
-    private companion object {
-        const val NO_PUBLIC_CONSTRUCTOR_ERROR_MESSAGE = "%s에 public 기본 생성자가 없습니다."
-    }
+    ): T =
+        AppContainer.resolve(
+            clazz = modelClass.kotlin,
+            overrides = mapOf(SavedStateHandle::class to extras.createSavedStateHandle()),
+        )
 }
