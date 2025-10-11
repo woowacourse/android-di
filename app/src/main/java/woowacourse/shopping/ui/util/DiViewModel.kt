8a0d795a26ad3401a3b5910b3fc76eb73f38dd5c
MunJangHun <105299421+moondev03@users.noreply.github.com>
2025-10-11 16:32:19 +0900

    [만들면서 배우는 DI 1단계] 다이스 미션 제출합니다. (#153)
    * docs: 0.5단계 기능 요구 사항 작성
    
    * feat: CartRepository Interface 정의
    
    * feat: ProductRepository Interface 정의
    
    * docs: 0.5단계 해결된 기능 요구 사항 체크
    
    * docs: 1단계 기능 요구 사항 작성
    
    * feat: ViewModel에서 Repository가 정상적으로 주입되도록 수정
    
    * feat: DependencyInjector 객체 생성 및 구현
    
    * feat: ViewModel 생성 시 의존성 자동 주입 기능 추가
    
    * feat: App 클래스 생성 및 Repository 의존성 주입
    
    * feat: ViewModel 의존성 자동 주입
    
    * docs: 1단계 요구사항 완료 표시
    
    * test: `CartRepositoryImpl` 단위 테스트 추가
    
    - 장바구니에 상품을 추가하고 삭제하는 기능 테스트
    - 존재하지 않는 인덱스로 삭제 시 예외 발생 테스트
    - 추가와 삭제 반복 시 일관성 유지 테스트
    
    * test: `ProductRepositoryImpl` 단위 테스트 추가
    
    - 전체 상품 조회 기능 테스트
    
    * refactor: Product 관련 UI 파일 위치 이동
    
    * test: Product Fixture 추가
    
    * test: FakeRepository 추가
    
    * test: MainViewModel 테스트 코드 작성
    
    * test: CartActivity 테스트 코드 작성
    
    * test: CartViewModel 테스트 코드 작성
    
    * refactor: viewModels 개선
    
    * docs: 1단계 선택 요구 사항 완료 표시
    
    * refactor: Simplify repository provision in RepositoryModule
    
    * refactor: Application 제거
    
    * refactor: AppContainer 모듈 단위로 주입 가능하도록 개선
    
    * refactor: resolve 수행 시 추가 캐싱하지 않도록 변경
    
    * test: 삭제된 상품이 목록에 없는지 확인
    
    `deleteCartProduct` 테스트에서 상품 삭제 성공 여부만 확인하던 것을, 실제로 장바구니 목록에서 해당 상품이 제거되었는지까지 검증하도록 수정
    
    * test: ProductFixture 활용하도록 변경
    
    * refactor: ViewModel 의존성 주입 방식 개선

---
+++ app/src/main/java/woowacourse/shopping/ui/util/DiViewModel.kt
@@ -0,0 +1,9 @@
+package woowacourse.shopping.ui.util
+
+import androidx.activity.ComponentActivity
+import androidx.activity.viewModels
+import androidx.annotation.MainThread
+import androidx.lifecycle.ViewModel
+
+@MainThread
+inline fun <reified VM : ViewModel> ComponentActivity.diViewModels() = viewModels<VM> { DIViewModelFactory() }
---
+++ app/src/test/java/woowacourse/shopping/ui/product/MainViewModelTest.kt
@@ -0,0 +1,56 @@
+package woowacourse.shopping.ui.product
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.SoftAssertions.assertSoftly
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import woowacourse.shopping.fixture.FakeCartRepository
+import woowacourse.shopping.fixture.FakeProductRepository
+import woowacourse.shopping.fixture.ProductFixture
+import woowacourse.shopping.fixture.ProductsFixture
+import woowacourse.shopping.getOrAwaitValue
+
+class MainViewModelTest {
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var viewModel: MainViewModel
+
+    @Before
+    fun setUp() {
+        viewModel =
+            MainViewModel(
+                cartRepository = FakeCartRepository(),
+                productRepository = FakeProductRepository(ProductsFixture),
+            )
+    }
+
+    @Test
+    fun `전체 상품 조회`() {
+        // when
+        viewModel.getAllProducts()
+
+        // then
+        val products = viewModel.products.getOrAwaitValue()
+
+        assertSoftly { softly ->
+            softly.assertThat(products).hasSize(3)
+            softly
+                .assertThat(products)
+                .extracting("name")
+                .containsExactly("우테코 과자", "우테코 쥬스", "우테코 아이스크림")
+        }
+    }
+
+    @Test
+    fun `상품 추가`() {
+        // when
+        viewModel.addCartProduct(ProductFixture)
+
+        // then
+        val onProductAdded = viewModel.onProductAdded.getOrAwaitValue()
+        assertThat(onProductAdded).isTrue
+    }
+}
