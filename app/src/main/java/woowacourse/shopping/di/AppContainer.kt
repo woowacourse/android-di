0a56c90f9c130c5d43fa0ccab22293c6c7326295
moondev03 <moondev03@gmail.com>
2025-10-11 19:25:33 +0900

    feat: MyInject 어노테이션이 있는 경우만 생성자, 필드 주입 되도록 개선

--- app/src/main/java/woowacourse/shopping/di/AppContainer.kt
+++ app/src/main/java/woowacourse/shopping/di/AppContainer.kt
@@ -1,6 +1,11 @@
 package woowacourse.shopping.di
 
 import kotlin.reflect.KClass
+import kotlin.reflect.KFunction
+import kotlin.reflect.KMutableProperty1
+import kotlin.reflect.KParameter
+import kotlin.reflect.full.declaredMemberProperties
+import kotlin.reflect.full.hasAnnotation
 import kotlin.reflect.full.memberProperties
 import kotlin.reflect.full.primaryConstructor
 import kotlin.reflect.jvm.isAccessible
@@ -12,19 +17,42 @@ object AppContainer {
         provideModule(RepositoryModule::class)
     }
 
-    fun <T : Any> resolve(clazz: KClass<T>): T = providers[clazz] as? T ?: createInstance(clazz)
+    fun <T : Any> resolve(
+        clazz: KClass<T>,
+        overrides: Map<KClass<*>, Any> = emptyMap(),
+    ): T = (providers[clazz] as? T) ?: createInstance(clazz, overrides)
 
-    private fun <T : Any> createInstance(clazz: KClass<T>): T {
+    private fun <T : Any> createInstance(
+        clazz: KClass<T>,
+        overrides: Map<KClass<*>, Any>,
+    ): T {
         val constructor =
             requireNotNull(clazz.primaryConstructor) { "${clazz.simpleName}에 public 생성자가 없습니다." }
-
-        return constructor.parameters
-            .associateWith { param ->
-                requireNotNull(param.type.classifier as? KClass<*>) {
-                    "타입 정보를 알 수 없습니다: $param"
-                }.let(::resolve)
-            }.let(constructor::callBy)
+        val instance = constructor.resolveConstructorParameters(overrides).let(constructor::callBy)
+        clazz.createPropertyInstance(instance, overrides)
+        return instance
     }
+
+    private fun <T : Any> KFunction<T>.resolveConstructorParameters(overrides: Map<KClass<*>, Any>): Map<KParameter, Any?> =
+        parameters
+            .filter { parameter -> parameter.hasAnnotation<MyInjector>() }
+            .associateWith { param ->
+                val kClazz = param.type.classifier as KClass<*>
+                overrides[kClazz] ?: resolve(kClazz, overrides)
+            }
+
+    private fun <T : Any> KClass<T>.createPropertyInstance(
+        instance: T,
+        overrides: Map<KClass<*>, Any>,
+    ) = declaredMemberProperties
+        .filter { property -> property.hasAnnotation<MyInjector>() }
+        .forEach { property ->
+            property.isAccessible = true
+            val k = property.returnType.classifier as KClass<*>
+            val propertyInstance = overrides[k] ?: resolve(k, overrides)
+            val mutableProperty = requireNotNull(property as KMutableProperty1<T, Any>)
+            mutableProperty.set(instance, propertyInstance)
+        }
 
     private fun <T : Any> provideModule(moduleKClazz: KClass<T>) {
         val module = moduleKClazz.objectInstance ?: return
