a97d5ceb9bb4064257ccfb21e0e0a5cf4eb62c24
cucumber99 <dlsguq1@gmail.com>
2025-10-13 03:31:55 +0900

    refactor: 기존 CartRepository 구현체 네이밍 변경

--- app/src/main/java/woowacourse/shopping/data/InMemoryCartRepository.kt
+++ app/src/main/java/woowacourse/shopping/data/InMemoryCartRepository.kt
@@ -3,19 +3,16 @@ package woowacourse.shopping.data
 import woowacourse.shopping.domain.model.Product
 import woowacourse.shopping.domain.repository.CartRepository
 
-// TODO: Step2 - CartProductDao를 참조하도록 변경
-class CartRepositoryImpl : CartRepository {
+class InMemoryCartRepository : CartRepository {
     private val cartProducts: MutableList<Product> = mutableListOf()
 
-    override fun addCartProduct(product: Product) {
+    override suspend fun addCartProduct(product: Product) {
         cartProducts.add(product)
     }
 
-    override fun getAllCartProducts(): List<Product> {
-        return cartProducts.toList()
-    }
+    override suspend fun getAllCartProducts(): List<Product> = cartProducts.toList()
 
-    override fun deleteCartProduct(id: Int) {
+    override suspend fun deleteCartProduct(id: Int) {
         cartProducts.removeAt(id)
     }
 }
