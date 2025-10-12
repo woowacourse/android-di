3188382b96e3676586cf58724ad127a471c983c3
moondev03 <moondev03@gmail.com>
2025-10-12 16:04:03 +0900

    feat: ShoppingDatabase implement singleton pattern

--- app/src/main/java/woowacourse/shopping/data/ShoppingDatabase.kt
+++ app/src/main/java/woowacourse/shopping/data/ShoppingDatabase.kt
@@ -1,10 +1,29 @@
 package woowacourse.shopping.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
-
 
 @Database(entities = [CartProductEntity::class], version = 1, exportSchema = false)
 abstract class ShoppingDatabase : RoomDatabase() {
     abstract fun cartProductDao(): CartProductDao
+
+    companion object {
+        private const val DATABASE_NAME = "shopping.db"
+
+        @Volatile
+        private var instance: ShoppingDatabase? = null
+
+        fun getInstance(context: Context): ShoppingDatabase =
+            instance ?: synchronized(this) {
+                instance ?: Room
+                    .databaseBuilder(
+                        context.applicationContext,
+                        ShoppingDatabase::class.java,
+                        DATABASE_NAME,
+                    ).build()
+                    .also { instance = it }
+            }
+    }
 }
