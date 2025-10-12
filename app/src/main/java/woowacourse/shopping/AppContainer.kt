6ca834ccecfc02225acad54c51e5a8bc566101c5
Dongjoo Seo <83579348+etama123@users.noreply.github.com>
2025-10-12 16:24:19 +0900

    refactor: AppContainer에 재귀적으로 의존성 주입하는 기능 추가

--- app/src/main/java/woowacourse/shopping/AppContainer.kt
+++ app/src/main/java/woowacourse/shopping/AppContainer.kt
@@ -2,12 +2,14 @@ package woowacourse.shopping
 
 import android.content.Context
 import androidx.room.Room
+import woowacourse.shopping.data.CartProductDao
 import woowacourse.shopping.data.DefaultCartRepository
 import woowacourse.shopping.data.DefaultProductRepository
 import woowacourse.shopping.data.ShoppingDatabase
 import woowacourse.shopping.model.repository.CartRepository
 import woowacourse.shopping.model.repository.ProductRepository
 import kotlin.reflect.KClass
+import kotlin.reflect.full.primaryConstructor
 
 class AppContainer(
     private val context: Context,
@@ -23,12 +25,32 @@ class AppContainer(
     private val providers = mutableMapOf<KClass<*>, Any>()
 
     init {
-        providers[CartRepository::class] =
-            DefaultCartRepository(dao = database.cartProductDao())
+        providers[CartProductDao::class] = database.cartProductDao()
+        providers[CartRepository::class] = DefaultCartRepository(get(CartProductDao::class))
         providers[ProductRepository::class] = DefaultProductRepository()
     }
 
-    fun <T : Any> get(clazz: KClass<T>): T =
-        providers[clazz] as? T
-            ?: throw IllegalArgumentException("${clazz.simpleName} provider를 찾을 수 없습니다")
+    @Suppress("UNCHECKED_CAST")
+    fun <T : Any> get(clazz: KClass<T>): T {
+        // 이미 생성된 인스턴스 반환
+        providers[clazz]?.let { return it as T }
+
+        // 생성자 확인
+        val constructor =
+            clazz.primaryConstructor
+                ?: throw IllegalArgumentException("${clazz.simpleName}은 주 생성자가 없습니다")
+
+        // 생성자 파라미터 재귀 DI
+        val args =
+            constructor.parameters
+                .associateWith { param ->
+                    val dependencyClass = param.type.classifier as KClass<*>
+                    param to get(dependencyClass) // 재귀 호출
+                }.toMap()
+
+        val instance = constructor.callBy(args)
+        providers[clazz] = instance
+
+        return instance
+    }
 }
