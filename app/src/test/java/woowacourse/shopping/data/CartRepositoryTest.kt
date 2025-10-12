22e54b6d35d09d63106ee9fe06db5014e2de7da8
junseo511 <prime54@g.skku.edu>
2025-10-12 16:44:06 +0900

    test: 장바구니에 상품 추가 및 조회 테스트 케이스 분리

--- app/src/test/java/woowacourse/shopping/data/CartRepositoryTest.kt
+++ app/src/test/java/woowacourse/shopping/data/CartRepositoryTest.kt
@@ -6,15 +6,29 @@ import woowacourse.shopping.model.Product
 
 class CartRepositoryTest {
     @Test
-    fun `장바구니에_상품을_추가하면_순서대로_조회된다`() {
+    fun `상품을_추가하면_장바구니에_저장된다`() {
+        // given
+        val repository: CartRepository = CartRepositoryImpl()
+        val product = Product("A", 1000, "a")
+
+        // when
+        repository.addCartProduct(product)
+        val result = repository.getAllCartProducts()
+
+        // then
+        assertThat(result).contains(product)
+    }
+
+    @Test
+    fun `장바구니를_조회하면_추가한_상품들이_순서대로_나온다`() {
         // given
         val repository: CartRepository = CartRepositoryImpl()
         val a = Product("A", 1000, "a")
         val b = Product("B", 2000, "b")
-
-        // when
         repository.addCartProduct(a)
         repository.addCartProduct(b)
+
+        // when
         val result = repository.getAllCartProducts()
 
         // then
