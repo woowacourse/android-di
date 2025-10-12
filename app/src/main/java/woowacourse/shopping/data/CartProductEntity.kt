01c66c501bb81cba6221d7f2c5250ae6d9056a1e
cucumber99 <dlsguq1@gmail.com>
2025-10-13 03:29:45 +0900

    feat: 도메인 모델과 엔티티 간 변환 로직 추가

--- app/src/main/java/woowacourse/shopping/data/CartProductEntity.kt
+++ app/src/main/java/woowacourse/shopping/data/CartProductEntity.kt
@@ -2,11 +2,33 @@ package woowacourse.shopping.data
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import woowacourse.shopping.domain.model.Product
 
 @Entity(tableName = "cart_products")
-data class CartProductEntity(val name: String, val price: Int, val imageUrl: String) {
+data class CartProductEntity(
+    val name: String,
+    val price: Int,
+    val imageUrl: String,
+) {
     @PrimaryKey(autoGenerate = true)
     var id: Long = 0
 
     var createdAt: Long = System.currentTimeMillis()
+
+    fun toDomain(): Product =
+        Product(
+            name = name,
+            price = price,
+            imageUrl = imageUrl,
+            createdAt = createdAt,
+        )
+
+    companion object {
+        fun fromDomain(product: Product): CartProductEntity =
+            CartProductEntity(
+                name = product.name,
+                price = product.price,
+                imageUrl = product.imageUrl,
+            )
+    }
 }
