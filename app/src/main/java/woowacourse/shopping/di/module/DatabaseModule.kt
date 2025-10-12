65662eddec8c672c698edc1e03d8094b49da4543
yrsel <xxknu88@gmail.com>
2025-10-13 02:11:08 +0900

    feat: 인메모리 데이터베이스 정의 추가

--- app/src/main/java/woowacourse/shopping/di/module/DatabaseModule.kt
+++ app/src/main/java/woowacourse/shopping/di/module/DatabaseModule.kt
@@ -7,37 +7,54 @@ import woowacourse.shopping.data.ShoppingDatabase
 import woowacourse.shopping.di.Provider
 import woowacourse.shopping.di.definition.DefinitionInformation
 import woowacourse.shopping.di.definition.Kind
+import woowacourse.shopping.di.definition.Qualifier
 
-class DatabaseModule : InjectionModule {
+class DatabaseModule(
+    private val context: Context,
+) : InjectionModule {
     override fun provideDefinitions(): List<DefinitionInformation<*>> =
         listOf(
             DefinitionInformation(
                 kclass = ShoppingDatabase::class,
-                qualifier = null,
+                qualifier = Qualifier.Named("room"),
                 kind = Kind.SINGLETON,
-                provider = { _ -> Provider { localDatabase } },
+                provider = { _ -> Provider { provideRoomDatabase() } },
+            ),
+            DefinitionInformation(
+                kclass = ShoppingDatabase::class,
+                qualifier = Qualifier.Named("memory"),
+                kind = Kind.FACTORY,
+                provider = { _ -> Provider { provideInMemoryDatabase() } },
             ),
             DefinitionInformation(
                 kclass = CartProductDao::class,
                 qualifier = null,
                 kind = Kind.SINGLETON,
-                provider = { _ -> Provider { localDatabase.cartProductDao() } },
+                provider = { injector ->
+                    Provider {
+                        val db = injector.get<ShoppingDatabase>(Qualifier.Named("memory"))
+                        db.cartProductDao()
+                    }
+                },
             ),
         )
 
+    private fun provideRoomDatabase(): ShoppingDatabase =
+        Room
+            .databaseBuilder(
+                context,
+                ShoppingDatabase::class.java,
+                DATABASE_NAME,
+            ).build()
+
+    private fun provideInMemoryDatabase(): ShoppingDatabase =
+        Room
+            .inMemoryDatabaseBuilder(
+                context,
+                ShoppingDatabase::class.java,
+            ).build()
+
     companion object {
         private const val DATABASE_NAME = "shopping_db"
-        lateinit var localDatabase: ShoppingDatabase
-            private set
-
-        fun init(context: Context) {
-            localDatabase =
-                Room
-                    .databaseBuilder(
-                        context,
-                        ShoppingDatabase::class.java,
-                        DATABASE_NAME,
-                    ).build()
-        }
     }
 }
