bc62f875ea5a0c5a988d2e619b13018d22579b30
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-10-12 18:18:22 +0900

    refactor: CartProductDao를 컨테이너에 추가

--- app/src/main/java/woowacourse/shopping/ui/ShoppingApplication.kt
+++ app/src/main/java/woowacourse/shopping/ui/ShoppingApplication.kt
@@ -1,8 +1,11 @@
 package woowacourse.shopping.ui
 
 import android.app.Application
+import androidx.room.Room
+import woowacourse.shopping.data.CartProductDao
 import woowacourse.shopping.data.DefaultCartRepository
 import woowacourse.shopping.data.DefaultProductRepository
+import woowacourse.shopping.data.ShoppingDatabase
 import woowacourse.shopping.di.AppContainer
 import woowacourse.shopping.di.instance
 import woowacourse.shopping.model.CartRepository
@@ -20,6 +23,18 @@ class ShoppingApplication :
             when (type) {
                 ProductRepository::class.createType() -> instance<DefaultProductRepository>()
                 CartRepository::class.createType() -> instance<DefaultCartRepository>()
+                CartProductDao::class.createType() -> {
+                    val database: ShoppingDatabase =
+                        Room
+                            .databaseBuilder(
+                                this,
+                                ShoppingDatabase::class.java,
+                                "cartProduct",
+                            ).build()
+
+                    database.cartProductDao()
+                }
+
                 else -> error("Don't know how to inject $type")
             }
         }
