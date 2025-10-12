d0409dcf2c6b65c995438638d023ccdfb859dd88
박지민 <pjm5489@naver.com>
2025-10-12 19:18:15 +0900

    test: 테스트 코드 수정

--- app/src/test/java/woowacourse/shopping/cart/CartViewModelTest.kt
+++ app/src/test/java/woowacourse/shopping/cart/CartViewModelTest.kt
@@ -1,9 +1,17 @@
 package woowacourse.shopping.cart
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import io.mockk.every
+import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.mockk
-import io.mockk.verify
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
@@ -13,45 +21,65 @@ import woowacourse.shopping.fixture.TEST_PRODUCTS
 import woowacourse.shopping.getOrAwaitValue
 import woowacourse.shopping.ui.cart.CartViewModel
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class CartViewModelTest {
     @get:Rule
     val instantExecutorRule = InstantTaskExecutorRule()
+
+    private val testDispatcher = StandardTestDispatcher()
 
     private lateinit var cartRepository: CartRepository
     private lateinit var cartViewModel: CartViewModel
 
     @Before
     fun setUp() {
+        Dispatchers.setMain(testDispatcher)
+
         cartRepository = mockk<CartRepository>()
-        cartViewModel = CartViewModel(cartRepository)
+        cartViewModel = CartViewModel()
+        val field = CartViewModel::class.java.getDeclaredField("cartRepository")
+        field.isAccessible = true
+        field.set(cartViewModel, cartRepository)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
     }
 
     @Test
-    fun `모든 장바구니 상품을 조회할 수 있다`() {
-        // given
-        every { cartRepository.getAllCartProducts() } returns TEST_PRODUCTS
+    fun `모든 장바구니 상품을 조회할 수 있다`() =
+        runTest {
+            // given
+            coEvery { cartRepository.getAllCartProducts() } returns Result.success(TEST_PRODUCTS)
 
-        // when
-        cartViewModel.getAllCartProducts()
-
-        // then
-        verify { cartRepository.getAllCartProducts() }
-        Assert.assertEquals(
-            TEST_PRODUCTS,
-            cartViewModel.cartProducts.getOrAwaitValue(),
-        )
-    }
+            // when
+            cartViewModel.getAllCartProducts()
+            advanceUntilIdle()
+            // then
+            coVerify { cartRepository.getAllCartProducts() }
+            Assert.assertEquals(
+                TEST_PRODUCTS,
+                cartViewModel.cartProducts.getOrAwaitValue(),
+            )
+        }
 
     @Test
-    fun `상품을 제거할 수 있다`() {
-        // given
-        every { cartRepository.deleteCartProduct(0) } returns Unit
+    fun `상품을 제거할 수 있다`() =
+        runTest {
+            // given
+            coEvery { cartRepository.getAllCartProducts() } returns Result.success(TEST_PRODUCTS)
+            coEvery { cartRepository.deleteCartProduct(1L) } returns Result.success(Unit)
 
-        // when
-        cartViewModel.deleteCartProduct(0)
+            // when
+            cartViewModel.getAllCartProducts()
+            cartViewModel.deleteCartProduct(1L)
+            advanceUntilIdle()
 
-        // then
-        verify { cartRepository.deleteCartProduct(0) }
-        Assert.assertTrue(cartViewModel.onCartProductDeleted.getOrAwaitValue())
-    }
+            // then
+            coVerify { cartRepository.deleteCartProduct(1L) }
+            coVerify { cartRepository.getAllCartProducts() }
+            val result = cartViewModel.onCartProductDeleted.getOrAwaitValue()
+            Assert.assertTrue(result)
+        }
 }
