755a361845dfac180d3519517d261263723c28ce
moondev03 <moondev03@gmail.com>
2025-10-12 16:03:39 +0900

    feat: Add `createdAt` to Product and mapping logic

--- app/src/main/java/woowacourse/shopping/data/mapper/ProductMapper.kt
+++ app/src/main/java/woowacourse/shopping/data/mapper/ProductMapper.kt
@@ -3,10 +3,17 @@ package woowacourse.shopping.data.mapper
 import woowacourse.shopping.data.CartProductEntity
 import woowacourse.shopping.model.Product
 
-fun Product.toEntity(): CartProductEntity {
-    return CartProductEntity(
+fun Product.toEntity(): CartProductEntity =
+    CartProductEntity(
         name = name,
         price = price,
         imageUrl = imageUrl,
     )
-}
+
+fun CartProductEntity.toDomain(): Product =
+    Product(
+        name = name,
+        price = price,
+        imageUrl = imageUrl,
+        createdAt = createdAt,
+    )
