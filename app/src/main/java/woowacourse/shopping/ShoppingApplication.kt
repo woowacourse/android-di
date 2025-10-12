52fc237552eb3a907fdb198df9dcdd2201c3ee88
jiyunlee <02leejiyun@naver.com>
2025-10-12 20:30:15 +0900

    refactor: AppContainer를 인터페이스로 분리

--- app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
+++ app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
@@ -1,11 +1,11 @@
 package woowacourse.shopping
 
 import android.app.Application
-import woowacourse.shopping.di.AppContainer
+import woowacourse.shopping.di.AppContainerImpl
 import woowacourse.shopping.di.ViewModelFactory
 
 class ShoppingApplication : Application() {
     val viewModelFactory: ViewModelFactory by lazy {
-        ViewModelFactory(AppContainer)
+        ViewModelFactory(AppContainerImpl)
     }
 }
