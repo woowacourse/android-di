7aa44570980313ac94ec265bc32c4bd3ee5f0296
Dongjoo Seo <83579348+etama123@users.noreply.github.com>
2025-10-12 15:22:44 +0900

    feat: 필드 주입을 위한 MyInjector 어노테이션 및 로직 추가

--- app/src/main/java/woowacourse/shopping/util/ShoppingViewModelFactory.kt
+++ app/src/main/java/woowacourse/shopping/util/ShoppingViewModelFactory.kt
@@ -3,8 +3,13 @@ package woowacourse.shopping.util
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import woowacourse.shopping.AppContainer
+import woowacourse.shopping.di.MyInjector
 import kotlin.reflect.KClass
+import kotlin.reflect.KMutableProperty1
+import kotlin.reflect.full.hasAnnotation
+import kotlin.reflect.full.memberProperties
 import kotlin.reflect.full.primaryConstructor
+import kotlin.reflect.jvm.isAccessible
 
 class ShoppingViewModelFactory(
     private val appContainer: AppContainer,
@@ -30,6 +35,31 @@ class ShoppingViewModelFactory(
         val constructorArguments = primaryConstructor.parameters.zip(dependencyInstances).toMap()
 
         // 리플랙션을 이용해 ViewModel 인스턴스 생성 및 반환
-        return primaryConstructor.callBy(constructorArguments)
+        val viewModel = primaryConstructor.callBy(constructorArguments)
+
+        // 필드 주입
+        injectField(viewModel)
+
+        return viewModel
+    }
+
+    private fun injectField(viewModel: ViewModel) {
+        val viewModelKClass = viewModel::class
+
+        // 해당 클래스에 있는 모든 프로퍼티를 가져옴
+        viewModelKClass.memberProperties.forEach { property ->
+            // KMutableProperty1: 프로퍼티의 값을 변경할 수 있는 프로퍼티
+            if (property is KMutableProperty1 && property.hasAnnotation<MyInjector>()) {
+                val dependencyClass =
+                    property.returnType.classifier as? KClass<*>
+                        ?: return@forEach
+                val dependency = appContainer.get(dependencyClass)
+
+                // isAccessible: 프로퍼티의 접근 지시자를 변경할 수 있는 속성
+                property.isAccessible = true
+                // setter.call: 프로퍼티의 setter를 호출하여 값을 설정
+                property.setter.call(viewModel, dependency)
+            }
+        }
     }
 }
