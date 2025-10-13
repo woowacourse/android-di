0df54ac0e5918801acc5fee39f16daf18769e324
devfeijoa <devfeijoa@gmail.com>
2025-10-13 11:14:04 +0900

    feat: @Inject 어노테이션을 이용한 필드 주입 기능 구현

---
+++ app/src/main/java/woowacourse/shopping/di/Inject.kt
@@ -0,0 +1,5 @@
+package woowacourse.shopping.di
+
+@Target(AnnotationTarget.PROPERTY)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Inject
--- app/src/main/java/woowacourse/shopping/di/ViewModelFactory.kt
+++ app/src/main/java/woowacourse/shopping/di/ViewModelFactory.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import androidx.lifecycle.AbstractSavedStateViewModelFactory
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import androidx.savedstate.SavedStateRegistryOwner
+import kotlin.reflect.KClass
+import kotlin.reflect.KMutableProperty1
+import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.full.memberProperties
 import kotlin.reflect.full.primaryConstructor
+import kotlin.reflect.jvm.isAccessible
 
 class ViewModelFactory(
     private val container: AppContainer,
@@ -37,6 +41,37 @@ class ViewModelFactory(
                     }
                 }.toTypedArray()
 
-        return constructor.call(*parameters)
+        constructor.isAccessible = true
+        val viewModel = constructor.call(*parameters)
+
+        kClass.memberProperties
+            .filter { it.findAnnotation<Inject>() != null }
+            .forEach { property ->
+                val mutable =
+                    (property as? KMutableProperty1<Any, Any?>)
+                        ?: error("@Inject target must be a 'var' mutable property: ${property.name} in $modelClass")
+
+                val targetType =
+                    property.returnType.classifier as? KClass<*>
+                        ?: error("@Inject target must be a 'var' mutable property: ${property.name} in $modelClass")
+
+                val dependency =
+                    when (targetType) {
+                        SavedStateHandle::class -> handle
+                        else -> {
+                            container::class
+                                .memberProperties
+                                .firstOrNull { it.name == targetType.simpleName }
+                                ?.getter
+                                ?.call(container)
+                                ?: throw IllegalArgumentException("No matching dependency for ${targetType.simpleName} in $modelClass")
+                        }
+                    }
+
+                mutable.isAccessible = true
+                mutable.setter.isAccessible = true
+                mutable.setter.call(viewModel, dependency)
+            }
+        return viewModel
     }
 }
