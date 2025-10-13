e1c0fe0ff808327df104e2f32b5052d21d60b099
ijh1298 <imjh0417@gmail.com>
2025-10-13 13:17:32 +0900

    test: ViewModel 테스트를 코루틴 환경에 맞게 수정

--- app/src/test/java/woowacourse/shopping/ui/cart/CartViewModelTest.kt
+++ app/src/test/java/woowacourse/shopping/ui/cart/CartViewModelTest.kt
@@ -1,30 +1,51 @@
 package woowacourse.shopping.ui.cart
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.After
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import woowacourse.shopping.fixture.FakeCartRepository
 import woowacourse.shopping.getOrAwaitValue
 import woowacourse.shopping.model.Product
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class CartViewModelTest {
     @get:Rule
     val instantExecutorRule = InstantTaskExecutorRule()
 
+    @Before
+    fun setup() {
+        Dispatchers.setMain(StandardTestDispatcher())
+    }
+
+    @After
+    fun teardown() {
+        Dispatchers.resetMain()
+    }
+
     @Test
-    fun `장바구니 상품을 조회하면 목록에 반영된다`() {
+    fun `장바구니 상품을 조회하면 목록에 반영된다`() = runTest {
         // given
         val cartViewModel =
-            CartViewModel(
-                FakeCartRepository().apply {
-                    addCartProduct(Product("과자", 1000, ""))
-                    addCartProduct(Product("음료수", 1000, ""))
-                },
-            )
+            CartViewModel().apply {
+                cartRepository = FakeCartRepository().apply {
+                    cartProducts.add(Product(0, "과자", 1000, "", 0L))
+                    cartProducts.add(Product(1, "음료수", 1000, "", 0L))
+                }
+            }
 
         // when
         cartViewModel.getAllCartProducts()
+        advanceUntilIdle()
 
         // then
         val cartProducts = cartViewModel.cartProducts.getOrAwaitValue()
@@ -32,19 +53,20 @@ class CartViewModelTest {
     }
 
     @Test
-    fun `장바구니 상품을 삭제하면 목록에 반영된다`() {
+    fun `장바구니 상품을 삭제하면 목록에 반영된다`() = runTest {
         // given
         val cartViewModel =
-            CartViewModel(
-                FakeCartRepository().apply {
-                    addCartProduct(Product("과자", 1000, ""))
-                    addCartProduct(Product("음료수", 1000, ""))
-                },
-            )
+            CartViewModel().apply {
+                cartRepository = FakeCartRepository().apply {
+                    cartProducts.add(Product(0, "과자", 1000, "", 0L))
+                    cartProducts.add(Product(1, "음료수", 1000, "", 0L))
+                }
+            }
 
         // when
         cartViewModel.deleteCartProduct(0)
         cartViewModel.getAllCartProducts()
+        advanceUntilIdle()
 
         // then
         val cartProducts = cartViewModel.cartProducts.getOrAwaitValue()
