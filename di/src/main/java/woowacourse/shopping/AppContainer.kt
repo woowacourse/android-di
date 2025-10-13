4deb78dd4a53449b999843d6162ea7d34afc84fc
cucumber99 <dlsguq1@gmail.com>
2025-10-13 19:54:59 +0900

    refactor: 의존성 주입 로직 개선

--- di/src/main/java/woowacourse/shopping/AppContainer.kt
+++ di/src/main/java/woowacourse/shopping/AppContainer.kt
@@ -12,9 +12,37 @@ open class AppContainer {
         clazz: KClass<T>,
         qualifier: KClass<out Annotation>? = null,
     ): T {
-        val qualifierKey = QualifierKey(clazz, qualifier)
-        instances[qualifierKey]?.let { return it as T }
+        findInstance(clazz, qualifier)?.let { instance -> return instance as T }
+        return createInstance(clazz).also { instance -> bind(clazz, instance, qualifier) }
+    }
 
+    fun injectField(instance: Any) =
+        instance::class.java
+            .declaredFields
+            .filter { field -> field.isAnnotationPresent(Inject::class.java) }
+            .forEach { field ->
+                val depClass = field.type.kotlin
+                val depQualifier =
+                    field.annotations
+                        .map { it.annotationClass }
+                        .firstOrNull { it != Inject::class }
+                val dependency = getInstance(depClass, depQualifier)
+                field.isAccessible = true
+                field.set(instance, dependency)
+            }
+
+    fun <T : Any> bind(
+        clazz: KClass<T>,
+        instance: T,
+        qualifier: KClass<out Annotation>? = null,
+    ) = instances.putIfAbsent(QualifierKey(clazz, qualifier), instance)
+
+    private fun findInstance(
+        clazz: KClass<*>,
+        qualifier: KClass<out Annotation>?,
+    ): Any? = instances[QualifierKey(clazz, qualifier)] ?: instances[QualifierKey(clazz, null)]
+
+    private fun <T : Any> createInstance(clazz: KClass<T>): T {
         val constructor =
             requireNotNull(clazz.primaryConstructor) {
                 "${clazz.qualifiedName} 클래스에 생성자가 존재하지 않습니다."
@@ -27,30 +55,15 @@ open class AppContainer {
                         requireNotNull(param.type.classifier as KClass<*>) {
                             "지원하지 않는 타입입니다: ${param.type}"
                         }
-                    val depQualifier = param.annotations.firstOrNull()?.annotationClass
+                    val depQualifier =
+                        param.annotations
+                            .map { it.annotationClass }
+                            .firstOrNull { it != Inject::class }
                     getInstance(depClass, depQualifier)
                 }.toTypedArray()
 
         return constructor.call(*args)
     }
-
-    fun injectField(instance: Any) =
-        instance::class.java
-            .declaredFields
-            .filter { field -> field.isAnnotationPresent(Inject::class.java) }
-            .forEach { field ->
-                val depClass = field.type.kotlin
-                val depQualifier = field.annotations.firstOrNull()?.annotationClass
-                val dependency = getInstance(depClass, depQualifier)
-                field.isAccessible = true
-                field.set(instance, dependency)
-            }
-
-    fun <T : Any> bind(
-        clazz: KClass<T>,
-        instance: T,
-        qualifier: KClass<out Annotation>? = null,
-    ) = instances.putIfAbsent(QualifierKey(clazz, qualifier), instance)
 
     private data class QualifierKey(
         val clazz: KClass<*>,
