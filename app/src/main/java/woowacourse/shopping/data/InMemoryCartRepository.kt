c3a0c250071472da9213da994aebfa6710f1fad6
moondev03 <moondev03@gmail.com>
2025-10-13 15:18:12 +0900

    refactor: data class로 변경 및 InMemoryCartRepository id 할당

--- app/src/main/java/woowacourse/shopping/data/InMemoryCartRepository.kt
+++ app/src/main/java/woowacourse/shopping/data/InMemoryCartRepository.kt
@@ -2,14 +2,16 @@ package woowacourse.shopping.data
 
 import woowacourse.shopping.domain.CartRepository
 import woowacourse.shopping.model.Product
+import java.util.concurrent.atomic.AtomicLong
 
 class InMemoryCartRepository(
     initialProducts: List<Product> = emptyList(),
 ) : CartRepository {
+    private val idGenerator: AtomicLong = AtomicLong()
     private val cartProducts: MutableList<Product> = initialProducts.toMutableList()
 
     override suspend fun addCartProduct(product: Product) {
-        cartProducts.add(product)
+        cartProducts.add(product.copy(id = idGenerator.incrementAndGet()))
     }
 
     override suspend fun getAllCartProducts(): List<Product> = cartProducts.toList()
