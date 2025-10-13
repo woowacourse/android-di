e13501ab45135482baed0b30cba65c252707aabc
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-10-13 21:32:25 +0900

    refactor: 불필요한 Qualifier 제거

--- app/src/main/java/woowacourse/shopping/di/Qualifiers.kt
+++ app/src/main/java/woowacourse/shopping/di/Qualifiers.kt
@@ -1,12 +1,7 @@
 package woowacourse.shopping.di
 
-@Target(AnnotationTarget.ANNOTATION_CLASS)
-annotation class Qualifier
-
 @Target(AnnotationTarget.PROPERTY)
-@Qualifier
 annotation class InjectRoomCartRepository
 
 @Target(AnnotationTarget.PROPERTY)
-@Qualifier
 annotation class InjectInMemoryCartRepository
