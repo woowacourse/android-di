4aca93f13e69d3e0e64842aace80e4da6c8d9c60
yrsel <xxknu88@gmail.com>
2025-10-13 16:08:45 +0900

    refactor: di fixture 추가

--- di/src/test/java/com/shopping/fixture/InjectFixture.kt
+++ di/src/test/java/com/shopping/fixture/InjectFixture.kt
@@ -1,19 +1,37 @@
 package com.shopping.fixture
 
+import com.shopping.di.annotation.Inject
+import com.shopping.di.annotation.QualifierTag
+
 interface FixtureCar
 
 data class ElectricFixtureCarImpl(
-    val name: String,
+    val name: String = "",
 ) : FixtureCar
 
 data class EngineFixtureCarImpl(
-    val name: String,
+    val name: String = "",
 ) : FixtureCar
 
 data class SingletonFixtureCar(
-    val name: String,
+    val name: String = "",
 )
 
 data class FactoryFixtureCar(
-    val name: String,
+    val name: String = "",
+)
+
+class FieldInjectFixtureCar {
+    @Inject
+    lateinit var fixtureCar: FixtureCar
+}
+
+class FieldInjectQualifierEngineFixtureCar {
+    @Inject
+    @QualifierTag("engine")
+    lateinit var fixtureCar: FixtureCar
+}
+
+data class ConstructorInjectFixtureCar(
+    val fixtureCar: FixtureCar,
 )
