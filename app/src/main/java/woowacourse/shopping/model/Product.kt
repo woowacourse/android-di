6ce05d92e221e9060227f6ae57efda7fc3bae0b6
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-10-13 17:41:30 +0900

    feat: CartProduct 모델을 별도 파일로 분리

--- app/src/main/java/woowacourse/shopping/model/Product.kt
+++ app/src/main/java/woowacourse/shopping/model/Product.kt
@@ -1,6 +1,3 @@
 package woowacourse.shopping.model
 
-import java.time.LocalDate
-
 class Product(val name: String, val price: Int, val imageUrl: String)
-class CartProduct(val name: String, val price: Int, val imageUrl: String, val createdAt: Long)
