ee5b5318cc9d36844ff8878712352c0741caafc4
devfeijoa <devfeijoa@gmail.com>
2025-10-13 13:59:33 +0900

    test: ViewModelFactory DI 테스트 코드 작성

---
+++ app/src/test/java/woowacourse/shopping/di/InjectViewModel.kt
@@ -0,0 +1,11 @@
+package woowacourse.shopping.di
+
+import androidx.lifecycle.ViewModel
+import woowacourse.shopping.domain.ProductRepository
+
+class InjectViewModel : ViewModel() {
+    @Inject
+    lateinit var productRepository: ProductRepository
+
+    fun getProductName(): String = productRepository.getAllProducts().first().name
+}
