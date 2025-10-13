c57897aa724507224960eed7bda44a69fe6ee22c
devfeijoa <devfeijoa@gmail.com>
2025-10-13 17:05:30 +0900

    feat: 쇼핑 카트 화면 뷰에서 날짜 정보 확인하도록 구현

--- app/src/main/java/woowacourse/shopping/data/CartRepositoryImpl.kt
+++ app/src/main/java/woowacourse/shopping/data/CartRepositoryImpl.kt
@@ -3,6 +3,7 @@ package woowacourse.shopping.data
 import woowacourse.shopping.data.mapper.toEntity
 import woowacourse.shopping.data.mapper.toModel
 import woowacourse.shopping.domain.CartRepository
+import woowacourse.shopping.model.CartProduct
 import woowacourse.shopping.model.Product
 
 // TODO: Step2 - CartProductDao를 참조하도록 변경
@@ -13,7 +14,7 @@ class CartRepositoryImpl(
         dao.insert(product.toEntity())
     }
 
-    override suspend fun getAllCartProducts(): List<Product> = dao.getAll().map { it.toModel() }
+    override suspend fun getAllCartProducts(): List<CartProduct> = dao.getAll().map { it.toModel() }
 
     override suspend fun deleteCartProduct(id: Int) {
         dao.delete(id.toLong())
