784fff68cd118929212620be7b6b110d8dff500b
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-10-13 14:44:25 +0900

    refactor: Annotation을 붙여서 필요한 요소에만 의존성을 주입하도록 기능 추가

--- app/src/main/java/woowacourse/shopping/ui/ViewModelStoreOwner.kt
+++ app/src/main/java/woowacourse/shopping/ui/ViewModelStoreOwner.kt
@@ -7,7 +7,13 @@ import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.AP
 import androidx.lifecycle.ViewModelStoreOwner
 import androidx.lifecycle.viewmodel.CreationExtras
 import woowacourse.shopping.di.AppContainer
+import woowacourse.shopping.di.Inject
 import woowacourse.shopping.di.instance
+import kotlin.reflect.KProperty1
+import kotlin.reflect.full.hasAnnotation
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.jvm.isAccessible
+import kotlin.reflect.jvm.javaField
 
 inline fun <reified VM : ViewModel> ViewModelStoreOwner.viewModel(): Lazy<VM> =
     lazy {
@@ -22,8 +28,18 @@ inline fun <reified VM : ViewModel> ViewModelStoreOwner.viewModel(): Lazy<VM> =
                     ): T {
                         val application: Application = checkNotNull(extras[APPLICATION_KEY])
                         val appContainer: AppContainer = application as AppContainer
+                        val viewModel = appContainer.instance<VM>() as T
+                        val fieldsToBeInjected: List<KProperty1<out T, *>> =
+                            viewModel::class.memberProperties.filter { it.hasAnnotation<Inject>() }
 
-                        return appContainer.instance<VM>() as T
+                        fieldsToBeInjected.forEach { property: KProperty1<out T, *> ->
+                            property.apply {
+                                isAccessible = true
+                                javaField?.set(viewModel, appContainer.dependency(returnType))
+                            }
+                        }
+
+                        return viewModel
                     }
                 },
         )[VM::class.java]
