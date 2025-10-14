67d4ab0a632a5780d080b67ceda61332c4e6b2b1
yrsel <xxknu88@gmail.com>
2025-10-14 17:43:58 +0900

    refactor: 주석 내용 제거 및 테스트용 객체로 변환하도록 수정

--- app/src/test/java/woowacourse/shopping/fixture/FakeCartRepository.kt
+++ app/src/test/java/woowacourse/shopping/fixture/FakeCartRepository.kt
@@ -1,29 +1,33 @@
 package woowacourse.shopping.fixture
 
 import woowacourse.shopping.domain.CartRepository
+import woowacourse.shopping.model.CartProduct
 import woowacourse.shopping.model.Product
+import java.util.concurrent.atomic.AtomicLong
 
 class FakeCartRepository : CartRepository {
-    private val storage = mutableListOf<Product>()
+    private val storage = mutableListOf<CartProduct>()
+    private var id: AtomicLong = AtomicLong(1L)
 
     override suspend fun addCartProduct(product: Product) {
-        storage.add(product)
+        val insertId = id.getAndIncrement()
+        storage.add(product.toTestCartProduct(insertId, System.currentTimeMillis()))
     }
 
-    override suspend fun getAllCartProducts(): List<Product> = storage.toList()
+    override suspend fun getAllCartProducts(): List<CartProduct> = storage.toList()
 
     override suspend fun deleteCartProduct(id: Long) {
         storage.removeIf { it.id == id }
     }
 }
 
-// private fun Product.toTestEntity(): CartProductEntity {
-//    val entity =
-//        CartProductEntity(
-//            name = name,
-//            price = price,
-//            imageUrl = imageUrl,
-//        )
-//    entity.id = this.id
-//    return entity
-// }
+private fun Product.toTestCartProduct(
+    id: Long,
+    createdAt: Long,
+) = CartProduct(
+    id = id,
+    name = name,
+    price = price,
+    imageUrl = imageUrl,
+    createdAt = createdAt,
+)
