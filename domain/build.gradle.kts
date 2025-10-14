6369ceea62d2e931da9ac753b240e51fa0ce0787
gahyunkim <mapledt001@naver.com>
2025-10-14 21:28:48 +0900

    build: java 및 jvmTarget 버전 21로 변경

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.jvm)
 }
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 kotlin {
     compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
     }
 }
