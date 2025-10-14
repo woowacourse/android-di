ffed13681fb963838bc5d3bea151f6880742283c
oungsi2000 <oungsi1000@gmail.com>
2025-10-15 02:45:10 +0900

    feat: Inject 어노테이션의 name 파라미터에 기본값 추가

--- di/src/main/java/com/daedan/compactAndroidDi/annotation/Inject.kt
+++ di/src/main/java/com/daedan/compactAndroidDi/annotation/Inject.kt
@@ -2,5 +2,5 @@ package com.daedan.compactAndroidDi.annotation
 
 @Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.PROPERTY)
 annotation class Inject(
-    val name: String,
+    val name: String = "",
 )
