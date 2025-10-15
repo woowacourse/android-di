1cf3878b1ac4e217d44dffe7e4c9dfa0c161caae
chanho0908 <uni070@naver.com>
2025-10-15 16:23:49 +0900

    refactor: remove AppContainer property from App class

--- app/src/main/java/woowacourse/shopping/App.kt
+++ app/src/main/java/woowacourse/shopping/App.kt
@@ -4,12 +4,9 @@ import android.app.Application
 import woowacourse.peto.di.DependencyInjector
 
 class App : Application() {
-    lateinit var container: AppContainer
-        private set
-
     override fun onCreate() {
         super.onCreate()
-        container = AppContainer(this)
+        val container = AppContainer(this)
         DependencyInjector.init(container.dependencyContainer)
     }
 }
