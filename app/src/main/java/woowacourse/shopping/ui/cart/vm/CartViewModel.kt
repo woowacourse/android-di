d044f85f60a1bc9804bd8ce94b1d5f227bd50bd3
chanho0908 <uni070@naver.com>
2025-10-15 15:14:23 +0900

    refactor:🗿제거

--- app/src/main/java/woowacourse/shopping/ui/cart/vm/CartViewModel.kt
+++ app/src/main/java/woowacourse/shopping/ui/cart/vm/CartViewModel.kt
@@ -24,7 +24,6 @@ class CartViewModel : ViewModel() {
 
     fun getAllCartProducts() {
         viewModelScope.launch {
-            println("dsadsadsadsadas")
             _cartProducts.value = cartRepository.getAllCartProducts()
         }
     }
