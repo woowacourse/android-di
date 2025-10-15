7f39ec6003233fddc4a5141bb3f2bff566f546e9
moondev03 <moondev03@gmail.com>
2025-10-16 01:27:05 +0900

    refactor: BindingKey 팩토리 메서드 추가

--- di/src/main/java/com/example/di/model/BindingKey.kt
+++ di/src/main/java/com/example/di/model/BindingKey.kt
@@ -1,6 +1,9 @@
 package com.example.di.model
 
+import com.example.di.util.findSingleQualifierOrNull
 import kotlin.reflect.KClass
+import kotlin.reflect.KFunction
+import kotlin.reflect.KParameter
 
 /**
  * 바인딩을 식별하는 키: (타입 + 선택적 Qualifier)
@@ -16,6 +19,18 @@ data class BindingKey(
         ): BindingKey {
             val q = qualifierAnn?.annotationClass
             return BindingKey(type, q)
+        }
+
+        fun from(param: KParameter): BindingKey {
+            val kClass = param.type.classifier as KClass<out Any>
+            val qualifierAnn = findSingleQualifierOrNull(param.annotations)
+            return BindingKey(kClass, qualifierAnn)
+        }
+
+        fun from(fn: KFunction<*>): BindingKey {
+            val kClass = fn.returnType.classifier as KClass<out Any>
+            val qualifierAnn = findSingleQualifierOrNull(fn.annotations)
+            return BindingKey(kClass, qualifierAnn)
         }
     }
 }
