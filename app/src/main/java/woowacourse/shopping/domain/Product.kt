547a69879096a90e8b5c30c652e2acf829780d0d
Ham BeomJoon <hbj0802@naver.com>
2025-10-15 17:40:37 +0900

    refactor: data layer가 presentation layer를 참조하지 않도록 Mapper 분리

--- app/src/main/java/woowacourse/shopping/domain/Product.kt
+++ app/src/main/java/woowacourse/shopping/domain/Product.kt
@@ -1,7 +1,23 @@
 package woowacourse.shopping.domain
 
+import woowacourse.shopping.data.CartProductEntity
+
 data class Product(
     val name: String,
     val price: Int,
     val imageUrl: String,
 )
+
+fun Product.toData(): CartProductEntity =
+    CartProductEntity(
+        name = name,
+        price = price,
+        imageUrl = imageUrl,
+    )
+
+fun CartProductEntity.toDomain(): Product =
+    Product(
+        name = this.name,
+        price = this.price,
+        imageUrl = this.imageUrl,
+    )
--- app/src/main/java/woowacourse/shopping/ui/model/CartUiModel.kt
+++ app/src/main/java/woowacourse/shopping/ui/model/CartUiModel.kt
@@ -1,9 +1,18 @@
 package woowacourse.shopping.ui.model
 
+import woowacourse.shopping.data.CartProductEntity
 import woowacourse.shopping.domain.Product
+import woowacourse.shopping.domain.toDomain
 
 data class CartUiModel(
     val id: Long,
     val product: Product,
     val createdAt: Long,
 )
+
+fun CartProductEntity.toPresentation(): CartUiModel =
+    CartUiModel(
+        id = this.id,
+        product = this.toDomain(),
+        createdAt = this.createdAt,
+    )
