e57faa3aa622b264c5c2df41b80e2175a4020b70
devfeijoa <devfeijoa@gmail.com>
2025-10-15 21:31:38 +0900

    refactor: DI 컨테이너 구현 및 Repository 의존성 주입

--- app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
+++ app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
@@ -1,14 +1,40 @@
 package woowacourse.shopping
 
 import android.app.Application
-import woowacourse.shopping.AppContainer
+import androidx.room.Room
+import woowacourse.shopping.data.CartRepositoryImpl
+import woowacourse.shopping.data.InMemoryCartRepository
+import woowacourse.shopping.data.ProductRepositoryImpl
+import woowacourse.shopping.data.ShoppingDatabase
+import woowacourse.shopping.domain.CartRepository
+import woowacourse.shopping.domain.ProductRepository
+import woowacouse.shopping.di.Container
+import woowacouse.shopping.di.annotation.Qualifier
 
 class ShoppingApplication : Application() {
-    lateinit var container: AppContainer
-        private set
+    private val database: ShoppingDatabase by lazy {
+        Room
+            .databaseBuilder(
+                this,
+                ShoppingDatabase::class.java,
+                "shopping.db",
+            ).build()
+    }
+
+    private val cartProductDao by lazy { database.cartProductDao() }
+
+    private val productRepository: ProductRepository by lazy { ProductRepositoryImpl() }
+
+    private val roomCartRepository: CartRepository by lazy { CartRepositoryImpl(cartProductDao) }
+
+    private val inMemoryCartRepository: CartRepository by lazy { InMemoryCartRepository() }
+
+    val container = Container()
 
     override fun onCreate() {
         super.onCreate()
-        container = AppContainer(this)
+        container.registerInstances(ProductRepository::class, productRepository)
+        container.registerInstances(CartRepository::class, roomCartRepository, "room")
+        container.registerInstances(CartRepository::class, inMemoryCartRepository, "inMemory")
     }
 }
