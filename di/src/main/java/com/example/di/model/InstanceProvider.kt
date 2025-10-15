d9150349d8f206fdf98082537cddc3937f9a36e4
moondev03 <moondev03@gmail.com>
2025-10-16 01:26:48 +0900

    refactor: Provider 재정의

--- di/src/main/java/com/example/di/model/InstanceProvider.kt
+++
@@ -1,7 +0,0 @@
-package com.example.di.model
-
-internal class InstanceProvider<T : Any>(
-    private val instance: T,
-) : Provider<T> {
-    override fun get(overrides: Map<BindingKey, Any>): T = instance
-}
