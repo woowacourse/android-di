f0bd77d7a6b9e532f632338b60188dcc07169067
gahyunkim <mapledt001@naver.com>
2025-10-15 12:53:05 +0900

    refactor: AppContainerImpl의 registry 키 타입을 KClass로 변경

--- di/src/main/java/woowacourse/shopping/di/AppContainerImpl.kt
+++ di/src/main/java/woowacourse/shopping/di/AppContainerImpl.kt
@@ -1,25 +1,24 @@
 package woowacourse.shopping.di
 
+import kotlin.reflect.KClass
 import kotlin.reflect.KType
 
 class AppContainerImpl : Container {
-    private val registry: MutableMap<Pair<KType, Annotation?>, (Container) -> Any> = mutableMapOf()
+    private val registry = mutableMapOf<Pair<KType, KClass<out Annotation>?>, (Container) -> Any>()
 
     fun <T : Any> register(
         type: KType,
         qualifier: Annotation? = null,
         factory: (Container) -> T,
     ) {
-        registry[Pair(type, qualifier)] = factory as (Container) -> Any
+        registry[type to qualifier?.annotationClass] = factory as (Container) -> Any
     }
 
     override fun resolve(
         requestedType: KType,
         qualifier: Annotation?,
     ): Any? {
-        val key = Pair(requestedType, qualifier)
-        val factoryFunction = registry[key] ?: return null
-
-        return factoryFunction(this)
+        val factory = registry[requestedType to qualifier?.annotationClass] ?: return null
+        return factory(this)
     }
 }
