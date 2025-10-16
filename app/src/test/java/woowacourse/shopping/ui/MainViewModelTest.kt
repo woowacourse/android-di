4efdf17ef105e3df95709cf12662ccbbd0ac5d52
cucumber99 <dlsguq1@gmail.com>
2025-10-16 15:39:14 +0900

    test: ViewModel 테스트

--- app/src/test/java/woowacourse/shopping/ui/MainViewModelTest.kt
+++ app/src/test/java/woowacourse/shopping/ui/MainViewModelTest.kt
@@ -2,60 +2,73 @@ package woowacourse.shopping.ui
 
 import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.collections.shouldContain
-import io.mockk.Runs
-import io.mockk.coEvery
-import io.mockk.coVerify
-import io.mockk.just
-import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import woowacourse.shopping.Container
 import woowacourse.shopping.DEFAULT_PRODUCT
+import woowacourse.shopping.annotation.Room
 import woowacourse.shopping.domain.repository.CartRepository
 import woowacourse.shopping.domain.repository.ProductRepository
 
+@OptIn(ExperimentalCoroutinesApi::class)
 @ExtendWith(InstantTaskExecutorExtension::class)
 class MainViewModelTest {
-    private lateinit var productRepository: ProductRepository
-    private lateinit var cartRepository: CartRepository
+    private lateinit var container: Container
     private lateinit var viewModel: MainViewModel
+    private val testDispatcher: TestDispatcher = StandardTestDispatcher()
 
     @BeforeEach
     fun setUp() {
-        productRepository = mockk<ProductRepository>(relaxed = true)
-        cartRepository = mockk<CartRepository>(relaxed = true)
-        viewModel = MainViewModel()
+        Dispatchers.setMain(testDispatcher)
+        container =
+            Container().apply {
+                bind(ProductRepository::class) { FakeProductRepository() }
+                bind(
+                    type = CartRepository::class,
+                    provider = { FakeCartRepository() },
+                    qualifier = Room::class,
+                )
+            }
+        viewModel = container.get()
+    }
+
+    @AfterEach
+    fun tearDown() {
+        Dispatchers.resetMain()
     }
 
     @DisplayName("상품을 추가하면 상태가 변경된다")
     @Test
     fun addCartProductTest() =
-        runTest {
-            // given
-            coEvery { cartRepository.addCartProduct(DEFAULT_PRODUCT) } just Runs
-
+        runTest(testDispatcher) {
             // when
             viewModel.addCartProduct(DEFAULT_PRODUCT)
+            advanceUntilIdle()
 
             // then
             viewModel.onProductAdded.getOrAwaitValue().shouldBeTrue()
-            coVerify(exactly = 1) { cartRepository.addCartProduct(DEFAULT_PRODUCT) }
         }
 
     @DisplayName("상품 목록을 가져온다")
     @Test
-    fun getAllProductsTest() {
-        // given
-        coEvery { productRepository.getAllProducts() } returns listOf(DEFAULT_PRODUCT)
+    fun getAllProductsTest() =
+        runTest(testDispatcher) {
+            // when
+            viewModel.getAllProducts()
+            val products = viewModel.products.getOrAwaitValue()
 
-        // when
-        viewModel.getAllProducts()
-        val products = viewModel.products.getOrAwaitValue()
-
-        // then
-        products.shouldContain(DEFAULT_PRODUCT)
-        coVerify(exactly = 1) { productRepository.getAllProducts() }
-    }
+            // then
+            products.shouldContain(DEFAULT_PRODUCT)
+        }
 }
