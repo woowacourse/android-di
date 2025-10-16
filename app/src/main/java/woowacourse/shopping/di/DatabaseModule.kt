d0334f8ece0d427866f91ed763784a11bcbf05a6
yrsel <xxknu88@gmail.com>
2025-10-17 05:44:17 +0900

    feat: 서비스에 사용할 모듈 정의

--- app/src/main/java/woowacourse/shopping/di/DatabaseModule.kt
+++ app/src/main/java/woowacourse/shopping/di/DatabaseModule.kt
@@ -1,48 +1,41 @@
 package woowacourse.shopping.di
 
+import InMemory
+import Local
 import android.content.Context
 import androidx.room.Room
-import com.shopping.di.InjectContainer
-import com.shopping.di.InjectionModule
-import com.shopping.di.Provider
-import com.shopping.di.definition.Qualifier
+import com.yrsel.di.Module
+import com.yrsel.di.annotation.Provides
+import com.yrsel.di.annotation.Singleton
 import woowacourse.shopping.data.CartProductDao
 import woowacourse.shopping.data.ShoppingDatabase
 
 class DatabaseModule(
     private val context: Context,
-) : InjectionModule {
-    override fun provideDefinitions(container: InjectContainer) {
-        container.apply {
-            registerSingleton<ShoppingDatabase>(Qualifier.Named("room")) {
-                Provider { provideRoomDatabase() }
-            }
-            registerSingleton<ShoppingDatabase>(Qualifier.Named("memory")) {
-                Provider { provideInMemoryDatabase() }
-            }
-            registerSingleton<CartProductDao>(Qualifier.Named("memory")) { container ->
-                Provider {
-                    val db = container.get<ShoppingDatabase>(Qualifier.Named("memory"))
-                    db.cartProductDao()
-                }
-            }
-        }
-    }
+) : Module {
+    @Provides
+    @Singleton
+    @Local
+    fun provideRoomDatabase(): ShoppingDatabase = Room.databaseBuilder(context, ShoppingDatabase::class.java, DATABASE_NAME).build()
 
-    private fun provideRoomDatabase(): ShoppingDatabase =
-        Room
-            .databaseBuilder(
-                context,
-                ShoppingDatabase::class.java,
-                DATABASE_NAME,
-            ).build()
+    @Provides
+    @Singleton
+    @InMemory
+    fun provideInMemoryDatabase(): ShoppingDatabase = Room.inMemoryDatabaseBuilder(context, ShoppingDatabase::class.java).build()
 
-    private fun provideInMemoryDatabase(): ShoppingDatabase =
-        Room
-            .inMemoryDatabaseBuilder(
-                context,
-                ShoppingDatabase::class.java,
-            ).build()
+    @Provides
+    @Singleton
+    @Local
+    fun provideLocalCartProductDao(
+        @Local shoppingDatabase: ShoppingDatabase,
+    ): CartProductDao = shoppingDatabase.cartProductDao()
+
+    @Provides
+    @Singleton
+    @InMemory
+    fun provideInMemoryCartProductDao(
+        @InMemory shoppingDatabase: ShoppingDatabase,
+    ): CartProductDao = shoppingDatabase.cartProductDao()
 
     companion object {
         private const val DATABASE_NAME = "shopping_db"
--- app/src/main/java/woowacourse/shopping/di/RepositoryModule.kt
+++ app/src/main/java/woowacourse/shopping/di/RepositoryModule.kt
@@ -1,22 +1,37 @@
-package woowacourse.shopping.di
-
-import com.shopping.di.InjectContainer
-import com.shopping.di.InjectionModule
-import com.shopping.di.Provider
-import com.shopping.di.definition.Qualifier
+import com.yrsel.di.Module
+import com.yrsel.di.annotation.Provides
+import com.yrsel.di.annotation.Qualifier
+import com.yrsel.di.annotation.Singleton
+import woowacourse.shopping.data.CartProductDao
 import woowacourse.shopping.data.CartRepositoryImpl
 import woowacourse.shopping.data.ProductRepositoryImpl
 import woowacourse.shopping.domain.CartRepository
 import woowacourse.shopping.domain.ProductRepository
 
-class RepositoryModule : InjectionModule {
-    override fun provideDefinitions(container: InjectContainer) {
-        container.apply {
-            registerSingleton<ProductRepository> { Provider { ProductRepositoryImpl() } }
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY, AnnotationTarget.VALUE_PARAMETER)
+@Qualifier
+annotation class InMemory
 
-            registerSingleton<CartRepository>(Qualifier.Named("memory")) {
-                Provider { CartRepositoryImpl(get(Qualifier.Named("memory"))) }
-            }
-        }
-    }
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY, AnnotationTarget.VALUE_PARAMETER)
+@Qualifier
+annotation class Local
+
+class RepositoryModule : Module {
+    @Provides
+    @Singleton
+    fun provideProductRepository(): ProductRepository = ProductRepositoryImpl()
+
+    @Provides
+    @Singleton
+    @Local
+    fun provideLocalCartRepository(
+        @Local dao: CartProductDao,
+    ): CartRepository = CartRepositoryImpl(dao)
+
+    @Provides
+    @Singleton
+    @InMemory
+    fun provideInMemoryCartRepository(
+        @InMemory dao: CartProductDao,
+    ): CartRepository = CartRepositoryImpl(dao)
 }
