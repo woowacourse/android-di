15486089ab82215f821db07c9e2fe23e42771f9a
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-10-16 17:35:01 +0900

    refactor: FieldInjector의 inject 함수를 for-loop에서 filter와 forEach로 변경

--- di/src/main/java/com/example/di/FieldInjector.kt
+++ di/src/main/java/com/example/di/FieldInjector.kt
@@ -1,5 +1,6 @@
 package com.example.di
 
+import com.sun.tools.javac.code.TypeAnnotationPosition.field
 import kotlin.jvm.java
 
 @Target(AnnotationTarget.FIELD, AnnotationTarget.PROPERTY)
@@ -15,12 +16,13 @@ object FieldInjector {
         container: ShoppingContainer,
     ) {
         val clazz = target.javaClass
-        for (field in clazz.declaredFields) {
-            if (field.isAnnotationPresent(Inject::class.java)) {
-                val dependency = container.get(field.type.kotlin)
-                field.isAccessible = true
-                field.set(target, dependency)
-            }
+        clazz.declaredFields
+            .filter { field ->
+            field.isAnnotationPresent(Inject::class.java)
+            }.forEach { field ->
+            val dependency = container.get(field.type.kotlin)
+            field.isAccessible = true
+            field.set(target, dependency)
         }
     }
 }
