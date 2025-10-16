c5334548eabea514b5abf1d6e52ad91ca3cd4d15
cucumber99 <dlsguq1@gmail.com>
2025-10-16 13:49:17 +0900

    feat: RepositoryModule 추가

---
+++ app/src/main/java/woowacourse/shopping/di/RepositoryModule.kt
@@ -0,0 +1,20 @@
+package woowacourse.shopping.di
+
+import woowacourse.shopping.annotation.InMemory
+import woowacourse.shopping.annotation.Room
+import woowacourse.shopping.data.CartProductDao
+import woowacourse.shopping.data.repository.DefaultCartRepository
+import woowacourse.shopping.data.repository.InMemoryCartRepository
+import woowacourse.shopping.data.repository.ProductRepositoryImpl
+import woowacourse.shopping.domain.repository.CartRepository
+import woowacourse.shopping.domain.repository.ProductRepository
+
+object RepositoryModule {
+    @Room
+    fun provideDefaultCartRepository(dao: CartProductDao): CartRepository = DefaultCartRepository(dao)
+
+    @InMemory
+    fun provideInMemoryCartRepository(): CartRepository = InMemoryCartRepository()
+
+    fun provideProductRepository(): ProductRepository = ProductRepositoryImpl()
+}
