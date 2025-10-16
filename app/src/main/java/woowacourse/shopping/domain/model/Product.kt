2355512e5417dacbc770db6800004a0e9c2665b5
cucumber99 <dlsguq1@gmail.com>
2025-10-16 16:00:32 +0900

    feat: Product에서 ID, 생성 시간 프로퍼티 제거

--- app/src/main/java/woowacourse/shopping/domain/model/Product.kt
+++ app/src/main/java/woowacourse/shopping/domain/model/Product.kt
@@ -1,9 +1,7 @@
 package woowacourse.shopping.domain.model
 
 data class Product(
-    val id: Long? = null,
     val name: String,
     val price: Int,
     val imageUrl: String,
-    val createdAt: Long = System.currentTimeMillis(),
 )
