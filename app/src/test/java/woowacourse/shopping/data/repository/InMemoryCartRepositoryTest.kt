3b354844792757aeb87d2a850a2cab6f1c7d5a57
cucumber99 <dlsguq1@gmail.com>
2025-10-16 15:45:52 +0900

    test: InMemoryCartRepository 테스트 수정

--- app/src/test/java/woowacourse/shopping/data/repository/InMemoryCartRepositoryTest.kt
+++ app/src/test/java/woowacourse/shopping/data/repository/InMemoryCartRepositoryTest.kt
@@ -1,9 +1,7 @@
 package woowacourse.shopping.data.repository
 
-import io.kotest.assertions.assertSoftly
 import io.kotest.matchers.collections.shouldContain
 import io.kotest.matchers.collections.shouldNotContain
-import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
@@ -29,10 +27,7 @@ class InMemoryCartRepositoryTest {
             val products = repository.getAllCartProducts()
 
             // then
-            assertSoftly(products) {
-                size shouldBe 1
-                shouldContain(DEFAULT_PRODUCT)
-            }
+            products.shouldContain(DEFAULT_PRODUCT)
         }
 
     @DisplayName("장바구니에서 상품을 제거한다")
@@ -43,8 +38,7 @@ class InMemoryCartRepositoryTest {
             repository.addCartProduct(DEFAULT_PRODUCT)
 
             // when
-            val products = repository.getAllCartProducts()
-            repository.deleteCartProduct(1L)
+            DEFAULT_PRODUCT.id?.let { repository.deleteCartProduct(it) }
 
             // then
             repository.getAllCartProducts().shouldNotContain(DEFAULT_PRODUCT)
