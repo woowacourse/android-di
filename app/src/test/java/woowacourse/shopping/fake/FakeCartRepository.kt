5fbed3be03c5cdeae332988cf5363d6251e9299c
hwannow <vnfdlvgid061@naver.com>
2025-10-16 10:42:53 +0900

    test: FakeCartRepository가 CartProduct를 사용하도록 수정

--- app/src/test/java/woowacourse/shopping/fake/FakeCartRepository.kt
+++ app/src/test/java/woowacourse/shopping/fake/FakeCartRepository.kt
@@ -1,21 +1,24 @@
 package woowacourse.shopping.fake
 
+import woowacourse.shopping.data.mapper.toEntity
+import woowacourse.shopping.data.toDomain
 import woowacourse.shopping.domain.repository.CartRepository
-import woowacourse.shopping.fixture.GOBCHANG
-import woowacourse.shopping.fixture.MALATANG
-import woowacourse.shopping.fixture.TONKATSU
+import woowacourse.shopping.fixture.GOBCHANG_CART
+import woowacourse.shopping.fixture.MALATANG_CART
+import woowacourse.shopping.fixture.TONKATSU_CART
 import woowacourse.shopping.model.CartProduct
 import woowacourse.shopping.model.Product
 
 class FakeCartRepository : CartRepository {
-    private val cartProducts = mutableListOf<Product>(TONKATSU, MALATANG, GOBCHANG)
+    private val cartProducts =
+        mutableListOf<CartProduct>(TONKATSU_CART, MALATANG_CART, GOBCHANG_CART)
 
     override suspend fun addCartProduct(product: Product) {
-        cartProducts.add(product)
+        cartProducts.add(product.toEntity().toDomain())
     }
 
     override suspend fun getAllCartProducts(): List<CartProduct> {
-        return cartProducts.toList().map { CartProduct(it, 2) }
+        return cartProducts.toList()
     }
 
     override suspend fun deleteCartProduct(id: Long) {
--- app/src/test/java/woowacourse/shopping/fixture/ProductFixture.kt
+++ app/src/test/java/woowacourse/shopping/fixture/ProductFixture.kt
@@ -4,10 +4,10 @@ import woowacourse.shopping.model.CartProduct
 import woowacourse.shopping.model.Product
 
 val TONKATSU = Product("돈가스", 11000, "image_url")
-val TONKATSU_CART = CartProduct(TONKATSU, 2)
+val TONKATSU_CART = CartProduct(0, TONKATSU, 2)
 
 val MALATANG = Product("마라탕", 14000, "image_url")
-val MALATANG_CART = CartProduct(MALATANG, 2)
+val MALATANG_CART = CartProduct(1, MALATANG, 2)
 
 val GOBCHANG = Product("곱창", 40000, "image_url")
-val GOBCHANG_CART = CartProduct(GOBCHANG, 2)
+val GOBCHANG_CART = CartProduct(2, GOBCHANG, 2)
