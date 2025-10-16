f3d04a9052d43b027aa14e1f231dd47300c7295a
yrsel <xxknu88@gmail.com>
2025-10-17 05:30:25 +0900

    feat: Annotation 구현

---
+++ inject/src/main/java/com/yrsel/di/annotation/Inject.kt
@@ -0,0 +1,5 @@
+package com.yrsel.di.annotation
+
+@Target(AnnotationTarget.PROPERTY, AnnotationTarget.CONSTRUCTOR)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Inject
---
+++ inject/src/main/java/com/yrsel/di/annotation/Provides.kt
@@ -0,0 +1,5 @@
+package com.yrsel.di.annotation
+
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Provides
