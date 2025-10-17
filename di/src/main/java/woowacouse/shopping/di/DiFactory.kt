1874d0511444694824faab1652a6b22f04652f76
devfeijoa <devfeijoa@gmail.com>
2025-10-17 16:40:01 +0900

    feat: DiFactory 구현

---
+++ di/src/main/java/woowacouse/shopping/di/DiFactory.kt
@@ -0,0 +1,78 @@
+package woowacouse.shopping.di
+
+import android.content.Context
+import android.os.Bundle
+import androidx.lifecycle.SavedStateHandle
+import androidx.savedstate.SavedStateRegistryOwner
+import woowacouse.shopping.di.annotation.Inject
+import woowacouse.shopping.di.annotation.Qualifier
+import kotlin.reflect.KClass
+import kotlin.reflect.KMutableProperty1
+import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.full.primaryConstructor
+import kotlin.reflect.jvm.isAccessible
+
+class DiFactory(
+    private val container: Container,
+    private val context: Context,
+) {
+    fun <T : Any> create(
+        key: String,
+        modelClass: KClass<T>,
+        handle: SavedStateHandle,
+    ): T {
+        val constructor =
+            modelClass.primaryConstructor
+                ?: throw IllegalArgumentException("No primary constructor found for $modelClass")
+
+        val parameters =
+            constructor.parameters
+                .map { parameter ->
+                    val kParameter =
+                        parameter.type.classifier as? KClass<*>
+                            ?: throw IllegalArgumentException("No matching dependency for ${parameter.name}")
+
+                    when (kParameter) {
+                        Context::class -> context
+                        SavedStateHandle::class -> handle
+                        else ->
+                            container.get(
+                                kParameter,
+                                parameter.findAnnotation<Qualifier>()?.value,
+                            )
+                    }
+                }.toTypedArray()
+
+        constructor.isAccessible = true
+        val instance = constructor.call(*parameters)
+
+        modelClass.memberProperties
+            .filter { it.findAnnotation<Inject>() != null }
+            .forEach { property ->
+                val mutable =
+                    (property as? KMutableProperty1<Any, Any?>)
+                        ?: error("@Inject target must be a 'var' mutable property: ${property.name} in $modelClass")
+
+                val targetType =
+                    property.returnType.classifier as? KClass<*>
+                        ?: error("@Inject target must be a 'var' mutable property: ${property.name} in $modelClass")
+
+                val qualifier = property.findAnnotation<Qualifier>()?.value
+                val dependency =
+                    when (targetType) {
+                        Context::class -> context
+                        SavedStateHandle::class -> handle
+                        else -> {
+                            container.get(targetType, qualifier)
+                                ?: throw IllegalArgumentException("No matching dependency for ${targetType.simpleName} in $modelClass")
+                        }
+                    }
+
+                mutable.isAccessible = true
+                mutable.setter.isAccessible = true
+                mutable.setter.call(instance, dependency)
+            }
+        return instance
+    }
+}
