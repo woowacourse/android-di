0ed0bb830c297d0626c0860ea2b36bf6efa475cc
Ham BeomJoon <hbj0802@naver.com>
2025-10-17 15:40:18 +0900

    refactor: Formatter 인터페이스 분리

--- app/src/main/java/woowacourse/shopping/ui/cart/DateFormatter.kt
+++ app/src/main/java/woowacourse/shopping/ui/cart/DateFormatter.kt
@@ -1,6 +1,7 @@
 package woowacourse.shopping.ui.cart
 
 import android.content.Context
+import com.example.di.Formatter
 import woowacourse.shopping.R
 import java.text.SimpleDateFormat
 import java.util.Date
@@ -8,12 +9,12 @@ import java.util.Locale
 
 class DateFormatter(
     context: Context,
-) {
+) : Formatter {
     private val formatter =
         SimpleDateFormat(
             context.getString(R.string.date_format),
             Locale.KOREA,
         )
 
-    fun formatDate(timestamp: Long): String = formatter.format(Date(timestamp))
+    override fun formatDate(timestamp: Long): String = formatter.format(Date(timestamp))
 }
---
+++ di/src/main/java/com/example/di/Formatter.kt
@@ -0,0 +1,5 @@
+package com.example.di
+
+interface Formatter {
+    fun formatDate(timestamp: Long): String
+}
