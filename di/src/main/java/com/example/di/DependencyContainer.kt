10ba45e0f1ba12f8691e26259929b54856fb4e55
DoableTuple <doabletuple@gmail.com>
2025-10-17 17:59:19 +0900

    feat: Lifecycle에 따라 의존성 인스턴스가 제거되도록 변경

--- di/src/main/java/com/example/di/DependencyContainer.kt
+++ di/src/main/java/com/example/di/DependencyContainer.kt
@@ -8,8 +8,6 @@ import kotlin.reflect.full.hasAnnotation
 import kotlin.reflect.full.memberProperties
 
 object DependencyContainer {
-    private val dependencyGetters: MutableMap<Identifier, () -> Any> = mutableMapOf()
-
     private val applicationDependencyGetters: MutableMap<Identifier, () -> Any> = mutableMapOf()
     private val viewModelDependencyGetters: MutableMap<Identifier, () -> Any> = mutableMapOf()
     private val activityDependencyGetters: MutableMap<Identifier, () -> Any> = mutableMapOf()
@@ -22,14 +20,28 @@ object DependencyContainer {
         module.forEach(::initialize)
     }
 
-    fun dependency(identifier: Identifier): Any = dependencyGetters[identifier]?.invoke() ?: error("No dependency defined for $identifier.")
+    fun dependency(identifier: Identifier): Any {
+        val getters: MutableMap<Identifier, () -> Any> =
+            when (identifier.lifespan) {
+                is ViewModelLifespan -> viewModelDependencyGetters
+                is ActivityLifespan -> activityDependencyGetters
+                else -> applicationDependencyGetters
+            }
+        return getters[identifier]?.invoke() ?: error("No dependency defined for $identifier.")
+    }
 
     private fun initialize(module: Module) {
         module::class.memberProperties.forEach { property: KProperty1<out Module, *> ->
             if (!property.hasAnnotation<Dependency>()) return@forEach
 
             val identifier = Identifier.from(property)
-            dependencyGetters[identifier] = {
+            val getters: MutableMap<Identifier, () -> Any> =
+                when (identifier.lifespan) {
+                    is ViewModelLifespan -> viewModelDependencyGetters
+                    is ActivityLifespan -> activityDependencyGetters
+                    else -> applicationDependencyGetters
+                }
+            getters[identifier] = {
                 property.getter.call(module) ?: error("$property's getter returned null.")
             }
         }
