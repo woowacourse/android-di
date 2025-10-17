7456ad46ab5c144125c96420fba7200bb92ce830
Ham BeomJoon <hbj0802@naver.com>
2025-10-17 18:00:57 +0900

    feat: 스코프 어노테이션에 Retention 추가

--- di/src/main/java/com/example/di/Annotations.kt
+++ di/src/main/java/com/example/di/Annotations.kt
@@ -11,12 +11,15 @@ annotation class RoomDatabase
 annotation class InMemory
 
 @Target(AnnotationTarget.FIELD)
+@Retention(AnnotationRetention.RUNTIME)
 annotation class Singleton
 
 @Target(AnnotationTarget.FIELD)
+@Retention(AnnotationRetention.RUNTIME)
 annotation class ActivityScope
 
 @Target(AnnotationTarget.FIELD)
+@Retention(AnnotationRetention.RUNTIME)
 annotation class ViewModelScope
 
 @Target(AnnotationTarget.FIELD)
