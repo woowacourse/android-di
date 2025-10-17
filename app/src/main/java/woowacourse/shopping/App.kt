35bf7f56b561e86b6526384c6470f86a84c053ed
chanho0908 <uni070@naver.com>
2025-10-17 16:29:20 +0900

    feat: ActivityLifecycleCallbacks 구현하여 Activity 생명주기에 따라 의존성 주입/제거 기능 추가

--- app/src/main/java/woowacourse/shopping/App.kt
+++ app/src/main/java/woowacourse/shopping/App.kt
@@ -1,12 +1,44 @@
 package woowacourse.shopping
 
+import android.app.Activity
 import android.app.Application
+import android.os.Bundle
+import woowacourse.peto.di.DependencyContainer
 import woowacourse.peto.di.DependencyInjector
+import woowacourse.peto.di.annotation.Scope
 
-class App : Application() {
+class App : Application(), Application.ActivityLifecycleCallbacks {
+    private lateinit var dependencyContainer: DependencyContainer
+
     override fun onCreate() {
         super.onCreate()
-        val container = AppContainer(this)
-        DependencyInjector.init(container.dependencyContainer)
+        val appContainer = AppContainer(this)
+        dependencyContainer = appContainer.dependencyContainer
+        DependencyInjector.init(dependencyContainer)
+        registerActivityLifecycleCallbacks(this)
     }
+
+    override fun onActivityCreated(
+        activity: Activity,
+        bundle: Bundle?,
+    ) {
+        dependencyContainer.inject(activity, Scope.ACTIVITY)
+    }
+
+    override fun onActivityDestroyed(activity: Activity) {
+        dependencyContainer.clearScope(Scope.ACTIVITY)
+    }
+
+    override fun onActivityPaused(activity: Activity) {}
+
+    override fun onActivityResumed(activity: Activity) {}
+
+    override fun onActivitySaveInstanceState(
+        activity: Activity,
+        p1: Bundle,
+    ) {}
+
+    override fun onActivityStarted(activity: Activity) {}
+
+    override fun onActivityStopped(activity: Activity) {}
 }
