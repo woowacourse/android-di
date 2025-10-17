3ff4bae4f965b701ee59e4bc680aad99fb25a5c6
DoableTuple <doabletuple@gmail.com>
2025-10-17 17:26:53 +0900

    feat: Lifespan 관련 sealed class 추가

--- app/src/main/java/woowacourse/shopping/di/RepositoryModule.kt
+++ app/src/main/java/woowacourse/shopping/di/RepositoryModule.kt
@@ -1,8 +1,10 @@
 package woowacourse.shopping.di
 
 import android.content.Context
+import com.example.di.ApplicationLifespan
 import com.example.di.Dependency
 import com.example.di.Module
+import com.example.di.ViewModelLifespan
 import woowacourse.shopping.data.DatabaseCartRepository
 import woowacourse.shopping.data.DefaultProductRepository
 import woowacourse.shopping.data.InMemoryCartRepository
@@ -16,13 +18,16 @@ class RepositoryModule(
     private val shoppingDatabase: ShoppingDatabase = ShoppingDatabase.Companion.instance(context)
 
     @Dependency
+    @ViewModelLifespan
     val productRepository: ProductRepository by lazy { DefaultProductRepository() }
 
     @Dependency
+    @ApplicationLifespan
     @DatabaseRepository
     val databaseCartRepository: CartRepository by lazy { DatabaseCartRepository(shoppingDatabase.cartProductDao()) }
 
     @Dependency
+    @ApplicationLifespan
     @InMemoryRepository
     val inMemoryCartRepository: CartRepository by lazy { InMemoryCartRepository() }
 }
--- di/src/main/java/com/example/di/Annotations.kt
+++ di/src/main/java/com/example/di/Annotations.kt
@@ -9,11 +9,17 @@ annotation class Dependency
 @Target(AnnotationTarget.ANNOTATION_CLASS)
 annotation class Qualifier
 
+@Target(AnnotationTarget.ANNOTATION_CLASS)
+annotation class Lifespan
+
 @Target(AnnotationTarget.PROPERTY, AnnotationTarget.VALUE_PARAMETER)
+@Lifespan
 annotation class ApplicationLifespan
 
 @Target(AnnotationTarget.PROPERTY, AnnotationTarget.VALUE_PARAMETER)
+@Lifespan
 annotation class ViewModelLifespan
 
 @Target(AnnotationTarget.PROPERTY, AnnotationTarget.VALUE_PARAMETER)
+@Lifespan
 annotation class ActivityLifespan
