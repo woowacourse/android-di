8bef9ae5aeb743f1367846de953abba52e92cc00
DoableTuple <doabletuple@gmail.com>
2025-10-17 16:57:09 +0900

    feat: Lifespan 관련 annotation 정의

--- di/src/main/java/com/example/di/Annotations.kt
+++ di/src/main/java/com/example/di/Annotations.kt
@@ -8,3 +8,12 @@ annotation class Dependency
 
 @Target(AnnotationTarget.ANNOTATION_CLASS)
 annotation class Qualifier
+
+@Target(AnnotationTarget.PROPERTY, AnnotationTarget.VALUE_PARAMETER)
+annotation class ApplicationLifespan
+
+@Target(AnnotationTarget.PROPERTY, AnnotationTarget.VALUE_PARAMETER)
+annotation class ViewModelLifespan
+
+@Target(AnnotationTarget.PROPERTY, AnnotationTarget.VALUE_PARAMETER)
+annotation class ActivityLifespan
