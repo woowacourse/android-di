6acb7da46f96101462b9f286b2917dc5ef93cb03
devfeijoa <devfeijoa@gmail.com>
2025-10-17 16:39:51 +0900

    feat: Inject 어노테이션에 FIELD 타겟 추가

--- di/src/main/java/woowacouse/shopping/di/annotation/Inject.kt
+++ di/src/main/java/woowacouse/shopping/di/annotation/Inject.kt
@@ -1,5 +1,5 @@
 package woowacouse.shopping.di.annotation
 
-@Target(AnnotationTarget.PROPERTY)
+@Target(AnnotationTarget.PROPERTY, AnnotationTarget.FIELD)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class Inject
