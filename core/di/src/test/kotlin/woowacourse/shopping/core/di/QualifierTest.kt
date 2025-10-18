15854e8b9250f16938c05b68b9a6fd0d86c189f8
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-10-18 20:27:39 +0900

    test: Qualifier 테스트

---
+++ core/di/src/test/kotlin/woowacourse/shopping/core/di/QualifierTest.kt
@@ -0,0 +1,79 @@
+package woowacourse.shopping.core.di
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Assert.assertThrows
+import org.junit.Before
+import org.junit.Test
+import woowacourse.shopping.core.di.NewDependencyContainer.instance
+import woowacourse.shopping.core.di.NewDependencyContainer.register
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.jvm.isAccessible
+
+class QualifierTest {
+    @Before
+    fun resetDependencyContainer() {
+        val field =
+            NewDependencyContainer::class
+                .memberProperties
+                .first { it.name == "dependencies" }
+                .apply { isAccessible = true }
+
+        @Suppress("UNCHECKED_CAST")
+        val map = field.getter.call() as MutableMap<*, *>
+        map.clear()
+    }
+
+    @Test
+    fun `Qualifier 이름이 일치하는 의존성이 생성자 주입된다`() {
+        // given
+        register(Repository::class, DefaultRepository(), "default")
+
+        val service = instance(ServiceWithQualifiedConstructor::class)
+
+        // then
+        assertThat(service.repository).isInstanceOf(DefaultRepository::class.java)
+    }
+
+    @Test
+    fun `Qualifier 이름이 일치하는 의존성이 필드 주입된다`() {
+        // given
+        register(Repository::class, DefaultRepository(), "default")
+
+        val service = instance(ServiceWithQualifiedField::class)
+
+        // then
+        assertThat(service.repository).isInstanceOf(DefaultRepository::class.java)
+    }
+
+    @Test
+    fun `등록되지 않은 Qualifier를 사용하면 예외가 발생한다`() {
+        // given
+        register(Repository::class, DefaultRepository(), "default")
+
+        assertThrows(IllegalStateException::class.java) {
+            instance(ServiceWithUnknownQualifier::class)
+        }
+    }
+
+    interface Repository
+
+    class DefaultRepository : Repository
+
+    class ServiceWithQualifiedConstructor
+        @Inject
+        constructor(
+            @Qualifier("default") val repository: Repository,
+        )
+
+    class ServiceWithQualifiedField {
+        @Inject
+        @Qualifier("default")
+        lateinit var repository: Repository
+    }
+
+    class ServiceWithUnknownQualifier {
+        @Inject
+        @Qualifier("unknown")
+        lateinit var repository: Repository
+    }
+}
