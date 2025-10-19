a4a7ada3a2764c33d90d49dd6dd834c2e246c72d
devfeijoa <devfeijoa@gmail.com>
2025-10-20 03:02:51 +0900

    feat: `get` 메소드에서 provider를 이용한 인스턴스 생성 기능 추가

--- di/src/main/java/woowacourse/shopping/di/Container.kt
+++ di/src/main/java/woowacourse/shopping/di/Container.kt
@@ -26,5 +26,18 @@ class Container {
     fun <T : Any> get(
         kClass: KClass<T>,
         qualifier: String? = null,
-    ): T? = dependencies[DependencyKey(kClass, qualifier)] as? T
+    ): T? {
+        val key = DependencyKey(kClass, qualifier)
+
+        // 이미 존재하는 인스턴스면 그대로 반환
+        val existing = dependencies[key] as? T
+        if (existing != null) return existing
+
+        // provider가 등록되어 있다면 새 인스턴스를 생성하고 저장
+        val provider = providers[key] ?: return null
+        val newInstance = provider.invoke()
+        dependencies[key] = newInstance
+
+        return newInstance as T
+    }
 }
