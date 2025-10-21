1191942f22ef2758fe9f387dbfe44a5691b4c9c1
hwannow <vnfdlvgid061@naver.com>
2025-10-21 17:44:10 +0900

    test: scopeKey 관련 테스트 추가

--- di/src/test/java/woowacourse/di/DIScopeManagerTest.kt
+++ di/src/test/java/woowacourse/di/DIScopeManagerTest.kt
@@ -11,24 +11,28 @@ class DIScopeManagerTest {
 
     annotation class QualifierB
 
+    private val activityScopeKey = "Activity1"
+    private val anotherActivityScopeKey = "Activity2"
+    private val viewModelScopeKey = "ViewModel"
+
     @BeforeEach
     fun setup() {
-        DIScopeManager.clearScope(ScopeType.Singleton)
-        DIScopeManager.clearScope(ScopeType.Activity)
-        DIScopeManager.clearScope(ScopeType.ViewModel)
+        DIScopeManager.clearScope(activityScopeKey)
+        DIScopeManager.clearScope(anotherActivityScopeKey)
+        DIScopeManager.clearScope(viewModelScopeKey)
     }
 
     @Test
     fun `putInstance 후 getInstance 로 같은 객체를 가져온다`() {
         // given
         val repo = DummyRepository()
-        val key = DummyRepository::class to null
+        val dependencyKey = DummyRepository::class to null
 
         // when
-        DIScopeManager.putInstance(ScopeType.Singleton, key, repo)
+        DIScopeManager.putInstance(ScopeType.Singleton, "", dependencyKey, repo)
 
         // then
-        val retrieved = DIScopeManager.getInstance(ScopeType.Singleton, key)
+        val retrieved = DIScopeManager.getInstance(ScopeType.Singleton, "", dependencyKey)
         assertThat(repo).isEqualTo(retrieved)
     }
 
@@ -38,16 +42,28 @@ class DIScopeManagerTest {
         val repoA = DummyRepository()
         val repoB = DummyRepository()
 
-        val keyA = DummyRepository::class to QualifierA::class
-        val keyB = DummyRepository::class to QualifierB::class
+        val dependencyKeyA = DummyRepository::class to QualifierA::class
+        val dependencyKeyB = DummyRepository::class to QualifierB::class
 
         // when
-        DIScopeManager.putInstance(ScopeType.Activity, keyA, repoA)
-        DIScopeManager.putInstance(ScopeType.Activity, keyB, repoB)
+        DIScopeManager.putInstance(ScopeType.Activity, activityScopeKey, dependencyKeyA, repoA)
+        DIScopeManager.putInstance(ScopeType.Activity, activityScopeKey, dependencyKeyB, repoB)
 
         // then
-        assertThat(repoA).isEqualTo(DIScopeManager.getInstance(ScopeType.Activity, keyA))
-        assertThat(repoB).isEqualTo(DIScopeManager.getInstance(ScopeType.Activity, keyB))
+        assertThat(repoA).isEqualTo(
+            DIScopeManager.getInstance(
+                ScopeType.Activity,
+                activityScopeKey,
+                dependencyKeyA,
+            ),
+        )
+        assertThat(repoB).isEqualTo(
+            DIScopeManager.getInstance(
+                ScopeType.Activity,
+                activityScopeKey,
+                dependencyKeyB,
+            ),
+        )
     }
 
     @Test
@@ -55,15 +71,32 @@ class DIScopeManagerTest {
         // given
         val singletonRepo = DummyRepository()
         val viewModelRepo = DummyRepository()
-        val key = DummyRepository::class to null
+        val dependencyKey = DummyRepository::class to null
 
         // when
-        DIScopeManager.putInstance(ScopeType.Singleton, key, singletonRepo)
-        DIScopeManager.putInstance(ScopeType.ViewModel, key, viewModelRepo)
+        DIScopeManager.putInstance(ScopeType.Singleton, "", dependencyKey, singletonRepo)
+        DIScopeManager.putInstance(
+            ScopeType.ViewModel,
+            viewModelScopeKey,
+            dependencyKey,
+            viewModelRepo,
+        )
 
         // then
-        assertThat(singletonRepo).isEqualTo(DIScopeManager.getInstance(ScopeType.Singleton, key))
-        assertThat(viewModelRepo).isEqualTo(DIScopeManager.getInstance(ScopeType.ViewModel, key))
+        assertThat(singletonRepo).isEqualTo(
+            DIScopeManager.getInstance(
+                ScopeType.Singleton,
+                "",
+                dependencyKey,
+            ),
+        )
+        assertThat(viewModelRepo).isEqualTo(
+            DIScopeManager.getInstance(
+                ScopeType.ViewModel,
+                viewModelScopeKey,
+                dependencyKey,
+            ),
+        )
     }
 
     @Test
@@ -71,16 +104,64 @@ class DIScopeManagerTest {
         // given
         val singletonRepo = DummyRepository()
         val activityRepo = DummyRepository()
-        val key = DummyRepository::class to null
+        val dependencyKey = DummyRepository::class to null
 
-        DIScopeManager.putInstance(ScopeType.Singleton, key, singletonRepo)
-        DIScopeManager.putInstance(ScopeType.Activity, key, activityRepo)
+        DIScopeManager.putInstance(ScopeType.Singleton, "", dependencyKey, singletonRepo)
+        DIScopeManager.putInstance(
+            ScopeType.Activity,
+            activityScopeKey,
+            dependencyKey,
+            activityRepo,
+        )
 
         // when
-        DIScopeManager.clearScope(ScopeType.Activity)
+        DIScopeManager.clearScope(activityScopeKey)
 
         // then
-        assertThat(DIScopeManager.getInstance(ScopeType.Activity, key)).isNull()
-        assertThat(singletonRepo).isEqualTo(DIScopeManager.getInstance(ScopeType.Singleton, key))
+        assertThat(
+            DIScopeManager.getInstance(
+                ScopeType.Activity,
+                activityScopeKey,
+                dependencyKey,
+            ),
+        ).isNull()
+        assertThat(singletonRepo).isEqualTo(
+            DIScopeManager.getInstance(
+                ScopeType.Singleton,
+                "",
+                dependencyKey,
+            ),
+        )
+    }
+
+    @Test
+    fun `다른 scopeKey를 가진 Activity 스코프는 서로 간섭하지 않는다`() {
+        // given
+        val activity1Repo = DummyRepository()
+        val activity2Repo = DummyRepository()
+        val dependencyKey = DummyRepository::class to null
+
+        // when
+        DIScopeManager.putInstance(
+            ScopeType.Activity,
+            activityScopeKey,
+            dependencyKey,
+            activity1Repo,
+        )
+        DIScopeManager.putInstance(
+            ScopeType.Activity,
+            anotherActivityScopeKey,
+            dependencyKey,
+            activity2Repo,
+        )
+
+        // then
+        val retrieved1 =
+            DIScopeManager.getInstance(ScopeType.Activity, activityScopeKey, dependencyKey)
+        val retrieved2 =
+            DIScopeManager.getInstance(ScopeType.Activity, anotherActivityScopeKey, dependencyKey)
+        assertThat(activity1Repo).isEqualTo(retrieved1)
+        assertThat(activity2Repo).isEqualTo(retrieved2)
+        assertThat(retrieved1).isNotEqualTo(retrieved2)
     }
 }
