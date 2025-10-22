e5cca585c2b27e71240728a770d4bd06a9ffd002
yrsel <xxknu88@gmail.com>
2025-10-22 15:50:33 +0900

    refactor: Scope 관련 클래스 target, retention 정의

--- di/src/main/java/com/yrsel/di/annotation/Scope.kt
+++ di/src/main/java/com/yrsel/di/annotation/Scope.kt
@@ -1,16 +1,25 @@
 package com.yrsel.di.annotation
 
+@Target(AnnotationTarget.ANNOTATION_CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class Scope
 
 @Scope
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FIELD)
+@Retention(AnnotationRetention.RUNTIME)
 annotation class SingletonScope
 
 @Scope
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FIELD)
+@Retention(AnnotationRetention.RUNTIME)
 annotation class ApplicationScope
 
 @Scope
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FIELD)
+@Retention(AnnotationRetention.RUNTIME)
 annotation class ActivityScope
 
 @Scope
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FIELD)
+@Retention(AnnotationRetention.RUNTIME)
 annotation class ViewModelScope
