2966da7cebaddaedbf72150f6f03f1a702557f6e
jerry8282 <ka06196@naver.com>
2025-10-22 22:50:33 +0900

    refactor: DI 스코프 적용 및 싱글톤 구성

--- app/src/main/java/woowacourse/shopping/di/LocalStorageModule.kt
+++ app/src/main/java/woowacourse/shopping/di/LocalStorageModule.kt
@@ -1,6 +1,7 @@
 package woowacourse.shopping.di
 
 import android.content.Context
+import com.on.di_library.di.MySingleTon
 import com.on.di_library.di.annotation.MyModule
 import com.on.di_library.di.annotation.MyProvider
 import woowacourse.shopping.data.CartProductDao
@@ -9,8 +10,10 @@ import woowacourse.shopping.data.ShoppingDatabase
 @MyModule
 object LocalStorageModule {
     @MyProvider
+    @MySingleTon
     fun shoppingDatabase(context: Context) = ShoppingDatabase.getDataBase(context)
 
     @MyProvider
+    @MySingleTon
     fun cartProductDao(database: ShoppingDatabase): CartProductDao = database.cartProductDao()
 }
