93093f8097b19f448c574681af47e526e69df4bc
jiyunlee <02leejiyun@naver.com>
2025-10-23 22:39:27 +0900

    refactor: Activity 재생성 시에도 DI 스코프가 유지되도록 수정

--- app/src/main/java/woowacourse/shopping/ui/BaseScopedActivity.kt
+++ app/src/main/java/woowacourse/shopping/ui/BaseScopedActivity.kt
@@ -3,19 +3,35 @@ package woowacourse.shopping.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import woowacourse.shopping.ShoppingApplication
+import java.util.UUID
 
 abstract class BaseScopedActivity : AppCompatActivity() {
     private val app by lazy { application as ShoppingApplication }
-    private val scopeName = "${this::class.java.name}_${this.hashCode()}"
+
+    private lateinit var scopeName: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        scopeName = savedInstanceState?.getString(KEY_SCOPE_NAME)
+            ?: "${this::class.java.name}_${UUID.randomUUID()}"
+
         app.appContainer.createScope(scopeName)
         app.dependencyInjector.injectFields(this, this::class, scopeName)
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(KEY_SCOPE_NAME, scopeName)
+    }
+
     override fun onDestroy() {
         super.onDestroy()
-        app.appContainer.clearScope(scopeName)
+        if (isFinishing) {
+            app.appContainer.clearScope(scopeName)
+        }
+    }
+
+    companion object {
+        private const val KEY_SCOPE_NAME = "KEY_SCOPE_NAME"
     }
 }
