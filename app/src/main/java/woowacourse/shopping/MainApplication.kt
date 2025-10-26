0dd67367eacf7e452590664137280f0096d79808
oungsi2000 <oungsi1000@gmail.com>
2025-10-27 03:05:59 +0900

    refactor: Hilt를 사용하여 DI 프레임워크 전환
    기존에 사용하던 자체 DI(Dependency Injection) 프레임워크를 Hilt로 교체했습니다.
    
    - `@HiltAndroidApp`을 `MainApplication`에 적용했습니다.
    - `Activity`와 `ViewModel`에 `@AndroidEntryPoint` 및 `@HiltViewModel` 애노테이션을 추가하여 Hilt 사용을 설정했습니다.
    - 생성자 주입(Constructor Injection)을 사용하도록 `ViewModel`과 `Repository`를 수정했습니다.
    - 더 이상 필요하지 않은 기존 DI 관련 모듈과 `AppContainerStore`를 삭제했습니다.
    - ViewModel 인스턴스화 방식을 `autoViewModels`에서 Hilt의 `viewModels`로 변경했습니다.

--- app/src/main/java/woowacourse/shopping/MainApplication.kt
+++ app/src/main/java/woowacourse/shopping/MainApplication.kt
@@ -1,25 +1,7 @@
 package woowacourse.shopping
 
 import android.app.Application
-import com.daedan.di.AppContainerStore
-import com.daedan.di.DiComponent
-import woowacourse.shopping.di.dataModule
-import woowacourse.shopping.di.dateFormatterModule
-import woowacourse.shopping.di.repositoryModule
-import woowacourse.shopping.di.viewModelModule
+import dagger.hilt.android.HiltAndroidApp
 
-class MainApplication :
-    Application(),
-    DiComponent {
-    override val appContainerStore = AppContainerStore()
-
-    override fun onCreate() {
-        super.onCreate()
-        register(
-            dataModule(this),
-            repositoryModule(),
-            viewModelModule(),
-            dateFormatterModule(),
-        )
-    }
-}
+@HiltAndroidApp
+class MainApplication : Application()
