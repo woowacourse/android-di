ee95d4934bfb4d748d869ecbe56a2bcba912d9ce
Eunyeong Jang <devfeijoa@gmail.com>
2025-11-03 21:13:07 +0900

    [만들면서 배우는 DI 4단계] 이든 미션 제출합니다. (#215)
    * docs : Step4 요구사항 작성
    
    * feat: Inject 어노테이션에 FIELD 타겟 추가
    
    * feat: DiFactory 구현
    
    * feat: DiFactory 및 Container 적용
    
    * refactor: ViewModelFactory 로직 DiFactory로 위임
    
    * feat: CartActivity에 DateFormatter 의존성 주입
    
    * refactor: ViewModelFactory에서 SavedStateHandle 주입 로직 제거
    
    * feat: DiFactory에 inject 기능 추가 및 CartActivity에 적용
    
    * refactor: ViewModelFactory 구현 방식 변경
    
    ViewModelFactory에서 리플렉션을 사용하던 로직을 제거하고, DiFactory를 통해 의존성을 주입받도록 구조를 변경했습니다. 이에 따라 관련 없는 import 구문들이 삭제되었습니다.
    
    * docs: Step4 요구사항 체크
    
    * refactor: di 모듈 패키지 이름 변경
    
    `woowacouse.shopping.di`에서 `woowacourse.shopping.di`로 패키지 이름을 수정했습니다.
    이에 따라 관련 import 구문들을 함께 변경했습니다.
    
    * refactor: ProductRepository 인스턴스 생성 로직을 registerProvider로 변경
    
    * feat: `get` 메소드에서 provider를 이용한 인스턴스 생성 기능 추가

---
+++ di/src/main/java/woowacourse/shopping/di/ViewModelFactory.kt
@@ -0,0 +1,20 @@
+package woowacourse.shopping.di
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class ViewModelFactory(
+    private val diFactory: DiFactory,
+    private val handle: SavedStateHandle,
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        val viewModel: T =
+            diFactory.create(
+                modelClass.simpleName ?: "",
+                modelClass.kotlin,
+                handle = handle,
+            )
+        return viewModel
+    }
+}
