bef5a869d2a444a6b3321328c4ccb5c4c7f13a21
BaekCCI <bsy8470@naver.com>
2026-09-15 13:26:25 +0900

    feat: ViewModelProvider.Factory를 이용해 productsViewModel 주입

--- app/src/main/java/woowacourse/shopping/ui/products/ProductsScreen.kt
+++ app/src/main/java/woowacourse/shopping/ui/products/ProductsScreen.kt
@@ -30,10 +30,14 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil3.compose.AsyncImage
 import woowacourse.shopping.R
+import woowacourse.shopping.data.CartRepository
+import woowacourse.shopping.data.ProductRepository
 import woowacourse.shopping.model.Product
 import woowacourse.shopping.ui.theme.ShoppingTheme
 
@@ -41,7 +45,17 @@ import woowacourse.shopping.ui.theme.ShoppingTheme
 fun ProductsScreen(
     onNavigateToCart: () -> Unit,
     modifier: Modifier = Modifier,
-    viewModel: ProductsViewModel = viewModel(),
+    viewModel: ProductsViewModel =
+        viewModel(
+            factory =
+                object : ViewModelProvider.Factory {
+                    override fun <T : ViewModel> create(modelClass: Class<T>): T =
+                        ProductsViewModel(
+                            productRepository = ProductRepository(),
+                            cartRepository = CartRepository(),
+                        ) as T
+                },
+        ),
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
     val context = LocalContext.current
@@ -90,7 +104,10 @@ fun ProductsContent(
     ) { innerPadding ->
         LazyVerticalGrid(
             columns = GridCells.Fixed(2),
-            modifier = Modifier.fillMaxSize().padding(innerPadding),
+            modifier =
+                Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding),
         ) {
             items(uiState.products) { product ->
                 ProductItem(
@@ -120,7 +137,10 @@ fun ProductItem(
             model = product.imageUrl,
             contentDescription = null,
             contentScale = ContentScale.Crop,
-            modifier = Modifier.fillMaxWidth().aspectRatio(1f),
+            modifier =
+                Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(1f),
         )
         Text(
             text = product.name,
