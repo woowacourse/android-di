47cb4045c1b1bcf5a9e6d77559428f99a2b2b1d8
nadajinny <nadajinny@gmail.com>
2026-09-15 17:45:02 +0900

    feat: 생성자 의존성을 재귀적으로 주입

--- app/src/main/java/woowacourse/shopping/di/DIContainer.kt
+++ app/src/main/java/woowacourse/shopping/di/DIContainer.kt
@@ -9,7 +9,15 @@ class DIContainer {
             requireNotNull(type.primaryConstructor) {
                 "${type.simpleName}의 주 생성자를 찾을 수 없습니다."
             }
+        val dependencies =
+            constructor.parameters.map { parameter ->
+                val dependencyType =
+                    requireNotNull(parameter.type.classifier as? KClass<*>) {
+                        "${parameter.name}의 타입을 확인할 수 없습니다."
+                    }
+                get(dependencyType)
+            }
 
-        return constructor.call()
+        return constructor.call(*dependencies.toTypedArray())
     }
 }
--- app/src/test/java/woowacourse/shopping/di/DIContainerTest.kt
+++ app/src/test/java/woowacourse/shopping/di/DIContainerTest.kt
@@ -3,6 +3,7 @@ package woowacourse.shopping.di
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 import woowacourse.shopping.data.ProductRepository
+import woowacourse.shopping.ui.products.ProductsViewModel
 
 class DIContainerTest {
     private val container = DIContainer()
@@ -12,5 +13,12 @@ class DIContainerTest {
         val productRepository = container.get(ProductRepository::class)
 
         assertThat(productRepository).isInstanceOf(ProductRepository::class.java)
+    }
+
+    @Test
+    fun `생성자에 필요한 의존성을 자동으로 주입한다`() {
+        val viewModel = container.get(ProductsViewModel::class)
+
+        assertThat(viewModel).isInstanceOf(ProductsViewModel::class.java)
     }
 }
