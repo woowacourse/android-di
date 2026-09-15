1a8dee46b2a2ba892214afa94dd36de960d2d7b9
Yun Ji <20220723@duksung.ac.kr>
2026-09-15 11:25:25 +0900

    feat: ProductsViewModel 의존성을 수동으로 주입

--- app/src/main/java/woowacourse/shopping/ui/products/ProductsScreen.kt
+++ app/src/main/java/woowacourse/shopping/ui/products/ProductsScreen.kt
@@ -32,8 +32,12 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import coil3.compose.AsyncImage
 import woowacourse.shopping.R
+import woowacourse.shopping.data.CartRepository
+import woowacourse.shopping.data.ProductRepository
 import woowacourse.shopping.model.Product
 import woowacourse.shopping.ui.theme.ShoppingTheme
 
@@ -41,7 +45,18 @@ import woowacourse.shopping.ui.theme.ShoppingTheme
 fun ProductsScreen(
     onNavigateToCart: () -> Unit,
     modifier: Modifier = Modifier,
-    viewModel: ProductsViewModel = viewModel(),
+    viewModel: ProductsViewModel =
+        viewModel(
+            factory =
+                viewModelFactory {
+                    initializer {
+                        ProductsViewModel(
+                            productRepository = ProductRepository(),
+                            cartRepository = CartRepository(),
+                        )
+                    }
+                },
+        ),
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
     val context = LocalContext.current
