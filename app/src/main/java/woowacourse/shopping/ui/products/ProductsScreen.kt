81d7cc983f917819d5ee4583451f5036f3a800a4
first-woosun <dbdntjs620@gmail.com>
2026-09-15 23:24:54 +0900

    feat: 펙터리 메서드 호출 추가

--- app/src/main/java/woowacourse/shopping/ui/products/ProductsScreen.kt
+++ app/src/main/java/woowacourse/shopping/ui/products/ProductsScreen.kt
@@ -34,6 +34,8 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil3.compose.AsyncImage
 import woowacourse.shopping.R
+import woowacourse.shopping.data.CartRepository
+import woowacourse.shopping.data.ProductRepository
 import woowacourse.shopping.model.Product
 import woowacourse.shopping.ui.theme.ShoppingTheme
 
@@ -41,7 +43,10 @@ import woowacourse.shopping.ui.theme.ShoppingTheme
 fun ProductsScreen(
     onNavigateToCart: () -> Unit,
     modifier: Modifier = Modifier,
-    viewModel: ProductsViewModel = viewModel(),
+    viewModel: ProductsViewModel = viewModel(factory = ProductsViewModel.provideFactory(
+        productRepository = ProductRepository(),
+        cartRepository = CartRepository(),
+    )),
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
     val context = LocalContext.current
