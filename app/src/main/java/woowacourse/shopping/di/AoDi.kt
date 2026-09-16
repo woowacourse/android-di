da520f5d6bdbaccb57751379da0c57de0258c642
ujeong <commit3921@gmail.com>
2026-09-16 21:40:31 +0900

    feat: DI 생성

---
+++ app/src/main/java/woowacourse/shopping/di/AoDi.kt
@@ -0,0 +1,67 @@
+package woowacourse.shopping.di
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import kotlin.reflect.KClass
+import kotlin.reflect.KFunction
+import kotlin.reflect.full.primaryConstructor
+
+object AoDi : ViewModelProvider.Factory {
+    var store = listOf<Any>()
+
+    override fun <T : ViewModel> create(
+        modelClass: KClass<T>,
+        extras: CreationExtras,
+    ): T {
+        val constructor = modelClass.primaryConstructor!!
+
+        val dependencies = getByConstructor(constructor)
+
+        val viewModel = constructor.call(*dependencies.toTypedArray())
+        return viewModel
+    }
+
+    fun getByConstructor(constructor: KFunction<Any>): List<Any> {
+        val types = constructor.parameters.map { it.type.classifier as KClass<*> }
+        val dependencies: List<Any> =
+            types.map { type ->
+                if (store.find { it::class == type } != null) {
+                    store.find { it::class == type }!!
+                } else {
+                    val subConstructor = type.primaryConstructor!!
+
+                    val subConstructorParameters =
+                        subConstructor.parameters.map { it.type.classifier as KClass<*> }
+                    var subDependencies = listOf<Any>()
+                    if (subConstructorParameters.isNotEmpty()) {
+                        subDependencies = getByConstructor(subConstructor)
+                    }
+                    val dependency = subConstructor.call(*subDependencies.toTypedArray())
+                    store = store.plus(dependency)
+
+                    dependency
+                }
+            }
+
+        return dependencies
+    }
+}
+
+/*
+*
+ViewModel 생성 요청을 받으면:
+    1. 요청받은 ViewModel 클래스 정보를 확인한다
+    2. 그 클래스의 생성자를 찾는다
+    3. 생성자가 요구하는 파라미터 타입들을 읽는다
+    4. 각 타입의 객체를 재귀적으로 구한다
+    5. 준비된 객체들을 넣어 생성자를 호출한다
+    6. 완성된 ViewModel을 반환한다
+
+Repository 객체를 구할 때:
+    1. 보관함을 먼저 조회한다
+    2. 있으면 기존 객체를 반환한다
+    3. 없으면 생성자를 읽어 새 객체를 생성한다
+    4. 생성한 객체를 보관함에 넣는다
+    5. 객체를 반환한다
+* */
