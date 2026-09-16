d9a60bcb74fae9abcc249edf74c6908489189de5
ujeong <commit3921@gmail.com>
2026-09-16 21:40:55 +0900

    feat: DI 연결

--- app/src/main/java/woowacourse/shopping/ui/products/ProductsScreen.kt
+++ app/src/main/java/woowacourse/shopping/ui/products/ProductsScreen.kt
@@ -34,6 +34,7 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil3.compose.AsyncImage
 import woowacourse.shopping.R
+import woowacourse.shopping.di.AoDi
 import woowacourse.shopping.model.Product
 import woowacourse.shopping.ui.theme.ShoppingTheme
 
@@ -41,7 +42,7 @@ import woowacourse.shopping.ui.theme.ShoppingTheme
 fun ProductsScreen(
     onNavigateToCart: () -> Unit,
     modifier: Modifier = Modifier,
-    viewModel: ProductsViewModel = viewModel(factory = ProductsViewModel.Factory),
+    viewModel: ProductsViewModel = viewModel(factory = AoDi),
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
     val context = LocalContext.current
@@ -90,7 +91,10 @@ fun ProductsContent(
     ) { innerPadding ->
         LazyVerticalGrid(
             columns = GridCells.Fixed(2),
-            modifier = Modifier.fillMaxSize().padding(innerPadding),
+            modifier =
+                Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding),
         ) {
             items(uiState.products) { product ->
                 ProductItem(
@@ -120,7 +124,10 @@ fun ProductItem(
             model = product.imageUrl,
             contentDescription = null,
             contentScale = ContentScale.Crop,
-            modifier = Modifier.fillMaxWidth().aspectRatio(1f),
+            modifier =
+                Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(1f),
         )
         Text(
             text = product.name,
