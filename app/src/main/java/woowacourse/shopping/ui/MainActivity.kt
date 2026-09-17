54a27f5f276195494b8614397e1811244e9a20eb
first-woosun <dbdntjs620@gmail.com>
2026-09-17 20:05:10 +0900

    feat: 공통 ViewModelFactory 주입

--- app/src/main/java/woowacourse/shopping/ui/MainActivity.kt
+++ app/src/main/java/woowacourse/shopping/ui/MainActivity.kt
@@ -4,15 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import woowacourse.shopping.ShoppingApplication
+import woowacourse.shopping.ui.di.CommonViewModelFactory
 import woowacourse.shopping.ui.theme.ShoppingTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val app = application as ShoppingApplication
+
         enableEdgeToEdge()
         setContent {
             ShoppingTheme {
-                ShoppingNavHost()
+                ShoppingNavHost(
+                    viewModelFactory = CommonViewModelFactory(app.repositoryContainer)
+                )
             }
         }
     }
