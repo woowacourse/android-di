39e0d7a2bb5027cf413b40d276119676f2201a88
todays-sun-day <178suna@gmail.com>
2026-09-17 16:51:16 +0900

    feat: 범용 ViewModel 자동 주입 구현

---
+++ app/src/main/java/woowacourse/shopping/di/AutoViewModelFactory.kt
@@ -0,0 +1,21 @@
+package woowacourse.shopping.di
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import kotlin.reflect.KClass
+import kotlin.reflect.full.primaryConstructor
+
+class AutoViewModelFactory : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        val classReflection = modelClass.kotlin
+        val constructor = classReflection.primaryConstructor ?: throw IllegalArgumentException()
+        val parameters = constructor.parameters
+        val args = parameters.map { parameter ->
+            val dependencyClass = parameter.type.classifier as? KClass<*>
+                ?: throw IllegalArgumentException()
+
+            DependencyContainer.getInstance(dependencyClass)
+        }
+        return constructor.call(*args.toTypedArray())
+    }
+}
---
+++ app/src/main/java/woowacourse/shopping/di/DependencyContainer.kt
@@ -0,0 +1,15 @@
+package woowacourse.shopping.di
+
+import kotlin.reflect.KClass
+import kotlin.reflect.full.createInstance
+
+object DependencyContainer {
+    private val instances = mutableMapOf<KClass<*>, Any>()
+    fun getInstance(type: KClass<*>): Any {
+        val objectInstance = type.objectInstance
+        if (objectInstance != null) return objectInstance
+        return instances.getOrPut(type) {
+            type.createInstance()
+        }
+    }
+}
