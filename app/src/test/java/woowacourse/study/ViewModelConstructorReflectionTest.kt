1e1cd8352d6ce76f64751b25df19801e24f35dba
Yun Ji <20220723@duksung.ac.kr>
2026-09-17 16:57:02 +0900

    test: 리플렉션을 사용하여 ViewModel 생성자 파라미터 타입을 검증하는 테스트 추가

---
+++ app/src/test/java/woowacourse/study/ViewModelConstructorReflectionTest.kt
@@ -0,0 +1,42 @@
+package woowacourse.study
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import woowacourse.shopping.data.CartRepository
+import woowacourse.shopping.data.ProductRepository
+import woowacourse.shopping.ui.cart.CartViewModel
+import woowacourse.shopping.ui.products.ProductsViewModel
+import kotlin.reflect.KClass
+import kotlin.reflect.full.primaryConstructor
+
+class ViewModelConstructorReflectionTest {
+
+    @Test
+    fun `ProductsViewModel 생성자의 파라미터 타입을 확인한다`() {
+        // given
+        val constructor = ProductsViewModel::class.primaryConstructor!!
+
+        val parameterTypes = constructor.parameters.map{
+            parameter -> parameter.type.classifier as KClass<*>
+        }
+
+        assertThat(parameterTypes).containsExactly(
+            ProductRepository::class,
+            CartRepository::class
+        )
+    }
+
+    @Test
+    fun `CartViewModel 생성자의 파라미터 타입을 확인한다`() {
+        // given
+        val constructor = CartViewModel::class.primaryConstructor!!
+
+        val parameterTypes = constructor.parameters.map{
+            parameter -> parameter.type.classifier as KClass<*>
+        }
+
+        assertThat(parameterTypes).containsExactly(
+            CartRepository::class
+        )
+    }
+}
