8e1b929fb4b0757f2f7c57e1fc37cfe0b2ef425f
Eungyeong Jo <joeungyeong23@gmail.com>
2026-09-18 21:39:07 +0900

    feat: 특정 클래스 타입에 해당하는 객체들을 찾아주는 함수 추가

--- app/src/main/java/woowacourse/shopping/DIContainer.kt
+++ app/src/main/java/woowacourse/shopping/DIContainer.kt
@@ -2,7 +2,6 @@ package woowacourse.shopping
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import woowacourse.shopping.DIContainer.repositories
 import woowacourse.shopping.data.CartRepository
 import woowacourse.shopping.data.ProductRepository
 import kotlin.jvm.kotlin
@@ -13,13 +12,18 @@ object DIContainer {
     private val productRepository = ProductRepository()
     private val cartRepository = CartRepository()
     val repositories = listOf(productRepository, cartRepository)
+
+    fun findDependencies(types: List<KClass<*>>): List<Any> =
+        repositories.filter {
+            types.contains(it::class)
+        }
 }
 
 class ViewModelFactory : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         val constructor = modelClass.kotlin.primaryConstructor!!
         val types = constructor.parameters.map { it.type.classifier as KClass<*> }
-        val repos = repositories.filter { types.contains(it::class) }
+        val repos = DIContainer.findDependencies(types)
         return constructor.call(*repos.toTypedArray())
     }
 }
