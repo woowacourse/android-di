75eb011b8efe2b2f5108dca8146820199ffce0c1
Eungyeong Jo <joeungyeong23@gmail.com>
2026-09-18 22:51:13 +0900

    feat: 런타임에 의존성을 생성하도록 구현

--- app/src/main/java/woowacourse/shopping/DIContainer.kt
+++ app/src/main/java/woowacourse/shopping/DIContainer.kt
@@ -2,20 +2,23 @@ package woowacourse.shopping
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import woowacourse.shopping.data.CartRepository
-import woowacourse.shopping.data.ProductRepository
 import kotlin.jvm.kotlin
 import kotlin.reflect.KClass
 import kotlin.reflect.full.primaryConstructor
 
 object DIContainer {
-    private val productRepository = ProductRepository()
-    private val cartRepository = CartRepository()
-    val repositories = listOf(productRepository, cartRepository)
+    val dependencies = mutableListOf<Pair<KClass<*>, Any>>()
 
     fun findDependencies(types: List<KClass<*>>): List<Any> =
-        repositories.filter {
-            types.contains(it::class)
+        types.map { type ->
+            if (dependencies.map { it.first }.contains(type)) {
+                dependencies.find { it.first == type }!!.second
+            } else {
+                val constructor = type.primaryConstructor!!
+                val dependency = constructor.call()
+                dependencies.add(type to dependency)
+                dependency
+            }
         }
 }
 
@@ -23,7 +26,7 @@ class ViewModelFactory : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         val constructor = modelClass.kotlin.primaryConstructor!!
         val types = constructor.parameters.map { it.type.classifier as KClass<*> }
-        val repos = DIContainer.findDependencies(types)
-        return constructor.call(*repos.toTypedArray())
+        val dependencies = DIContainer.findDependencies(types)
+        return constructor.call(*dependencies.toTypedArray())
     }
 }
