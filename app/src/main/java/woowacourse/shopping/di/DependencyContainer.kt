265b3089f5538a52b43975a6569aa792831e151e
todays-sun-day <178suna@gmail.com>
2026-09-19 19:07:36 +0900

    refactor: 의존성 재귀 생성 및 자동 주입 구조 개선

--- app/src/main/java/woowacourse/shopping/di/DependencyContainer.kt
+++ app/src/main/java/woowacourse/shopping/di/DependencyContainer.kt
@@ -1,16 +1,34 @@
 package woowacourse.shopping.di
 
 import kotlin.reflect.KClass
-import kotlin.reflect.full.createInstance
+import kotlin.reflect.full.primaryConstructor
 
 object DependencyContainer {
     private val instances = mutableMapOf<KClass<*>, Any>()
 
+    fun create(type: KClass<*>): Any {
+        val objectInstance = type.objectInstance
+        if (objectInstance != null) return objectInstance
+
+        val constructor = type.primaryConstructor
+            ?: throw IllegalArgumentException()
+        val parameters = constructor.parameters
+        val args = parameters.map { parameter ->
+            val dependencyClass =
+                parameter.type.classifier as? KClass<*>
+                    ?: throw IllegalArgumentException()
+
+            getInstance(dependencyClass)
+        }
+        return constructor.call(*args.toTypedArray())
+    }
+
     fun getInstance(type: KClass<*>): Any {
         val objectInstance = type.objectInstance
         if (objectInstance != null) return objectInstance
+
         return instances.getOrPut(type) {
-            type.createInstance()
+            create(type)
         }
     }
 }
