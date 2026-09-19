faf0ae46eb28f006ffe5382ec58a7d99f44fa2e5
first-woosun <dbdntjs620@gmail.com>
2026-09-19 14:30:21 +0900

    refactor: 레포지터리 인스턴스를 자동으로 찾고 초기화하도록 변경

--- app/src/main/java/woowacourse/shopping/data/RepositoryContainer.kt
+++ app/src/main/java/woowacourse/shopping/data/RepositoryContainer.kt
@@ -1,16 +1,32 @@
 package woowacourse.shopping.data
 
 import kotlin.reflect.KClass
+import kotlin.reflect.full.createInstance
 
-class RepositoryContainer(
-    private val productRepository: ProductRepository,
-    private val cartRepository: CartRepository,
-) {
-    private val repositoryMap = mapOf(
-        ProductRepository::class to productRepository,
-        CartRepository::class to cartRepository,
-    )
+class RepositoryContainer{
+    private val repositoryStore = mutableMapOf<KClass<*>, Any>()
 
-    fun getInstance(type: KClass<*>): Any =
-        repositoryMap[type] ?: throw IllegalArgumentException("존재하지 않는 타입: $type")
+    fun getInstance(type: KClass<*>): Any {
+        val storedInstance = findInstance(type)
+
+        if(storedInstance != null) return storedInstance
+
+        val newInstance = createInstance(type)
+        storeInstance(type, newInstance)
+
+        return newInstance
+    }
+
+    private fun findInstance(type: KClass<*>): Any? =
+        repositoryStore[type]
+
+    private fun createInstance(type: KClass<*>): Any =
+        type.createInstance()
+
+    private fun storeInstance(
+        type: KClass<*>,
+        instance: Any
+    ) {
+        repositoryStore[type] = instance
+    }
 }
