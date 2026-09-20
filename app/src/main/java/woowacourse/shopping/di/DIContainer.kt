a71fba2694b62e9ebd29a10a12cfd4b58a28c355
조은경 <108224432+eungyeong12@users.noreply.github.com>
2026-09-20 19:39:53 +0900

    [만들면서 배우는 DI 1단계] 엘리 미션 제출합니다. (#238)
    * feat: ViewModel 팩토리 구현
    
    * refactor: factory 함수가 생성자를 통해 Repository를 주입받도록 수정
    
    * docs: 0.5단계 기능 목록 작성
    
    * docs: 0.5단계 목적과 문제점 작성
    
    * docs: 1단계 기능 목록 작성
    
    * feat: 특정 ViewModel에서만이 아닌, 범용적으로 활용될 수 있는 자동 주입 로직 작성
    
    * docs: 1단계 기능 목록 추가
    
    * feat: 특정 클래스 타입에 해당하는 객체들을 찾아주는 함수 추가
    
    * docs: 1단계 기능 목록 추가
    
    * feat: 런타임에 의존성을 생성하도록 구현
    
    * refactor: DIContainer를 di 패키지로 이동
    
    * refactor: 변수명, 함수명 수정, Pair를 Map 구조로 수정
    
    * refactor: instances를 private으로 변경
    
    * refactor: 생성자의 파라미터를 분석하여 의존성 인스턴스들을 반환해주는 함수 분리
    
    * refactor: getOrPut 함수 사용하도록 수정
    
    * refactor: 인스턴스 생성 함수 분리
    
    * docs: 기능 목록 수정
    
    * refactor: ViewModelFactory를 object로 변경

---
+++ app/src/main/java/woowacourse/shopping/di/DIContainer.kt
@@ -0,0 +1,34 @@
+package woowacourse.shopping.di
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import kotlin.jvm.kotlin
+import kotlin.reflect.KClass
+import kotlin.reflect.KFunction
+import kotlin.reflect.full.primaryConstructor
+
+object DIContainer {
+    private val instances = mutableMapOf<KClass<*>, Any>()
+
+    fun <T : Any> createInstance(modelClass: KClass<T>): T {
+        val constructor = modelClass.primaryConstructor!!
+        val dependencies = findDependencies(constructor)
+        return constructor.call(*dependencies.toTypedArray())
+    }
+
+    private fun <T : Any> findDependencies(constructor: KFunction<T>): List<Any> {
+        val types = constructor.parameters.map { it.type.classifier as KClass<*> }
+        return getInstances(types)
+    }
+
+    private fun getInstances(types: List<KClass<*>>): List<Any> =
+        types.map { type ->
+            instances.getOrPut(type) {
+                createInstance(type)
+            }
+        }
+}
+
+object ViewModelFactory : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T = DIContainer.createInstance(modelClass.kotlin)
+}
