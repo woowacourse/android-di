d9ce722b5fedcd1f06ce4200178710a0510fc15e
first-woosun <105399878+first-woosun@users.noreply.github.com>
2026-09-20 19:21:20 +0900

    [만들면서 배우는 DI] 1단계 투핸더 미션 제출합니다. (#249)
    * chore: Gradle Deamon JDK 버전 고정
    
    * docs: 변경사항 작성
    
    * feat: viewModel별 펙터리 메서드 추가
    
    * feat: 펙터리 메서드 호출 추가
    
    * docs: 1단계 변경사항 작성
    
    * refactor: 각 viewModel 별로 선언되어 있던 factory 메서드 제거
    
    * feat: 공통 ViewModelFactory 생성
    
    * feat: RepositoryContainer 추가
    
    * feat: 커스텀 Application 추가 및 읜존성 컨테이너 연결
    
    * feat: 공통 ViewModelFactory 주입
    
    * refactor: 레포지터리 인스턴스를 자동으로 찾고 초기화하도록 변경
    
    * refactor: 의존성 주입 파라미터 변경
    
    * refactor: viewModelFactory의 인스턴스 초기화 위치 변경
    
    - setContent() 내부 -> ShoppingApplication
    
    * style: 파일 개행 경고 제거

--- app/src/main/java/woowacourse/shopping/ui/ShoppingNavHost.kt
+++ app/src/main/java/woowacourse/shopping/ui/ShoppingNavHost.kt
@@ -1,11 +1,13 @@
 package woowacourse.shopping.ui
 
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import woowacourse.shopping.ui.cart.CartScreen
+import woowacourse.shopping.ui.di.CommonViewModelFactory
 import woowacourse.shopping.ui.products.ProductsScreen
 
 object ShoppingRoute {
@@ -14,16 +16,24 @@ object ShoppingRoute {
 }
 
 @Composable
-fun ShoppingNavHost(navController: NavHostController = rememberNavController()) {
+fun ShoppingNavHost(
+    viewModelFactory: CommonViewModelFactory,
+    navController: NavHostController = rememberNavController(),
+) {
     NavHost(
         navController = navController,
         startDestination = ShoppingRoute.PRODUCTS,
     ) {
         composable(ShoppingRoute.PRODUCTS) {
-            ProductsScreen(onNavigateToCart = { navController.navigate(ShoppingRoute.CART) })
+            ProductsScreen(
+                onNavigateToCart = { navController.navigate(ShoppingRoute.CART) },
+                viewModel = viewModel(factory = viewModelFactory)
+            )
         }
         composable(ShoppingRoute.CART) {
-            CartScreen(onNavigateUp = { navController.navigateUp() })
+            CartScreen(onNavigateUp = { navController.navigateUp() },
+                viewModel = viewModel(factory = viewModelFactory)
+            )
         }
     }
 }
