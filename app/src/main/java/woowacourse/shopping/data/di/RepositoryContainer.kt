7c9457753ced30bfc8e8ba09f80676aad2265a3a
first-woosun <dbdntjs620@gmail.com>
2026-09-21 22:03:34 +0900

    feat: 필드 의존성 주입 inject 함수 추가

--- app/src/main/java/woowacourse/shopping/data/di/RepositoryContainer.kt
+++ app/src/main/java/woowacourse/shopping/data/di/RepositoryContainer.kt
@@ -1,10 +1,24 @@
-package woowacourse.shopping.data
+package woowacourse.shopping.data.di
 
 import kotlin.reflect.KClass
 import kotlin.reflect.full.createInstance
 
 class RepositoryContainer{
     private val repositoryStore = mutableMapOf<KClass<*>, Any>()
+
+    fun inject(target: Any) {
+        target::class.java.declaredFields
+            .filter { field ->
+                field.isAnnotationPresent(CustomFieldInjection::class.java)
+            }
+            .forEach { field ->
+                val dependencyType = field.type.kotlin
+                val dependency = getInstance(dependencyType)
+
+                field.isAccessible = true
+                field.set(target, dependency)
+            }
+    }
 
     fun getInstance(type: KClass<*>): Any {
         val storedInstance = findInstance(type)
