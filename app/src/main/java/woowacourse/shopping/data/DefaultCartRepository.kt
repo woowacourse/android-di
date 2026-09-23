a69cdb741871b39a6d79cbe8a3016f2d4bc52353
first-woosun <dbdntjs620@gmail.com>
2026-09-24 02:22:31 +0900

    feat: CartRepository를 Room 기반 구현으로 전환

---
+++ app/src/main/java/woowacourse/shopping/data/DefaultCartRepository.kt
@@ -0,0 +1,23 @@
+package woowacourse.shopping.data
+
+import woowacourse.shopping.data.mapper.toEntity
+import woowacourse.shopping.data.mapper.toModel
+import woowacourse.shopping.model.CartProduct
+import woowacourse.shopping.model.Product
+
+// TODO: Step2 - CartProductDao를 참조하도록 변경
+class DefaultCartRepository(
+    private val dao: CartProductDao
+): CartRepository {
+    override suspend fun addCartProduct(product: Product) {
+        dao.insert(product.toEntity())
+    }
+
+    override suspend fun getAllCartProducts(): List<CartProduct> {
+        return dao.getAll().map { it.toModel() }
+    }
+
+    override suspend fun deleteCartProduct(id: Long) {
+        dao.delete(id)
+    }
+}
