b7bb52b35bc978547c38c9105030021e6f3abaf3
first-woosun <dbdntjs620@gmail.com>
2026-09-24 22:16:02 +0900

    feat: qualifier 어노테이션 추가

--- app/src/main/java/woowacourse/shopping/data/di/DIAnnotation.kt
+++ app/src/main/java/woowacourse/shopping/data/di/DIAnnotation.kt
@@ -3,3 +3,17 @@ package woowacourse.shopping.data.di
 @Target(AnnotationTarget.FIELD)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class CustomFieldInjection
+
+@Target(AnnotationTarget.ANNOTATION_CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Qualifier
+
+@Qualifier
+@Target(AnnotationTarget.FIELD, AnnotationTarget.VALUE_PARAMETER)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class RoomBacked
+
+@Qualifier
+@Target(AnnotationTarget.FIELD, AnnotationTarget.VALUE_PARAMETER)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class InMemory
