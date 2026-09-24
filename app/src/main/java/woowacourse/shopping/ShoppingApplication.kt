54fef0a2b252ef9044fd065d7742cf439509d8b1
first-woosun <dbdntjs620@gmail.com>
2026-09-24 22:22:03 +0900

    feat: repository 컨테이너 생성 방식 변경

--- app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
+++ app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
@@ -2,9 +2,15 @@ package woowacourse.shopping
 
 import android.app.Application
 import androidx.room.Room
+import woowacourse.shopping.data.CartRepository
+import woowacourse.shopping.data.DefaultCartRepository
+import woowacourse.shopping.data.ProductRepository
 import woowacourse.shopping.data.ShoppingDatabase
 import woowacourse.shopping.data.di.DataContainer
+import woowacourse.shopping.data.di.InMemory
+import woowacourse.shopping.data.di.RepositoryBinding
 import woowacourse.shopping.data.di.RepositoryContainer
+import woowacourse.shopping.data.di.RoomBacked
 import woowacourse.shopping.ui.di.CommonViewModelFactory
 
 class ShoppingApplication: Application() {
@@ -21,7 +27,21 @@ class ShoppingApplication: Application() {
         ).build()
 
         dataContainer = DataContainer.create(database)
-        repositoryContainer = RepositoryContainer(dataContainer)
+        repositoryContainer = RepositoryContainer(
+            dataContainer = dataContainer,
+            bindings = listOf(
+                RepositoryBinding(
+                    type = CartRepository::class,
+                    implementation = DefaultCartRepository::class,
+                    qualifier = RoomBacked::class
+                ),
+                RepositoryBinding(
+                    type = ProductRepository::class,
+                    implementation = ProductRepository::class,
+                    qualifier = InMemory::class
+                )
+            )
+        )
         viewModelFactory = CommonViewModelFactory(repositoryContainer)
     }
 }
